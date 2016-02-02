0e58dba9d60b94a2d447d7a237e577a2c70997fe
Randall Hauch <rhauch@gmail.com>
2016-02-02 16:58:48 -0600

    DBZ-1 Renamed the connector modules and packages

---
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlConnectorConfig.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright Debezium Authors.
+ * 
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.mysql;
+
+import java.util.Collection;
+import java.util.function.Consumer;
+
+import io.debezium.config.Configuration;
+import io.debezium.config.Field;
+import io.debezium.relational.history.KafkaDatabaseHistory;
+import io.debezium.util.Collect;
+
+/**
+ * The configuration properties.
+ */
+@SuppressWarnings("unchecked")
+public class MySqlConnectorConfig {
+
+    public static final Field USER = Field.create("database.user")
+                                          .withDescription("Name of the database user to be used when connecting to the database.")
+                                          .withValidation(Field::isRequired);
+
+    public static final Field PASSWORD = Field.create("database.password")
+                                              .withDescription("Password to be used when connecting to the database.")
+                                              .withValidation(Field::isRequired);
+
+    public static final Field HOSTNAME = Field.create("database.hostname")
+                                              .withDescription("IP address of the MySQL database server.")
+                                              .withValidation(Field::isRequired);
+
+    public static final Field PORT = Field.create("database.port")
+                                          .withDescription("Port of the MySQL database server.")
+                                          .withDefault(3306)
+                                          .withValidation(Field::isRequired, Field::isInteger);
+
+    public static final Field SERVER_ID = Field.create("database.server.id")
+                                               .withDescription("A numeric ID of this database client, which must be unique across all currently-running database processes in the cluster. This is required because this connector essentially joins the MySQL database cluster as another server (with this unique ID) so it can read the binlog.")
+                                               .withValidation(Field::isRequired, Field::isInteger);
+
+    public static final Field SERVER_NAME = Field.create("database.server.name")
+                                                 .withDescription("A unique name that identifies the database server that this connector monitors. Each database server should be monitored by at most one Debezium connector, since this server name delineates all persisted data eminating from this server. Defaults to 'host:port'")
+                                                 .withValidation(Field::isRequired);
+
+    public static final Field CONNECTION_TIMEOUT_MS = Field.create("connect.timeout.ms")
+                                                           .withDescription("Maximum time in milliseconds to wait after trying to connect to the database before timing out.")
+                                                           .withDefault(30 * 1000)
+                                                           .withValidation(Field::isPositiveInteger);
+
+    public static final Field KEEP_ALIVE = Field.create("connect.keep.alive")
+                                                .withDescription("Whether a separate thread should be used to ensure the connection is kept alive.")
+                                                .withDefault(true)
+                                                .withValidation(Field::isBoolean);
+
+    public static final Field MAX_QUEUE_SIZE = Field.create("max.queue.size")
+                                                    .withDescription("Maximum size of the queue for change events read from the database log but not yet recorded or forwarded. Defaults to 2048, and should always be larger than the maximum batch size.")
+                                                    .withDefault(2048)
+                                                    .withValidation(MySqlConnectorConfig::validateMaxQueueSize);
+
+    public static final Field MAX_BATCH_SIZE = Field.create("max.batch.size")
+                                                    .withDescription("Maximum size of each batch of source records. Defaults to 1024.")
+                                                    .withDefault(1024)
+                                                    .withValidation(Field::isPositiveInteger);
+
+    public static final Field POLL_INTERVAL_MS = Field.create("poll.interval.ms")
+                                                      .withDescription("Frequency in milliseconds to poll for new change events.")
+                                                      .withDefault(1 * 1000)
+                                                      .withValidation(Field::isPositiveInteger);
+
+    public static final Field DATABASE_HISTORY = Field.create("database.history")
+                                                      .withDescription("The name of the DatabaseHistory class that should be used to store and recover database schema changes. "
+                                                              + "The configuration properties for the history can be specified with the 'database.history.' prefix.")
+                                                      .withDefault(KafkaDatabaseHistory.class.getName());
+
+    public static final Field INCLUDE_SCHEMA_CHANGES = Field.create("include.schema.changes")
+                                                            .withDescription("Whether schema changes should be included in the change events")
+                                                            .withDefault(false)
+                                                            .withValidation(Field::isBoolean);
+
+    public static Collection<Field> ALL_FIELDS = Collect.arrayListOf(USER, PASSWORD, HOSTNAME, PORT, SERVER_ID,
+                                                                     SERVER_NAME, CONNECTION_TIMEOUT_MS, KEEP_ALIVE,
+                                                                     MAX_QUEUE_SIZE, MAX_BATCH_SIZE, POLL_INTERVAL_MS,
+                                                                     DATABASE_HISTORY, INCLUDE_SCHEMA_CHANGES);
+
+    private static int validateMaxQueueSize(Configuration config, Field field, Consumer<String> problems) {
+        int maxQueueSize = config.getInteger(field);
+        int maxBatchSize = config.getInteger(MAX_BATCH_SIZE);
+        int count = 0;
+        if (maxQueueSize <= 0) {
+            maxBatchSize = maxQueueSize / 2;
+            problems.accept("The " + MAX_QUEUE_SIZE + " value '" + maxQueueSize + "' must be positive");
+            ++count;
+        }
+        if (maxQueueSize <= maxBatchSize) {
+            maxBatchSize = maxQueueSize / 2;
+            problems.accept("The " + MAX_QUEUE_SIZE + " value '" + maxQueueSize + "' must be larger than " +
+                    MAX_BATCH_SIZE + " of '" + maxBatchSize + ".");
+            ++count;
+        }
+        return count;
+    }
+}
---
+++ debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlDdlParserTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright Debezium Authors.
+ * 
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.mysql;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Types;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import io.debezium.relational.Column;
+import io.debezium.relational.Table;
+import io.debezium.relational.TableId;
+import io.debezium.relational.Tables;
+import io.debezium.relational.ddl.DdlParser;
+import io.debezium.util.IoUtil;
+import io.debezium.util.Testing;
+
+public class MySqlDdlParserTest {
+
+    private DdlParser parser;
+    private Tables tables;
+
+    @Before
+    public void beforeEach() {
+        parser = new MySqlDdlParser();
+        tables = new Tables();
+    }
+
+    @Test
+    public void shouldParseMultipleStatements() {
+        String ddl = "CREATE TABLE foo ( " + System.lineSeparator()
+                + " c1 INTEGER NOT NULL, " + System.lineSeparator()
+                + " c2 VARCHAR(22) " + System.lineSeparator()
+                + "); " + System.lineSeparator()
+                + "-- This is a comment" + System.lineSeparator()
+                + "DROP TABLE foo;" + System.lineSeparator();
+        parser.parse(ddl, tables);
+        assertThat(tables.size()).isEqualTo(0); // table created and dropped
+    }
+
+    @Test
+    public void shouldParseCreateTableStatementWithSingleGeneratedAndPrimaryKeyColumn() {
+        String ddl = "CREATE TABLE foo ( " + System.lineSeparator()
+                + " c1 INTEGER NOT NULL AUTO_INCREMENT, " + System.lineSeparator()
+                + " c2 VARCHAR(22) " + System.lineSeparator()
+                + "); " + System.lineSeparator();
+        parser.parse(ddl, tables);
+        assertThat(tables.size()).isEqualTo(1);
+        Table foo = tables.forTable(new TableId(null, null, "foo"));
+        assertThat(foo).isNotNull();
+        assertThat(foo.columnNames()).containsExactly("c1", "c2");
+        assertThat(foo.primaryKeyColumnNames()).isEmpty();
+        assertColumn(foo, "c1", "INTEGER", Types.INTEGER, -1, -1, false, true, true);
+        assertColumn(foo, "c2", "VARCHAR", Types.VARCHAR, 22, -1, true, false, false);
+    }
+
+    @Test
+    public void shouldParseCreateTableStatementWithSingleGeneratedColumnAsPrimaryKey() {
+        String ddl = "CREATE TABLE my.foo ( " + System.lineSeparator()
+                + " c1 INTEGER NOT NULL AUTO_INCREMENT, " + System.lineSeparator()
+                + " c2 VARCHAR(22), " + System.lineSeparator()
+                + " PRIMARY KEY (c1)" + System.lineSeparator()
+                + "); " + System.lineSeparator();
+        parser.parse(ddl, tables);
+        assertThat(tables.size()).isEqualTo(1);
+        Table foo = tables.forTable(new TableId("my", null, "foo"));
+        assertThat(foo).isNotNull();
+        assertThat(foo.columnNames()).containsExactly("c1", "c2");
+        assertThat(foo.primaryKeyColumnNames()).containsExactly("c1");
+        assertColumn(foo, "c1", "INTEGER", Types.INTEGER, -1, -1, false, true, true);
+        assertColumn(foo, "c2", "VARCHAR", Types.VARCHAR, 22, -1, true, false, false);
+
+        parser.parse("DROP TABLE my.foo", tables);
+        assertThat(tables.size()).isEqualTo(0);
+    }
+    
+    @Test
+    public void shouldParseCreateStatements() {
+        parser.parse(readFile("ddl/mysql-test-create.ddl"), tables);
+        Testing.print(tables);
+    }
+
+    @Test
+    public void shouldParseTestStatements() {
+        parser.parse(readFile("ddl/mysql-test-statements.ddl"), tables);
+        Testing.print(tables);
+    }
+
+    @Test
+    public void shouldParseSomeLinesFromCreateStatements() {
+        parser.parse(readLines(189,"ddl/mysql-test-create.ddl"), tables);
+    }
+
+    protected String readFile( String classpathResource ) {
+        try ( InputStream stream = getClass().getClassLoader().getResourceAsStream(classpathResource); ) {
+            assertThat(stream).isNotNull();
+            return IoUtil.read(stream);
+        } catch ( IOException e ) {
+            fail("Unable to read '" + classpathResource + "'");
+        }
+        assert false : "should never get here";
+        return null;
+    }
+
+    /**
+     * Reads the lines starting with a given line number from the specified file on the classpath. Any lines preceding the
+     * given line number will be included as empty lines, meaning the line numbers will match the input file.
+     * @param startingLineNumber the 1-based number designating the first line to be included
+     * @param classpathResource the path to the file on the classpath
+     * @return the string containing the subset of the file contents; never null but possibly empty
+     */
+    protected String readLines( int startingLineNumber, String classpathResource ) {
+        try ( InputStream stream = getClass().getClassLoader().getResourceAsStream(classpathResource); ) {
+            assertThat(stream).isNotNull();
+            StringBuilder sb = new StringBuilder();
+            AtomicInteger counter = new AtomicInteger();
+            IoUtil.readLines(stream,line->{
+                if (counter.incrementAndGet() >= startingLineNumber) sb.append(line);
+                sb.append(System.lineSeparator());
+            });
+            return sb.toString();
+        } catch ( IOException e ) {
+            fail("Unable to read '" + classpathResource + "'");
+        }
+        assert false : "should never get here";
+        return null;
+    }
+
+    protected void assertColumn(Table table, String name, String typeName, int jdbcType, int length, int scale,
+                                boolean optional, boolean generated, boolean autoIncremented) {
+        Column column = table.columnWithName(name);
+        assertThat(column.name()).isEqualTo(name);
+        assertThat(column.typeName()).isEqualTo(typeName);
+        assertThat(column.jdbcType()).isEqualTo(jdbcType);
+        assertThat(column.length()).isEqualTo(length);
+        assertThat(column.scale()).isEqualTo(scale);
+        assertThat(column.isOptional()).isEqualTo(optional);
+        assertThat(column.isGenerated()).isEqualTo(generated);
+        assertThat(column.isAutoIncremented()).isEqualTo(autoIncremented);
+    }
+
+}
