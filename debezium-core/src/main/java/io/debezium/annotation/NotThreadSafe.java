2732d26ff0e7abdc4341abf5a177d3d94caf30c7
Horia Chiorean <hchiorea@redhat.com>
2016-08-24 13:41:14 +0300

    Fixes the DBZ header required by checkstyle This commit removes an extra space character from the first blank line of the header

--- debezium-core/src/main/java/io/debezium/annotation/NotThreadSafe.java
+++ debezium-core/src/main/java/io/debezium/annotation/NotThreadSafe.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.annotation;
 
 import java.lang.annotation.Documented;
@@ -6,19 +11,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Copyright (c) 2005 Brian Goetz and Tim Peierls.<br />
- * Released under the Creative Commons Attribution License<br />
- * (http://creativecommons.org/licenses/by/2.5)<br />
- * Official home: http://www.jcip.net<br />
- * Adopted from Java Concurrency in Practice.
- * <p>
- * This annotation documents the class as <i>not</i> being thread-safe, meaning the caller is expected to properly handle and
- * guard all concurrent operations on an instance.
- * </p>
- * 
- * @see ThreadSafe
- */
 @Documented
 @Target( ElementType.TYPE )
 @Retention( RetentionPolicy.RUNTIME )
