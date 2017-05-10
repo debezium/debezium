98cda44e50db4dd8b4158b27ccc42b30f4264d9f
wangshaono1 <wangshaono1@gmail.com>
2017-05-10 17:23:47 +0800

    remove hader

--- debezium-core/src/main/java/io/debezium/function/JDBCConnectionRequest.java
+++ debezium-core/src/main/java/io/debezium/function/JDBCConnectionRequest.java
@@ -1,8 +1,3 @@
-/*
- * Copyright Debezium Authors.
- *
- * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
- */
 package io.debezium.function;
 
 import java.sql.SQLException;
