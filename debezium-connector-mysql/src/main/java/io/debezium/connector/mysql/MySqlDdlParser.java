b4a0ac0c3584831695660e14a195f2ba6f446537
Satyajit Vegesna <varma.satyajit@gmail.com>
2017-08-16 15:22:17 -0700

    DBZ-324 Support for PAGE_CHECKSUM=1

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlDdlParser.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlDdlParser.java
@@ -408,7 +408,7 @@ public class MySqlDdlParser extends DdlParser {
             return true;
         } else if (tokens.canConsumeAnyOf("CHECKSUM", "ENGINE", "AVG_ROW_LENGTH", "MAX_ROWS", "MIN_ROWS", "ROW_FORMAT",
                                           "DELAY_KEY_WRITE", "INSERT_METHOD", "KEY_BLOCK_SIZE", "PACK_KEYS",
-                                          "STATS_AUTO_RECALC", "STATS_PERSISTENT", "STATS_SAMPLE_PAGES")) {
+                                          "STATS_AUTO_RECALC", "STATS_PERSISTENT", "STATS_SAMPLE_PAGES" , "PAGE_CHECKSUM" )) {
             // One option token followed by '=' by a single value
             tokens.canConsume('=');
             tokens.consume();
