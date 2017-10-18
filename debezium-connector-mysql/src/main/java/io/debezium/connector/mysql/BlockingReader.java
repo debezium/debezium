ba7478ffd70699deaf28506dbb6d09bb515962ce
Peter Goransson <pgoransson@gmail.com>
2017-10-18 12:24:35 -0400

    DBZ-396 Adding BlockingReader to avoid high CPU utilization in "snapshot_only" mode;
    No binlog readers will be set up in this mode, causing the idle snapshot
    reader to be polled with high frequency after it has done its work.
    Therefore a blocking reader is added in that case, which prevents the
    polling.

---
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/BlockingReader.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.mysql;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.apache.kafka.connect.source.SourceRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A component that blocks doing nothing until the connector task is stopped
+ * 
+ * @author Peter Goransson
+ *
+ */
+public class BlockingReader implements Reader {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());    
+    private final AtomicReference<Runnable> uponCompletion = new AtomicReference<>();
+    private final AtomicReference<State> state = new AtomicReference<>();
+    
+    private final CountDownLatch latch = new CountDownLatch(1);
+    
+    private final String name;
+    
+    public BlockingReader(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Waits indefinitely until the connector task is shut down
+     */
+    @Override
+    public List<SourceRecord> poll() throws InterruptedException {                
+        latch.await();   
+        state.set(State.STOPPING);
+
+        return null;
+    }
+        
+    @Override
+    public State state() {
+        return state.get();
+    }
+    
+    @Override
+    public void uponCompletion(Runnable handler) {
+        assert this.uponCompletion.get() == null;
+        this.uponCompletion.set(handler);        
+    }
+    
+    @Override
+    public void start() {
+        state.set(State.RUNNING);
+        logger.info("Connector has completed all of its work but will continue in the running state. It can be shutdown at any time.");
+    }
+    
+    @Override
+    public void stop() {
+        try {
+            try {
+                state.set(State.STOPPED);
+            }
+            finally {
+                latch.countDown();
+            }            
+                                  
+            // Cleanup Resources
+            Runnable completionHandler = uponCompletion.getAndSet(null); // set to null so that we call it only once
+            if (completionHandler != null) {
+                completionHandler.run();
+            }
+            
+        } finally {
+            logger.info("Blocking Reader has completed.");
+        }
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+}
