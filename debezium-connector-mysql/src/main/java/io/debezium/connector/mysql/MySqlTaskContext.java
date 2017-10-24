940d2c3cf32cfec2ab337cca5d600655eb1ccde4
Jiri Pechanec <jpechane@redhat.com>
2017-10-24 14:57:57 +0200

    DBZ-406 Support for look-ahead buffer with unlimited tx size
    Also incorporates DBZ-405

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlTaskContext.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlTaskContext.java
@@ -166,6 +166,10 @@ public final class MySqlTaskContext extends MySqlJdbcContext {
         return config.getLong(MySqlConnectorConfig.ROW_COUNT_FOR_STREAMING_RESULT_SETS);
     }
 
+    public int bufferSizeForBinlogReader() {
+        return config.getInteger(MySqlConnectorConfig.BUFFER_SIZE_FOR_BINLOG_READER);
+    }
+
     public boolean includeSchemaChangeRecords() {
         return config.getBoolean(MySqlConnectorConfig.INCLUDE_SCHEMA_CHANGES);
     }
