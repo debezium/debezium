29400045af48039b07c9cfb9f5d6388ab5048b43
sairam polavarapu <spolavarapu@sairams-MacBook-Pro.local>
2017-11-12 17:01:53 -0600

    DBZ-409 Formatted the classes.

--- debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/StringToJSON.java
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/StringToJSON.java
@@ -3,56 +3,76 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.debezium.transforms;
+package io.debezium.connector.mongodb;
 
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.transforms.ExtractField;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.bson.BsonDocument;
 import org.bson.BsonValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.debezium.util.MongoDataConverter;
-
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
 public class StringToJSON<R extends ConnectRecord<R>> implements Transformation<R> {
 
-private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    final ExtractField<R> delegate = new ExtractField.Value<R>();
+    final ExtractField<R> delegate1 = new ExtractField.Value<R>();
 
-@Override
-public R apply(R r) {
-SchemaBuilder schemabuilder = SchemaBuilder.struct();
-BsonDocument value = BsonDocument.parse(r.value().toString());
-Set<Entry<String, BsonValue>> keyValues = value.entrySet();
+    @Override
+    public R apply(R r) {
+        SchemaBuilder schemabuilder = SchemaBuilder.struct();
+        BsonDocument value = null;
 
-for (Entry<String, BsonValue> keyValuesforSchema : keyValues) {
-MongoDataConverter.addFieldSchema(keyValuesforSchema, schemabuilder);
-}
-Schema finalSchema = schemabuilder.build();
-Struct finalStruct = new Struct(finalSchema);
+        final R newRecord = delegate.apply(r);
+        final R newRecord1 = delegate1.apply(r);
 
-for (Entry<String, BsonValue> keyvalueforStruct : keyValues) {
+        if (newRecord.value() == null) {
+            value = BsonDocument.parse(newRecord1.value().toString());
+            } else {
+                value = BsonDocument.parse(newRecord.value().toString());
+            }
 
-MongoDataConverter.convertRecord(keyvalueforStruct, finalSchema, finalStruct);
-}
+        Set<Entry<String, BsonValue>> keyValues = value.entrySet();
 
-return r.newRecord(r.topic(), r.kafkaPartition(), r.keySchema(), r.key(), finalSchema, finalStruct,r.timestamp());
-}
+        for (Entry<String, BsonValue> keyValuesforSchema : keyValues) {
+            MongoDataConverter.addFieldSchema(keyValuesforSchema, schemabuilder);
+        }
+        
+        Schema finalSchema = schemabuilder.build();
+        Struct finalStruct = new Struct(finalSchema);
 
-public ConfigDef config() {
-return new ConfigDef();
-}
+        for (Entry<String, BsonValue> keyvalueforStruct : keyValues) {
+            MongoDataConverter.convertRecord(keyvalueforStruct, finalSchema, finalStruct);
+        }
 
-public void close() {
-}
+        return r.newRecord(r.topic(), r.kafkaPartition(), r.keySchema(), r.key(), finalSchema, finalStruct,r.timestamp());
+        }
 
-public void configure(Map<String, ?> map) {
-}
+    @Override
+    public ConfigDef config() {
+        return new ConfigDef();
+    }
+
+    public void close() {
+    }
+    
+    @Override
+    public void configure(final Map<String, ?> map) {
+        final Map<String, String> delegateConfig = new HashMap<>();
+        delegateConfig.put("field", "after");
+        final Map<String, String> delegateConfig1 = new HashMap<>();
+        delegateConfig1.put("field", "patch");
+        delegate.configure(delegateConfig);
+        delegate1.configure(delegateConfig1);
+    }
 }
