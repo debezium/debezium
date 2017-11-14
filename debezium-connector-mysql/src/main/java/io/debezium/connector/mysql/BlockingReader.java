79a2e4a6c2e426e5f01da8b291bb6f2a3df2fa65
Gunnar Morling <gunnar.morling@googlemail.com>
2017-11-14 11:37:28 +0100

    DBZ-396 Formatting and typo fix;
    Also adding Peter to COPYRIGHT.txt

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/BlockingReader.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/BlockingReader.java
@@ -15,20 +15,19 @@ import org.slf4j.LoggerFactory;
 
 /**
  * A component that blocks doing nothing until the connector task is stopped
- * 
- * @author Peter Goransson
  *
+ * @author Peter Goransson
  */
 public class BlockingReader implements Reader {
 
-    protected final Logger logger = LoggerFactory.getLogger(getClass());    
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
     private final AtomicReference<Runnable> uponCompletion = new AtomicReference<>();
     private final AtomicReference<State> state = new AtomicReference<>();
-    
+
     private final CountDownLatch latch = new CountDownLatch(1);
-    
+
     private final String name;
-    
+
     public BlockingReader(String name) {
         this.name = name;
     }
@@ -37,30 +36,30 @@ public class BlockingReader implements Reader {
      * Waits indefinitely until the connector task is shut down
      */
     @Override
-    public List<SourceRecord> poll() throws InterruptedException {                
-        latch.await();   
+    public List<SourceRecord> poll() throws InterruptedException {
+        latch.await();
         state.set(State.STOPPING);
 
         return null;
     }
-        
+
     @Override
     public State state() {
         return state.get();
     }
-    
+
     @Override
     public void uponCompletion(Runnable handler) {
         assert this.uponCompletion.get() == null;
-        this.uponCompletion.set(handler);        
+        this.uponCompletion.set(handler);
     }
-    
+
     @Override
     public void start() {
         state.set(State.RUNNING);
-        logger.info("Connector has completed all of its work but will continue in the running state. It can be shutdown at any time.");
+        logger.info("Connector has completed all of its work but will continue in the running state. It can be shut down at any time.");
     }
-    
+
     @Override
     public void stop() {
         try {
@@ -69,14 +68,14 @@ public class BlockingReader implements Reader {
             }
             finally {
                 latch.countDown();
-            }            
-                                  
+            }
+
             // Cleanup Resources
             Runnable completionHandler = uponCompletion.getAndSet(null); // set to null so that we call it only once
             if (completionHandler != null) {
                 completionHandler.run();
             }
-            
+
         } finally {
             logger.info("Blocking Reader has completed.");
         }
