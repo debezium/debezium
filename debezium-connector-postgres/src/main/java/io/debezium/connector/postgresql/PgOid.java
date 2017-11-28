0e9d775804c213c3f7fa415633a8c02836385250
Jiri Pechanec <jpechane@redhat.com>
2017-11-28 15:40:45 +0100

    WIP: DBZ-379 Postgres connector minimizes use of JDBC metadata

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PgOid.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PgOid.java
@@ -7,11 +7,15 @@
 package io.debezium.connector.postgresql;
 
 import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.postgresql.core.Oid;
 import org.postgresql.util.PSQLException;
 
+import io.debezium.connector.postgresql.connection.ReplicationMessage;
 import io.debezium.relational.Column;
+import io.debezium.relational.ColumnEditor;
 
 /**
  * Extension to the {@link org.postgresql.core.Oid} class which contains Postgres specific datatypes not found currently in the
@@ -33,11 +37,29 @@ public final class PgOid extends Oid {
 
     public static final int TSTZRANGE_OID = 3910;
 
+    private static final Map<String, String> longTypeNames = new HashMap<>();
+    static {
+        longTypeNames.put("bigint", "int8");
+        longTypeNames.put("bit varying", "varbit");
+        longTypeNames.put("boolean", "bool");
+        longTypeNames.put("character", "bpchar");
+        longTypeNames.put("character varying", "varchar");
+        longTypeNames.put("double precision", "float8");
+        longTypeNames.put("integer", "int4");
+        longTypeNames.put("real", "float4");
+        longTypeNames.put("smallint", "int2");
+        longTypeNames.put("timestamp without time zone", "timestamp");
+        longTypeNames.put("timestamp with time zone", "timestamptz");
+        longTypeNames.put("time without time zone", "time");
+        longTypeNames.put("time with time zone", "timetz");
+    }
+
     private PgOid() {
     }
 
     protected static int jdbcColumnToOid(Column column) {
         String typeName = column.typeName();
+
         if (typeName.toUpperCase().equals("TSTZRANGE")) {
             return TSTZRANGE_OID;
         } else if (typeName.toUpperCase().equals("SMALLSERIAL")) {
@@ -58,4 +80,29 @@ public final class PgOid extends Oid {
             return Oid.UNSPECIFIED;
         }
     }
+
+    public static String normalizeTypeName(String typeName) {
+        return longTypeNames.getOrDefault(typeName, typeName);
+    }
+
+    public static void reconcileJdbcOidTypeConstraints(final ReplicationMessage.Column column,
+            final ColumnEditor columnEditor) {
+        switch (column.getTypeName()) {
+            case "money":
+                // JDBC returns scale 0 but decoder plugin returns -1 (unscaled)
+                columnEditor.scale(0);
+                break;
+            case "timestamp":
+                // JDBC returns length/scale 29/6 but decoder plugin returns -1 (unlimited)
+                columnEditor.length(29);
+                columnEditor.scale(6);
+                break;
+            case "time":
+                // JDBC returns length/scale 15/6 but decoder plugin returns -1 (unlimited)
+                columnEditor.length(15);
+                columnEditor.scale(6);
+                break;
+        }
+    }
+
 }
---
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/AbstractReplicationMessageColumn.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.postgresql.connection;
+
+import java.sql.Types;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.kafka.connect.errors.ConnectException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.debezium.connector.postgresql.PgOid;
+
+/**
+ * Extracts type information from replication messages and associates them with each column.
+ * The metadata are parsed lazily.
+ * 
+ * @author Jiri Pechanec
+ *
+ */
+public abstract class AbstractReplicationMessageColumn implements ReplicationMessage.Column {
+
+    public static class TypeMetadata {
+        private static final Logger LOGGER = LoggerFactory.getLogger(TypeMetadata.class);
+
+        private static final int[] EMPTY_TYPE_MODIFIERS = {};
+        private static final Pattern TYPE_PATTERN = Pattern.compile("^(?<full>(?<base>[^(\\[]+)(?:\\((?<mod>.+)\\))?(?<suffix>.*?))(?<array>\\[\\])?$");
+        private static final Pattern TYPEMOD_PATTERN = Pattern.compile("\\s*,\\s*");
+            // "text"; "character varying(255)"; "numeric(12,3)"; "geometry(MultiPolygon,4326)"; "timestamp (12) with time zone"; "int[]"
+
+        private String baseType;
+        private String fullType;
+        private int[] typeModifiers;
+        private boolean isArray;
+        private String normalizedTypeName;
+        private boolean optional;
+
+        public TypeMetadata(String columnName, String typeWithModifiers, boolean optional) {
+            this.optional = optional;
+            Matcher m = TYPE_PATTERN.matcher(typeWithModifiers);
+            if (!m.matches()) {
+                LOGGER.error("Failed to parse columnType for {} '{}'", columnName, typeWithModifiers);
+                throw new ConnectException(String.format("Failed to parse columnType '%s' for column %s", typeWithModifiers, columnName));
+            }
+            fullType = m.group("full");
+            baseType = m.group("base").trim();
+            if (!Objects.toString(m.group("suffix"), "").isEmpty()) {
+                baseType = String.join(" ", baseType, m.group("suffix").trim());
+            }
+            typeModifiers = EMPTY_TYPE_MODIFIERS;
+            if (m.group("mod") != null) {
+                final String[] typeModifiersStr = TYPEMOD_PATTERN.split(m.group("mod"));
+                typeModifiers = new int[typeModifiersStr.length];
+                for (int i = 0; i < typeModifiersStr.length; i++) {
+                    try {
+                    typeModifiers[i] = Integer.parseInt(typeModifiersStr[i]);
+                    } catch (NumberFormatException e) {
+                        LOGGER.error("Failed to parse type modifier for {} '{}'", columnName, typeModifiersStr[i]);
+                        throw new ConnectException(String.format("Failed to parse type modifier '%s' for column %s", typeModifiersStr[i], columnName));
+                    }
+                }
+            }
+            isArray = (m.group("array") != null);
+
+            if (baseType.startsWith("_")) {
+                // old-style type specifiers use an _ prefix for arrays
+                // e.g. int4[] would be "_int4"
+                baseType = baseType.substring(1);
+                fullType = fullType.substring(1);
+                isArray = true;
+            }
+            normalizedTypeName = PgOid.normalizeTypeName(baseType);
+
+            if (isArray) {
+                normalizedTypeName = "_" + normalizedTypeName;
+            }
+        }
+
+        public String getBaseType() {
+            return baseType;
+        }
+
+        public String getFullType() {
+            return fullType;
+        }
+
+        public int[] getTypeModifiers() {
+            return typeModifiers;
+        }
+
+        public boolean isArray() {
+            return isArray;
+        }
+
+        public String getNormalizedTypeName() {
+            return normalizedTypeName;
+        }
+
+        public boolean isOptional() {
+            return optional;
+        }
+    }
+
+
+    private String columnName;
+    private String typeWithModifiers;
+    private boolean optional;
+    private TypeMetadata typeMetadata;
+    private boolean hasMetadata;
+
+    public AbstractReplicationMessageColumn(String columnName, String typeWithModifiers, boolean optional, ReplicationMessage message) {
+        super();
+        this.columnName = columnName;
+        this.typeWithModifiers = typeWithModifiers;
+        this.optional = optional;
+        this.hasMetadata = message.hasMetadata();
+    }
+
+    private void initMetadata() {
+        assert hasMetadata : "Metadata not available";
+        typeMetadata = new TypeMetadata(columnName, typeWithModifiers, optional);
+    }
+
+    /**
+     * @return OID value of the type
+     */
+    @Override
+    public int getType() {
+        if (hasMetadata) {
+            initMetadata();
+            return typeMetadata.isArray() ? Types.ARRAY : getOidType();
+        }
+        return getOidType();
+    }
+
+    /**
+     * @return OID type of elements for arrays
+     */
+    @Override
+    public int getArrayElementOidType() {
+        initMetadata();
+        assert typeMetadata.isArray();
+        return getOidType();
+    }
+
+    /**
+     * @return the type of the field in format as used in JDBC driver
+     */
+    @Override
+    public String getTypeName() {
+        initMetadata();
+        return typeMetadata.getNormalizedTypeName();
+    }
+
+    @Override
+    public String getName() {
+        return columnName;
+    }
+
+    /**
+     * @return the type modifiers associated with type
+     */
+    @Override
+    public int[] getTypeModifiers() {
+        initMetadata();
+        return typeMetadata.getTypeModifiers();
+    }
+
+    /**
+     * @return true if the column is optional
+     */
+    @Override
+    public boolean isOptional() {
+        assert hasMetadata : "Metadata not available";
+        return optional;
+    }
+
+    protected abstract int getOidType();
+
+    protected TypeMetadata getTypeMetadata() {
+        initMetadata();
+        return typeMetadata;
+    }
+}
