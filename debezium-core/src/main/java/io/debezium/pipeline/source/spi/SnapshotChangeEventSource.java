1e69fd68ab4a007bfee72d9be00fe9cf6f18ab64
Gunnar Morling <gunnar.morling@googlemail.com>
2018-02-28 12:47:56 +0100

    DBZ-20 Some more docs

--- debezium-core/src/main/java/io/debezium/pipeline/source/spi/SnapshotChangeEventSource.java
+++ debezium-core/src/main/java/io/debezium/pipeline/source/spi/SnapshotChangeEventSource.java
@@ -7,7 +7,22 @@ package io.debezium.pipeline.source.spi;
 
 import io.debezium.pipeline.spi.SnapshotResult;
 
+/**
+ * A change event source that emits events for taking a consistent snapshot of the captured tables, which may include
+ * schema and data information.
+ *
+ * @author Gunnar Morling
+ */
 public interface SnapshotChangeEventSource extends ChangeEventSource {
 
+    /**
+     * Executes this source. Implementations should regularly check the current thread's interrupted status. If the
+     * thread is interrupted, they should abort their processing and perform any clean-up needed, such as rolling back
+     * pending transactions, releasing locks etc.
+     *
+     * @return an indicator to the position at which the snapshot was aken
+     * @throws InterruptedException
+     *             in case the snapshot was aborted before completion
+     */
     SnapshotResult execute() throws InterruptedException;
 }
--- debezium-core/src/main/java/io/debezium/pipeline/source/spi/StreamingChangeEventSource.java
+++ debezium-core/src/main/java/io/debezium/pipeline/source/spi/StreamingChangeEventSource.java
@@ -5,9 +5,23 @@
  */
 package io.debezium.pipeline.source.spi;
 
+/**
+ * A change event source that emits events from a DB log, such as MySQL's binlog or similar.
+ *
+ * @author Gunnar Morling
+ */
 public interface StreamingChangeEventSource extends ChangeEventSource {
 
+    /**
+     * Starts this source. Implementations must return immediately, spawning a separate executor, thread or similar
+     * which runs their event handler.
+     */
     void start();
 
+    /**
+     * Stops this source.
+     *
+     * @throws InterruptedException in case stopping was interrupted.
+     */
     void stop() throws InterruptedException;
 }
