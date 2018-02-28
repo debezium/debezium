d8a9865e515f18d2a9d7ac12e98eb41ce3a82509
Gunnar Morling <gunnar.morling@googlemail.com>
2018-02-28 12:47:56 +0100

    DBZ-20 Some more docs

--- debezium-core/src/main/java/io/debezium/pipeline/source/spi/ChangeEventSourceFactory.java
+++ debezium-core/src/main/java/io/debezium/pipeline/source/spi/ChangeEventSourceFactory.java
@@ -7,6 +7,11 @@ package io.debezium.pipeline.source.spi;
 
 import io.debezium.pipeline.spi.OffsetContext;
 
+/**
+ * A factory for creating @l# {@link ChangeEventSource}s specific to one database.
+ *
+ * @author Gunnar Morling
+ */
 public interface ChangeEventSourceFactory {
 
     SnapshotChangeEventSource getSnapshotChangeEventSource();
