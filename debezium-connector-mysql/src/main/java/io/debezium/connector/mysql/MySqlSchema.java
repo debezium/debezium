8ce2c9fa3749062be5e8ebbab25846de2baa8bd3
rkuchar <kucharrom@gmail.com>
2018-04-30 10:06:58 +0200

    DBZ-252 remove unused import

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSchema.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSchema.java
@@ -18,7 +18,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.debezium.annotation.NotThreadSafe;
-import io.debezium.antlr.mysql.MySqlAntlrDdlParser;
 import io.debezium.antlr.mysql.MySqlSystemVariables.MySqlScope;
 import io.debezium.config.Configuration;
 import io.debezium.connector.mysql.MySqlConnectorConfig.BigIntUnsignedHandlingMode;
