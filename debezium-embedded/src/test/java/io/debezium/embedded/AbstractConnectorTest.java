b23a1ecd96c9f5c8faa83c6debcf6804908ecc9f
Jiri Pechanec <jpechane@redhat.com>
2018-06-20 12:42:34 +0200

    DBZ-20 Restore default test timeout

--- debezium-embedded/src/test/java/io/debezium/embedded/AbstractConnectorTest.java
+++ debezium-embedded/src/test/java/io/debezium/embedded/AbstractConnectorTest.java
@@ -82,7 +82,7 @@ public abstract class AbstractConnectorTest implements Testing {
     private ExecutorService executor;
     protected EmbeddedEngine engine;
     private BlockingQueue<SourceRecord> consumedLines;
-    protected long pollTimeoutInMs = TimeUnit.SECONDS.toMillis(120);
+    protected long pollTimeoutInMs = TimeUnit.SECONDS.toMillis(5);
     protected final Logger logger = LoggerFactory.getLogger(getClass());
     private CountDownLatch latch;
     private JsonConverter keyJsonConverter = new JsonConverter();
