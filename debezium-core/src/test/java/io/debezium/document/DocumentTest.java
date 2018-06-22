357102158a8ad4a0ce20f690caf14cb15f2eecf5
Gunnar Morling <gunnar.morling@googlemail.com>
2018-06-22 11:03:00 +0200

    DBZ-759 Fixing broken Document#setArray() method

--- debezium-core/src/test/java/io/debezium/document/DocumentTest.java
+++ debezium-core/src/test/java/io/debezium/document/DocumentTest.java
@@ -5,6 +5,8 @@
  */
 package io.debezium.document;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -12,25 +14,25 @@ import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.fest.assertions.Assertions.assertThat;
+import io.debezium.doc.FixFor;
 
 /**
  * @author Randall Hauch
  *
  */
 public class DocumentTest {
-    
+
     private Document doc;
     private Map<Path,Value> found = new LinkedHashMap<>();
     private Iterator<Map.Entry<Path, Value>> iterator;
-    
+
     @Before
     public void beforeEach() {
         doc = null;
         found = new LinkedHashMap<>();
         iterator = null;
     }
-    
+
     @Test
     public void shouldPerformForEachOnFlatDocument() {
         doc = Document.create("a","A","b","B");
@@ -40,15 +42,28 @@ public class DocumentTest {
         assertPair(iterator,"/b","B");
         assertNoMore(iterator);
     }
-    
+
+    @Test
+    @FixFor("DBZ-759")
+    public void shouldCreateArrayFromValues() {
+        Document document = Document.create();
+        document.setArray("my_field", 1, 2, 3);
+
+        assertThat(document.toString()).isEqualTo(
+                "{\n" +
+                "  \"my_field\" : [ 1, 2, 3 ]\n" +
+                "}"
+        );
+    }
+
     protected void assertPair( Iterator<Map.Entry<Path, Value>> iterator, String path, Object value ) {
         Map.Entry<Path,Value> entry = iterator.next();
         assertThat((Object)entry.getKey()).isEqualTo(Path.parse(path));
         assertThat(entry.getValue()).isEqualTo(Value.create(value));
     }
-    
+
     protected void assertNoMore( Iterator<Map.Entry<Path, Value>> iterator ) {
         assertThat(iterator.hasNext()).isFalse();
     }
-    
+
 }
