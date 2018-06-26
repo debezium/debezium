b27421ddd0e71efe9eaa119f2781419f9790dce5
Gunnar Morling <gunnar.morling@googlemail.com>
2018-06-26 12:03:53 +0200

    DBZ-759 Testing via SchemaUtil;
    * Making RecordWriter private
    * Removing quotes around string representation of byte arrays

--- debezium-core/src/main/java/io/debezium/data/SchemaUtil.java
+++ debezium-core/src/main/java/io/debezium/data/SchemaUtil.java
@@ -21,17 +21,17 @@ import org.apache.kafka.connect.source.SourceRecord;
 
 /**
  * Utilities for obtaining JSON string representations of {@link Schema}, {@link Struct}, and {@link Field} objects.
- * 
+ *
  * @author Randall Hauch
  */
 public class SchemaUtil {
-    
+
     private SchemaUtil() {
     }
 
     /**
      * Obtain a JSON string representation of the specified field.
-     * 
+     *
      * @param field the field; may not be null
      * @return the JSON string representation
      */
@@ -41,7 +41,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified field.
-     * 
+     *
      * @param field the field; may not be null
      * @return the JSON string representation
      */
@@ -51,7 +51,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified {@link Struct}.
-     * 
+     *
      * @param struct the {@link Struct}; may not be null
      * @return the JSON string representation
      */
@@ -61,7 +61,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified {@link Schema}.
-     * 
+     *
      * @param schema the {@link Schema}; may not be null
      * @return the JSON string representation
      */
@@ -71,7 +71,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified {@link SourceRecord}.
-     * 
+     *
      * @param record the {@link SourceRecord}; may not be null
      * @return the JSON string representation
      */
@@ -81,7 +81,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified field.
-     * 
+     *
      * @param field the field; may not be null
      * @return the JSON string representation
      */
@@ -91,7 +91,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified {@link Struct}.
-     * 
+     *
      * @param struct the {@link Struct}; may not be null
      * @return the JSON string representation
      */
@@ -101,7 +101,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified {@link Schema}.
-     * 
+     *
      * @param schema the {@link Schema}; may not be null
      * @return the JSON string representation
      */
@@ -111,7 +111,7 @@ public class SchemaUtil {
 
     /**
      * Obtain a JSON string representation of the specified {@link SourceRecord}.
-     * 
+     *
      * @param record the {@link SourceRecord}; may not be null
      * @return the JSON string representation
      */
@@ -119,7 +119,7 @@ public class SchemaUtil {
         return new RecordWriter().detailed(true).append(record).toString();
     }
 
-    public static class RecordWriter {
+    private static class RecordWriter {
         private final StringBuilder sb = new StringBuilder();
         private boolean detailed = false;
 
@@ -257,8 +257,7 @@ public class SchemaUtil {
         }
 
         protected void append(byte[] b) {
-            String arrayString = Arrays.toString(b);
-            sb.append('"').append(arrayString).append('"');
+            sb.append(Arrays.toString(b));
         }
 
         protected void appendFirst(String name, Object value) {
