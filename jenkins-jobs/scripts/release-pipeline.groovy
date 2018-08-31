8950c586d9e3d3d73c90aa8353d53a000dfe407e
Jiri Pechanec <jpechane@redhat.com>
2018-08-31 13:32:26 +0200

    DBZ-884 Validate version format

--- jenkins-jobs/scripts/release-pipeline.groovy
+++ jenkins-jobs/scripts/release-pipeline.groovy
@@ -178,6 +178,15 @@ def mvnRelease(repoDir, repoName, branchName, buildArgs = '') {
 
 node('Slave') {
 
+    stage ('Validate parameters') {
+        if (!(RELEASE_VERSION ==~ /\d+\.\d+.\d+\.(Final|(Alpha|Beta|CR)\d+)/)) {
+            error "Release version '$RELEASE_VERSION' is not of the required format x.y.z.suffix"
+        }
+        if (!(DEVELOPMENT_VERSION ==~ /\d+\.\d+.\d+\-SNAPSHOT/)) {
+            error "Development version '$DEVELOPMENT_VERSION' is not of the required format x.y.z-SNAPSHOT"
+        }
+    }
+
     stage ('Initialize') {
         dir('.') {
             deleteDir()
