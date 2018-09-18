10ec38569f89c3442c9a1a4559a3dda940f403b1
Gunnar Morling <gunnar.morling@googlemail.com>
2018-09-18 15:47:50 +0200

    DBZ-899 Using custom statement to obtain SQL type info in order to avoid N+1 issues with PgDatabaseMetaData#getTypeInfo()

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/PostgresConnection.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/PostgresConnection.java
@@ -11,12 +11,17 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.kafka.connect.errors.ConnectException;
 import org.postgresql.core.BaseConnection;
 import org.postgresql.core.TypeInfo;
+import org.postgresql.jdbc.PgDatabaseMetaData;
 import org.postgresql.replication.LogSequenceNumber;
 import org.postgresql.util.PSQLState;
 import org.slf4j.Logger;
@@ -29,6 +34,7 @@ import io.debezium.jdbc.JdbcConfiguration;
 import io.debezium.jdbc.JdbcConnection;
 import io.debezium.relational.TableId;
 import io.debezium.util.Clock;
+import io.debezium.util.Collect;
 import io.debezium.util.Metronome;
 
 /**
@@ -49,6 +55,7 @@ public class PostgresConnection extends JdbcConnection {
     private static final String SQL_NON_ARRAY_TYPES = "SELECT t.oid AS oid, t.typname AS name "
             + "FROM pg_catalog.pg_type t JOIN pg_catalog.pg_namespace n ON (t.typnamespace = n.oid) "
             + "WHERE n.nspname != 'pg_toast' AND t.typcategory <> 'A'";
+
     private static final String SQL_ARRAY_TYPES = "SELECT t.oid AS oid, t.typname AS name, t.typelem AS element "
             + "FROM pg_catalog.pg_type t JOIN pg_catalog.pg_namespace n ON (t.typnamespace = n.oid) "
             + "WHERE n.nspname != 'pg_toast' AND t.typcategory = 'A'";
@@ -318,10 +325,12 @@ public class PostgresConnection extends JdbcConnection {
     }
 
     private static TypeRegistry initTypeRegistry(Connection db) {
-        final TypeInfo typeInfo = ((BaseConnection)db).getTypeInfo();
-
+        final TypeInfo typeInfo = ((BaseConnection) db).getTypeInfo();
         TypeRegistry.Builder typeRegistryBuilder = TypeRegistry.create(typeInfo);
+
         try {
+            SqlTypeMapper sqlTypeMapper = new SqlTypeMapper(db, typeInfo);
+
             try (final Statement statement = db.createStatement()) {
                 // Read non-array types
                 try (final ResultSet rs = statement.executeQuery(SQL_NON_ARRAY_TYPES)) {
@@ -333,7 +342,7 @@ public class PostgresConnection extends JdbcConnection {
                         typeRegistryBuilder.addType(new PostgresType(
                                 typeName,
                                 oid,
-                                typeInfo.getSQLType(typeName),
+                                sqlTypeMapper.getSqlType(typeName),
                                 typeInfo
                         ));
                     }
@@ -348,7 +357,7 @@ public class PostgresConnection extends JdbcConnection {
                         typeRegistryBuilder.addType(new PostgresType(
                                 typeName,
                                 oid,
-                                typeInfo.getSQLType(typeName),
+                                sqlTypeMapper.getSqlType(typeName),
                                 typeInfo,
                                 typeRegistryBuilder.get((int)rs.getLong("element"))
                         ));
@@ -364,5 +373,103 @@ public class PostgresConnection extends JdbcConnection {
 
     public TypeRegistry getTypeRegistry() {
         return typeRegistry;
+    }
+
+    /**
+     * Allows to obtain the SQL type corresponding to PG types. This uses a custom statement instead of going through
+     * {@link PgDatabaseMetaData#getTypeInfo()} as the latter causes N+1 SELECTs, making it very slow on installations
+     * with many custom types.
+     *
+     * @author Gunnar Morling
+     * @see DBZ-899
+     */
+    private static class SqlTypeMapper {
+
+        /**
+         * Based on org.postgresql.jdbc.TypeInfoCache.getSQLType(String). To emulate the original statement's behavior
+         * (which works for single types only), PG's DISTINCT ON extension is used to just return the first entry should a
+         * type exist in multiple schemas.
+         */
+        private static final String SQL_TYPE_DETAILS = "SELECT DISTINCT ON (typname) typname, typinput='array_in'::regproc, typtype, sp.r, pg_type.oid "
+                + "  FROM pg_catalog.pg_type "
+                + "  LEFT "
+                + "  JOIN (select ns.oid as nspoid, ns.nspname, r.r "
+                + "          from pg_namespace as ns "
+                // -- go with older way of unnesting array to be compatible with 8.0
+                + "          join ( select s.r, (current_schemas(false))[s.r] as nspname "
+                + "                   from generate_series(1, array_upper(current_schemas(false), 1)) as s(r) ) as r "
+                + "         using ( nspname ) "
+                + "       ) as sp "
+                + "    ON sp.nspoid = typnamespace "
+                + " ORDER BY typname, sp.r, pg_type.oid;";
+
+        private final TypeInfo typeInfo;
+        private final Set<String> preloadedSqlTypes;
+        private final Map<String, Integer> sqlTypesByPgTypeNames;
+
+        private SqlTypeMapper(Connection db, TypeInfo typeInfo) throws SQLException {
+            this.typeInfo = typeInfo;
+            this.preloadedSqlTypes = Collect.unmodifiableSet(typeInfo.getPGTypeNamesWithSQLTypes());
+            this.sqlTypesByPgTypeNames = getSqlTypes(db, typeInfo);
+        }
+
+        public int getSqlType(String typeName) throws SQLException {
+            boolean isCoreType = preloadedSqlTypes.contains(typeName);
+
+            // obtain core types such as bool, int2 etc. from the driver, as it correctly maps these types to the JDBC
+            // type codes. Also those values are cached in TypeInfoCache.
+            if (isCoreType) {
+                return typeInfo.getSQLType(typeName);
+            }
+            if (typeName.endsWith("[]")) {
+                return Types.ARRAY;
+            }
+            // get custom type mappings from the map which was built up with a single query
+            else {
+                try {
+                    return sqlTypesByPgTypeNames.get(typeName);
+                }
+                catch(Exception e) {
+                    LOGGER.warn("Failed to obtain SQL type information for type {} via custom statement, falling back to TypeInfo#getSQLType()", typeName, e);
+                    return typeInfo.getSQLType(typeName);
+                }
+            }
+        }
+
+        /**
+         * Builds up a map of SQL (JDBC) types by PG type name; contains only values for non-core types.
+         */
+        private static Map<String, Integer> getSqlTypes(Connection db, TypeInfo typeInfo) throws SQLException {
+            Map<String, Integer> sqlTypesByPgTypeNames = new HashMap<>();
+
+            try (final Statement statement = db.createStatement()) {
+                try (final ResultSet rs = statement.executeQuery(SQL_TYPE_DETAILS)) {
+                    while (rs.next()) {
+                        int type;
+                        boolean isArray = rs.getBoolean(2);
+                        String typtype = rs.getString(3);
+                        if (isArray) {
+                            type = Types.ARRAY;
+                        }
+                        else if ("c".equals(typtype)) {
+                            type = Types.STRUCT;
+                        }
+                        else if ("d".equals(typtype)) {
+                            type = Types.DISTINCT;
+                        }
+                        else if ("e".equals(typtype)) {
+                            type = Types.VARCHAR;
+                        }
+                        else {
+                            type = Types.OTHER;
+                        }
+
+                        sqlTypesByPgTypeNames.put(rs.getString(1), type);
+                    }
+                }
+            }
+
+            return sqlTypesByPgTypeNames;
+        }
     }
 }
