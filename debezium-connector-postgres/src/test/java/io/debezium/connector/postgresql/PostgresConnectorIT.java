c1dc78fcc27df76b527b2ef050a4575a877bdfed
Jiri Pechanec <jpechane@redhat.com>
2018-11-13 08:47:21 +0100

    DBZ-965 Reduce the test time

--- debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/PostgresConnectorIT.java
+++ debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/PostgresConnectorIT.java
@@ -453,7 +453,6 @@ public class PostgresConnectorIT extends AbstractConnectorTest {
                                          .with(PostgresConnectorConfig.SNAPSHOT_MODE, NEVER.getValue())
                                          .with(PostgresConnectorConfig.DROP_SLOT_ON_STOP, Boolean.TRUE)
                                          .with(PostgresConnectorConfig.TABLE_WHITELIST, "s1.a")
-                                         .with(WorkerConfig.OFFSET_COMMIT_INTERVAL_MS_CONFIG, "1000")
                                          .build();
         start(PostgresConnector.class, config);
         assertConnectorIsRunning();
@@ -471,7 +470,7 @@ public class PostgresConnectorIT extends AbstractConnectorTest {
                 assertThat(actualRecords.topics().size()).isEqualTo(1);
                 assertThat(actualRecords.recordsForTopic(topicName("s1.a")).size()).isEqualTo(1);
 
-                TimeUnit.MILLISECONDS.sleep(1_100);
+                TimeUnit.MILLISECONDS.sleep(20);
                 flushLsn.add(getConfirmedFlushLsn(connection));
             }
         }
