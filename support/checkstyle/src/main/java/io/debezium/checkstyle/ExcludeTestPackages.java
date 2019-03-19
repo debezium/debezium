ead46b7e6783a12af6eac79f04d8f0ed4a2d6b40
shubham <shubhamrwt02@gmail.com>
2019-03-19 15:50:03 +0530

    update if with braces

--- support/checkstyle/src/main/java/io/debezium/checkstyle/ExcludeTestPackages.java
+++ support/checkstyle/src/main/java/io/debezium/checkstyle/ExcludeTestPackages.java
@@ -1,6 +1,6 @@
 /*
  * Copyright Debezium Authors.
- * 
+ *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.debezium.checkstyle;
@@ -16,7 +16,7 @@ import com.puppycrawl.tools.checkstyle.api.Filter;
  * "[not required for tests]".
  * <p>
  * A SuppressionFilter is too generic, and requires per-module configuration.
- * 
+ *
  * @author Sanne Grinovero <sanne@hibernate.org>
  */
 public class ExcludeTestPackages implements Filter {
@@ -25,7 +25,7 @@ public class ExcludeTestPackages implements Filter {
     private static final String MESSAGE_DISABLE_KEYWORD = "[not required for tests]";
 
     @Override
-    public boolean accept( AuditEvent aEvent ) {
+    public boolean accept(AuditEvent aEvent) {
         final String fileName = aEvent.getFileName();
         if (fileName != null && fileName.contains(SUB_PATH)) {
             return acceptTestfileEvent(aEvent);
@@ -33,7 +33,7 @@ public class ExcludeTestPackages implements Filter {
         return true;
     }
 
-    private boolean acceptTestfileEvent( AuditEvent aEvent ) {
+    private boolean acceptTestfileEvent(AuditEvent aEvent) {
         final String message = aEvent.getMessage();
         if (message != null && message.contains(MESSAGE_DISABLE_KEYWORD)) {
             return false;
