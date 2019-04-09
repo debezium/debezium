9c7f181174ec886c6c07758e08ee2e3314815833
Jiri Pechanec <jpechane@redhat.com>
2019-04-09 20:18:04 +0200

    DBZ-1143 Year adjustment configurable; test 19xx years

--- debezium-connector-mysql/src/test/resources/ddl/year_test.sql
+++ debezium-connector-mysql/src/test/resources/ddl/year_test.sql
@@ -13,6 +13,15 @@ CREATE TABLE dbz_1143_year_test (
       dt18 DATETIME,
       dt0018 DATETIME,
       dt2018 DATETIME,
+      y78 YEAR,
+      y0078 YEAR,
+      y1978 YEAR,
+      d78 DATE,
+      d0078 DATE,
+      d1978 DATE,
+      dt78 DATETIME,
+      dt0078 DATETIME,
+      dt1978 DATETIME,
      PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8;
 
@@ -26,5 +35,14 @@ INSERT INTO dbz_1143_year_test VALUES (
     '2018-04-01',
     '18-04-01 12:34:56',
     '0018-04-01 12:34:56',
-    '2018-04-01 12:34:56'
+    '2018-04-01 12:34:56',
+    '78',
+    '0078',
+    '1978',
+    '78-04-01',
+    '0078-04-01',
+    '1978-04-01',
+    '78-04-01 12:34:56',
+    '0078-04-01 12:34:56',
+    '1978-04-01 12:34:56'
 );
