50fbe3211303a2a78447357bc0811441ffb29d3c
Jiri Pechanec <jpechane@redhat.com>
2019-04-10 11:15:15 +0200

    DBZ-1220 Integration test for schema metadata

--- debezium-connector-mysql/src/test/resources/ddl/source_type_as_schema_parameter_test.sql
+++ debezium-connector-mysql/src/test/resources/ddl/source_type_as_schema_parameter_test.sql
@@ -8,7 +8,9 @@ CREATE TABLE dbz_644_source_type_mapped_as_schema_parameter_test (
       c2 MEDIUMINT,
       c3a NUMERIC(5,2),
       c3b VARCHAR(128),
+      f1 FLOAT(10),
+      f2 FLOAT(8,4),
       PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8;
 
-INSERT INTO dbz_644_source_type_mapped_as_schema_parameter_test VALUES (default, 123, 456, 789.01, 'test');
+INSERT INTO dbz_644_source_type_mapped_as_schema_parameter_test VALUES (default, 123, 456, 789.01, 'test', 1.228, 234.56);
