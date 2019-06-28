884def88d79a9d4e1bf7ffa6bea721606aeed265
Yang Yang <yang.yang@afterpaytouch.com>
2019-06-28 10:00:03 +1000

    DBZ-1604 Added a new converter to enable byte[] payload

---
+++ debezium-core/src/main/java/io/debezium/converters/ByteBufferConverter.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.converters;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.storage.Converter;
+import org.apache.kafka.connect.storage.ConverterConfig;
+import org.apache.kafka.connect.storage.HeaderConverter;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * A customized value converter to allow avro message to be delivered as it is (byte[]) to kafka, this is used
+ * for outbox pattern where payload is serialized by KafkaAvroSerializer, the consumer need to get the deseralized payload.
+ *
+ * To enabled the converter in a connector, the following value need to be specified
+ * "value.converter": "io.debezium.converters.ByteBufferConverter"
+ *
+ * @author Yang Yang
+ */
+public class ByteBufferConverter implements Converter, HeaderConverter {
+
+    private static final ConfigDef CONFIG_DEF = ConverterConfig.newConfigDef();
+
+    @Override
+    public ConfigDef config() {
+        return CONFIG_DEF;
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+
+    @Override
+    public byte[] fromConnectData(String topic, Schema schema, Object value) {
+        validateSchemaType(schema);
+        validateValueType(value);
+        return value == null ? null : ((ByteBuffer) value).array();
+    }
+
+    private void validateValueType(Object value) {
+        if (value != null && !(value instanceof ByteBuffer)) {
+            throw new DataException("ByteBufferConverter is not compatible with objects of type " + value.getClass());
+        }
+    }
+
+    private void validateSchemaType(Schema schema) {
+        if (schema != null && schema.type() != Schema.Type.BYTES) {
+            throw new DataException("Invalid schema type for ByteBufferConverter: " + schema.type().toString());
+        }
+    }
+
+    @Override
+    public SchemaAndValue toConnectData(String topic, byte[] value) {
+        return new SchemaAndValue(Schema.OPTIONAL_BYTES_SCHEMA, value == null ? null : ByteBuffer.wrap(value));
+    }
+
+    @Override
+    public byte[] fromConnectHeader(String topic, String headerKey, Schema schema, Object value) {
+        return fromConnectData(topic, schema, value);
+    }
+
+    @Override
+    public SchemaAndValue toConnectHeader(String topic, String headerKey, byte[] value) {
+        return toConnectData(topic, value);
+    }
+
+    @Override
+    public void close() {
+        // do nothing
+    }
+}
---
+++ debezium-core/src/test/java/io/debezium/converters/ByteBufferConverterTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.debezium.converters;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.errors.DataException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+import static junit.framework.TestCase.fail;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class ByteBufferConverterTest {
+
+    private static final String TOPIC = "topic";
+    private static final byte[] SAMPLE_BYTES = "sample string".getBytes(StandardCharsets.UTF_8);
+
+    private ByteBufferConverter converter = new ByteBufferConverter();
+
+    @Before
+    public void setUp() {
+        converter.configure(Collections.emptyMap(), false);
+    }
+
+    @Test
+    public void shouldConvertFromConnectData() {
+        byte[] bytes = converter.fromConnectData(TOPIC, Schema.BYTES_SCHEMA, ByteBuffer.wrap(SAMPLE_BYTES));
+
+        assertThat(bytes).isEqualTo(SAMPLE_BYTES);
+    }
+
+    @Test
+    public void shouldConvertFromConnectDataForOptionalBytesSchema() {
+        byte[] bytes = converter.fromConnectData(TOPIC, Schema.OPTIONAL_BYTES_SCHEMA, ByteBuffer.wrap(SAMPLE_BYTES));
+
+        assertThat(bytes).isEqualTo(SAMPLE_BYTES);
+    }
+
+    @Test
+    public void shouldConvertFromConnectDataWithoutSchema() {
+        byte[] bytes = converter.fromConnectData(TOPIC, null, ByteBuffer.wrap(SAMPLE_BYTES));
+
+        assertThat(bytes).isEqualTo(SAMPLE_BYTES);
+    }
+
+    @Test
+    public void shouldConvertNullFromConnectData() {
+        byte[] bytes = converter.fromConnectData(TOPIC, null, null);
+
+        assertThat(bytes).isNull();
+    }
+
+    @Test
+    public void shouldThrowWhenConvertNonByteSchemaFromConnectData() {
+        try {
+            converter.fromConnectData(TOPIC, Schema.BOOLEAN_SCHEMA, null);
+            fail("now expected exception thrown");
+        }
+        catch (Exception e) {
+            assertThat(e).isExactlyInstanceOf(DataException.class);
+        }
+    }
+
+    @Test
+    public void shouldThrowWhenConvertRawByteArrayFromConnectData() {
+        try {
+            converter.fromConnectData(TOPIC, Schema.BOOLEAN_SCHEMA, null);
+            fail("now expected exception thrown");
+        }
+        catch (Exception e) {
+            assertThat(e).isExactlyInstanceOf(DataException.class);
+        }
+
+    }
+
+    @Test
+    public void shouldConvertToConnectData() {
+        SchemaAndValue schemaAndValue = converter.toConnectData(TOPIC, SAMPLE_BYTES);
+
+        assertThat(schemaAndValue.schema()).isEqualTo(Schema.OPTIONAL_BYTES_SCHEMA);
+        assertThat(schemaAndValue.value()).isEqualTo(ByteBuffer.wrap(SAMPLE_BYTES));
+    }
+
+    @Test
+    public void shouldConvertToConnectDataForNullValue() {
+        SchemaAndValue schemaAndValue = converter.toConnectData(TOPIC, null);
+
+        assertThat(schemaAndValue.schema()).isEqualTo(Schema.OPTIONAL_BYTES_SCHEMA);
+        assertThat(schemaAndValue.value()).isNull();
+    }
+}
