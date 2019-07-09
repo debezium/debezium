51c0fb83974c7c92a551c1614950429a9734e0e2
Gunnar Morling <gunnar.morling@googlemail.com>
2019-07-09 10:07:22 +0200

    DBZ-1371 Restoring original timeouts

--- debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/TestHelper.java
+++ debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/TestHelper.java
@@ -235,7 +235,7 @@ public final class TestHelper {
     }
 
     public static int waitTimeForRecords() {
-        return Integer.parseInt(System.getProperty(TEST_PROPERTY_PREFIX + "records.waittime", "20"));
+        return Integer.parseInt(System.getProperty(TEST_PROPERTY_PREFIX + "records.waittime", "2"));
     }
 
     protected static SourceInfo sourceInfo() {
