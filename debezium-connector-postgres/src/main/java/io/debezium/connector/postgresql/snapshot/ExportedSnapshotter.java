3301755dbdd248080161ca0fc7853c03562a09b8
Chris Cranford <chris@hibernate.org>
2019-07-26 09:22:06 -0400

    DBZ-1035 Checkstyle fixup

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/snapshot/ExportedSnapshotter.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/snapshot/ExportedSnapshotter.java
@@ -10,8 +10,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
-import org.postgresql.replication.LogSequenceNumber;
-
 import io.debezium.connector.postgresql.PostgresConnectorConfig;
 import io.debezium.connector.postgresql.spi.OffsetState;
 import io.debezium.connector.postgresql.spi.SlotCreationResult;
