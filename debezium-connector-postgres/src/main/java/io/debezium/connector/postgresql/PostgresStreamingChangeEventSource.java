ee7a56ec36bf065e636a567f116cd283a670ab79
Jiri Pechanec <jpechane@redhat.com>
2019-10-15 16:03:49 +0200

    DBZ-892 Flush on empty messages

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresStreamingChangeEventSource.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresStreamingChangeEventSource.java
@@ -102,6 +102,7 @@ public class PostgresStreamingChangeEventSource implements StreamingChangeEventS
                     if (message == null) {
                         LOGGER.trace("Received empty message");
                         lastCompletelyProcessedLsn = lsn;
+                        offsetContext.updateWalPosition(lsn, lastCompletelyProcessedLsn, null, null, null, taskContext.getSlotXmin(connection));
                         dispatcher.dispatchHeartbeatEvent(offsetContext);
                         return;
                     }
