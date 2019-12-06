bed7c1c1aef37aba764e4af03e2558e251a395af
Jiri Pechanec <jpechane@redhat.com>
2019-12-06 09:25:42 +0100

    DBZ-1533 Added documentation

--- debezium-core/src/main/java/io/debezium/serde/Serdes.java
+++ debezium-core/src/main/java/io/debezium/serde/Serdes.java
@@ -19,6 +19,21 @@ import io.debezium.serde.json.JsonSerde;
 @Incubating
 public class Serdes {
 
+    /**
+     * Provides a {@link Serde} implementation that maps JSON Debezium message into a {@code T} Java class.<br />
+     * When used as key deserializer then the key fields are directly mapped into Java object key.<br />
+     * When used as value deserializer its behaviour is driven by value of {@code from.field} config option
+     * <ul>
+     * <li>not set - maps complete message envelope</li>
+     * <li>{@code before/after} - extracts the given field from the envelope and maps it
+     * </ul>
+     * If schema is enabled then the serde will extract the {@code payload} field to get the envelope and applies
+     * the rules above.
+     * 
+     * @param <T> type to which JSON is mapped
+     * @param objectType type to which JSON is mapped
+     * @return serializer/deserializer to convert JSON ro/from Java class
+     */
     public static <T> Serde<T> payloadJson(Class<T> objectType) {
         return new JsonSerde<>(objectType);
     }
