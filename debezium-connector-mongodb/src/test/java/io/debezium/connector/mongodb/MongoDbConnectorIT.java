cd309982fc747fc6c976c2b8414c467a747c6442
Gunnar Morling <gunnar.morling@googlemail.com>
2020-01-10 17:39:30 +0100

    DBZ-1292 Removing static state in CloudEventsConverterTest

--- debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/MongoDbConnectorIT.java
+++ debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/MongoDbConnectorIT.java
@@ -34,10 +34,10 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.InsertOneOptions;
 import com.mongodb.util.JSON;
 
-import io.debezium.converters.CloudEventsConverterTest;
 import io.debezium.config.CommonConnectorConfig;
 import io.debezium.config.Configuration;
 import io.debezium.connector.mongodb.ConnectionContext.MongoPrimary;
+import io.debezium.converters.CloudEventsConverterTest;
 import io.debezium.data.Envelope;
 import io.debezium.data.Envelope.Operation;
 import io.debezium.doc.FixFor;
@@ -848,6 +848,7 @@ public class MongoDbConnectorIT extends AbstractConnectorTest {
         List<SourceRecord> topicRecords = records.recordsForTopic("mongo.dbit.restaurants");
         for (SourceRecord record : topicRecords) {
             CloudEventsConverterTest.shouldConvertToCloudEventsInJson(record);
+            CloudEventsConverterTest.shouldConvertToCloudEventsInJsonWithDataAsAvro(record);
             CloudEventsConverterTest.shouldConvertToCloudEventsInAvro(record);
         }
 
@@ -855,9 +856,10 @@ public class MongoDbConnectorIT extends AbstractConnectorTest {
 
         // Wait until we can consume the 4 documents we just added ...
         SourceRecords records2 = consumeRecordsByTopic(4);
-        List<SourceRecord> topicRecords2 = records.recordsForTopic("mongo.dbit.restaurants");
+        List<SourceRecord> topicRecords2 = records2.recordsForTopic("mongo.dbit.restaurants");
         for (SourceRecord record : topicRecords2) {
             CloudEventsConverterTest.shouldConvertToCloudEventsInJson(record);
+            CloudEventsConverterTest.shouldConvertToCloudEventsInJsonWithDataAsAvro(record);
             CloudEventsConverterTest.shouldConvertToCloudEventsInAvro(record);
         }
 
