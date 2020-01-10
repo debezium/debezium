5049f517666677c893283510929bf1b8f294ec06
Gunnar Morling <gunnar.morling@googlemail.com>
2020-01-10 15:30:02 +0100

    DBZ-1292 Refactoring of options;
    * Avoiding hard dependency to Avro
    * Passing through all options from JSON and Avro converters by means of "json." and "avro." sub-namespaces

--- debezium-core/src/main/java/io/debezium/converters/CloudEventsConverterConfig.java
+++ debezium-core/src/main/java/io/debezium/converters/CloudEventsConverterConfig.java
@@ -5,7 +5,6 @@
  */
 package io.debezium.converters;
 
-import java.util.List;
 import java.util.Map;
 
 import org.apache.kafka.common.config.ConfigDef;
@@ -16,20 +15,13 @@ import org.apache.kafka.connect.storage.ConverterConfig;
  */
 public class CloudEventsConverterConfig extends ConverterConfig {
 
-    public static final String CLOUDEVENTS_SERIALIZER_TYPE_CONFIG = "cloudevents.serializer.type";
+    public static final String CLOUDEVENTS_SERIALIZER_TYPE_CONFIG = "serializer.type";
     public static final String CLOUDEVENTS_SERIALIZER_TYPE_DEFAULT = "json";
     private static final String CLOUDEVENTS_SERIALIZER_TYPE_DOC = "Specify a serializer to serialize CloudEvents values";
 
-    public static final String CLOUDEVENTS_DATA_SERIALIZER_TYPE_CONFIG = "cloudevents.data.serializer.type";
+    public static final String CLOUDEVENTS_DATA_SERIALIZER_TYPE_CONFIG = "data.serializer.type";
     public static final String CLOUDEVENTS_DATA_SERIALIZER_TYPE_DEFAULT = "json";
     private static final String CLOUDEVENTS_DATA_SERIALIZER_TYPE_DOC = "Specify a serializer to serialize the data field of CloudEvents values";
-
-    public static final String CLOUDEVENTS_JSON_SCHEMAS_ENABLE_CONFIG = "json.schemas.enable";
-    public static final boolean CLOUDEVENTS_JSON_SCHEMAS_ENABLE_DEFAULT = false;
-    private static final String CLOUDEVENTS_JSON_SCHEMAS_ENABLE_DOC = "Include json schemas within each of the serialized CloudEvents data.";
-
-    public static final String CLOUDEVENTS_SCHEMA_REGISTRY_URL_CONFIG = "schema.registry.url";
-    private static final String CLOUDEVENTS_SCHEMA_REGISTRY_URL_DOC = "Comma-separated list of URLs for schema registry instances that can be used to register or look up schemas for CloudEvents.";
 
     private static final ConfigDef CONFIG;
 
@@ -40,10 +32,6 @@ public class CloudEventsConverterConfig extends ConverterConfig {
                 CLOUDEVENTS_SERIALIZER_TYPE_DOC);
         CONFIG.define(CLOUDEVENTS_DATA_SERIALIZER_TYPE_CONFIG, ConfigDef.Type.STRING, CLOUDEVENTS_DATA_SERIALIZER_TYPE_DEFAULT, ConfigDef.Importance.HIGH,
                 CLOUDEVENTS_DATA_SERIALIZER_TYPE_DOC);
-        CONFIG.define(CLOUDEVENTS_JSON_SCHEMAS_ENABLE_CONFIG, ConfigDef.Type.BOOLEAN, CLOUDEVENTS_JSON_SCHEMAS_ENABLE_DEFAULT, ConfigDef.Importance.MEDIUM,
-                CLOUDEVENTS_JSON_SCHEMAS_ENABLE_DOC);
-        CONFIG.define(CLOUDEVENTS_SCHEMA_REGISTRY_URL_CONFIG, ConfigDef.Type.LIST, CLOUDEVENTS_SCHEMA_REGISTRY_URL_DOC, ConfigDef.Importance.MEDIUM,
-                CLOUDEVENTS_SCHEMA_REGISTRY_URL_DOC);
     }
 
     public static ConfigDef configDef() {
@@ -70,23 +58,5 @@ public class CloudEventsConverterConfig extends ConverterConfig {
      */
     public SerializerType cloudeventsDataSerializerTypeConfig() {
         return SerializerType.withName(getString(CLOUDEVENTS_DATA_SERIALIZER_TYPE_CONFIG));
-    }
-
-    /**
-     * Return whether schemas of the data field are enabled when using json serializer.
-     *
-     * @return true if enabled, or false otherwise
-     */
-    public Boolean cloudeventsJsonSchemasEnable() {
-        return getBoolean(CLOUDEVENTS_JSON_SCHEMAS_ENABLE_CONFIG);
-    }
-
-    /**
-     * Return a list of URLs for schema registry instances that can be used to register or look up schemas for CloudEvents.
-     *
-     * @return a list of schema registry URLs
-     */
-    public List<String> cloudeventsSchemaRegistryUrls() {
-        return getList(CLOUDEVENTS_SCHEMA_REGISTRY_URL_CONFIG);
     }
 }
