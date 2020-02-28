e83de1863b22b03ca7c7672bf68d3587e98d1cb1
Gunnar Morling <gunnar.morling@googlemail.com>
2020-02-28 21:57:05 +0100

    DBZ-1134 JavaDoc

--- debezium-api/src/main/java/io/debezium/spi/converter/CustomConverter.java
+++ debezium-api/src/main/java/io/debezium/spi/converter/CustomConverter.java
@@ -18,25 +18,32 @@ import io.debezium.common.annotation.Incubating;
 public interface CustomConverter<S, F extends ConvertedField> {
 
     /**
-     * An Actual conversion converting data from one type to another.
-     *
+     * A conversion converting data from one type to another.
      */
     @FunctionalInterface
     interface Converter {
         Object convert(Object input);
     }
 
+    /**
+     * Callback for registering a converter.
+     */
     public interface ConverterRegistration<S> {
+
+        /**
+         * Registers the given schema and converter for the current field. Should not be
+         * invoked more than once for the same field.
+         */
         void register(S fieldSchema, Converter converter);
     }
 
     void configure(Properties props);
 
     /**
-     * A custom converter injected by the Debezium user.
+     * Allows to register a custom value and schema converter for a given field-
      *
-     * @param field - converted field metadata
-     * @return empty if the converter is not applicable for this field
+     * @param field the field of interest
+     * @param registration a callback that allows to register a schema and value converter for the given field.
      */
     void converterFor(F field, ConverterRegistration<S> registration);
 }
