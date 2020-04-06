3be41a87299a6aa318f2413c2c6bfafa1bb8c57c
Chris Cranford <chris@hibernate.org>
2020-04-06 09:07:58 -0400

    DBZ-1925 Introduced SkipWhenKafkaVersion annotation support

--- debezium-core/src/test/java/io/debezium/junit/SkipTestRule.java
+++ debezium-core/src/test/java/io/debezium/junit/SkipTestRule.java
@@ -5,8 +5,14 @@
  */
 package io.debezium.junit;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+
+import io.debezium.util.Testing;
 
 /**
  * JUnit rule that inspects the presence of the {@link SkipLongRunning} annotation either on a test method or on a test suite. If
@@ -38,6 +44,56 @@ public class SkipTestRule extends AnnotationBasedTestRule {
                         return emptyStatement(skipOnOSAnnotation.description(), description);
                     }
                 }
+            }
+        }
+
+        SkipWhenKafkaVersion skipWhenKafkaVersionAnnotation = hasAnnotation(description, SkipWhenKafkaVersion.class);
+        if (skipWhenKafkaVersionAnnotation != null) {
+            SkipWhenKafkaVersion.KafkaVersion kafkaVersion = skipWhenKafkaVersionAnnotation.value();
+            SkipWhenKafkaVersion.EqualityCheck check = skipWhenKafkaVersionAnnotation.check();
+            try (InputStream stream = Testing.class.getResourceAsStream("/kafka/kafka-version.properties")) {
+                if (stream != null) {
+                    final Properties properties = new Properties();
+                    properties.load(stream);
+
+                    final String version = properties.getProperty("version");
+                    final String[] versionArray = version.split("\\.");
+
+                    int major = (versionArray.length >= 1) ? Integer.parseInt(versionArray[0]) : 0;
+                    int minor = (versionArray.length >= 2) ? Integer.parseInt(versionArray[1]) : 1;
+                    int patch = (versionArray.length >= 3) ? Integer.parseInt(versionArray[2]) : 2;
+
+                    switch (check) {
+                        case LESS_THAN:
+                            if (kafkaVersion.isLessThan(major, minor, patch)) {
+                                return emptyStatement(skipWhenKafkaVersionAnnotation.description(), description);
+                            }
+                            break;
+                        case LESS_THAN_OR_EQUAL:
+                            if (kafkaVersion.isLessThanOrEqualTo(major, minor, patch)) {
+                                return emptyStatement(skipWhenKafkaVersionAnnotation.description(), description);
+                            }
+                            break;
+                        case EQUAL:
+                            if (kafkaVersion.isEqualTo(major, minor, patch)) {
+                                return emptyStatement(skipWhenKafkaVersionAnnotation.description(), description);
+                            }
+                            break;
+                        case GREATER_THAN_OR_EQUAL:
+                            if (kafkaVersion.isGreaterThanOrEqualTo(major, minor, patch)) {
+                                return emptyStatement(skipWhenKafkaVersionAnnotation.description(), description);
+                            }
+                            break;
+                        case GREATER_THAN:
+                            if (kafkaVersion.isGreaterThan(major, minor, patch)) {
+                                return emptyStatement(skipWhenKafkaVersionAnnotation.description(), description);
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (IOException e) {
+                // In the event of an error determining the kafka version, run test.
             }
         }
 
