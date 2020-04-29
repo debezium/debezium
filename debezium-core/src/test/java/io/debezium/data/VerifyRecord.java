34a8651b60c18a9356dd6fc3b4afd53de7bf74e7
Gunnar Morling <gunnar.morling@googlemail.com>
2020-04-29 11:59:46 +0200

    DBZ-1969 Fixing incorrect assertion and utility which concealed this

--- debezium-core/src/test/java/io/debezium/data/VerifyRecord.java
+++ debezium-core/src/test/java/io/debezium/data/VerifyRecord.java
@@ -1090,9 +1090,7 @@ public class VerifyRecord {
             valueSchemasEqual = Objects.equals(schema1.valueSchema(), schema2.valueSchema());
         }
         else if (schema1.type() == Type.ARRAY && schema2.type() == Type.ARRAY) {
-            String valueSchema1String = SchemaUtil.asString(schema1.valueSchema());
-            String valueSchema2String = SchemaUtil.asString(schema2.valueSchema());
-            valueSchemasEqual = valueSchema1String.equals(valueSchema2String);
+            valueSchemasEqual = areConnectSchemasEqual(schema1.valueSchema(), schema2.valueSchema());
         }
         else if (schema1.type() == Type.STRUCT && schema2.type() == Type.STRUCT) {
             fieldsEqual = areFieldListsEqual(schema1.fields(), schema2.fields());
