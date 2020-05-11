7c56f394147fb033f74b2a3a2f0245ac91b8f701
Jiri Pechanec <jpechane@redhat.com>
2020-05-11 15:56:44 +0200

    DBZ-651 Added logging

--- debezium-standalone-quarkus/src/main/java/io/debezium/standalone/quarkus/ConnectorLifecycle.java
+++ debezium-standalone-quarkus/src/main/java/io/debezium/standalone/quarkus/ConnectorLifecycle.java
@@ -12,6 +12,8 @@ import javax.inject.Inject;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.standalone.quarkus.events.ConnectorCompletedEvent;
@@ -30,6 +32,8 @@ import io.debezium.standalone.quarkus.events.TaskStoppedEvent;
 @Liveness
 @ApplicationScoped
 public class ConnectorLifecycle implements HealthCheck, DebeziumEngine.ConnectorCallback, DebeziumEngine.CompletionCallback {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectorLifecycle.class);
 
     private volatile boolean live = false;
 
@@ -50,33 +54,39 @@ public class ConnectorLifecycle implements HealthCheck, DebeziumEngine.Connector
 
     @Override
     public void connectorStarted() {
+        LOGGER.debug("Connector started");
         connectorStartedEvent.fire(new ConnectorStartedEvent());
     }
 
     @Override
     public void connectorStopped() {
+        LOGGER.debug("Connector stopped");
         connectorStoppedEvent.fire(new ConnectorStoppedEvent());
     }
 
     @Override
     public void taskStarted() {
+        LOGGER.debug("Task started");
         taskStartedEvent.fire(new TaskStartedEvent());
         live = true;
     }
 
     @Override
     public void taskStopped() {
+        LOGGER.debug("Task stopped");
         taskStoppedEvent.fire(new TaskStoppedEvent());
     }
 
     @Override
     public void handle(boolean success, String message, Throwable error) {
+        LOGGER.info("Connector completed: success = '{}', message = '{}', error = '{}'", success, message, error);
         connectorCompletedEvent.fire(new ConnectorCompletedEvent(success, message, error));
         live = false;
     }
 
     @Override
     public HealthCheckResponse call() {
+        LOGGER.trace("Healthcheck called - live = '{}'", live);
         return HealthCheckResponse.named("debezium").state(live).build();
     }
 
