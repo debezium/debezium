c3b36c2417b5e2f5170b331d7dba7aa6ed4f9845
Chris Cranford <chris@hibernate.org>
2020-06-08 14:59:54 -0400

    DBZ-2149 Log begin/end of schema recovery

--- debezium-core/src/main/java/io/debezium/relational/history/DatabaseHistoryMetrics.java
+++ debezium-core/src/main/java/io/debezium/relational/history/DatabaseHistoryMetrics.java
@@ -110,11 +110,13 @@ public class DatabaseHistoryMetrics extends Metrics implements DatabaseHistoryLi
     public void recoveryStarted() {
         status = DatabaseHistoryStatus.RECOVERING;
         recoveryStartTime = Instant.now();
+        LOGGER.info("Started database history recovery");
     }
 
     @Override
     public void recoveryStopped() {
         status = DatabaseHistoryStatus.RUNNING;
+        LOGGER.info("Finished database history recovery of {} change(s) in {} ms", changesRecovered.get(), Duration.between(recoveryStartTime, Instant.now()).toMillis());
     }
 
     @Override
