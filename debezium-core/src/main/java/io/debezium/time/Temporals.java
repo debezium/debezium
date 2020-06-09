5651ad24e9497415e722fb8b54681b968f457d63
Balázs Németh <nbali@users.noreply.github.com>
2020-06-09 10:24:50 +0200

    DBZ-2169 Fix for misuse of ConfigurationDefaults.RETURN_CONTROL_INTERVAL;
    * Implemented Temporals.min()
    * Fixed pollInterval calculation based on the documentation

--- debezium-core/src/main/java/io/debezium/time/Temporals.java
+++ debezium-core/src/main/java/io/debezium/time/Temporals.java
@@ -22,4 +22,13 @@ public class Temporals {
     public static Duration max(Duration d1, Duration d2) {
         return d1.compareTo(d2) == 1 ? d1 : d2;
     }
+
+    /**
+     * Returns that duration from the given ones which represents the smaller amount
+     * of time ("is shorted"). If both durations are equal, that same value will be
+     * returned.
+     */
+    public static Duration min(Duration d1, Duration d2) {
+        return d1.compareTo(d2) == 1 ? d2 : d1;
+    }
 }
