6d12ac040656c46e2d6277883c20cbdc3f1d2ece
Ismail Simsek <ismail.simsek@ppro.com>
2020-06-21 20:42:45 +0200

    add guava dependency

--- debezium-server/debezium-server-s3/src/main/java/io/debezium/server/s3/TimeBasedDailyObjectKeyMapper.java
+++ debezium-server/debezium-server-s3/src/main/java/io/debezium/server/s3/TimeBasedDailyObjectKeyMapper.java
@@ -9,10 +9,13 @@ package io.debezium.server.s3;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Objects;
-
+import io.debezium.engine.format.Json;
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.microprofile.config.ConfigProvider;
 
-public class TimeBasedDailyObjectKeyMapper extends DefaultObjectKeyMapper {
+public class TimeBasedDailyObjectKeyMapper implements ObjectKeyMapper {
+    final String objectKeyPrefix = ConfigProvider.getConfig().getValue("debezium.sink.s3.objectkey.prefix", String.class);
+    final String valueFormat = ConfigProvider.getConfig().getOptionalValue("debezium.format.value", String.class).orElse(Json.class.getSimpleName().toLowerCase());
 
     @Override
     public String map(String destination, LocalDateTime batchTime, String recordId) {
