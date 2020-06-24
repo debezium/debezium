dcb89aa1f9e93f20de9f5d96ede3fb80a43b937a
jgraf50 <johngraf@gmail.com>
2020-06-24 13:33:40 -0700

    DBZ-2141 Add custom MongoDB connector error handler to do retries on connection errors

---
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/MongoDbErrorHandler.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.mongodb;
+
+import io.debezium.connector.base.ChangeEventQueue;
+import io.debezium.pipeline.ErrorHandler;
+
+/**
+ * Error handler for MongoDB.
+ *
+ * @author John Graf
+ */
+public class MongoDbErrorHandler extends ErrorHandler {
+
+    public MongoDbErrorHandler(String logicalName, ChangeEventQueue<?> queue) {
+        super(MongoDbConnector.class, logicalName, queue);
+    }
+
+    @Override
+    protected boolean isRetriable(Throwable throwable) {
+        if (throwable instanceof org.apache.kafka.connect.errors.ConnectException) {
+            return true;
+        }
+
+        return false;
+    }
+}
