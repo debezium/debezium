1ebb23601153af142d0b9248d52e952a50a9682f
weiyan.2017 <weiyan.2017@yanex.com>
2020-07-07 09:59:28 +0800

    DBZ-2305 fix mysql decimal deminsion issue

--- debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlSchemaTest.java
+++ debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlSchemaTest.java
@@ -174,6 +174,22 @@ public class MySqlSchemaTest {
         assertHistoryRecorded();
     }
 
+    @Test
+    public void shouldAllowDecimalPrecision() {
+        mysql = build
+                .with(DatabaseHistory.SKIP_UNPARSEABLE_DDL_STATEMENTS, false)
+                .storeDatabaseHistoryInFile(TEST_FILE_PATH)
+                .serverName(SERVER_NAME)
+                .createSchemas();
+        mysql.start();
+        source.setBinlogStartPoint("binlog-001", 400);
+        mysql.applyDdl(source, "db1", readFile("ddl/mysql-decimal-issue.ddl"), this::printStatements);
+
+        assertTableIncluded("connector_test.business_order");
+        assertTableIncluded("connector_test.business_order_detail");
+        assertHistoryRecorded();
+    }
+
     protected void assertTableIncluded(String fullyQualifiedTableName) {
         TableId tableId = TableId.parse(fullyQualifiedTableName);
         TableSchema tableSchema = mysql.schemaFor(tableId);
