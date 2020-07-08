0c3e3219dd96aa59595c919e6af87fbd5e602c06
Chris Cranford <chris@hibernate.org>
2020-07-08 13:11:00 -0400

    DBZ-2238 Fix test failure - MySqlSourceTypeInSchemaIT#shouldPropagateSourceTypeAsSchemaParameter

--- debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlSourceTypeInSchemaIT.java
+++ debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlSourceTypeInSchemaIT.java
@@ -66,7 +66,7 @@ public class MySqlSourceTypeInSchemaIT extends AbstractConnectorTest {
         // Use the DB configuration to define the connector's configuration ...
         config = DATABASE.defaultConfig()
                 .with(MySqlConnectorConfig.SNAPSHOT_MODE, MySqlConnectorConfig.SnapshotMode.NEVER)
-                .with("column.propagate.source.type", ".*c1,.*c2,.*c3.*,.*f.")
+                .with("column.propagate.source.type", ".*\\.c1,.*\\.c2,.*\\.c3.*,.*\\.f.")
                 .build();
 
         // Start the connector ...
