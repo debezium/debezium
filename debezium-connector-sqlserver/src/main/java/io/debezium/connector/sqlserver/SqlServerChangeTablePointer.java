f91ffddd8b3c25c8b608b52618d43aab4e920eb5
Guy <gpascarella@ironeaglex.com>
2020-08-27 10:17:31 -0400

    DBZ-1068 Fix for Error Handling ITs

--- debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerChangeTablePointer.java
+++ debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerChangeTablePointer.java
@@ -130,7 +130,7 @@ public class SqlServerChangeTablePointer extends ChangeTableResultSet<SqlServerC
         else {
             final IndicesMapping indicesMapping = new IndicesMapping(sourceTableColumns, resultColumns);
             return resultSet -> {
-                final Object[] data = new Object[sourceColumnCount];
+                final Object[] data = new Object[resultColumnCount];
                 for (int i = 0; i < resultColumnCount; i++) {
                     int index = indicesMapping.getSourceTableColumnIndex(i);
                     data[index] = getColumnData(resultSet, columnDataOffset + i);
