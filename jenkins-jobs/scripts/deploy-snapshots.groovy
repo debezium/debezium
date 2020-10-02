26c8aecff701f1c07158726b8b3a6714f4f0426f
Jiri Pechanec <jpechane@redhat.com>
2020-10-02 11:29:56 +0200

    DBZ-2618 Deploy snapshots for Vitess connector

--- jenkins-jobs/scripts/deploy-snapshots.groovy
+++ jenkins-jobs/scripts/deploy-snapshots.groovy
@@ -4,8 +4,7 @@ import java.util.stream.*
 if (
     !DEBEZIUM_REPOSITORY ||
     !DEBEZIUM_BRANCH ||
-    !DEBEZIUM_INCUBATOR_REPOSITORY ||
-    !DEBEZIUM_INCUBATOR_BRANCH
+    !DEBEZIUM_ADDITIONAL_REPOSITORIES
 ) {
     error 'Input parameters not provided'
 }
@@ -13,12 +12,12 @@ if (
 GIT_CREDENTIALS_ID = 'debezium-github'
 
 DEBEZIUM_DIR = 'debezium'
-INCUBATOR_DIR = 'debezium-incubator'
 HOME_DIR = '/home/cloud-user'
 
 ORACLE_ARTIFACT_DIR = "$HOME_DIR/oracle-libs/12.2.0.1.0"
 ORACLE_ARTIFACT_VERSION = '12.2.0.1'
 
+def additionalDirs = []
 node('Slave') {
 
     stage ('Initialize') {
@@ -33,14 +32,22 @@ node('Slave') {
                 userRemoteConfigs: [[url: "https://$DEBEZIUM_REPOSITORY", credentialsId: GIT_CREDENTIALS_ID]]
             ]
         )
-        checkout([$class: 'GitSCM', 
-            branches: [[name: "*/$DEBEZIUM_INCUBATOR_BRANCH"]], 
-            doGenerateSubmoduleConfigurations: false, 
-            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: INCUBATOR_DIR]], 
-                submoduleCfg: [], 
-                userRemoteConfigs: [[url: "https://$DEBEZIUM_INCUBATOR_REPOSITORY", credentialsId: GIT_CREDENTIALS_ID]]
-            ]
-        )
+        DEBEZIUM_ADDITIONAL_REPOSITORIES.split().each {
+            def (id, repository, branch) = it.split('#')
+            checkout([$class: 'GitSCM', 
+                branches: [[name: "*/$branch"]], 
+                doGenerateSubmoduleConfigurations: false, 
+                extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: id]], 
+                    submoduleCfg: [], 
+                    userRemoteConfigs: [[url: "https://$repository", credentialsId: GIT_CREDENTIALS_ID]]
+                ]
+            )
+            additionalDirs << id
+        }
+        dir(ORACLE_ARTIFACT_DIR) {
+            sh "mvn install:install-file -DgroupId=com.oracle.instantclient -DartifactId=ojdbc8 -Dversion=$ORACLE_ARTIFACT_VERSION -Dpackaging=jar -Dfile=ojdbc8.jar"
+            sh "mvn install:install-file -DgroupId=com.oracle.instantclient -DartifactId=xstreams -Dversion=$ORACLE_ARTIFACT_VERSION -Dpackaging=jar -Dfile=xstreams.jar"
+        }
     }
 
     stage ('Build and deploy Debezium') {
@@ -49,13 +56,11 @@ node('Slave') {
         }
     }
 
-    stage ('Build and deploy Debezium Incubator') {
-        dir(ORACLE_ARTIFACT_DIR) {
-            sh "mvn install:install-file -DgroupId=com.oracle.instantclient -DartifactId=ojdbc8 -Dversion=$ORACLE_ARTIFACT_VERSION -Dpackaging=jar -Dfile=ojdbc8.jar"
-            sh "mvn install:install-file -DgroupId=com.oracle.instantclient -DartifactId=xstreams -Dversion=$ORACLE_ARTIFACT_VERSION -Dpackaging=jar -Dfile=xstreams.jar"
-        }
-        dir(INCUBATOR_DIR) {
-            sh "mvn clean deploy -U -s $HOME/.m2/settings-snapshots.xml -DdeployAtEnd=true -DskipITs -DskipTests -Passembly,oracle"
+    additionalDirs.each { id ->
+        stage ("Build and deploy Debezium ${id.capitalize()}") {
+            dir(id) {
+                sh "mvn clean deploy -U -s $HOME/.m2/settings-snapshots.xml -DdeployAtEnd=true -DskipITs -DskipTests -P${id == 'incubator' ? 'assembly,oracle' : 'assembly'}"
+            }
         }
     }
 }
