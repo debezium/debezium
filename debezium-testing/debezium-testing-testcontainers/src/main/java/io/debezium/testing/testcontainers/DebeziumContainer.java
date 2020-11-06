f291b07b6b12a02861bdc0cea1025759beeaa4e7
rkerner <rkerner.mobil@gmail.com>
2020-11-06 12:06:10 +0100

    DBZ-2724 cleanup code and apply PR feedback

--- debezium-testing/debezium-testing-testcontainers/src/main/java/io/debezium/testing/testcontainers/DebeziumContainer.java
+++ debezium-testing/debezium-testing-testcontainers/src/main/java/io/debezium/testing/testcontainers/DebeziumContainer.java
@@ -8,7 +8,6 @@ package io.debezium.testing.testcontainers;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -35,6 +34,7 @@ import okhttp3.ResponseBody;
 public class DebeziumContainer extends GenericContainer<DebeziumContainer> {
 
     private static final int KAFKA_CONNECT_PORT = 8083;
+    private static final String TEST_PROPERTY_PREFIX = "debezium.test.";
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     protected static final ObjectMapper MAPPER = new ObjectMapper();
     protected static final OkHttpClient CLIENT = new OkHttpClient();
@@ -77,6 +77,10 @@ public class DebeziumContainer extends GenericContainer<DebeziumContainer> {
         return self();
     }
 
+    public static int waitTimeForRecords() {
+        return Integer.parseInt(System.getProperty(TEST_PROPERTY_PREFIX + "records.waittime", "2"));
+    }
+
     public String getTarget() {
         return "http://" + getContainerIpAddress() + ":" + getMappedPort(8083);
     }
@@ -109,153 +113,172 @@ public class DebeziumContainer extends GenericContainer<DebeziumContainer> {
         return getConnectorURI(connectorName) + "/status";
     }
 
-    public void registerConnector(String name, ConnectorConfiguration configuration) throws IOException {
-        Connector connector = Connector.from(name, configuration);
+    public void registerConnector(String name, ConnectorConfiguration configuration) {
+        final Connector connector = Connector.from(name, configuration);
 
         registerConnectorToDebezium(connector.toJson(), getConnectorsURI());
 
         // To avoid a 409 error code meanwhile connector is being configured.
         // This is just a guard, probably in most of use cases you won't need that as preparation time of the test might be enough to configure connector.
         Awaitility.await()
-                .atMost(10, TimeUnit.SECONDS)
+                .atMost(waitTimeForRecords() * 5, TimeUnit.SECONDS)
                 .until(() -> isConnectorConfigured(connector.getName()));
     }
 
-    private void registerConnectorToDebezium(final String payload, final String fullUrl) throws IOException {
+    private static void handleFailedResponse(Response response) {
+        String responseBodyContent = "{empty response body}";
+        try (final ResponseBody responseBody = response.body()) {
+            if (null != responseBody) {
+                responseBodyContent = responseBody.string();
+            }
+            throw new IllegalStateException("Unexpected response: " + response + " ; Response Body: " + responseBodyContent);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Error connecting to Debezium container", e);
+        }
+    }
+
+    private void registerConnectorToDebezium(final String payload, final String fullUrl) {
         final RequestBody body = RequestBody.create(payload, JSON);
         final Request request = new Request.Builder().url(fullUrl).post(body).build();
 
-        try (Response response = CLIENT.newCall(request).execute()) {
+        try (final Response response = CLIENT.newCall(request).execute()) {
             if (!response.isSuccessful()) {
-                throw new IOException("Unexpected code " + response + "Message: " + Objects.requireNonNull(response.body()).string());
+                handleFailedResponse(response);
             }
         }
+        catch (IOException e) {
+            throw new RuntimeException("Error connecting to Debezium container", e);
+        }
     }
 
-    protected static Response executeRequest(Request request) throws IOException {
-        return CLIENT.newCall(request).execute();
+    protected static Response executeGETRequest(Request request) {
+        try {
+            return CLIENT.newCall(request).execute();
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Error connecting to Debezium container", e);
+        }
     }
 
-    protected static Response executeRequestSuccessful(Request request) throws IOException {
-        Response response = executeRequest(request);
-        String responseBodyContent = "{empty response body}";
-        if (!response.isSuccessful()) {
-            ResponseBody responseBody = response.body();
-            if (null != responseBody) {
-                responseBodyContent = responseBody.string();
-                responseBody.close();
+    protected static Response executeGETRequestSuccessfully(Request request) {
+        try (final Response response = executeGETRequest(request)) {
+            if (!response.isSuccessful()) {
+                handleFailedResponse(response);
             }
-            throw new IOException("Unexpected response: " + response + " Response Body: " + responseBodyContent);
-        }
-        return response;
-    }
-
-    public boolean connectorIsNotRegistered(String connectorName) throws IOException {
-        try (Response response = executeRequest(new Request.Builder().url(getConnectorURI(connectorName)).get().build())) {
-            boolean connectorIsNotRegistered = response.code() == 404;
-            response.close();
-            return connectorIsNotRegistered;
+            return response;
         }
     }
 
-    protected void deleteDebeziumConnector(String connectorName) throws IOException {
-        executeRequestSuccessful(new Request.Builder().url(getConnectorURI(connectorName)).delete().build()).close();
+    public boolean connectorIsNotRegistered(String connectorName) {
+        final Request request = new Request.Builder().url(getConnectorURI(connectorName)).build();
+        try (final Response response = executeGETRequest(request)) {
+            return response.code() == 404;
+        }
     }
 
-    public void deleteConnector(String connectorName) throws IOException {
+    protected void deleteDebeziumConnector(String connectorName) {
+        final Request request = new Request.Builder().url(getConnectorURI(connectorName)).delete().build();
+        executeGETRequestSuccessfully(request).close();
+    }
+
+    public void deleteConnector(String connectorName) {
         deleteDebeziumConnector(connectorName);
 
         Awaitility.await()
-                .atMost(10, TimeUnit.SECONDS)
+                .atMost(waitTimeForRecords() * 5, TimeUnit.SECONDS)
                 .until(() -> connectorIsNotRegistered(connectorName));
     }
 
-    public List<String> getRegisteredConnectors() throws IOException {
-        Request request = new Request.Builder().url(getConnectorsURI()).get().build();
-        try (ResponseBody responseBody = executeRequestSuccessful(request).body()) {
+    public List<String> getRegisteredConnectors() {
+        final Request request = new Request.Builder().url(getConnectorsURI()).build();
+        try (final ResponseBody responseBody = executeGETRequestSuccessfully(request).body()) {
             if (null != responseBody) {
-                String string = responseBody.string();
-                responseBody.close();
-                return MAPPER.readValue(string, new TypeReference<List<String>>() {
+                return MAPPER.readValue(responseBody.string(), new TypeReference<List<String>>() {
                 });
             }
         }
         catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new IllegalStateException("Error fetching list of registered connectors", e);
         }
         return Collections.emptyList();
     }
 
-    public boolean isConnectorConfigured(String connectorName) throws IOException {
-        Request request = new Request.Builder().url(getConnectorURI(connectorName)).get().build();
-        try (Response response = executeRequest(request)) {
-            boolean isConnectorConfigured = response.isSuccessful();
-            response.close();
-            return isConnectorConfigured;
+    public boolean isConnectorConfigured(String connectorName) {
+        final Request request = new Request.Builder().url(getConnectorURI(connectorName)).build();
+        try (final Response response = executeGETRequest(request)) {
+            return response.isSuccessful();
         }
     }
 
     public void ensureConnectorRegistered(String connectorName) {
         Awaitility.await()
-                .atMost(10, TimeUnit.SECONDS)
+                .atMost(waitTimeForRecords() * 5, TimeUnit.SECONDS)
                 .until(() -> isConnectorConfigured(connectorName));
     }
 
-    public void deleteAllConnectors() throws IOException {
-        List<String> connectorNames = getRegisteredConnectors();
+    public void deleteAllConnectors() {
+        final List<String> connectorNames = getRegisteredConnectors();
 
         for (String connectorName : connectorNames) {
             deleteDebeziumConnector(connectorName);
         }
 
         Awaitility.await()
-                .atMost(10, TimeUnit.SECONDS)
+                .atMost(waitTimeForRecords() * 5, TimeUnit.SECONDS)
                 .until(() -> getRegisteredConnectors().size() == 0);
     }
 
-    public Connector.State getConnectorState(String connectorName) throws IOException {
-        final Request request = new Request.Builder().url(getConnectorStatusURI(connectorName)).get().build();
-        ResponseBody responseBody = executeRequestSuccessful(request).body();
-        if (null != responseBody) {
-            ObjectNode parsedObject = (ObjectNode) MAPPER.readTree(responseBody.string());
-            responseBody.close();
-            return Connector.State.valueOf(parsedObject.get("connector").get("state").asText());
+    public Connector.State getConnectorState(String connectorName) {
+        final Request request = new Request.Builder().url(getConnectorStatusURI(connectorName)).build();
+        try (final ResponseBody responseBody = executeGETRequestSuccessfully(request).body()) {
+            if (null != responseBody) {
+                final ObjectNode parsedObject = (ObjectNode) MAPPER.readTree(responseBody.string());
+                return Connector.State.valueOf(parsedObject.get("connector").get("state").asText());
+            }
+            return null;
         }
-        return null;
+        catch (IOException e) {
+            throw new IllegalStateException("Error fetching connector state for connector: " + connectorName, e);
+        }
     }
 
-    public Connector.State getConnectorTaskState(String connectorName, int taskNumber) throws IOException {
+    public Connector.State getConnectorTaskState(String connectorName, int taskNumber) {
         final Request request = new Request.Builder().url(getConnectorStatusURI(connectorName)).get().build();
-        ResponseBody responseBody = executeRequestSuccessful(request).body();
-        if (null != responseBody) {
-            ObjectNode parsedObject = (ObjectNode) MAPPER.readTree(responseBody.string());
-            responseBody.close();
-            return Connector.State.valueOf(parsedObject.get("tasks").get(taskNumber).get("state").asText());
+        try (final ResponseBody responseBody = executeGETRequestSuccessfully(request).body()) {
+            if (null != responseBody) {
+                final ObjectNode parsedObject = (ObjectNode) MAPPER.readTree(responseBody.string());
+                return Connector.State.valueOf(parsedObject.get("tasks").get(taskNumber).get("state").asText());
+            }
+            return null;
         }
-        return null;
+        catch (IOException e) {
+            throw new IllegalStateException("Error fetching connector task state for connector task: "
+                    + connectorName + "#" + taskNumber, e);
+        }
     }
 
-    public void pauseConnector(String connectorName) throws IOException {
+    public void pauseConnector(String connectorName) {
         final Request request = new Request.Builder()
                 .url(getPauseConnectorURI(connectorName))
                 .put(RequestBody.create("", JSON))
                 .build();
-        executeRequestSuccessful(request).close();
+        executeGETRequestSuccessfully(request).close();
 
         Awaitility.await()
-                .atMost(10, TimeUnit.SECONDS)
+                .atMost(waitTimeForRecords() * 5, TimeUnit.SECONDS)
                 .until(() -> getConnectorState(connectorName) == Connector.State.PAUSED);
     }
 
-    public void ensureConnectorState(String connectorName, Connector.State status) throws IOException {
+    public void ensureConnectorState(String connectorName, Connector.State status) {
         Awaitility.await()
-                .atMost(10, TimeUnit.SECONDS)
+                .atMost(waitTimeForRecords() * 5, TimeUnit.SECONDS)
                 .until(() -> getConnectorState(connectorName) == status);
     }
 
-    public void ensureConnectorTaskState(String connectorName, int taskNumber, Connector.State status) throws IOException {
+    public void ensureConnectorTaskState(String connectorName, int taskNumber, Connector.State status) {
         Awaitility.await()
-                .atMost(10, TimeUnit.SECONDS)
+                .atMost(waitTimeForRecords() * 5, TimeUnit.SECONDS)
                 .until(() -> getConnectorTaskState(connectorName, taskNumber) == status);
     }
 }
