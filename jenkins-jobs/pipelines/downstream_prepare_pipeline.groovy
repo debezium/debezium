8da5bab9e5c14e4e6e0d941854d6b52f4f3187ff
Jiri Pechanec <jpechane@redhat.com>
2020-11-11 06:46:47 +0100

    [release] Provide correct build results in emails

--- jenkins-jobs/pipelines/downstream_prepare_pipeline.groovy
+++ jenkins-jobs/pipelines/downstream_prepare_pipeline.groovy
@@ -80,7 +80,7 @@ pipeline {
     post {
         always {
             mail to: 'jcechace@redhat.com', subject: "Debezium OpenShift test run #${BUILD_NUMBER} finished", body: """
-${currentBuild.projectName} run ${BUILD_URL} finished with result: ${currentBuild.result}
+${currentBuild.projectName} run ${BUILD_URL} finished with result: ${currentBuild.currentResult}
 """
         }
         success {
