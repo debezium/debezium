4f4b63c3f641fb2864101da62f3b7bfa176c0ad2
Gunnar Morling <gunnar.morling@googlemail.com>
2020-12-01 17:47:37 +0100

    DBZ-2001 Moving Db2 connector to dedicated repo at https://github.com/debezium/debezium-connector-db2

--- debezium-connector-db2/src/test/resources/startup-cdc-demo.sql
+++
@@ -1,9 +0,0 @@
-
-VALUES ASNCDC.ASNCDCSERVICES('status','asncdc');
-
-CALL ASNCDC.ADDTABLE('db2inst1', 'products' ); 
-CALL ASNCDC.ADDTABLE('db2inst1', 'products_on_hand' ); 
-CALL ASNCDC.ADDTABLE('db2inst1', 'customers' );
-CALL ASNCDC.ADDTABLE('db2inst1', 'orders' ); 
-
-VALUES ASNCDC.ASNCDCSERVICES('reinit','asncdc');
