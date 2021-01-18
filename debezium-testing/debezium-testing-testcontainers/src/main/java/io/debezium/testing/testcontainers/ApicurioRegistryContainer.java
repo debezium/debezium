8fb835f17ff114c3e8ba63cc908f9b62a6b0667e
ani-sha <anishamohanty23@gmail.com>
2021-01-18 12:13:43 +0530

    DBZ-2945 Fetch correct latest apicurio version for testcontainers

--- debezium-testing/debezium-testing-testcontainers/src/main/java/io/debezium/testing/testcontainers/ApicurioRegistryContainer.java
+++ debezium-testing/debezium-testing-testcontainers/src/main/java/io/debezium/testing/testcontainers/ApicurioRegistryContainer.java
@@ -10,15 +10,22 @@ import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 
 public class ApicurioRegistryContainer extends GenericContainer<ApicurioRegistryContainer> {
 
-    private static final String DEBEZIUM_VERSION = DebeziumContainer.getDebeziumStableVersion();
+    private static final String APICURIO_VERSION = getApicurioVersion();
     private static final Integer APICURIO_PORT = 8080;
+    private static final String TEST_PROPERTY_PREFIX = "debezium.test.";
 
     public ApicurioRegistryContainer() {
-        super("apicurio/apicurio-registry-mem:" + DEBEZIUM_VERSION);
+        super("apicurio/apicurio-registry-mem:" + APICURIO_VERSION);
 
         this.waitStrategy = new LogMessageWaitStrategy()
                 .withRegEx(".*apicurio-registry-app.*started in.*");
 
         addExposedPort(APICURIO_PORT);
+    }
+
+    public static String getApicurioVersion() {
+        String APICURIO_VERSION_TEST_PROPERTY = System.getProperty(TEST_PROPERTY_PREFIX + "apicurio.version");
+        return APICURIO_VERSION_TEST_PROPERTY != null ? APICURIO_VERSION_TEST_PROPERTY
+                : DebeziumContainer.getStableVersion("https://hub.docker.com/v2/repositories/apicurio/apicurio-registry-mem/tags/");
     }
 }
