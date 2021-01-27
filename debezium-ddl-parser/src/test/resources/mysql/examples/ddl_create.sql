735b43f888f00684ebb4b9776ad709a834cbd069
Jiri Pechanec <jpechane@redhat.com>
2021-01-27 09:14:24 +0100

    DBZ-2972 Handle IP only for user to avoid false matches
    If for exmaple label is used without space like `label1:BEGIN` the
    tokenize would create tokens `label1`, `:BE` and `GIN` as `:BE` matches
    `IP_ADDRESS` token. As the tokenization is globally executed it is
    better to skip a separate token and link IPv6/IPv4 directly to the
    username token.

--- debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
+++ debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
@@ -252,3 +252,15 @@ BEGIN
   DECLARE var3 INT unsigned default 2 + var1;
 END -- //-- delimiter ;
 #end
+#begin
+-- Create procedure
+-- delimiter //
+CREATE PROCEDURE doiterate(p1 INT)
+label2:BEGIN
+  label1:LOOP
+    SET p1 = p1 + 1;
+    IF p1 < 10 THEN ITERATE label1; END IF;
+    LEAVE label1;
+  END LOOP label1;
+END -- //-- delimiter ;
+#end
