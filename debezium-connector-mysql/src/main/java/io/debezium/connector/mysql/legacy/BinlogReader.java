1850d01b966e72cd9cf14a8e5ba5b4e1877d9bfc
Gunnar Morling <gunnar.morling@googlemail.com>
2021-04-21 11:18:02 +0200

    DBZ-3233 Propagating socket exceptions

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/legacy/BinlogReader.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/legacy/BinlogReader.java
@@ -252,6 +252,15 @@ public class BinlogReader extends AbstractReader {
                 }
                 // DBZ-217 In case an event couldn't be read we create a pseudo-event for the sake of logging
                 catch (EventDataDeserializationException edde) {
+                    // DBZ-3233 As of Java 15, when reaching EOF in the binlog stream, the polling loop in
+                    // BinaryLogClient#listenForEventPackets() keeps returning values != -1 from peek();
+                    // this causes the loop to never finish
+                    // Propagating the exception (either EOF or socket closed) causes the loop to be aborted
+                    // in this case
+                    if (edde.getCause() instanceof IOException) {
+                        throw edde;
+                    }
+
                     EventHeaderV4 header = new EventHeaderV4();
                     header.setEventType(EventType.INCIDENT);
                     header.setTimestamp(edde.getEventHeader().getTimestamp());
