52cae501596e2d58b10549b014e52f56d155501a
Jiri Pechanec <jpechane@redhat.com>
2021-05-03 14:48:00 +0200

    DBZ-3473 Share read JDBC value between regular and incremental snapshot

--- debezium-core/src/main/java/io/debezium/relational/RelationalSnapshotChangeEventSource.java
+++ debezium-core/src/main/java/io/debezium/relational/RelationalSnapshotChangeEventSource.java
@@ -456,7 +456,7 @@ public abstract class RelationalSnapshotChangeEventSource extends AbstractSnapsh
 
     @Deprecated
     protected Object getColumnValue(ResultSet rs, int columnIndex, Column column) throws SQLException {
-        return rs.getObject(columnIndex);
+        return jdbcConnection.getColumnValue(rs, columnIndex, column, null, schema);
     }
 
     /**
