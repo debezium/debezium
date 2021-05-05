967b945e0f034677c696af02bafecb415409a81a
Chris Cranford <chris@hibernate.org>
2021-05-05 18:06:38 -0400

    DBZ-3322 Bubble up exceptions from RowMapper

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/RowMapper.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/RowMapper.java
@@ -8,13 +8,10 @@ package io.debezium.connector.oracle.logminer;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.Instant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.debezium.DebeziumException;
-import io.debezium.connector.oracle.OracleStreamingChangeEventSourceMetrics;
 import io.debezium.connector.oracle.Scn;
 import io.debezium.relational.TableId;
 import io.debezium.util.HexConverter;
@@ -55,91 +52,46 @@ public class RowMapper {
     // private static final int RS_ID = 12;
     // private static final int SSN = 12;
 
-    public static String getOperation(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getString(OPERATION);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "OPERATION");
-            return null;
-        }
+    public static String getOperation(ResultSet rs) throws SQLException {
+        return rs.getString(OPERATION);
     }
 
-    public static String getUsername(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getString(USERNAME);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "USERNAME");
-            return null;
-        }
+    public static String getUsername(ResultSet rs) throws SQLException {
+        return rs.getString(USERNAME);
     }
 
-    public static int getOperationCode(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getInt(OPERATION_CODE);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "OPERATION_CODE");
-            return 0;
-        }
+    public static int getOperationCode(ResultSet rs) throws SQLException {
+        return rs.getInt(OPERATION_CODE);
     }
 
-    public static String getTableName(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getString(TABLE_NAME);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "TABLE_NAME");
-            return "";
-        }
+    public static String getTableName(ResultSet rs) throws SQLException {
+        return rs.getString(TABLE_NAME);
     }
 
-    public static String getSegOwner(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getString(SEG_OWNER);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "SEG_OWNER");
-            return "";
-        }
+    public static String getSegOwner(ResultSet rs) throws SQLException {
+        return rs.getString(SEG_OWNER);
     }
 
-    public static Timestamp getChangeTime(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getTimestamp(CHANGE_TIME);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "CHANGE_TIME");
-            return new Timestamp(Instant.now().getEpochSecond());
-        }
+    public static Timestamp getChangeTime(ResultSet rs) throws SQLException {
+        return rs.getTimestamp(CHANGE_TIME);
     }
 
-    public static Scn getScn(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return Scn.valueOf(rs.getString(SCN));
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "SCN");
+    public static Scn getScn(ResultSet rs) throws SQLException {
+        final String scn = rs.getString(SCN);
+        if (scn == null) {
             return Scn.NULL;
         }
+        return Scn.valueOf(scn);
     }
 
-    public static String getTransactionId(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return HexConverter.convertToHexString(rs.getBytes(TX_ID));
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "TX_ID");
-            return "";
-        }
+    public static String getTransactionId(ResultSet rs) throws SQLException {
+        return HexConverter.convertToHexString(rs.getBytes(TX_ID));
     }
 
     /**
      * It constructs REDO_SQL. If REDO_SQL  is in a few lines, it truncates after first 40_000 characters
      * It also records LogMiner history info if isDml is true
      *
-     * @param streaingMetrics the streaming metrics
      * @param rs result set
      * @param isDml flag indicating if operation code is a DML
      * @param historyRecorder history recorder
@@ -151,77 +103,51 @@ public class RowMapper {
      * @param txId transaction ID
      * @return the redo SQL
      */
-    public static String getSqlRedo(OracleStreamingChangeEventSourceMetrics streaingMetrics, ResultSet rs, boolean isDml,
-                                    HistoryRecorder historyRecorder, Scn scn, String tableName,
-                                    String segOwner, int operationCode, Timestamp changeTime, String txId) {
+    public static String getSqlRedo(ResultSet rs, boolean isDml, HistoryRecorder historyRecorder, Scn scn, String tableName,
+                                    String segOwner, int operationCode, Timestamp changeTime, String txId)
+            throws SQLException {
         int lobLimitCounter = 9; // todo : decide on approach ( XStream chunk option) and Lob limit
-        StringBuilder result = new StringBuilder(4000);
-        try {
-            String redoSql = rs.getString(SQL_REDO);
-            if (redoSql == null) {
-                return null;
-            }
-            result = new StringBuilder(redoSql);
 
-            int csf = rs.getInt(CSF);
+        String redoSql = rs.getString(SQL_REDO);
+        if (redoSql == null) {
+            return null;
+        }
+
+        StringBuilder result = new StringBuilder(redoSql);
+        int csf = rs.getInt(CSF);
+        if (isDml) {
+            historyRecorder.record(scn, tableName, segOwner, operationCode, changeTime, txId, csf, redoSql);
+        }
+
+        // 0 - indicates SQL_REDO is contained within the same row
+        // 1 - indicates that either SQL_REDO is greater than 4000 bytes in size and is continued in
+        // the next row returned by the ResultSet
+        while (csf == 1) {
+            rs.next();
+            if (lobLimitCounter-- == 0) {
+                LOGGER.warn("LOB value was truncated due to the connector limitation of {} MB", 40);
+                break;
+            }
+            result.append(rs.getString(SQL_REDO));
+            csf = rs.getInt(CSF);
             if (isDml) {
-                historyRecorder.record(scn, tableName, segOwner, operationCode, changeTime, txId, csf, redoSql);
+                historyRecorder.record(scn, tableName, segOwner, operationCode, changeTime, txId, csf, rs.getString(SQL_REDO));
             }
-
-            // 0 - indicates SQL_REDO is contained within the same row
-            // 1 - indicates that either SQL_REDO is greater than 4000 bytes in size and is continued in
-            // the next row returned by the ResultSet
-            while (csf == 1) {
-                rs.next();
-                if (lobLimitCounter-- == 0) {
-                    LOGGER.warn("LOB value was truncated due to the connector limitation of {} MB", 40);
-                    break;
-                }
-                result.append(rs.getString(SQL_REDO));
-                csf = rs.getInt(CSF);
-                if (isDml) {
-                    historyRecorder.record(scn, tableName, segOwner, operationCode, changeTime, txId, csf, rs.getString(SQL_REDO));
-                }
-            }
-
         }
-        catch (SQLException e) {
-            logError(streaingMetrics, e, "SQL_REDO");
-        }
+
         return result.toString();
     }
 
-    public static String getRowId(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getString(ROW_ID);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "ROW_ID");
-            return null;
-        }
+    public static String getRowId(ResultSet rs) throws SQLException {
+        return rs.getString(ROW_ID);
     }
 
-    public static int getRollbackFlag(OracleStreamingChangeEventSourceMetrics streamingMetrics, ResultSet rs) {
-        try {
-            return rs.getInt(ROLLBACK_FLAG);
-        }
-        catch (SQLException e) {
-            logError(streamingMetrics, e, "ROLLBACK");
-            return 0;
-        }
+    public static int getRollbackFlag(ResultSet rs) throws SQLException {
+        return rs.getInt(ROLLBACK_FLAG);
     }
 
-    private static void logError(OracleStreamingChangeEventSourceMetrics streamingMetrics, SQLException e, String s) {
-        LogMinerHelper.logError(streamingMetrics, "Cannot get {}. This entry from LogMiner will be lost due to the {}", s, e);
-    }
-
-    public static TableId getTableId(String catalogName, ResultSet rs) {
-        try {
-            return new TableId(catalogName, rs.getString(SEG_OWNER), rs.getString(TABLE_NAME));
-        }
-        catch (SQLException e) {
-            throw new DebeziumException("Cannot resolve TableId from result set data", e);
-        }
+    public static TableId getTableId(String catalogName, ResultSet rs) throws SQLException {
+        return new TableId(catalogName, rs.getString(SEG_OWNER), rs.getString(TABLE_NAME));
     }
 
 }
