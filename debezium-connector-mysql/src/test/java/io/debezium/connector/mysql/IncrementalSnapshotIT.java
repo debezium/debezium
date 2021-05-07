8db6835807629c20781ec9512ccdaf8ed3fbfe67
Jiri Pechanec <jpechane@redhat.com>
2021-05-07 09:13:56 +0200

    DBZ-3500 Provide common test for incremental snapshot testing

--- debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/IncrementalSnapshotIT.java
+++ debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/IncrementalSnapshotIT.java
@@ -6,37 +6,18 @@
 
 package io.debezium.connector.mysql;
 
-import java.nio.file.Path;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 
-import org.apache.kafka.connect.data.Struct;
-import org.apache.kafka.connect.source.SourceRecord;
-import org.fest.assertions.Assertions;
-import org.fest.assertions.MapAssert;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
 import io.debezium.config.Configuration;
 import io.debezium.connector.mysql.MySqlConnectorConfig.SnapshotMode;
-import io.debezium.embedded.AbstractConnectorTest;
 import io.debezium.jdbc.JdbcConnection;
+import io.debezium.pipeline.source.snapshot.incremental.AbstractIncrementalSnapshotTest;
 import io.debezium.util.Testing;
 
-public class IncrementalSnapshotIT extends AbstractConnectorTest {
-
-    private static final int ROW_COUNT = 1_000;
-    private static final int MAXIMUM_NO_RECORDS_CONSUMES = 2;
-
-    private static final Path DB_HISTORY_PATH = Testing.Files.createTestingPath("file-db-history-is.txt").toAbsolutePath();
+public class IncrementalSnapshotIT extends AbstractIncrementalSnapshotTest<MySqlConnector> {
 
     private static final String SERVER_NAME = "is_test";
     private final UniqueDatabase DATABASE = new UniqueDatabase(SERVER_NAME, "incremental_snapshot_test").withDbHistoryPath(DB_HISTORY_PATH);
@@ -59,281 +40,39 @@ public class IncrementalSnapshotIT extends AbstractConnectorTest {
         }
     }
 
-    private void populateTable() throws SQLException {
-        try (MySqlTestConnection db = MySqlTestConnection.forTestDatabase(DATABASE.getDatabaseName());) {
-            try (JdbcConnection connection = db.connect()) {
-                connection.setAutoCommit(false);
-                for (int i = 0; i < ROW_COUNT; i++) {
-                    connection.executeWithoutCommitting(String.format("INSERT INTO a (aa) VALUES (%s)", i));
-                }
-                connection.commit();
-            }
-        }
-    }
-
-    protected Map<Integer, Integer> consumeMixedWithIncrementalSnapshot(int recordCount) throws InterruptedException {
-        return consumeMixedWithIncrementalSnapshot(recordCount, x -> true, null);
-    }
-
-    protected Map<Integer, Integer> consumeMixedWithIncrementalSnapshot(int recordCount,
-                                                                        Predicate<Map.Entry<Integer, Integer>> dataCompleted, Consumer<List<SourceRecord>> recordConsumer)
-            throws InterruptedException {
-        final String topicName = DATABASE.topicForTable("a");
-        final Map<Integer, Integer> dbChanges = new HashMap<>();
-        int noRecords = 0;
-        for (;;) {
-            final SourceRecords records = consumeRecordsByTopic(1);
-            final List<SourceRecord> dataRecords = records.recordsForTopic(topicName);
-            if (records.allRecordsInOrder().isEmpty()) {
-                noRecords++;
-                Assertions.assertThat(noRecords).describedAs("Too many no data record results")
-                        .isLessThanOrEqualTo(MAXIMUM_NO_RECORDS_CONSUMES);
-                continue;
-            }
-            noRecords = 0;
-            if (dataRecords == null || dataRecords.isEmpty()) {
-                continue;
-            }
-            dataRecords.forEach(record -> {
-                final int id = ((Struct) record.key()).getInt32("pk");
-                final int value = ((Struct) record.value()).getStruct("after").getInt32("aa");
-                dbChanges.put(id, value);
-            });
-            if (recordConsumer != null) {
-                recordConsumer.accept(dataRecords);
-            }
-            if (dbChanges.size() >= recordCount) {
-                if (!dbChanges.entrySet().stream().anyMatch(dataCompleted.negate())) {
-                    break;
-                }
-            }
-        }
-
-        Assertions.assertThat(dbChanges).hasSize(recordCount);
-        return dbChanges;
-    }
-
-    @Test
-    public void snapshotOnly() throws Exception {
-        Testing.Print.enable();
-
-        populateTable();
-        final Configuration config = config();
-        start(MySqlConnector.class, config);
-        assertConnectorIsRunning();
-
-        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
-        // there shouldn't be any snapshot records
-        assertNoRecordsToConsume();
-
-        sendAdHocSnapshotSignal();
-
-        final int expectedRecordCount = ROW_COUNT;
-        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount);
-        for (int i = 0; i < expectedRecordCount; i++) {
-            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i));
-        }
-    }
-
-    protected void sendAdHocSnapshotSignal() throws SQLException {
-        try (MySqlTestConnection db = MySqlTestConnection.forTestDatabase(DATABASE.getDatabaseName());) {
-            try (JdbcConnection connection = db.connect()) {
-                connection.execute(
-                        "INSERT INTO debezium_signal VALUES('ad-hoc', 'execute-snapshot', '{\"data-collections\": [\"" + DATABASE.qualifiedTableName("a") + "\"]}')");
-            }
-        }
-    }
-
-    protected Configuration config() {
-        final Configuration config = DATABASE.defaultConfig()
+    protected Configuration.Builder config() {
+        return DATABASE.defaultConfig()
                 .with(MySqlConnectorConfig.USER, "mysqluser")
                 .with(MySqlConnectorConfig.PASSWORD, "mysqlpw")
                 .with(MySqlConnectorConfig.SNAPSHOT_MODE, SnapshotMode.SCHEMA_ONLY.getValue())
                 .with(MySqlConnectorConfig.INCLUDE_SCHEMA_CHANGES, false)
                 .with(MySqlConnectorConfig.SIGNAL_DATA_COLLECTION, DATABASE.qualifiedTableName("debezium_signal"))
                 .with(MySqlConnectorConfig.INCREMENTAL_SNAPSHOT_CHUNK_SIZE, 10)
-                .with(MySqlConnector.IMPLEMENTATION_PROP, "new")
-                .build();
-        return config;
-    }
-
-    @Test
-    public void inserts() throws Exception {
-        Testing.Print.enable();
-
-        populateTable();
-        final Configuration config = config();
-        start(MySqlConnector.class, config);
-        assertConnectorIsRunning();
-
-        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
-        // there shouldn't be any snapshot records
-        assertNoRecordsToConsume();
-
-        sendAdHocSnapshotSignal();
-
-        try (MySqlTestConnection db = MySqlTestConnection.forTestDatabase(DATABASE.getDatabaseName());) {
-            try (JdbcConnection connection = db.connect()) {
-                connection.setAutoCommit(false);
-                for (int i = 0; i < ROW_COUNT; i++) {
-                    connection.executeWithoutCommitting(String.format("INSERT INTO a (aa) VALUES (%s)", i + ROW_COUNT));
-                }
-                connection.commit();
-            }
-        }
-
-        final int expectedRecordCount = ROW_COUNT * 2;
-        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount);
-        for (int i = 0; i < expectedRecordCount; i++) {
-            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i));
-        }
-    }
-
-    @Test
-    public void updates() throws Exception {
-        Testing.Print.enable();
-
-        populateTable();
-        final Configuration config = config();
-        start(MySqlConnector.class, config);
-        assertConnectorIsRunning();
-
-        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
-        // there shouldn't be any snapshot records
-        assertNoRecordsToConsume();
-
-        sendAdHocSnapshotSignal();
-
-        final int batchSize = 10;
-        try (MySqlTestConnection db = MySqlTestConnection.forTestDatabase(DATABASE.getDatabaseName());) {
-            try (JdbcConnection connection = db.connect()) {
-                connection.setAutoCommit(false);
-                for (int i = 0; i < ROW_COUNT; i++) {
-                    connection.executeWithoutCommitting(String.format("UPDATE a SET aa = aa + 2000 WHERE pk > %s AND pk <= %s",
-                            i * batchSize, (i + 1) * batchSize));
-                    connection.commit();
-                }
-            }
-        }
-        final int expectedRecordCount = ROW_COUNT;
-        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount,
-                x -> x.getValue() >= 2000, null);
-        for (int i = 0; i < expectedRecordCount; i++) {
-            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i + 2000));
-        }
-    }
-
-    @Test
-    public void updatesWithRestart() throws Exception {
-        Testing.Print.enable();
-
-        populateTable();
-        final Configuration config = config();
-        startAndConsumeTillEnd(MySqlConnector.class, config);
-        assertConnectorIsRunning();
-
-        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
-        // there shouldn't be any snapshot records
-        assertNoRecordsToConsume();
-
-        sendAdHocSnapshotSignal();
-
-        final int batchSize = 10;
-        try (MySqlTestConnection db = MySqlTestConnection.forTestDatabase(DATABASE.getDatabaseName());) {
-            try (JdbcConnection connection = db.connect()) {
-                connection.setAutoCommit(false);
-                for (int i = 0; i < ROW_COUNT; i++) {
-                    connection.executeWithoutCommitting(String.format("UPDATE a SET aa = aa + 2000 WHERE pk > %s AND pk <= %s",
-                            i * batchSize, (i + 1) * batchSize));
-                    connection.commit();
-                }
-            }
-        }
-
-        final int expectedRecordCount = ROW_COUNT;
-        final AtomicInteger recordCounter = new AtomicInteger();
-        final AtomicBoolean restarted = new AtomicBoolean();
-        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount,
-                x -> x.getValue() >= 2000, x -> {
-                    if (recordCounter.addAndGet(x.size()) > 50 && !restarted.get()) {
-                        stopConnector();
-                        assertConnectorNotRunning();
-
-                        start(MySqlConnector.class, config);
-                        assertConnectorIsRunning();
-                        restarted.set(true);
-                    }
-                });
-        for (int i = 0; i < expectedRecordCount; i++) {
-            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i + 2000));
-        }
-    }
-
-    @Test
-    public void updatesLargeChunk() throws Exception {
-        Testing.Print.enable();
-
-        populateTable();
-        final Configuration config = config();
-        start(MySqlConnector.class, config);
-        assertConnectorIsRunning();
-
-        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
-        // there shouldn't be any snapshot records
-        assertNoRecordsToConsume();
-
-        sendAdHocSnapshotSignal();
-
-        try (MySqlTestConnection db = MySqlTestConnection.forTestDatabase(DATABASE.getDatabaseName());) {
-            try (JdbcConnection connection = db.connect()) {
-                connection.execute("UPDATE a SET aa = aa + 2000");
-            }
-        }
-
-        final int expectedRecordCount = ROW_COUNT;
-        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount,
-                x -> x.getValue() >= 2000, null);
-        for (int i = 0; i < expectedRecordCount; i++) {
-            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i + 2000));
-        }
-    }
-
-    @Test
-    public void snapshotOnlyWithRestart() throws Exception {
-        Testing.Print.enable();
-
-        populateTable();
-        final Configuration config = config();
-        startAndConsumeTillEnd(MySqlConnector.class, config);
-        assertConnectorIsRunning();
-
-        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
-        // there shouldn't be any snapshot records
-        assertNoRecordsToConsume();
-
-        sendAdHocSnapshotSignal();
-
-        final int expectedRecordCount = ROW_COUNT;
-        final AtomicInteger recordCounter = new AtomicInteger();
-        final AtomicBoolean restarted = new AtomicBoolean();
-        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount, x -> true,
-                x -> {
-                    if (recordCounter.addAndGet(x.size()) > 50 && !restarted.get()) {
-                        stopConnector();
-                        assertConnectorNotRunning();
-
-                        start(MySqlConnector.class, config);
-                        assertConnectorIsRunning();
-                        restarted.set(true);
-                    }
-                });
-        for (int i = 0; i < expectedRecordCount; i++) {
-            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i));
-        }
+                .with(MySqlConnector.IMPLEMENTATION_PROP, "new");
     }
 
     @Override
-    protected int getMaximumEnqueuedRecordCount() {
-        return ROW_COUNT * 3;
+    protected Class<MySqlConnector> connectorClass() {
+        return MySqlConnector.class;
+    }
+
+    @Override
+    protected JdbcConnection databaseConnection() {
+        return MySqlTestConnection.forTestDatabase(DATABASE.getDatabaseName());
+    }
+
+    @Override
+    protected String topicName() {
+        return DATABASE.topicForTable("a");
+    }
+
+    @Override
+    protected String tableName() {
+        return DATABASE.qualifiedTableName("a");
+    }
+
+    @Override
+    protected String signalTableName() {
+        return DATABASE.qualifiedTableName("debezium_signal");
     }
 }
---
+++ debezium-embedded/src/test/java/io/debezium/pipeline/source/snapshot/incremental/AbstractIncrementalSnapshotTest.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.pipeline.source.snapshot.incremental;
+
+import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceConnector;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.fest.assertions.Assertions;
+import org.fest.assertions.MapAssert;
+import org.junit.Test;
+
+import io.debezium.config.CommonConnectorConfig;
+import io.debezium.config.Configuration;
+import io.debezium.embedded.AbstractConnectorTest;
+import io.debezium.jdbc.JdbcConnection;
+import io.debezium.util.Testing;
+
+public abstract class AbstractIncrementalSnapshotTest<T extends SourceConnector> extends AbstractConnectorTest {
+
+    private static final int ROW_COUNT = 1_000;
+    private static final int MAXIMUM_NO_RECORDS_CONSUMES = 2;
+
+    protected static final Path DB_HISTORY_PATH = Testing.Files.createTestingPath("file-db-history-is.txt")
+            .toAbsolutePath();
+
+    protected abstract Class<T> connectorClass();
+
+    protected abstract JdbcConnection databaseConnection();
+
+    protected abstract String topicName();
+
+    protected abstract String tableName();
+
+    protected abstract String signalTableName();
+
+    protected abstract Configuration.Builder config();
+
+    protected void populateTable(JdbcConnection connection) throws SQLException {
+        connection.setAutoCommit(false);
+        for (int i = 0; i < ROW_COUNT; i++) {
+            connection.executeWithoutCommitting(String.format("INSERT INTO %s (aa) VALUES (%s)", tableName(), i));
+        }
+        connection.commit();
+    }
+
+    protected void populateTable() throws SQLException {
+        try (final JdbcConnection connection = databaseConnection()) {
+            populateTable(connection);
+        }
+    }
+
+    protected Map<Integer, Integer> consumeMixedWithIncrementalSnapshot(int recordCount) throws InterruptedException {
+        return consumeMixedWithIncrementalSnapshot(recordCount, x -> true, null);
+    }
+
+    protected Map<Integer, Integer> consumeMixedWithIncrementalSnapshot(int recordCount,
+                                                                        Predicate<Map.Entry<Integer, Integer>> dataCompleted, Consumer<List<SourceRecord>> recordConsumer)
+            throws InterruptedException {
+        final Map<Integer, Integer> dbChanges = new HashMap<>();
+        int noRecords = 0;
+        for (;;) {
+            final SourceRecords records = consumeRecordsByTopic(1);
+            final List<SourceRecord> dataRecords = records.recordsForTopic(topicName());
+            if (records.allRecordsInOrder().isEmpty()) {
+                noRecords++;
+                Assertions.assertThat(noRecords).describedAs("Too many no data record results")
+                        .isLessThanOrEqualTo(MAXIMUM_NO_RECORDS_CONSUMES);
+                continue;
+            }
+            noRecords = 0;
+            if (dataRecords == null || dataRecords.isEmpty()) {
+                continue;
+            }
+            dataRecords.forEach(record -> {
+                final int id = ((Struct) record.key()).getInt32("pk");
+                final int value = ((Struct) record.value()).getStruct("after").getInt32("aa");
+                dbChanges.put(id, value);
+            });
+            if (recordConsumer != null) {
+                recordConsumer.accept(dataRecords);
+            }
+            if (dbChanges.size() >= recordCount) {
+                if (!dbChanges.entrySet().stream().anyMatch(dataCompleted.negate())) {
+                    break;
+                }
+            }
+        }
+
+        Assertions.assertThat(dbChanges).hasSize(recordCount);
+        return dbChanges;
+    }
+
+    protected void sendAdHocSnapshotSignal() throws SQLException {
+        try (final JdbcConnection connection = databaseConnection()) {
+            connection.execute(
+                    String.format(
+                            "INSERT INTO %s VALUES('ad-hoc', 'execute-snapshot', '{\"data-collections\": [\"%s\"]}')",
+                            signalTableName(), tableName()));
+        }
+    }
+
+    protected void startConnector(Function<Configuration.Builder, Configuration.Builder> custConfig) {
+        final Configuration config = custConfig.apply(config()).build();
+        start(connectorClass(), config);
+        waitForConnectorToStart();
+
+        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
+        // there shouldn't be any snapshot records
+        assertNoRecordsToConsume();
+    }
+
+    protected void startConnector() {
+        startConnector(Function.identity());
+    }
+
+    protected void waitForConnectorToStart() {
+        assertConnectorIsRunning();
+    }
+
+    @Test
+    public void snapshotOnly() throws Exception {
+        Testing.Print.enable();
+
+        populateTable();
+        startConnector();
+
+        sendAdHocSnapshotSignal();
+
+        final int expectedRecordCount = ROW_COUNT;
+        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount);
+        for (int i = 0; i < expectedRecordCount; i++) {
+            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i));
+        }
+    }
+
+    @Test
+    public void inserts() throws Exception {
+        Testing.Print.enable();
+
+        populateTable();
+        startConnector();
+
+        sendAdHocSnapshotSignal();
+
+        try (JdbcConnection connection = databaseConnection()) {
+            connection.setAutoCommit(false);
+            for (int i = 0; i < ROW_COUNT; i++) {
+                connection.executeWithoutCommitting(
+                        String.format("INSERT INTO %s (aa) VALUES (%s)", tableName(), i + ROW_COUNT));
+            }
+            connection.commit();
+        }
+
+        final int expectedRecordCount = ROW_COUNT * 2;
+        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount);
+        for (int i = 0; i < expectedRecordCount; i++) {
+            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i));
+        }
+    }
+
+    @Test
+    public void updates() throws Exception {
+        Testing.Print.enable();
+
+        populateTable();
+        startConnector();
+
+        sendAdHocSnapshotSignal();
+
+        final int batchSize = 10;
+        try (JdbcConnection connection = databaseConnection()) {
+            connection.setAutoCommit(false);
+            for (int i = 0; i < ROW_COUNT; i++) {
+                connection.executeWithoutCommitting(
+                        String.format("UPDATE %s SET aa = aa + 2000 WHERE pk > %s AND pk <= %s", tableName(),
+                                i * batchSize, (i + 1) * batchSize));
+                connection.commit();
+            }
+        }
+
+        final int expectedRecordCount = ROW_COUNT;
+        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount,
+                x -> x.getValue() >= 2000, null);
+        for (int i = 0; i < expectedRecordCount; i++) {
+            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i + 2000));
+        }
+    }
+
+    @Test
+    public void updatesWithRestart() throws Exception {
+        Testing.Print.enable();
+
+        populateTable();
+        final Configuration config = config().build();
+        startAndConsumeTillEnd(connectorClass(), config);
+        waitForConnectorToStart();
+
+        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
+        // there shouldn't be any snapshot records
+        assertNoRecordsToConsume();
+
+        sendAdHocSnapshotSignal();
+
+        final int batchSize = 10;
+        try (JdbcConnection connection = databaseConnection()) {
+            connection.setAutoCommit(false);
+            for (int i = 0; i < ROW_COUNT; i++) {
+                connection.executeWithoutCommitting(
+                        String.format("UPDATE %s SET aa = aa + 2000 WHERE pk > %s AND pk <= %s", tableName(),
+                                i * batchSize, (i + 1) * batchSize));
+                connection.commit();
+            }
+        }
+
+        final int expectedRecordCount = ROW_COUNT;
+        final AtomicInteger recordCounter = new AtomicInteger();
+        final AtomicBoolean restarted = new AtomicBoolean();
+        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount,
+                x -> x.getValue() >= 2000, x -> {
+                    if (recordCounter.addAndGet(x.size()) > 50 && !restarted.get()) {
+                        stopConnector();
+                        assertConnectorNotRunning();
+
+                        start(connectorClass(), config);
+                        waitForConnectorToStart();
+                        restarted.set(true);
+                    }
+                });
+        for (int i = 0; i < expectedRecordCount; i++) {
+            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i + 2000));
+        }
+    }
+
+    @Test
+    public void updatesLargeChunk() throws Exception {
+        Testing.Print.enable();
+
+        populateTable();
+        startConnector(x -> x.with(CommonConnectorConfig.INCREMENTAL_SNAPSHOT_CHUNK_SIZE, ROW_COUNT));
+
+        sendAdHocSnapshotSignal();
+
+        try (JdbcConnection connection = databaseConnection()) {
+            connection.execute(String.format("UPDATE %s SET aa = aa + 2000", tableName()));
+        }
+
+        final int expectedRecordCount = ROW_COUNT;
+        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount,
+                x -> x.getValue() >= 2000, null);
+        for (int i = 0; i < expectedRecordCount; i++) {
+            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i + 2000));
+        }
+    }
+
+    @Test
+    public void snapshotOnlyWithRestart() throws Exception {
+        Testing.Print.enable();
+
+        populateTable();
+        final Configuration config = config().build();
+        startAndConsumeTillEnd(connectorClass(), config);
+        waitForConnectorToStart();
+
+        waitForAvailableRecords(100, TimeUnit.MILLISECONDS);
+        // there shouldn't be any snapshot records
+        assertNoRecordsToConsume();
+
+        sendAdHocSnapshotSignal();
+
+        final int expectedRecordCount = ROW_COUNT;
+        final AtomicInteger recordCounter = new AtomicInteger();
+        final AtomicBoolean restarted = new AtomicBoolean();
+        final Map<Integer, Integer> dbChanges = consumeMixedWithIncrementalSnapshot(expectedRecordCount, x -> true,
+                x -> {
+                    if (recordCounter.addAndGet(x.size()) > 50 && !restarted.get()) {
+                        stopConnector();
+                        assertConnectorNotRunning();
+
+                        start(connectorClass(), config);
+                        waitForConnectorToStart();
+                        restarted.set(true);
+                    }
+                });
+        for (int i = 0; i < expectedRecordCount; i++) {
+            Assertions.assertThat(dbChanges).includes(MapAssert.entry(i + 1, i));
+        }
+    }
+
+    @Override
+    protected int getMaximumEnqueuedRecordCount() {
+        return ROW_COUNT * 3;
+    }
+}
