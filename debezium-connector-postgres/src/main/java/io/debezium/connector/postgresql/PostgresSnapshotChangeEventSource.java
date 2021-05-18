a362760429a4774ccdce3452bd656e046041fbfe
Gunnar Morling <gunnar.morling@googlemail.com>
2021-05-18 21:47:16 +0200

    DBZ-2337 Removing superfluous log statement

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresSnapshotChangeEventSource.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresSnapshotChangeEventSource.java
@@ -109,11 +109,6 @@ public class PostgresSnapshotChangeEventSource extends RelationalSnapshotChangeE
             // now that we have the locks, refresh the schema
             schema.refresh(jdbcConnection, false);
         }
-        else {
-            // if we are not in an exported snapshot, this may result in some inconsistencies.
-            // Let the user know
-            LOGGER.info("Skipping locking each table in an exported snapshot");
-        }
     }
 
     @Override
