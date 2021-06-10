a74abe1759ff3cd97cdc69afa76195e0cb6f47a2
jcechace <jcechace@gmail.com>
2021-06-10 15:28:36 +0200

    DBZ-3566 added missing headers and javadocs

--- debezium-testing/debezium-testing-openshift/src/main/java/io/debezium/testing/openshift/tools/Deployer.java
+++ debezium-testing/debezium-testing-openshift/src/main/java/io/debezium/testing/openshift/tools/Deployer.java
@@ -1,14 +1,19 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.testing.openshift.tools;
 
 public interface Deployer<T> {
-
-    interface Builder<D extends Deployer<?>> {
-        D build();
-    }
 
     /**
      * Deploys resource
      * @return Controller for deployed resource
      */
     T deploy() throws InterruptedException;
+
+    interface Builder<D extends Deployer<?>> {
+        D build();
+    }
 }
