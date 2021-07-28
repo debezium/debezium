975f1690f29e223d83495042041cc5edaff8ffd0
Brennan Vincent <brennan@materialize.com>
2021-07-28 10:42:31 -0400

    DBZ-3801 Fix calculation of sequence numbers for Postgres

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/SourceInfo.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/SourceInfo.java
@@ -87,7 +87,7 @@ public final class SourceInfo extends BaseSourceInfo {
     private final String dbName;
 
     private Lsn lsn;
-    private Lsn[] sequence;
+    private Lsn lastCommitLsn;
     private Long txId;
     private Long xmin;
     private Instant timestamp;
@@ -109,16 +109,14 @@ public final class SourceInfo extends BaseSourceInfo {
      * @param txId the ID of the transaction that generated the transaction; may be null if this information is not available
      * @param tableId the table that should be included in the source info; may be null
      * @param xmin the xmin of the slot, may be null
+     * @param isCommit whether this event is a commit.
      * @return this instance
      */
-    protected SourceInfo update(Lsn lsn, Instant commitTime, Long txId, TableId tableId, Long xmin, Lsn lastCommitLsn) {
-        update(lsn, commitTime, txId, tableId, xmin);
-        this.sequence = new Lsn[]{ lastCommitLsn, lsn };
-        return this;
-    }
-
-    protected SourceInfo update(Lsn lsn, Instant commitTime, Long txId, TableId tableId, Long xmin) {
+    protected SourceInfo update(Lsn lsn, Instant commitTime, Long txId, TableId tableId, Long xmin, boolean isCommit) {
         this.lsn = lsn;
+        if (isCommit) {
+            this.lastCommitLsn = lsn;
+        }
         if (commitTime != null) {
             this.timestamp = commitTime;
         }
@@ -154,16 +152,14 @@ public final class SourceInfo extends BaseSourceInfo {
 
     public String sequence() {
         List<String> sequence = new ArrayList<String>();
-        if (this.sequence != null) {
-            for (Lsn lsn : this.sequence) {
-                if (lsn == null) {
-                    sequence.add(null);
-                }
-                else {
-                    sequence.add(Long.toString(lsn.asLong()));
-                }
-            }
-        }
+        String lastCommitLsn = (this.lastCommitLsn != null)
+                ? Long.toString(this.lastCommitLsn.asLong())
+                : null;
+        String lsn = (this.lsn != null)
+                ? Long.toString(this.lsn.asLong())
+                : null;
+        sequence.add(lastCommitLsn);
+        sequence.add(lsn);
         try {
             return MAPPER.writeValueAsString(sequence);
         }
@@ -213,8 +209,8 @@ public final class SourceInfo extends BaseSourceInfo {
         if (xmin != null) {
             sb.append(", xmin=").append(xmin);
         }
-        if (sequence != null) {
-            sb.append(", sequence=").append(sequence);
+        if (lastCommitLsn != null) {
+            sb.append(", lastCommitLsn=").append(lastCommitLsn);
         }
         if (timestamp != null) {
             sb.append(", timestamp=").append(timestamp);
