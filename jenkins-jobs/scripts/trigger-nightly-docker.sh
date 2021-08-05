47e5234c12b6c6083ef7990c385b6723c4cef608
Jiri Pechanec <jpechane@redhat.com>
2021-08-05 16:10:31 +0200

    DBZ-3809 Support images push from Jenkins

--- jenkins-jobs/scripts/trigger-nightly-docker.sh
+++ jenkins-jobs/scripts/trigger-nightly-docker.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" --data '{"docker_tag": "nightly"}' -X POST https://hub.docker.com/api/build/v1/source/97204472-110c-4586-a455-4fb08f3d8b06/trigger/f0629dc1-3568-47f8-b03f-0fb122280506/call/
+DEBEZIUM_REPOSITORY=debezium/debezium
+DEBEZIUM_BRANCH=master
+
+SNAPSHOT_VERSION=$(curl -s https://raw.githubusercontent.com/$DEBEZIUM_REPOSITORY/$DEBEZIUM_BRANCH/pom.xml | grep -o '<version>.*-SNAPSHOT</version>' | awk -F '[<>]' '{print $3}')
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build --build-arg DEBEZIUM_VERSION=$SNAPSHOT_VERSION -t debezium/connect:nightly connect/snapshot
+docker push debezium/connect:nightly
