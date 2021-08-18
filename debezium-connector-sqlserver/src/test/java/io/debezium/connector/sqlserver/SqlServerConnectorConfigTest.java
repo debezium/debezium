fc1d61fd0cfbb01d53284ec791d6c5f05990b6c4
Sergei Morozov <morozov@tut.by>
2021-08-17 17:28:29 -0700

    DBZ-2975: Introduce opt-in configuration for multi-partition mode

---
+++ debezium-connector-sqlserver/src/test/java/io/debezium/connector/sqlserver/SqlServerConnectorConfigTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.sqlserver;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.debezium.config.Configuration;
+import io.debezium.relational.history.KafkaDatabaseHistory;
+
+public class SqlServerConnectorConfigTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqlServerConnectorConfigTest.class);
+
+    @Test
+    public void noDatabaseName() {
+        final SqlServerConnectorConfig connectorConfig = new SqlServerConnectorConfig(
+                defaultConfig().build());
+        assertTrue(connectorConfig.validateAndRecord(SqlServerConnectorConfig.ALL_FIELDS, LOGGER::error));
+    }
+
+    @Test
+    public void onlyDatabaseName() {
+        final SqlServerConnectorConfig connectorConfig = new SqlServerConnectorConfig(
+                defaultConfig()
+                        .with(SqlServerConnectorConfig.DATABASE_NAME, "testDB")
+                        .build());
+        assertTrue(connectorConfig.validateAndRecord(SqlServerConnectorConfig.ALL_FIELDS, LOGGER::error));
+    }
+
+    @Test
+    public void onlyDatabaseNames() {
+        final SqlServerConnectorConfig connectorConfig = new SqlServerConnectorConfig(
+                defaultConfig()
+                        .with(SqlServerConnectorConfig.DATABASE_NAMES, "testDB")
+                        .build());
+        assertTrue(connectorConfig.validateAndRecord(SqlServerConnectorConfig.ALL_FIELDS, LOGGER::error));
+    }
+
+    @Test
+    public void databaseNameAndDatabaseNames() {
+        final SqlServerConnectorConfig connectorConfig = new SqlServerConnectorConfig(
+                defaultConfig()
+                        .with(SqlServerConnectorConfig.DATABASE_NAME, "testDB")
+                        .with(SqlServerConnectorConfig.DATABASE_NAMES, "testDB")
+                        .build());
+        assertFalse(connectorConfig.validateAndRecord(SqlServerConnectorConfig.ALL_FIELDS, LOGGER::error));
+    }
+
+    @Test
+    public void multipleDatabaseNames() {
+        final SqlServerConnectorConfig connectorConfig = new SqlServerConnectorConfig(
+                defaultConfig()
+                        .with(SqlServerConnectorConfig.DATABASE_NAMES, "testDB1,testDB2")
+                        .build());
+        assertFalse(connectorConfig.validateAndRecord(SqlServerConnectorConfig.ALL_FIELDS, LOGGER::error));
+    }
+
+    private Configuration.Builder defaultConfig() {
+        return Configuration.create()
+                .with(SqlServerConnectorConfig.SERVER_NAME, "server")
+                .with(SqlServerConnectorConfig.HOSTNAME, "localhost")
+                .with(SqlServerConnectorConfig.USER, "debezium")
+                .with(KafkaDatabaseHistory.BOOTSTRAP_SERVERS, "localhost:9092")
+                .with(KafkaDatabaseHistory.TOPIC, "history");
+    }
+}
