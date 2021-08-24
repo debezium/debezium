091ffbc7002c9724e2d80523d340a40caaa973fc
Jiri Pechanec <jpechane@redhat.com>
2021-08-24 17:30:06 +0200

    DBZ-3910 Use standard Java package naming
    Co-authored-by: Gunnar Morling <gunnar.morling@googlemail.com>

--- debezium-server/debezium-server-nats-streaming/src/main/java/io/debezium/server/nats/streaming/NatsStreamingChangeConsumer.java
+++ debezium-server/debezium-server-nats-streaming/src/main/java/io/debezium/server/nats/streaming/NatsStreamingChangeConsumer.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.debezium.server.nats_streaming;
+package io.debezium.server.nats.streaming;
 
 import java.util.List;
 
