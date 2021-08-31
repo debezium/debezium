abba3c9a6c453238e9c99f5af13c2687817c024e
Chris Cranford <chris@hibernate.org>
2021-08-31 14:49:42 -0400

    DBZ-3892 Correctly parse concatenated UNISTR values

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleValueConverters.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleValueConverters.java
@@ -29,6 +29,7 @@ import org.apache.kafka.connect.data.SchemaBuilder;
 
 import io.debezium.DebeziumException;
 import io.debezium.config.CommonConnectorConfig.BinaryHandlingMode;
+import io.debezium.connector.oracle.logminer.UnistrHelper;
 import io.debezium.data.SpecialValueDecimal;
 import io.debezium.data.VariableScaleDecimal;
 import io.debezium.jdbc.JdbcValueConverters;
@@ -94,8 +95,6 @@ public class OracleValueConverters extends JdbcValueConverters {
 
     private static final String EMPTY_BLOB_FUNCTION = "EMPTY_BLOB()";
     private static final String EMPTY_CLOB_FUNCTION = "EMPTY_CLOB()";
-    private static final String UNISTR_FUNCTION_START = "UNISTR('";
-    private static final String UNISTR_FUNCTION_END = "')";
     private static final String HEXTORAW_FUNCTION_START = "HEXTORAW('";
     private static final String HEXTORAW_FUNCTION_END = "')";
 
@@ -269,32 +268,12 @@ public class OracleValueConverters extends JdbcValueConverters {
             if (EMPTY_CLOB_FUNCTION.equals(s)) {
                 return column.isOptional() ? null : "";
             }
-            else if (s.startsWith(UNISTR_FUNCTION_START) && s.endsWith(UNISTR_FUNCTION_END)) {
-                return convertOracleUnistr(column, fieldDefn, s.substring(8, s.length() - 2));
+            else if (UnistrHelper.isUnistrFunction(s)) {
+                return UnistrHelper.convert(s);
             }
         }
 
         return super.convertString(column, fieldDefn, data);
-    }
-
-    private String convertOracleUnistr(Column column, Field fieldDefn, String data) {
-        if (data != null && data.length() > 0) {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < data.length(); ++i) {
-                char c = data.charAt(i);
-                if (c == '\\') {
-                    if (data.length() >= (i + 4)) {
-                        // Read next 4 character hex and convert to character.
-                        result.append(Character.toChars(Integer.parseInt(data.substring(i + 1, i + 5), 16)));
-                        i += 4;
-                        continue;
-                    }
-                }
-                result.append(c);
-            }
-            return result.toString();
-        }
-        return data;
     }
 
     @Override
---
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/UnistrHelperTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.oracle.logminer;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+import io.debezium.doc.FixFor;
+
+/**
+ * Unit tests for parsing {@code UNISTR} column values
+ *
+ * @author Chris Cranford
+ */
+public class UnistrHelperTest {
+    @Test
+    @FixFor("DBZ-3892")
+    public void shouldCorrectlyDetectIfUnistrValue() throws Exception {
+        assertThat(UnistrHelper.isUnistrFunction("UNISTR('\\0412\\044B')")).isTrue();
+        assertThat(UnistrHelper.isUnistrFunction("OTHER_FUNCTION('\\0412\\044B')")).isFalse();
+    }
+
+    @Test
+    @FixFor("DBZ-3892")
+    public void shouldConvertUnistrValues() throws Exception {
+        assertThat(UnistrHelper.convert("UNISTR('\\0412\\044B')")).isEqualTo("Вы");
+
+        // Concatenated values, using differing spacing techniques to validate behavior
+        assertThat(UnistrHelper.convert("UNISTR('\\0412\\044B') || UNISTR('\\0431\\0443')")).isEqualTo("Выбу");
+        assertThat(UnistrHelper.convert("UNISTR('\\0412\\044B')||UNISTR('\\0431\\0443')")).isEqualTo("Выбу");
+        assertThat(UnistrHelper.convert("UNISTR('\\0412\\044B') ||UNISTR('\\0431\\0443')")).isEqualTo("Выбу");
+        assertThat(UnistrHelper.convert("UNISTR('\\0412\\044B')|| UNISTR('\\0431\\0443')")).isEqualTo("Выбу");
+        assertThat(UnistrHelper.convert("UNISTR('\\0412\\044B')||  UNISTR('\\0431\\0443')")).isEqualTo("Выбу");
+        assertThat(UnistrHelper.convert("UNISTR('\\0412\\044B')  ||  UNISTR('\\0431\\0443')")).isEqualTo("Выбу");
+    }
+}
