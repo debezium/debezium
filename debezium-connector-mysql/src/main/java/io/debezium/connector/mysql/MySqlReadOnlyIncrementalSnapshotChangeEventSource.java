bc27c25bd16b8eeb3d32389be6965ea609bc1b94
Vivek Wassan <vivek.wassan@gmail.com>
2021-09-21 09:18:06 -0400

    DBZ-3951 Moving preReadChunk() to AbstractIncrementalSnapshotChangeEventSource

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlReadOnlyIncrementalSnapshotChangeEventSource.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlReadOnlyIncrementalSnapshotChangeEventSource.java
@@ -16,7 +16,6 @@ import io.debezium.config.CommonConnectorConfig;
 import io.debezium.jdbc.JdbcConnection;
 import io.debezium.pipeline.EventDispatcher;
 import io.debezium.pipeline.source.snapshot.incremental.AbstractIncrementalSnapshotChangeEventSource;
-import io.debezium.pipeline.source.snapshot.incremental.IncrementalSnapshotContext;
 import io.debezium.pipeline.source.spi.DataChangeEventListener;
 import io.debezium.pipeline.source.spi.SnapshotProgressListener;
 import io.debezium.pipeline.spi.OffsetContext;
@@ -190,17 +189,5 @@ public class MySqlReadOnlyIncrementalSnapshotChangeEventSource<T extends DataCol
 
     private MySqlReadOnlyIncrementalSnapshotContext<T> getContext() {
         return (MySqlReadOnlyIncrementalSnapshotContext<T>) context;
-    }
-
-    @Override
-    protected void preReadChunk(IncrementalSnapshotContext<T> context) {
-        try {
-            if (!jdbcConnection.isValid()) {
-                jdbcConnection.connect();
-            }
-        }
-        catch (SQLException e) {
-            throw new DebeziumException("Database error while checking jdbcConnection in preReadChunk", e);
-        }
     }
 }
