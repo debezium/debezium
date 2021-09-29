f62528a29784616d4ff0d73fdb85fcf4f2357c80
Anisha Mohanty <anishamohanty23@gmail.com>
2021-09-29 11:10:51 +0530

    DBZ-3907 Publish debezium tool images to dockerhub using Jenkins

---
+++ jenkins-jobs/job-dsl/deploy_debezium_tool_images.groovy
@@ -0,0 +1,28 @@
+pipelineJob('release-deploy-debezium-tool-images') {
+    displayName('Debezium Deploy Tool Images')
+    description('Build and deploy debezium tool images to the registry')
+
+    properties {
+        githubProjectUrl('https://github.com/debezium/docker-images')
+    }
+
+    logRotator {
+        daysToKeep(7)
+    }
+
+    triggers {
+        cron('0 0 * * 1')
+    }
+
+    parameters {
+        stringParam('IMAGES_REPOSITORY', 'github.com/debezium/docker-images.git', 'Repository with Debezium Dockerfiles')
+        stringParam('IMAGES_BRANCH', 'master', 'Branch used for images repository')
+        stringParam('TAG', 'latest', 'Tag used for building images')
+    }
+
+    definition {
+        cps {
+            script(readFileFromWorkspace('jenkins-jobs/pipelines/build-debezium-tool-images.groovy'))
+        }
+    }
+}
