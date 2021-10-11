028076bac8efca5c57cd4915e3156a584316d950
Anisha Mohanty <anishamohanty23@gmail.com>
2021-10-11 19:20:23 +0530

    DBZ-3993 Remove unused jdk matrix scripts

--- github-support/jdk-matrix.sh
+++
@@ -1,5 +0,0 @@
-# This script generates a JDK matrix workflow.
-# This workflow is based on the template jdk-matrix.yml and uses jobs.yml as the data-source for data-driven jobs.
-# The output of this script is automatically written to the .github/workflows/jdk-outreach-workflow.yml file.
-# When changes are made to the jobs.yml or jdk-matrix.yml template, this script should be used to regenerate the GitHub Actions workflow prior to being committed.
-ytt -f ./jdk-matrix.yml -f ./jobs.yml > ../.github/workflows/jdk-outreach-workflow.yml
