40c30dd515ba73f289d3162404080205edd0b8b2
fanrw <fanrenwei@cvte.com>
2021-10-14 20:20:16 +0800

    [bugfix]snapshot produce error results or different results from binlog

--- debezium-core/src/main/java/io/debezium/time/Timestamp.java
+++ debezium-core/src/main/java/io/debezium/time/Timestamp.java
@@ -6,9 +6,8 @@
 package io.debezium.time;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 import java.time.temporal.TemporalAdjuster;
-
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 
@@ -74,7 +73,8 @@ public class Timestamp {
             dateTime = dateTime.with(adjuster);
         }
 
-        return dateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
+        //return dateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
+        return dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(); //bugfix:snapshot produce error results or different results from binlog
     }
 
     private Timestamp() {
