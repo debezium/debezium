7799aba3f2ee1e1f316646b4d804d2f3d8e82cfd
Chris Cranford <chris@hibernate.org>
2021-10-29 06:46:49 -0400

    DBZ-4159 Cache LogMinerEvent instances in separate cache

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/infinispan/marshalling/LogMinerEventAdapter.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/infinispan/marshalling/LogMinerEventAdapter.java
@@ -11,47 +11,37 @@ import org.infinispan.protostream.annotations.ProtoAdapter;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
 
-import io.debezium.DebeziumException;
 import io.debezium.connector.oracle.Scn;
-import io.debezium.connector.oracle.logminer.events.DmlEvent;
 import io.debezium.connector.oracle.logminer.events.EventType;
-import io.debezium.connector.oracle.logminer.events.LobEraseEvent;
-import io.debezium.connector.oracle.logminer.events.LobWriteEvent;
 import io.debezium.connector.oracle.logminer.events.LogMinerEvent;
-import io.debezium.connector.oracle.logminer.events.SelectLobLocatorEvent;
-import io.debezium.connector.oracle.logminer.parser.LogMinerDmlEntryImpl;
 import io.debezium.relational.TableId;
-import io.debezium.util.Strings;
 
 /**
- * An Infinispan ProtoStream adapter to marshall {@link LogMinerEvent} instances and its subclasses.
+ * An Infinispan ProtoStream adapter to marshall {@link LogMinerEvent} instances.
  *
- * This class defines a factory for creating {@link LogMinerEvent} instances and its subclasses for when
- * hydrating records from the persisted datastore as well as field handlers to extract instance values
+ * This class defines a factory for creating {@link LogMinerEvent} instances when hydrating
+ * records from the persisted event datastore as well as field handlers to extract values
  * to be marshalled to the protocol buffer stream.
  *
  * The underlying protocol buffer record consists of the following structure:
  * <pre>
  *     message LogMinerEvent {
- *          required int32 eventType = 1;
- *          required string scn = 2;
- *          required string tableId = 3;
- *          required string rowId = 4;
- *          required object rsId = 5;
- *          required string changeTime = 6;
- *          LogMinerDmlEntryImpl entry = 7;
- *          string columnName = 8;
- *          boolean binary = 9;
- *          string data = 10;
+ *         required int32 eventType = 1;
+ *         required string scn = 2;
+ *         required string tableId = 3;
+ *         required string rowId = 4;
+ *         required object rsId = 5;
+ *         required string changeTime = 6;
  *     }
  * </pre>
+ *
  * @author Chris Cranford
  */
 @ProtoAdapter(LogMinerEvent.class)
 public class LogMinerEventAdapter {
 
     /**
-     * A ProtoStream factory that creates {@link LogMinerEvent} instances or one of its subclasses from field values.
+     * A ProtoStream factory that creates {@link LogMinerEvent} instances.
      *
      * @param eventType the event type
      * @param scn the system change number, must not be {@code null}
@@ -59,50 +49,11 @@ public class LogMinerEventAdapter {
      * @param rowId the Oracle row-id the change is associated with
      * @param rsId the Oracle rollback segment identifier
      * @param changeTime the time the change occurred
-     * @param entry the parsed SQL statement entry
-     * @param columnName the column name for a {@code SEL_LOB_LOCATOR} event type
-     * @param binary whether the data is binary for a {@code SEL_LOB_LOCATOR} event type
-     * @param data the data to be written by a {@code LOB_WRITE} event type
-     * @return the constructed LogMinerEvent or one of its subclasses
+     * @return the constructed DmlEvent
      */
     @ProtoFactory
-    public LogMinerEvent factory(int eventType, String scn, String tableId, String rowId, String rsId, String changeTime,
-                                 LogMinerDmlEntryImpl entry, String columnName, Boolean binary, String data) {
-        final EventType type = EventType.from(eventType);
-        final Scn eventScn = Scn.valueOf(scn);
-
-        final TableId id;
-        if (Strings.isNullOrEmpty(tableId)) {
-            id = null;
-        }
-        else {
-            id = TableId.parse(tableId);
-        }
-
-        final Instant time;
-        if (Strings.isNullOrEmpty(changeTime)) {
-            time = null;
-        }
-        else {
-            time = Instant.parse(changeTime);
-        }
-
-        switch (type) {
-            case INSERT:
-            case UPDATE:
-            case DELETE:
-                return new DmlEvent(type, eventScn, id, rowId, rsId, time, entry);
-            case SELECT_LOB_LOCATOR:
-                return new SelectLobLocatorEvent(type, eventScn, id, rowId, rsId, time, entry, columnName, binary);
-            case LOB_WRITE:
-                return new LobWriteEvent(type, eventScn, id, rowId, rsId, time, data);
-            case LOB_ERASE:
-                return new LobEraseEvent(type, eventScn, id, rowId, rsId, time);
-            case LOB_TRIM:
-                return new LogMinerEvent(type, eventScn, id, rowId, rsId, time);
-            default:
-                throw new DebeziumException("Unknown event type: " + eventType);
-        }
+    public LogMinerEvent factory(int eventType, String scn, String tableId, String rowId, String rsId, String changeTime) {
+        return new LogMinerEvent(EventType.from(eventType), Scn.valueOf(scn), TableId.parse(tableId), rowId, rsId, Instant.parse(changeTime));
     }
 
     /**
@@ -138,8 +89,6 @@ public class LogMinerEventAdapter {
      */
     @ProtoField(number = 3, required = true)
     public String getTableId(LogMinerEvent event) {
-        // We intentionally serialize the TableId as a string since string values are natively supported
-        // by protocol buffers and we don't need to write a special marshaller for the TableId class.
         return event.getTableId().identifier();
     }
 
@@ -173,69 +122,6 @@ public class LogMinerEventAdapter {
      */
     @ProtoField(number = 6, required = true)
     public String getChangeTime(LogMinerEvent event) {
-        // Serialized using Instant's normal toString() format since strings as handled natively by
-        // protocol buffers and will be parsed using Instant#parse in the factory method.
         return event.getChangeTime().toString();
-    }
-
-    /**
-     * A ProtoStream handler to extract the {@code entry} field from the {@link DmlEvent} and subclass types.
-     *
-     * @param event the event instance, must not be {@code null}
-     * @return the LogMinerDmlEntryImpl instance or {@code null} if the event type isn't related to DML events
-     */
-    @ProtoField(number = 7)
-    public LogMinerDmlEntryImpl getEntry(LogMinerEvent event) {
-        switch (event.getEventType()) {
-            case INSERT:
-            case UPDATE:
-            case DELETE:
-            case SELECT_LOB_LOCATOR:
-                return (LogMinerDmlEntryImpl) ((DmlEvent) event).getDmlEntry();
-            default:
-                return null;
-        }
-    }
-
-    /**
-     * A ProtoStream handler to extract the {@code columnName} field from a {@link SelectLobLocatorEvent} type.
-     *
-     * @param event the event instance, must not be {@code null}
-     * @return the column name or {@code null} if the event is not a SelectLobLocatorEvent type
-     */
-    @ProtoField(number = 8)
-    public String getColumnName(LogMinerEvent event) {
-        if (EventType.SELECT_LOB_LOCATOR.equals(event.getEventType())) {
-            return ((SelectLobLocatorEvent) event).getColumnName();
-        }
-        return null;
-    }
-
-    /**
-     * A ProtoStream handler to extract the {@code binary} field from a {@link SelectLobLocatorEvent} type.
-     *
-     * @param event the event instance, must not be {@code null}
-     * @return the binary data flag or {@code null} if the event is not a SelectLobLocatorEvent type
-     */
-    @ProtoField(number = 9)
-    public Boolean getBinary(LogMinerEvent event) {
-        if (EventType.SELECT_LOB_LOCATOR.equals(event.getEventType())) {
-            return ((SelectLobLocatorEvent) event).isBinary();
-        }
-        return null;
-    }
-
-    /**
-     * A ProtoStream handler to extract the {@code data} field from a {@link LobWriteEvent} type.
-     *
-     * @param event the event instance, must not be {@code null}
-     * @return the data to be written for a LOB field or {@code null} if the event is not a LobWriteEvent type
-     */
-    @ProtoField(number = 10)
-    public String getData(LogMinerEvent event) {
-        if (EventType.LOB_WRITE.equals(event.getEventType())) {
-            return ((LobWriteEvent) event).getData();
-        }
-        return null;
     }
 }
--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/infinispan/marshalling/TransactionAdapter.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/infinispan/marshalling/TransactionAdapter.java
@@ -6,63 +6,60 @@
 package io.debezium.connector.oracle.logminer.processor.infinispan.marshalling;
 
 import java.time.Instant;
-import java.util.List;
 
 import org.infinispan.protostream.annotations.ProtoAdapter;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
 
 import io.debezium.connector.oracle.Scn;
-import io.debezium.connector.oracle.logminer.events.LogMinerEvent;
-import io.debezium.connector.oracle.logminer.events.Transaction;
+import io.debezium.connector.oracle.logminer.processor.infinispan.InfinispanTransaction;
 
 /**
- * An Infinispan ProtoStream adapter for marshalling a {@link Transaction} instance.
+ * An Infinispan ProtoStream adapter for marshalling a {@link InfinispanTransaction} instance.
  *
- * This class defines a factory for creating {@link Transaction} instances when hydrating a transaction
+ * This class defines a factory for creating {@link InfinispanTransaction} instances when hydrating a transaction
  * record from the protocol buffer datastore as well as field handlers to extract values from a given
  * transaction instance for serializing the instance to a protocol buffer stream.
  *
  * @author Chris Cranford
  */
-@ProtoAdapter(Transaction.class)
+@ProtoAdapter(InfinispanTransaction.class)
 public class TransactionAdapter {
 
     /**
-     * A ProtoStream factory that creates a {@link Transaction} instance from field values.
+     * A ProtoStream factory that creates a {@link InfinispanTransaction} instance from field values.
      *
      * @param transactionId the transaction identifier
      * @param scn the starting system change number of the transaction
      * @param changeTime the starting time of the transaction
-     * @param events list of events that are part of the transaction
      * @param userName the user name
      * @param numberOfEvents the number of events in the transaction
      * @return the constructed Transaction instance
      */
     @ProtoFactory
-    public Transaction factory(String transactionId, String scn, String changeTime, List<LogMinerEvent> events, String userName, int numberOfEvents) {
-        return new Transaction(transactionId, Scn.valueOf(scn), Instant.parse(changeTime), events, userName, numberOfEvents);
+    public InfinispanTransaction factory(String transactionId, String scn, String changeTime, String userName, int numberOfEvents) {
+        return new InfinispanTransaction(transactionId, Scn.valueOf(scn), Instant.parse(changeTime), userName, numberOfEvents);
     }
 
     /**
-     * A ProtoStream handler to extract the {@code transactionId} field from the {@link Transaction}.
+     * A ProtoStream handler to extract the {@code transactionId} field from the {@link InfinispanTransaction}.
      *
      * @param transaction the transaction instance, must not be {@code null}
      * @return the transaction identifier, never {@code null}
      */
     @ProtoField(number = 1, required = true)
-    public String getTransactionId(Transaction transaction) {
+    public String getTransactionId(InfinispanTransaction transaction) {
         return transaction.getTransactionId();
     }
 
     /**
-     * A ProtoStream handler to extract the {@code startScn} field from the {@link Transaction}.
+     * A ProtoStream handler to extract the {@code startScn} field from the {@link InfinispanTransaction}.
      *
      * @param transaction the transaction instance, must not be {@code null}
      * @return the starting system change number, never {@code null}
      */
     @ProtoField(number = 2, required = true)
-    public String getScn(Transaction transaction) {
+    public String getScn(InfinispanTransaction transaction) {
         // We intentionally serialize the Scn class as a string to the protocol buffer datastore
         // and so the factory method also accepts a string parameter and converts the value to a
         // Scn instance during instantiation. This avoids the need for an additional adapter.
@@ -70,40 +67,29 @@ public class TransactionAdapter {
     }
 
     /**
-     * A ProtoStream handler to extract the {@code changeTime} field from the {@link Transaction}.
+     * A ProtoStream handler to extract the {@code changeTime} field from the {@link InfinispanTransaction}.
      *
      * @param transaction the transaction instance, must not be {@code null}
      * @return the starting time of the transaction, never {@code null}
      */
     @ProtoField(number = 3, required = true)
-    public String getChangeTime(Transaction transaction) {
+    public String getChangeTime(InfinispanTransaction transaction) {
         return transaction.getChangeTime().toString();
     }
 
-    /**
-     * A ProtoStream handler to extract the {@code events} field from the {@link Transaction}.
-     *
-     * @param transaction the transaction instance, must not be {@code null}
-     * @return list of events within the transaction
-     */
     @ProtoField(number = 4)
-    public List<LogMinerEvent> getEvents(Transaction transaction) {
-        return transaction.getEvents();
-    }
-
-    @ProtoField(number = 5)
-    public String getUserName(Transaction transaction) {
+    public String getUserName(InfinispanTransaction transaction) {
         return transaction.getUserName();
     }
 
     /**
-     * A ProtoStream handler to extract the {@code eventIds} field from the {@link Transaction}.
+     * A ProtoStream handler to extract the {@code eventIds} field from the {@link InfinispanTransaction}.
      *
      * @param transaction the transaction instance, must not be {@code null}
      * @return the number of events in the transaction
      */
-    @ProtoField(number = 6, defaultValue = "0")
-    public int getNumberOfEvents(Transaction transaction) {
+    @ProtoField(number = 5, defaultValue = "0")
+    public int getNumberOfEvents(InfinispanTransaction transaction) {
         return transaction.getNumberOfEvents();
     }
 }
