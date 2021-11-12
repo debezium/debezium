93e2a944c1a8dc872deb483d093858a9c64183a5
Anisha Mohanty <anishamohanty23@gmail.com>
2021-11-12 11:44:51 +0530

    DBZ-4225 Handle null lastOffset values

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlStreamingChangeEventSource.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlStreamingChangeEventSource.java
@@ -354,6 +354,9 @@ public class MySqlStreamingChangeEventSource implements StreamingChangeEventSour
             // Capture that we've completed another event ...
             offsetContext.completeEvent();
 
+            // Set the value of lastOffset
+            lastOffset = offsetContext.getOffset();
+
             if (skipEvent) {
                 // We're in the mode of skipping events and we just skipped this one, so decrement our skip count ...
                 --initialEventsToSkip;
