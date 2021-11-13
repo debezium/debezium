86a4632d7d61d8a3923ae9311844aa8c5904a374
camilesing <camilesing@gmail.com>
2021-11-13 11:40:03 +0800

    DBZ-4275 fix AbstractDatabaseHistory typo

--- debezium-core/src/main/java/io/debezium/relational/history/AbstractDatabaseHistory.java
+++ debezium-core/src/main/java/io/debezium/relational/history/AbstractDatabaseHistory.java
@@ -43,7 +43,7 @@ public abstract class AbstractDatabaseHistory implements DatabaseHistory {
             .withDefault(false)
             .withWidth(Width.SHORT)
             .withImportance(Importance.LOW)
-            .withDescription("Prefer DDL for schema reovery in case logica schema is present")
+            .withDescription("Prefer DDL for schema recovery in case logical schema is present")
             .withInvisibleRecommender()
             .withNoValidation();
 
