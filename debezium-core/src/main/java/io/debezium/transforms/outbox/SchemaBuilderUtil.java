af20ca96918c2d1410f31e04a04b557212eafd7e
Sungho Hwang <sgc109109@gmail.com>
2021-11-15 22:18:47 +0900

    DBZ-3528 turned access modifier of jsonNodeToSchemaBuilder() back to private

--- debezium-core/src/main/java/io/debezium/transforms/outbox/SchemaBuilderUtil.java
+++ debezium-core/src/main/java/io/debezium/transforms/outbox/SchemaBuilderUtil.java
@@ -33,7 +33,7 @@ public class SchemaBuilderUtil {
         return jsonNodeToSchemaBuilder(document).build();
     }
 
-    public static SchemaBuilder jsonNodeToSchemaBuilder(JsonNode document) {
+    private static SchemaBuilder jsonNodeToSchemaBuilder(JsonNode document) {
         final SchemaBuilder schemaBuilder = SchemaBuilder.struct().optional();
         if (document != null) {
             Iterator<Entry<String, JsonNode>> fieldsEntries = document.fields();
