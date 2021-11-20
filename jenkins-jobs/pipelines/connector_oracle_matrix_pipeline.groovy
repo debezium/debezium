a820220c781b7a0303efcfb0c52868bd13751025
jcechace <jcechace@gmail.com>
2021-11-20 21:40:55 +0100

    DBZ-4316 ignore SSL verification when performing tests over downstream bits

--- jenkins-jobs/pipelines/connector_oracle_matrix_pipeline.groovy
+++ jenkins-jobs/pipelines/connector_oracle_matrix_pipeline.groovy
@@ -26,6 +26,7 @@ pipeline {
             steps {
                 script {
                     env.MVN_PROFILE = '-Ppnc'
+                    env.MAVEN_OPTS = '-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true'
                 }
 
                 sh '''
