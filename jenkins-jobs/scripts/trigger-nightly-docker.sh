268837a5c197933e2978cf81f90ebce702bb1b9a
Jiri Pechanec <jpechane@redhat.com>
2021-11-25 18:15:18 +0100

    DBZ-4006 Log in to correct repository

--- jenkins-jobs/scripts/trigger-nightly-docker.sh
+++ jenkins-jobs/scripts/trigger-nightly-docker.sh
@@ -6,7 +6,7 @@ DEBEZIUM_BRANCH=main
 SNAPSHOT_VERSION=$(curl -s https://raw.githubusercontent.com/$DEBEZIUM_REPOSITORY/$DEBEZIUM_BRANCH/pom.xml | grep -o '<version>.*-SNAPSHOT</version>' | awk -F '[<>]' '{print $3}')
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker login -u ${QUAYIO_CREDENTIALS%:*} -p ${QUAYIO_CREDENTIALS#*:}
+docker login -u ${QUAYIO_CREDENTIALS%:*} -p ${QUAYIO_CREDENTIALS#*:} quay.io
 docker build --build-arg DEBEZIUM_VERSION=$SNAPSHOT_VERSION -t debezium/connect:nightly connect/snapshot
 docker push debezium/connect:nightly
 docker tag debezium/connect:nightly quay.io/debezium/connect:nightly
