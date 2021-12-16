23c7ef9a1c4af507bc362e67abb495332c3a2f56
Jiri Pechanec <jpechane@redhat.com>
2021-12-16 09:47:08 +0100

    DBZ-4373 Run CI during weekends

--- jenkins-jobs/job-dsl/connector_mongodb_matrix_test.groovy
+++ jenkins-jobs/job-dsl/connector_mongodb_matrix_test.groovy
@@ -29,7 +29,7 @@ matrixJob('connector-debezium-mongodb-matrix-test') {
         booleanParam('PRODUCT_BUILD', false, 'Is this a productised build?')
     }
     triggers {
-        cron('H 04 * * 1-5')
+        cron('H 04 * * *')
     }
 
     wrappers {
