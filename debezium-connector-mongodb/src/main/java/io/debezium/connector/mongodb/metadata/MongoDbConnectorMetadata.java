1de9091c7c2fdc675e2f7bf8a8e94ce7bfc70765
Anisha Mohanty <anishamohanty23@gmail.com>
2022-01-04 13:17:16 +0530

    DBZ-4460 Apply the plug-in to all connector modules

--- debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/metadata/MongoDbConnectorMetadata.java
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/metadata/MongoDbConnectorMetadata.java
@@ -3,9 +3,12 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.debezium.connector.mongodb;
+package io.debezium.connector.mongodb.metadata;
 
 import io.debezium.config.Field;
+import io.debezium.connector.mongodb.Module;
+import io.debezium.connector.mongodb.MongoDbConnector;
+import io.debezium.connector.mongodb.MongoDbConnectorConfig;
 import io.debezium.metadata.ConnectorDescriptor;
 import io.debezium.metadata.ConnectorMetadata;
 
---
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/metadata/MongoDbConnectorMetadataProvider.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.debezium.connector.mongodb.metadata;
+
+import io.debezium.metadata.ConnectorMetadata;
+import io.debezium.metadata.ConnectorMetadataProvider;
+
+public class MongoDbConnectorMetadataProvider implements ConnectorMetadataProvider {
+
+    @Override
+    public ConnectorMetadata getConnectorMetadata() {
+        return new MongoDbConnectorMetadata();
+    }
+}
