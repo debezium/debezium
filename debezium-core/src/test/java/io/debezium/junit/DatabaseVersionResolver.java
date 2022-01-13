610e8eba736a31486bbfbf053481e50fcf6c8331
Chris Cranford <chris@hibernate.org>
2022-01-13 12:26:36 -0500

    DBZ-4548 Fix DatabaseVersionResolver comparison logic

--- debezium-core/src/test/java/io/debezium/junit/DatabaseVersionResolver.java
+++ debezium-core/src/test/java/io/debezium/junit/DatabaseVersionResolver.java
@@ -45,13 +45,13 @@ public interface DatabaseVersionResolver {
                 return true;
             }
             else if (dbVersionMajor == major) {
-                if (minor == -1 || dbVersionMinor < minor) {
+                if (minor != -1 && dbVersionMinor < minor) {
+                    // i.e. db 11.2 and annotation is 11.3
                     return true;
                 }
                 else if (dbVersionMinor == minor) {
-                    if (patch == -1 || dbVersionPatch < patch) {
-                        return true;
-                    }
+                    // i.e. db 11.2.1 and annotation is 11.2.2
+                    return patch != -1 && dbVersionPatch < patch;
                 }
             }
             return false;
@@ -81,14 +81,25 @@ public interface DatabaseVersionResolver {
                 return true;
             }
             else if (dbVersionMajor == major) {
-                if (minor == -1 || dbVersionMinor > minor) {
-                    return true;
-                }
-                else if (dbVersionMinor == minor) {
-                    if (patch == -1 || dbVersionPatch > patch) {
-                        return true;
+                if (minor == -1) {
+                    if (dbVersionMinor == 0) {
+                        if (patch == -1) {
+                            if (dbVersionPatch == 0) {
+                                // effectively i.e. 11.0.0 compared to 11, which isn't greater than.
+                                // if this is needed, GreaterThanOrEqualTo should be used.
+                                return false;
+                            }
+                            // i.e. 11.0.1 compared to 11
+                            return dbVersionPatch > 0;
+                        }
+                        // i.e. 11.0.2 compared to 11.0.1
+                        return dbVersionPatch > patch;
                     }
+                    // i.e. 11.1 compared to 11
+                    return dbVersionMinor > 0;
                 }
+                // i.e. 11.2 compared to 11.1
+                return dbVersionMinor > minor;
             }
             return false;
         }
