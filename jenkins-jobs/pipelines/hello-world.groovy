00cbb68907b1698d04a1b88f10f35d2ede2b3411
Martin Medek <mmedek@redhat.com>
2022-01-18 12:22:38 +0100

    fixed sytax in hello-world 2

--- jenkins-jobs/pipelines/hello-world.groovy
+++ jenkins-jobs/pipelines/hello-world.groovy
@@ -5,14 +5,16 @@ pipeline {
 
     stages {
         stage('Hello') {
-            echo 'Hello World'
-            sh 'echo Hello World'
+            steps {
+                echo 'Hello World'
+                sh 'echo Hello World'
+            }
         }
     }
 
     post {
         always {
-            mail to: 'mmedek@redhat.com', subject: "Jenkins Hello World #${BUILD_NUMBER} finished", body: ""
+            mail to: 'mmedek@redhat.com', subject: "Jenkins Hello World #${BUILD_NUMBER} finished", body: ''
         }
     }
 }
