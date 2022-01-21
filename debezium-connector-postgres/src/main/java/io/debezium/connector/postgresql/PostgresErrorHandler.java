9b35294fe8807fd84e4a0a305cbd922a12d87eb9
Gunnar Morling <gunnar.morling@googlemail.com>
2022-01-21 13:39:09 +0100

    DBZ-4520 Making collection immutable

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresErrorHandler.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresErrorHandler.java
@@ -5,11 +5,13 @@
  */
 package io.debezium.connector.postgresql;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.postgresql.util.PSQLException;
 
+import io.debezium.annotation.Immutable;
 import io.debezium.connector.base.ChangeEventQueue;
 import io.debezium.pipeline.ErrorHandler;
 
@@ -20,13 +22,17 @@ import io.debezium.pipeline.ErrorHandler;
  */
 public class PostgresErrorHandler extends ErrorHandler {
 
-    private static final List<String> retryPostgresMessageContainsTexts = new ArrayList<>();
+    @Immutable
+    private static final Set<String> RETRIABLE_EXCEPTION_MESSSAGES;
 
     static {
-        retryPostgresMessageContainsTexts.add("Database connection failed when writing to copy");
-        retryPostgresMessageContainsTexts.add("Database connection failed when reading from copy");
-        retryPostgresMessageContainsTexts.add("FATAL: terminating connection due to administrator command");
-        retryPostgresMessageContainsTexts.add("An I/O error occurred while sending to the backend");
+        Set<String> tmp = new HashSet<>();
+        tmp.add("Database connection failed when writing to copy");
+        tmp.add("Database connection failed when reading from copy");
+        tmp.add("FATAL: terminating connection due to administrator command");
+        tmp.add("An I/O error occurred while sending to the backend");
+
+        RETRIABLE_EXCEPTION_MESSSAGES = Collections.unmodifiableSet(tmp);
     }
 
     public PostgresErrorHandler(PostgresConnectorConfig connectorConfig, ChangeEventQueue<?> queue) {
@@ -36,7 +42,7 @@ public class PostgresErrorHandler extends ErrorHandler {
     @Override
     protected boolean isRetriable(Throwable throwable) {
         if (throwable instanceof PSQLException && throwable.getMessage() != null) {
-            for (String messageText : retryPostgresMessageContainsTexts) {
+            for (String messageText : RETRIABLE_EXCEPTION_MESSSAGES) {
                 if (throwable.getMessage().contains(messageText)) {
                     return true;
                 }
