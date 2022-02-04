dfebe377848bcb7b45f38fc1d7bd1a9fd5270526
Martin Medek <mmedek@redhat.com>
2022-02-04 13:10:23 +0100

    DBZ-4558 - Fixed the issues mentioned in review - fixed the trigger job name, added mail_to param to remaining connector jobs, removed the throw block and refactored the labeling script

--- jenkins-jobs/job-dsl/connector_db2_matrix_test.groovy
+++ jenkins-jobs/job-dsl/connector_db2_matrix_test.groovy
@@ -15,6 +15,7 @@ matrixJob('connector-debezium-db2-matrix-test') {
     }
 
     parameters {
+        stringParam('MAIL_TO', 'debezium-qe@redhat.com')
         stringParam('REPOSITORY', 'https://github.com/debezium/debezium-connector-db2', 'Repository from which connector is built')
         stringParam('BRANCH', 'main', 'A branch/tag from which the connector is built')
         stringParam('SOURCE_URL', "", "URL to productised sources")
@@ -36,7 +37,7 @@ matrixJob('connector-debezium-db2-matrix-test') {
     publishers {
         archiveJunit('**/target/surefire-reports/*.xml')
         archiveJunit('**/target/failsafe-reports/*.xml')
-        mailer('jpechane@redhat.com', false, true)
+        mailer('$MAIL_TO', false, true)
     }
 
     logRotator {
