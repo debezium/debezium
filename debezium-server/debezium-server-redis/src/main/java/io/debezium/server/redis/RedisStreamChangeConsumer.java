5d7c660ae51cfd0747ece8416a3ff42a90807f60
Yossi Shirizli <yshirizli@gmail.com>
2022-02-08 18:05:03 +0200

    DBZ-4637 Streaming change records in batches to Redis

--- debezium-server/debezium-server-redis/src/main/java/io/debezium/server/redis/RedisStreamChangeConsumer.java
+++ debezium-server/debezium-server-redis/src/main/java/io/debezium/server/redis/RedisStreamChangeConsumer.java
@@ -8,12 +8,12 @@ package io.debezium.server.redis;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.Dependent;
-import javax.enterprise.inject.Instance;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.microprofile.config.Config;
@@ -27,12 +27,13 @@ import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.DebeziumEngine.RecordCommitter;
 import io.debezium.server.BaseChangeConsumer;
-import io.debezium.server.CustomConsumerBuilder;
 import io.debezium.util.DelayStrategy;
 
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 import redis.clients.jedis.exceptions.JedisConnectionException;
+import redis.clients.jedis.exceptions.JedisDataException;
 
 /**
  * Implementation of the consumer that delivers the messages into Redis (stream) destination.
@@ -51,10 +52,12 @@ public class RedisStreamChangeConsumer extends BaseChangeConsumer
     private static final String PROP_USER = PROP_PREFIX + "user";
     private static final String PROP_PASSWORD = PROP_PREFIX + "password";
 
-    private DelayStrategy delayStrategy;
     private HostAndPort address;
     private Optional<String> user;
     private Optional<String> password;
+
+    @ConfigProperty(name = PROP_PREFIX + "batch.size", defaultValue = "500")
+    Integer batchSize;
 
     @ConfigProperty(name = PROP_PREFIX + "retry.initial.delay.ms", defaultValue = "300")
     Integer initialRetryDelay;
@@ -70,26 +73,8 @@ public class RedisStreamChangeConsumer extends BaseChangeConsumer
 
     private Jedis client = null;
 
-    @Inject
-    @CustomConsumerBuilder
-    Instance<Jedis> customClient;
-
     @PostConstruct
     void connect() {
-        delayStrategy = DelayStrategy.exponential(initialRetryDelay, maxRetryDelay);
-
-        if (customClient.isResolvable()) {
-            client = customClient.get();
-            try {
-                client.ping();
-                LOGGER.info("Obtained custom configured Jedis '{}'", client);
-                return;
-            }
-            catch (Exception e) {
-                LOGGER.warn("Invalid custom configured Jedis '{}'", client);
-            }
-        }
-
         final Config config = ConfigProvider.getConfig();
         address = HostAndPort.from(config.getValue(PROP_ADDRESS, String.class));
         user = config.getOptionalValue(PROP_USER, String.class);
@@ -107,7 +92,7 @@ public class RedisStreamChangeConsumer extends BaseChangeConsumer
             client.ping();
         }
 
-        LOGGER.info("Using default Jedis '{}'", client);
+        LOGGER.info("Using Jedis '{}'", client);
     }
 
     @PreDestroy
@@ -118,62 +103,106 @@ public class RedisStreamChangeConsumer extends BaseChangeConsumer
         catch (Exception e) {
             LOGGER.warn("Exception while closing Jedis: {}", client, e);
         }
+        finally {
+            client = null;
+        }
+    }
+
+    /**
+    * Split collection to batches by batch size using a stream
+    */
+    private <T> Stream<List<T>> batches(List<T> source, int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("length = " + length);
+        }
+
+        int size = source.size();
+        if (size <= 0) {
+            return Stream.empty();
+        }
+
+        int fullChunks = (size - 1) / length;
+        return IntStream.range(0, fullChunks + 1).mapToObj(
+                n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length));
     }
 
     @Override
     public void handleBatch(List<ChangeEvent<Object, Object>> records,
                             RecordCommitter<ChangeEvent<Object, Object>> committer)
             throws InterruptedException {
+        DelayStrategy delayStrategy = DelayStrategy.exponential(initialRetryDelay, maxRetryDelay);
 
-        for (ChangeEvent<Object, Object> record : records) {
-            LOGGER.trace("Received event '{}'", record);
-
-            String destination = streamNameMapper.map(record.destination());
-            String key = (record.key() != null) ? getString(record.key()) : nullKey;
-            String value = (record.value() != null) ? getString(record.value()) : nullValue;
+        LOGGER.info("Handling a batch of {} records", records.size());
+        batches(records, batchSize).forEach(batch -> {
             boolean completedSuccessfully = false;
 
-            // As long as we failed to add the current record to the stream, we should retry if the reason was either a connection error or OOM in Redis.
+            // As long as we failed to execute the current batch to the stream, we should retry if the reason was either a connection error or OOM in Redis.
             while (!completedSuccessfully) {
-                try {
-                    // Add the record to the destination stream
-                    client.xadd(destination, null, Collections.singletonMap(key, value));
-                    completedSuccessfully = true;
-                }
-                catch (JedisConnectionException jce) {
+                if (client == null) {
                     // Try to reconnect
                     try {
                         connect();
+                        continue; // Managed to establish a new connection to Redis, avoid a redundant retry
                     }
                     catch (Exception e) {
+                        close();
                         LOGGER.error("Can't connect to Redis", e);
                     }
                 }
-                catch (Exception e) {
-                    // When Redis reaches its max memory limitation, a JedisDataException will be thrown with this message.
-                    // In this case, we will retry adding this record to the stream, assuming some memory will be freed eventually as result
-                    // of evicting elements from the stream by the target DB.
-                    if (e.getMessage().equals("OOM command not allowed when used memory > 'maxmemory'.")) {
-                        LOGGER.error("Redis runs OOM", e);
+                else {
+                    Transaction transaction;
+                    try {
+                        LOGGER.info("Preparing a Redis Transaction of {} records", batch.size());
+                        transaction = client.multi();
+
+                        for (ChangeEvent<Object, Object> record : batch) {
+                            String destination = streamNameMapper.map(record.destination());
+                            String key = (record.key() != null) ? getString(record.key()) : nullKey;
+                            String value = (record.value() != null) ? getString(record.value()) : nullValue;
+
+                            // Add the record to the destination stream
+                            transaction.xadd(destination, null, Collections.singletonMap(key, value));
+                        }
+                        transaction.exec();
+                        // Mark all the batch records as processed only when the transaction succeeds
+                        for (ChangeEvent<Object, Object> record : batch) {
+                            committer.markProcessed(record);
+                        }
+                        completedSuccessfully = true;
                     }
-                    // When Redis is starting, a JedisDataException will be thrown with this message.
-                    // We will retry communicating with the target DB as once of the Redis is available, this message will be gone.
-                    else if (e.getMessage().equals("LOADING Redis is loading the dataset in memory")) {
-                        LOGGER.error("Redis is starting", e);
+                    catch (JedisConnectionException jce) {
+                        close();
                     }
-                    // In case of unexpected runtime error, throw a DebeziumException which terminates the process
-                    else {
+                    catch (JedisDataException jde) {
+                        // When Redis reaches its max memory limitation, a JedisDataException will be thrown with one of the messages listed below.
+                        // In this case, we will retry execute the batch, assuming some memory will be freed eventually as result
+                        // of evicting elements from the stream by the target DB.
+                        if (jde.getMessage().equals("EXECABORT Transaction discarded because of previous errors.") ||
+                                (jde.getMessage().equals("EXECABORT Transaction discarded because of: OOM command not allowed when used memory > 'maxmemory'."))) {
+                            LOGGER.error("Redis runs OOM", jde);
+                        }
+                        // When Redis is starting, a JedisDataException will be thrown with this message.
+                        // We will retry communicating with the target DB as once of the Redis is available, this message will be gone.
+                        else if (jde.getMessage().equals("LOADING Redis is loading the dataset in memory")) {
+                            LOGGER.error("Redis is starting", jde);
+                        }
+                        else {
+                            LOGGER.error("Unexpected JedisDataException", jde);
+                            throw new DebeziumException(jde);
+                        }
+                    }
+                    catch (Exception e) {
+                        LOGGER.error("Unexpected Exception", e);
                         throw new DebeziumException(e);
                     }
                 }
 
-                // Failed to add the record to the stream, retry...
+                // Failed to execute the transaction, retry...
                 delayStrategy.sleepWhen(!completedSuccessfully);
             }
+        });
 
-            committer.markProcessed(record);
-        }
-
+        // Mark the whole batch as finished once the sub batches completed
         committer.markBatchFinished();
     }
 }
