94803611b331656a01c551ecf8083abd92a16348
zalmane <oren.elias@gmail.com>
2022-02-17 15:06:58 +0200

    DBZ-4509 updated after comments

--- debezium-server/debezium-server-redis/src/test/java/io/debezium/server/redis/RedisOffsetTestProfile.java
+++ debezium-server/debezium-server-redis/src/test/java/io/debezium/server/redis/RedisOffsetTestProfile.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.server.redis;
 
 import java.util.HashMap;
@@ -7,6 +12,7 @@ import io.quarkus.test.junit.QuarkusTestProfile;
 
 public class RedisOffsetTestProfile implements QuarkusTestProfile {
 
+    @Override
     public Map<String, String> getConfigOverrides() {
         Map<String, String> config = new HashMap<String, String>();
         config.put("debezium.source.offset.storage", "io.debezium.server.redis.RedisOffsetBackingStore");
