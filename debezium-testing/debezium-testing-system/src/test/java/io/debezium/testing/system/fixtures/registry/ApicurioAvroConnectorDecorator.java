a5c7ada37911eee18bd806440e1f6d6cc91ac936
jcechace <jcechace@gmail.com>
2022-02-28 15:16:06 +0100

    DBZ-4790 Removed Apicurio V1 support and switched to official api model

--- debezium-testing/debezium-testing-system/src/test/java/io/debezium/testing/system/fixtures/registry/ApicurioAvroConnectorDecorator.java
+++ debezium-testing/debezium-testing-system/src/test/java/io/debezium/testing/system/fixtures/registry/ApicurioAvroConnectorDecorator.java
@@ -5,8 +5,6 @@
  */
 package io.debezium.testing.system.fixtures.registry;
 
-import static io.debezium.testing.system.tools.ConfigProperties.APICURIO_CRD_VERSION;
-
 import io.debezium.testing.system.fixtures.TestSetupFixture;
 import io.debezium.testing.system.fixtures.connectors.ConnectorDecoratorFixture;
 import io.debezium.testing.system.tools.kafka.ConnectorConfigBuilder;
@@ -16,21 +14,9 @@ public interface ApicurioAvroConnectorDecorator
 
     @Override
     default void decorateConnectorConfig(ConnectorConfigBuilder config) {
-        switch (APICURIO_CRD_VERSION) {
-            case "v1":
-                config.addApicurioV2AvroSupport(
-                        getRegistryController()
-                                .orElseThrow(() -> new IllegalStateException("No registry controller"))
-                                .getRegistryApiAddress());
-                break;
-            case ("v1alpha1"):
-                config.addApicurioV1AvroSupport(
-                        getRegistryController()
-                                .orElseThrow(() -> new IllegalStateException("No registry controller"))
-                                .getRegistryApiAddress());
-                break;
-            default:
-                throw new IllegalStateException("Unsupported Apicurio CR version " + APICURIO_CRD_VERSION);
-        }
+        String registryApiAddress = getRegistryController()
+                .orElseThrow(() -> new IllegalStateException("No registry controller"))
+                .getRegistryApiAddress();
+        config.addApicurioV2AvroSupport(registryApiAddress);
     }
 }
