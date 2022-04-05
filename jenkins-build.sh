902060f6e160adeb60c0d175075ffaff421b63a6
Yang <ywu@stripe.com>
2022-04-05 16:03:57 -0700

    Test deploy-file

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -3,7 +3,7 @@
 set -eux
 . /usr/stripe/bin/docker/stripe-init-build
 
-release_version=$(mvn -q \
+release_version=stripe-$(mvn -q \
     -Dexec.executable=echo \
     -Dexec.args='${project.version}' \
     --non-recursive \
@@ -13,10 +13,11 @@ echo "Building Debezium version $release_version."
 mvn clean install -Passembly -DskipITs -DskipTests -pl debezium-connector-mongodb
 
 # stripe-sync-dir "debezium-connector-mongodb/target/debezium-connector-mongodb-$release_version-plugin.tar.gz" /build
+test debezium-connector-mongodb/target/debezium-connector-mongodb-$release_version-plugin.tar.gz
 
 echo "Publishing Debezium version $release_version to Artifactory."
 # if [[ "${GIT_BRANCH:-}" == "master" ]] || [[ "${GIT_BRANCH:-}" == *_FORCE_DEPLOY ]]; then
 # fi
-mvn deploy \
-    -DgroupId=io.debezium -DrepositoryId=stripe-artifactory-v2 \
-    -DskipTests -DskipITs -pl debezium-connector-mongodb
+mvn deploy:deploy-file \
+    -DgroupId=io.debezium -DrepositoryId=stripe-artifactory-v2 -Durl=https://artifactory-content.stripe.build/artifactory \
+    -Dpackaging=tar.gz -Dfile=debezium-connector-mongodb/target/debezium-connector-mongodb-$release_version-plugin.tar.gz
