c7b6cb3e802fdfb02a047afcd9e0be1de809ea69
Jiri Pechanec <jpechane@redhat.com>
2022-04-22 13:33:48 +0200

    DBZ-4491 Fix formatting

--- debezium-server/debezium-server-pubsub/src/test/java/io/debezium/server/pubsub/PubSubTestResourceLifecycleManager.java
+++ debezium-server/debezium-server-pubsub/src/test/java/io/debezium/server/pubsub/PubSubTestResourceLifecycleManager.java
@@ -1,8 +1,8 @@
-/* Copyright Debezium Authors.
-*
-* Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
-*/
-
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.server.pubsub;
 
 import java.util.Map;
