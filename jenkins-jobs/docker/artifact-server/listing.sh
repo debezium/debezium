e816e234ea0661d0687342d5255e76d5048d8947
Jiri Novotny <jnovotny@redhat.com>
2022-04-26 15:01:00 +0200

    DBZ-5051 Update listing.sh

--- jenkins-jobs/docker/artifact-server/listing.sh
+++ jenkins-jobs/docker/artifact-server/listing.sh
@@ -7,7 +7,7 @@ eval set -- "$OPTS"
 OUTPUT="$(pwd)/artifacts.txt"
 while true; do
   case "$1" in
-    -d | --dir )                DIR=$2;          shift; shift ;;
+    -d | --dir )                DIR=$2;                         shift; shift ;;
     -o | --output )             OUTPUT=$2;                      shift; shift ;;
     -h | --help )               PRINT_HELP=true;                shift ;;
     -- ) shift; break ;;
@@ -18,11 +18,28 @@ done
 shopt -s globstar nullglob
 pushd "$DIR" || exit
 rm -f "$OUTPUT"
-for file in **/*.{zip,jar}; do
-    prefix=$(echo "$file" | sed -rn 's@^(.*)-([[:digit:]].*([[:digit:]]|Final|SNAPSHOT))(.*)(\..*)$@\1@p')
-    suffix=$(echo "$file" | sed -rn 's@^(.*)-([[:digit:]].*([[:digit:]]|Final|SNAPSHOT))(.*)(\..*)$@\4@p')
-    artifact="$prefix$suffix"
+for connector in **/*connector*.{zip,jar}; do
+    name=$(echo "$connector" | sed -rn 's@^(.*)-[0-9]*\..*$@\1@p')
+    artifact="$name"
     echo "$artifact"
-    echo "$artifact:$file" >> "$OUTPUT"
+    echo "$artifact::$connector" >> "$OUTPUT"
 done
-popd
+
+scripting=$(ls **/*scripting*.{zip,jar})
+artifact="debezium-scripting"
+echo "$artifact"
+echo "$artifact::$scripting" >> "$OUTPUT"
+
+converter=$(ls **/*converter*.{zip,jar})
+artifact="connect-converter"
+echo "$artifact"
+echo "$artifact::$converter" >> "$OUTPUT"
+
+for driver in **/jdbc/*.{zip,jar}; do
+    name=$(echo "$driver" | sed -rn 's@^(.*)-([[:digit:]].*([[:digit:]]|Final|SNAPSHOT))(.*)(\..*)$@\1@p')
+    artifact="$name"
+    echo "$artifact"
+    echo "$artifact::$driver" >> "$OUTPUT"
+done
+
+popd || exit
