5c7802df654246c1defab61bca5e70a40ab2b7cc
Martin Medek <mmedek@redhat.com>
2022-05-04 13:04:47 +0200

    DBZ-5082 added github checkout step so that the script exists on slave machine, renamed job so it shows in system tests tab in jenkins

--- jenkins-jobs/job-dsl/maven_artifact_check.groovy
+++ jenkins-jobs/job-dsl/maven_artifact_check.groovy
@@ -1,11 +1,17 @@
-freeStyleJob('maven-artifact-check') {
+freeStyleJob('prepare-maven-artifact-check') {
     displayName('Maven artifact check')
     description('Verify integrity of maven artifact')
     label('Slave')
 
     parameters {
+        stringParam('REPOSITORY', 'https://github.com/debezium/debezium', 'Debezium repository where the script is located')
+        stringParam('BRANCH', '*/main', 'A branch/tag where the script is located')
         stringParam('ARTIFACT_URL', '', 'Artifact URL')
         textParam('COMPONENTS', 'debezium-connector-db2 debezium-connector-mongodb debezium-connector-mysql debezium-connector-oracle debezium-connector-postgres debezium-connector-sqlserver debezium-scripting', 'Space separated list of components in artifact')
+    }
+
+    scm {
+        git('$REPOSITORY', '$BRANCH')
     }
 
     steps {
