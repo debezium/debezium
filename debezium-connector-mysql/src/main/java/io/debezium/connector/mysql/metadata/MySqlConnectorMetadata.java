e7cbaf1d7dd9d845a461b7406cc3cbb0a282f4de
Chris Cranford <chris@hibernate.org>
2022-05-06 15:51:41 -0400

    DBZ-5101 Adjust schema generator for MySQL for database.server.id
    * Removes default value assignment from field
    * Removes auto-generation blurb from description

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/metadata/MySqlConnectorMetadata.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/metadata/MySqlConnectorMetadata.java
@@ -14,6 +14,17 @@ import io.debezium.metadata.ConnectorMetadata;
 
 public class MySqlConnectorMetadata implements ConnectorMetadata {
 
+    /**
+     * Defines a variant of SERVER_ID where we override the default connector behavior to only
+     * expose it as required with no default value and no default value generator.
+     */
+    private static Field SERVER_ID = MySqlConnectorConfig.SERVER_ID
+            .withNoDefault()
+            .withDescription("A numeric ID of this database client, which must be unique across all "
+                    + "currently running database processes in the cluster. This connector joins the "
+                    + "MySQL database cluster as another server (with this unique ID) so it can read "
+                    + "the binlog.");
+
     @Override
     public ConnectorDescriptor getConnectorDescriptor() {
         return new ConnectorDescriptor("mysql", "Debezium MySQL Connector", MySqlConnector.class.getName(), Module.version());
@@ -22,6 +33,8 @@ public class MySqlConnectorMetadata implements ConnectorMetadata {
     @Override
     public Field.Set getConnectorFields() {
         return MySqlConnectorConfig.ALL_FIELDS
-                .filtered(f -> f != MySqlConnectorConfig.GTID_NEW_CHANNEL_POSITION);
+                .filtered(f -> f != MySqlConnectorConfig.GTID_NEW_CHANNEL_POSITION)
+                .filtered(f -> f != MySqlConnectorConfig.SERVER_ID)
+                .with(SERVER_ID);
     }
 }
