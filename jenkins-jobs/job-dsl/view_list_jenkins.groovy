46f1a7374f1aca060eb5a0ba2ab9957fcdb7757f
Jiri Novotny <jnovotny@redhat.com>
2022-05-10 14:40:34 +0200

    DBZ-5094 Add cleanup job and ansible script

--- jenkins-jobs/job-dsl/view_list_jenkins.groovy
+++ jenkins-jobs/job-dsl/view_list_jenkins.groovy
@@ -4,6 +4,7 @@ listView('Jenkins') {
         name('job-configurator')
         name('MonitoringTest')
         name('node-snapshot-build')
+        name('node-snapshot-history-cleanup')
     }
     columns {
         status()
