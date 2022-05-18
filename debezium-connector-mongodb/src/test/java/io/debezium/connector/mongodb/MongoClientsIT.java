cc04a94fd5881d17969c39581286a5f9764feab7
jcechace <jcechace@gmail.com>
2022-05-18 12:38:21 +0200

    DBZ-4733 Ininital connection string filed introduction

--- debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/MongoClientsIT.java
+++ debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/MongoClientsIT.java
@@ -78,9 +78,8 @@ public class MongoClientsIT {
         MongoClient client2 = clients.clientForMembers(addresses);
         assertThat(client1).isSameAs(client2);
 
-        ServerAddress[] array = addresses.toArray(new ServerAddress[addresses.size()]);
-        MongoClient client3 = clients.clientForMembers(array);
-        MongoClient client4 = clients.clientForMembers(array);
+        MongoClient client3 = clients.clientForMembers(addresses);
+        MongoClient client4 = clients.clientForMembers(addresses);
         assertThat(client3).isSameAs(client4);
         assertThat(client3).isSameAs(client1);
 
