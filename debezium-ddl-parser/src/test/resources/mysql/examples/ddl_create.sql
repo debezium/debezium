fd7934598fc3f6380483210213ce3fd22a4e2237
Chris Cranford <chris@hibernate.org>
2022-05-19 22:51:06 -0400

    DBZ-4958 Support `CREATE OR REPLACE TABLE` syntax for MySQL

--- debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
+++ debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
@@ -125,6 +125,19 @@ CREATE TABLE `t_test_curdate` (
 `c1` datetime NOT NULL DEFAULT CURDATE() COMMENT 'error test',
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE OR REPLACE TABLE `t_table` (
+`info_no` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`product_no` int(11) unsigned NOT NULL,
+`member_id` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+`app_url` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`redirect_url` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`scope` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+PRIMARY KEY (`info_no`),
+UNIQUE KEY `UN_member_id` (`member_id`) USING BTREE,
+UNIQUE KEY `UN_product_no` (`product_no`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
 #end
 #begin
 -- Rename table
