77924e126a282461af0b55297aead7856073ddce
Jiri Novotny <jnovotny@redhat.com>
2022-06-01 15:00:11 +0200

    DBZ-5183 Add groovy plugins to build

--- jenkins-jobs/scripts/upstream-kafka-connect-prepare.sh
+++ jenkins-jobs/scripts/upstream-kafka-connect-prepare.sh
@@ -5,7 +5,7 @@ DOCKER_FILE=${DIR}/../docker/Dockerfile.Strimzi
 PLUGIN_DIR="plugins"
 
 
-OPTS=$(getopt -o d:f:r:o:t:a: --long dir:,dockerfile:,registry:,organisation:,tags:,auto-tag:,kc-base-tag:,kafka-version:,apicurio-version:,dest-login:,dest-pass:,img-output:,oracle-included: -n 'parse-options' -- "$@")
+OPTS=$(getopt -o d:f:r:o:t:a: --long dir:,dockerfile:,registry:,organisation:,tags:,auto-tag:,kc-base-tag:,kafka-version:,apicurio-version:,dest-login:,dest-pass:,img-output:,oracle-included:,maven-repo: -n 'parse-options' -- "$@")
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 eval set -- "$OPTS"
 
@@ -25,6 +25,7 @@ while true; do
     --dest-pass )               DEST_PASS=$2;                       shift; shift ;;
     --img-output )              IMAGE_OUTPUT_FILE=$2;               shift; shift ;;
     --oracle-included )         ORACLE=$2;                          shift; shift ;;
+    --maven-repo )              MAVEN_REPO=$2;                      shift; shift ;;
     -h | --help )               PRINT_HELP=true;                    shift ;;
     -- ) shift; break ;;
     * ) break ;;
@@ -47,19 +48,24 @@ project_version=$(cat pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]
 
 # Copy build debezium connectors and extra libs to plugins directory
 pushd "${PLUGIN_DIR}" || exit
-cp ~/.m2/repository/io/debezium/debezium-connector-*/"${project_version}"/debezium-connector-*.zip .
+cp "$MAVEN_REPO"/io/debezium/debezium-connector-*/"${project_version}"/debezium-connector-*.zip .
 unzip debezium-connector-\*.zip && rm debezium-connector-*.zip
-cp ~/.m2/repository/com/ibm/db2/jcc/*/jcc-*.jar debezium-connector-db2/
-cp ~/.m2/repository/com/oracle/database/jdbc/ojdbc8/*/ojdbc8-*.jar debezium-connector-oracle/
 
-cp ~/.m2/repository/io/apicurio/apicurio-registry-distro-connect-converter/*/apicurio-registry-*.zip .
+cp "$MAVEN_REPO"/com/ibm/db2/jcc/*/jcc-*.jar debezium-connector-db2/
+cp "$MAVEN_REPO"/com/oracle/database/jdbc/ojdbc8/*/ojdbc8-*.jar debezium-connector-oracle/
+
+cp "$MAVEN_REPO"/io/apicurio/apicurio-registry-distro-connect-converter/*/apicurio-registry-*.zip .
 
 CONNECTORS='debezium-connector-oracle debezium-connector-db2 debezium-connector-sqlserver'\
 ' debezium-connector-mongodb debezium-connector-postgres debezium-connector-mysql'
 
+# Copy groovy plugins and Apicurio to each connector
 for connector in $CONNECTORS
 do
 	unzip -o apicurio-registry-*.zip -d "${connector}/"
+	cp "$MAVEN_REPO"/org/codehaus/groovy/groovy/*/groovy-*.jar "${connector}/"
+  cp "$MAVEN_REPO"/org/codehaus/groovy/groovy-json/*/groovy-json-*.jar "${connector}/"
+  cp "$MAVEN_REPO"/org/codehaus/groovy/groovy-jsr223/*/groovy-jsr223-*.jar "${connector}/"
 done
 
 rm apicurio-registry-*.zip
