64ef812cc9812f05cb3fda7ce90797971e13b916
Vojtech Juranek <vjuranek@redhat.com>
2022-06-01 10:38:49 +0200

    DBZ-5045 Remove deprecated MAX_COPY_THREADS

--- debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/metadata/MongoDbConnectorMetadata.java
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/metadata/MongoDbConnectorMetadata.java
@@ -22,6 +22,6 @@ public class MongoDbConnectorMetadata implements ConnectorMetadata {
     @Override
     public Field.Set getConnectorFields() {
         return MongoDbConnectorConfig.ALL_FIELDS
-                .filtered(f -> f != MongoDbConnectorConfig.POLL_INTERVAL_SEC && f != MongoDbConnectorConfig.MAX_COPY_THREADS);
+                .filtered(f -> f != MongoDbConnectorConfig.POLL_INTERVAL_SEC);
     }
 }
