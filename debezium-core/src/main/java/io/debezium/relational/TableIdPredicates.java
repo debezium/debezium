25eba9749d1c4a7c3b5775246be6f152e8a4f366
Vojtech Juranek <vjuranek@redhat.com>
2022-06-09 17:31:26 +0200

    DBZ-5198 Add support for SQL server specific table delimiters
    Add suppoprt for `TableId` delimiters and provide implementation for
    SQL server. SQL server allows to use reserved words in table names or
    names with spaces, but they have to wrapped by `[]` characters, e.g.
    `[dbname].[table name]`.
    
    Debezium can handle spaces e.g. in table include list, but fails when
    parsing snapshot select, therefore the parsing with predicates is used
    only for parsing spanshot select for now. If useful, it can be used
    later on on other places as well.

--- debezium-core/src/main/java/io/debezium/relational/TableIdPredicates.java
+++ debezium-core/src/main/java/io/debezium/relational/TableIdPredicates.java
@@ -17,5 +17,12 @@ public interface TableIdPredicates {
     default boolean isQuotingChar(char c) {
         return c == '"' || c == '\'' || c == '`';
     }
-    
+
+    default boolean isStartDelimiter(char c) {
+        return false;
+    }
+
+    default boolean isEndDelimiter(char c) {
+        return false;
+    }
 }
