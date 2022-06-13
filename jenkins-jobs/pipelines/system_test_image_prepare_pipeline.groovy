7a265104bc87629e267d9fab6117e3031788a5f5
Martin Medek <mmedek@redhat.com>
2022-06-13 14:02:08 +0200

    DBZ-5165 added apicurio step and a jenkins job to build the testsuite image

---
+++ jenkins-jobs/pipelines/system_test_image_prepare_pipeline.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    agent {
+        label 'NodeXL'
+    }
+
+    stages {
+        stage('CleanWorkspace') {
+            steps {
+                cleanWs()
+            }
+        }
+
+        stage('Checkout') {
+            steps {
+                checkout([
+                        $class           : 'GitSCM',
+                        branches         : [[name: "${PARENT_DBZ_GIT_BRANCH}"]],
+                        userRemoteConfigs: [[url: "${PARENT_DBZ_GIT_REPOSITORY}"]],
+                        extensions       : [[$class           : 'RelativeTargetDirectory',
+                                             relativeTargetDir: 'debezium']],
+                ])
+                sh '''
+                set -x
+                '''
+            }
+        }
+
+        stage('Build') {
+            steps {
+                withCredentials([
+                    usernamePassword(credentialsId: "${QUAY_CREDENTIALS}", usernameVariable: 'QUAY_USERNAME', passwordVariable: 'QUAY_PASSWORD'),
+                    file(credentialsId: "${PULL_SECRET}", variable: 'SECRET_PATH'),
+                ]) {
+
+                    sh '''
+                    cp ${SECRET_PATH} debezium/jenkins-jobs/docker/debezium-testing-system/secret.yml
+                    pushd debezium/jenkins-jobs/docker/debezium-testing-system
+                    docker build --build-arg branch=${DBZ_GIT_BRANCH} --build-arg repository=${DBZ_GIT_REPOSITORY} -t testsuite:docker-test .
+                    docker tag testsuite:docker-test quay.io/rh_integration/dbz-testing-system:${TAG}
+                    docker login -u ${QUAY_USERNAME} -p ${QUAY_PASSWORD} quay.io
+                    docker push quay.io/rh_integration/dbz-testing-system:${TAG}
+                '''
+                }
+            }
+        }
+    }
+}
