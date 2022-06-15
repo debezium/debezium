67d279003d9f5ef3ae7409648907026d57933819
jcechace <jcechace@gmail.com>
2022-06-15 11:06:16 +0200

    DBZ-4733 adjusting poll interval in mongodb tests

--- debezium-embedded/src/test/java/io/debezium/embedded/AbstractConnectorTest.java
+++ debezium-embedded/src/test/java/io/debezium/embedded/AbstractConnectorTest.java
@@ -100,7 +100,7 @@ public abstract class AbstractConnectorTest implements Testing {
     private ExecutorService executor;
     protected EmbeddedEngine engine;
     protected BlockingQueue<SourceRecord> consumedLines;
-    protected long pollTimeoutInMs = TimeUnit.SECONDS.toMillis(5);
+    protected long pollTimeoutInMs = TimeUnit.SECONDS.toMillis(10);
     protected final Logger logger = LoggerFactory.getLogger(getClass());
     private CountDownLatch latch;
     private JsonConverter keyJsonConverter = new JsonConverter();
