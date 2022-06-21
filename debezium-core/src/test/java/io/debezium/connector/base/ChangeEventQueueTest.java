823a49fff48758bd417bf6d43a350397e68b49b4
Naveen Kumar KR <krnaveen14@gmail.com>
2022-06-21 18:00:01 +0530

    DBZ-5277 ChangeEventQueue Test with Multiple Writer and Reader Threads
    Assertion would fail if one or both of the scenario occurs
    
    1) Deadlock between Writer and Reader Threads
    2) Thread Starvation between Writer and Reader Threads

---
+++ debezium-core/src/test/java/io/debezium/connector/base/ChangeEventQueueTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.base;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import io.debezium.util.LoggingContext;
+
+@RunWith(Parameterized.class)
+public class ChangeEventQueueTest {
+
+    private static final String EVENT = "Change Data Capture Even via Debezium";
+
+    private final int noOfWriters;
+    private final int noOfReaders;
+    private final int noOfEventsPerWriter;
+    private final long totalNoOfEvents;
+    private final Thread[] writers;
+    private final Thread[] readers;
+    private final AtomicLong recordsRead;
+
+    public ChangeEventQueueTest(int noOfWriters, int noOfReaders, int noOfEventsPerWriter) {
+        this.noOfWriters = noOfWriters;
+        this.noOfReaders = noOfReaders;
+        this.noOfEventsPerWriter = noOfEventsPerWriter;
+        this.totalNoOfEvents = (long) noOfWriters * noOfEventsPerWriter;
+        this.writers = new Thread[noOfWriters];
+        this.readers = new Thread[noOfReaders];
+        this.recordsRead = new AtomicLong();
+    }
+
+    @Parameters(name = "{index}: testQueue({0} writers, {1} readers, {2} events)")
+    public static Collection<Object[]> data() {
+        int[] writers = { 1, 2, 4, 8, 16 };
+        int[] readers = { 1, 2, 4, 8, 16 };
+        int totalEvents = 1_000_000;
+        Object[][] params = new Object[writers.length * readers.length][];
+        int index = 0;
+        for (int writer : writers) {
+            for (int reader : readers) {
+                params[index++] = new Object[]{ writer, reader, totalEvents };
+            }
+        }
+        return Arrays.asList(params);
+    }
+
+    @Before
+    public void setup() {
+        ChangeEventQueue<String> queue = new ChangeEventQueue.Builder<String>()
+                .maxBatchSize(8192)
+                .maxQueueSize(8192 * 2)
+                .loggingContextSupplier(() -> LoggingContext.forConnector("a", "b", "c"))
+                .pollInterval(Duration.ofMillis(500))
+                .build();
+        for (int i = 0; i < noOfWriters; i++) {
+            writers[i] = getWriter(queue, noOfEventsPerWriter);
+        }
+        for (int i = 0; i < noOfReaders; i++) {
+            readers[i] = getReader(queue, totalNoOfEvents, recordsRead);
+        }
+    }
+
+    @Test
+    public void shouldQueueAndPollMessages() throws InterruptedException {
+        for (Thread thread : writers) {
+            thread.start();
+        }
+        for (Thread thread : readers) {
+            thread.start();
+        }
+
+        // 1_000_000 Events should write / read in about 1-2 seconds
+        // At max, we can wait up to 10 seconds
+        long maxWaitTimeout = TimeUnit.SECONDS.toMillis(10);
+
+        for (Thread writer : writers) {
+            writer.join(maxWaitTimeout);
+        }
+        for (Thread reader : readers) {
+            reader.join(maxWaitTimeout);
+        }
+        assertEquals(totalNoOfEvents, recordsRead.get());
+    }
+
+    @After
+    public void teardown() {
+        for (Thread thread : writers) {
+            thread.interrupt();
+        }
+        for (Thread thread : readers) {
+            thread.interrupt();
+        }
+    }
+
+    private static Thread getWriter(ChangeEventQueue<String> queue, int noOfEvents) {
+        return new Thread(() -> {
+            for (int i = 0; i < noOfEvents; i++) {
+                try {
+                    queue.doEnqueue(EVENT);
+                }
+                catch (InterruptedException ex) {
+                    // exit thread
+                }
+            }
+        });
+    }
+
+    private static Thread getReader(ChangeEventQueue<String> queue, long totalNoOfEvents, AtomicLong recordsRead) {
+        return new Thread(() -> {
+            while (recordsRead.get() < totalNoOfEvents) {
+                try {
+                    recordsRead.addAndGet(queue.poll().size());
+                }
+                catch (InterruptedException ex) {
+                    // exit thread
+                }
+            }
+        });
+    }
+
+}
