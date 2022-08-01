90a4c0059a5d0a81dd415290be6d6fb5b184f08c
Chris Cranford <chris@hibernate.org>
2022-08-01 14:36:09 -0400

    DBZ-5434 Fix test failure

--- debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/transforms/ExtractNewDocumentStateTestIT.java
+++ debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/transforms/ExtractNewDocumentStateTestIT.java
@@ -1689,6 +1689,8 @@ public class ExtractNewDocumentStateTestIT extends AbstractExtractNewDocumentSta
     @Test
     @FixFor("DBZ-5434")
     public void shouldSupportNestedArrays() throws InterruptedException {
+        waitForStreamingRunning();
+
         // Test insert
         primary().execute("insert", client -> {
             client.getDatabase(DB_NAME).getCollection(this.getCollectionName())
