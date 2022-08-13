2ba3ccbc0e2a3fa5f4cf050b14c6444a3e0dff2a
harveyyue <yw_yuewei@126.com>
2022-08-13 10:52:54 +0800

    DBZ-5508 DDL statement couldn't be parsed : mismatched input 'ENGINE'

--- debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
+++ debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
@@ -149,6 +149,14 @@ CREATE TABLE IF NOT EXISTS `contract_center`.`ent_folder_letter_relationship` (
 `data_update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP() COMMENT '更新时间',
 PRIMARY KEY(`id`)) ENGINE = InnoDB DEFAULT CHARACTER SET = UTF8MB4;
 
+CREATE TABLE `auth_realm_clients` (
+`pk_realm` int unsigned NOT NULL DEFAULT '0',
+`fk_realm` int unsigned DEFAULT NULL,
+`client_id` varchar(150) NOT NULL,
+`client_secret` blob NOT NULL,
+PRIMARY KEY (`pk_realm`),
+KEY `auth_realms_auth_realm_clients` (`fk_realm`)
+) START TRANSACTION ENGINE=InnoDB DEFAULT CHARSET=latin1;
 #end
 #begin
 -- Rename table
