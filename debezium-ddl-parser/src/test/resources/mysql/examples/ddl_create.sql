529ef45ecda799921f41754e95db95013dc21343
harveyyue <yw_yuewei@126.com>
2022-10-10 22:25:19 +0800

    DBZ-5708 MySqlConnector parse create view statement failed

--- debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
+++ debezium-ddl-parser/src/test/resources/mysql/examples/ddl_create.sql
@@ -296,6 +296,7 @@ create algorithm = merge view my_view2(col1, col2) as select * from t2 with chec
 create or replace definer = 'ivan'@'%' view my_view3 as select count(*) from t3;
 create or replace definer = current_user sql security invoker view my_view4(c1, 1c, _, c1_2) 
 	as select * from  (t1 as tt1, t2 as tt2) inner join t1 on t1.col1 = tt1.col1;
+create view v_some_table as (with a as (select * from some_table) select * from a);
 
 #end
 #begin
