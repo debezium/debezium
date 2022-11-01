4bde5380cebfb5c64dd5a1c02f8fdd76568fd32d
jcechace <jcechace@gmail.com>
2022-11-01 21:38:50 +0100

    DBZ-5779 Migrated debezium-testing-testcontainers to AssertJ

--- debezium-testing/debezium-testing-testcontainers/src/test/java/io/debezium/testing/testcontainers/ApicurioRegistryTest.java
+++ debezium-testing/debezium-testing-testcontainers/src/test/java/io/debezium/testing/testcontainers/ApicurioRegistryTest.java
@@ -5,7 +5,7 @@
  */
 package io.debezium.testing.testcontainers;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
