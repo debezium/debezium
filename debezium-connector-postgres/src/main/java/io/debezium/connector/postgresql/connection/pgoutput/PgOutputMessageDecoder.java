92544fef73e73aef912cff3c5ad3c8179d91b511
Rajendra Dangwal <31967338+rajdangwal@users.noreply.github.com>
2022-11-10 12:29:20 +0530

    DBZ-5792: Fix for - Message with LSN XYZ not present among LSNs seen in the location phase (#30)

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/pgoutput/PgOutputMessageDecoder.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/pgoutput/PgOutputMessageDecoder.java
@@ -130,6 +130,23 @@ public class PgOutputMessageDecoder extends AbstractMessageDecoder {
         try {
             MessageType type = MessageType.forType((char) buffer.get());
             LOGGER.trace("Message Type: {}", type);
+            switch (type) {
+                case TYPE:
+                case ORIGIN:
+                    // TYPE/ORIGIN
+                    // These should be skipped without calling shouldMessageBeSkipped. DBZ-5792
+                    LOGGER.trace("{} messages are always skipped without calling shouldMessageBeSkipped", type);
+                    return true;
+                case TRUNCATE:
+                    if (!isTruncateEventsIncluded()) {
+                        LOGGER.trace("{} messages are being skipped without calling shouldMessageBeSkipped", type);
+                        return true;
+                    }
+                    // else delegate to super.shouldMessageBeSkipped
+                    break;
+                default:
+                    // call super.shouldMessageBeSkipped for rest of the types
+            }
             final boolean candidateForSkipping = super.shouldMessageBeSkipped(buffer, lastReceivedLsn, startLsn, walPosition);
             switch (type) {
                 case COMMIT:
@@ -147,7 +164,7 @@ public class PgOutputMessageDecoder extends AbstractMessageDecoder {
                     LOGGER.trace("{} messages are always reprocessed", type);
                     return false;
                 default:
-                    // INSERT/UPDATE/DELETE/TRUNCATE/TYPE/ORIGIN/LOGICAL_DECODING_MESSAGE
+                    // INSERT/UPDATE/DELETE/TRUNCATE/LOGICAL_DECODING_MESSAGE
                     // These should be excluded based on the normal behavior, delegating to default method
                     return candidateForSkipping;
             }
