874214179857310ae819c21c388d2a940a949301
ggaborg <ggaborg@yahoo.com>
2022-11-11 12:57:07 +0200

    DBZ-4720 Unification and validation for Redis sink, offset storage and schema history configuration

--- debezium-storage/debezium-storage-redis/src/main/java/io/debezium/storage/redis/offset/RedisOffsetBackingStore.java
+++ debezium-storage/debezium-storage-redis/src/main/java/io/debezium/storage/redis/offset/RedisOffsetBackingStore.java
@@ -9,14 +9,13 @@ import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 import org.apache.kafka.connect.runtime.WorkerConfig;
 import org.apache.kafka.connect.storage.MemoryOffsetBackingStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.debezium.config.Field;
+import io.debezium.config.Configuration;
 import io.debezium.storage.redis.RedisClient;
 import io.debezium.storage.redis.RedisClientConnectionException;
 import io.debezium.storage.redis.RedisConnection;
@@ -31,123 +30,22 @@ public class RedisOffsetBackingStore extends MemoryOffsetBackingStore {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisOffsetBackingStore.class);
 
-    private static final String CONFIGURATION_FIELD_PREFIX_STRING = "offset.storage.redis.";
-    public static final Field PROP_ADDRESS = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "address")
-            .withDescription("The Redis url that will be used to access the database schema history");
-
-    public static final Field PROP_SSL_ENABLED = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "ssl.enabled")
-            .withDescription("Use SSL for Redis connection")
-            .withDefault("false");
-
-    public static final Field PROP_USER = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "user")
-            .withDescription("The Redis url that will be used to access the database schema history");
-
-    public static final Field PROP_PASSWORD = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "password")
-            .withDescription("The Redis url that will be used to access the database schema history");
-
-    public static final String DEFAULT_REDIS_KEY_NAME = "metadata:debezium:offsets";
-    public static final Field PROP_KEY_NAME = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "key")
-            .withDescription("The Redis key that will be used to store the database schema history")
-            .withDefault(DEFAULT_REDIS_KEY_NAME);
-
-    public static final Integer DEFAULT_RETRY_INITIAL_DELAY = 300;
-    public static final Field PROP_RETRY_INITIAL_DELAY = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "retry.initial.delay.ms")
-            .withDescription("Initial retry delay (in ms)")
-            .withDefault(DEFAULT_RETRY_INITIAL_DELAY);
-
-    public static final Integer DEFAULT_RETRY_MAX_DELAY = 10000;
-    public static final Field PROP_RETRY_MAX_DELAY = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "retry.max.delay.ms")
-            .withDescription("Maximum retry delay (in ms)")
-            .withDefault(DEFAULT_RETRY_MAX_DELAY);
-
-    public static final Integer DEFAULT_CONNECTION_TIMEOUT = 2000;
-    public static final Field PROP_CONNECTION_TIMEOUT = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "connection.timeout.ms")
-            .withDescription("Connection timeout (in ms)")
-            .withDefault(DEFAULT_CONNECTION_TIMEOUT);
-
-    public static final Integer DEFAULT_SOCKET_TIMEOUT = 2000;
-    public static final Field PROP_SOCKET_TIMEOUT = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "socket.timeout.ms")
-            .withDescription("Socket timeout (in ms)")
-            .withDefault(DEFAULT_SOCKET_TIMEOUT);
-
-    private static final boolean DEFAULT_WAIT_ENABLED = false;
-    private static final Field PROP_WAIT_ENABLED = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "wait.enabled")
-            .withDescription(
-                    "Enables wait for replica. In case Redis is configured with a replica shard, this allows to verify that the data has been written to the replica.")
-            .withDefault(DEFAULT_WAIT_ENABLED);
-
-    private static final long DEFAULT_WAIT_TIMEOUT = 1000L;
-    private static final Field PROP_WAIT_TIMEOUT = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "wait.timeout.ms")
-            .withDescription("Timeout when wait for replica")
-            .withDefault(DEFAULT_WAIT_TIMEOUT);
-
-    private static final boolean DEFAULT_WAIT_RETRY_ENABLED = false;
-    private static final Field PROP_WAIT_RETRY_ENABLED = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "wait.retry.enabled")
-            .withDescription("Enables retry on wait for replica failure")
-            .withDefault(DEFAULT_WAIT_RETRY_ENABLED);
-
-    private static final long DEFAULT_WAIT_RETRY_DELAY = 1000L;
-    private static final Field PROP_WAIT_RETRY_DELAY = Field.create(CONFIGURATION_FIELD_PREFIX_STRING + "wait.retry.delay.ms")
-            .withDescription("Delay of retry on wait for replica failure")
-            .withDefault(DEFAULT_WAIT_RETRY_DELAY);
-
-    private String redisKeyName;
-    private String address;
-    private String user;
-    private String password;
-    private boolean sslEnabled;
+    private RedisOffsetBackingStoreConfig config;
 
     private RedisClient client;
-    private Map<String, String> config;
-
-    private Integer initialRetryDelay;
-    private Integer maxRetryDelay;
-
-    private Integer connectionTimeout;
-    private Integer socketTimeout;
-
-    private boolean waitEnabled;
-    private long waitTimeout;
-    private boolean waitRetryEnabled;
-    private long waitRetryDelay;
-
-    public RedisOffsetBackingStore() {
-
-    }
 
     void connect() {
-        RedisConnection redisConnection = new RedisConnection(this.address, this.user, this.password, this.connectionTimeout, this.socketTimeout, this.sslEnabled);
-        client = redisConnection.getRedisClient(RedisConnection.DEBEZIUM_OFFSETS_CLIENT_NAME, this.waitEnabled, this.waitTimeout, this.waitRetryEnabled,
-                this.waitRetryDelay);
+        RedisConnection redisConnection = new RedisConnection(config.getAddress(), config.getUser(), config.getPassword(), config.getConnectionTimeout(),
+                config.getSocketTimeout(), config.isSslEnabled());
+        client = redisConnection.getRedisClient(RedisConnection.DEBEZIUM_OFFSETS_CLIENT_NAME, config.isWaitEnabled(), config.getWaitTimeout(),
+                config.isWaitRetryEnabled(), config.getWaitRetryDelay());
     }
 
     @Override
     public void configure(WorkerConfig config) {
         super.configure(config);
-        this.config = config.originalsStrings();
-
-        this.address = this.config.get(PROP_ADDRESS.name());
-        this.user = this.config.get(PROP_USER.name());
-        this.password = this.config.get(PROP_PASSWORD.name());
-        this.sslEnabled = Boolean.parseBoolean(this.config.get(PROP_SSL_ENABLED.name()));
-
-        this.redisKeyName = Optional.ofNullable(
-                this.config.get(PROP_KEY_NAME.name())).orElse(DEFAULT_REDIS_KEY_NAME);
-        // load retry settings
-        this.initialRetryDelay = Optional.ofNullable(
-                Integer.getInteger(this.config.get(PROP_RETRY_INITIAL_DELAY.name()))).orElse(DEFAULT_RETRY_INITIAL_DELAY);
-        this.maxRetryDelay = Optional.ofNullable(
-                Integer.getInteger(this.config.get(PROP_RETRY_MAX_DELAY.name()))).orElse(DEFAULT_RETRY_MAX_DELAY);
-        // load connection timeout settings
-        this.connectionTimeout = Optional.ofNullable(
-                Integer.getInteger(this.config.get(PROP_CONNECTION_TIMEOUT.name()))).orElse(DEFAULT_CONNECTION_TIMEOUT);
-        this.socketTimeout = Optional.ofNullable(
-                Integer.getInteger(this.config.get(PROP_SOCKET_TIMEOUT.name()))).orElse(DEFAULT_SOCKET_TIMEOUT);
-
-        this.waitEnabled = Optional.ofNullable(Boolean.getBoolean(this.config.get(PROP_WAIT_ENABLED.name()))).orElse(DEFAULT_WAIT_ENABLED);
-        this.waitTimeout = Optional.ofNullable(Long.getLong(this.config.get(PROP_WAIT_TIMEOUT.name()))).orElse(DEFAULT_WAIT_TIMEOUT);
-        this.waitRetryEnabled = Optional.ofNullable(Boolean.getBoolean(this.config.get(PROP_WAIT_RETRY_ENABLED.name()))).orElse(DEFAULT_WAIT_RETRY_ENABLED);
-        this.waitRetryDelay = Optional.ofNullable(Long.getLong(this.config.get(PROP_WAIT_RETRY_DELAY.name()))).orElse(DEFAULT_WAIT_RETRY_DELAY);
+        Configuration configuration = Configuration.from(config.originalsStrings());
+        this.config = new RedisOffsetBackingStoreConfig(configuration);
     }
 
     @Override
@@ -171,7 +69,7 @@ public class RedisOffsetBackingStore extends MemoryOffsetBackingStore {
     private void load() {
         // fetch the value from Redis
         Map<String, String> offsets = Uni.createFrom().item(() -> {
-            return (Map<String, String>) client.hgetAll(this.redisKeyName);
+            return (Map<String, String>) client.hgetAll(config.getRedisKeyName());
         })
                 // handle failures and retry
                 .onFailure().invoke(
@@ -185,7 +83,7 @@ public class RedisOffsetBackingStore extends MemoryOffsetBackingStore {
                             this.connect();
                         })
                 // retry on failure with backoff
-                .onFailure().retry().withBackOff(Duration.ofMillis(initialRetryDelay), Duration.ofMillis(maxRetryDelay)).indefinitely()
+                .onFailure().retry().withBackOff(Duration.ofMillis(config.getInitialRetryDelay()), Duration.ofMillis(config.getMaxRetryDelay())).indefinitely()
                 // write success trace message
                 .invoke(
                         item -> {
@@ -210,7 +108,7 @@ public class RedisOffsetBackingStore extends MemoryOffsetBackingStore {
             byte[] value = (mapEntry.getValue() != null) ? mapEntry.getValue().array() : null;
             // set the value in Redis
             Uni.createFrom().item(() -> {
-                return (Long) client.hset(this.redisKeyName.getBytes(), key, value);
+                return (Long) client.hset(config.getRedisKeyName().getBytes(), key, value);
             })
                     // handle failures and retry
                     .onFailure().invoke(
