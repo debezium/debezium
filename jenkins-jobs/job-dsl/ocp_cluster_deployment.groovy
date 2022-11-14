5362ae93d415bedcf9e8f82877a594b6ead924d7
Ondrej Babec <ond.babec@gmail.com>
2022-11-14 14:58:44 +0100

    DBZ-5844 Add Kubernetes plugin for cluster deployment

--- jenkins-jobs/job-dsl/ocp_cluster_deployment.groovy
+++ jenkins-jobs/job-dsl/ocp_cluster_deployment.groovy
@@ -11,17 +11,9 @@ pipelineJob('ocp-cluster-deployment') {
     }
 
     parameters {
-//      CREDENTIALS
-        stringParam('GITLAB_CREDENTIALS', 'gitlab-debeziumci-ssh', 'QE gitlab credentials id')
-        stringParam('ANSIBLE_VAULT_PASSWORD', 'ansible-vault-password', 'Password for ansible vault used for secrets encryption')
-//      OSIA CONFIGURATION
         stringParam('CLUSTER_NAME', 'cluster', 'Name of OCP cluster')
         stringParam('INSTALLER_VERSION', 'latest-4.10', 'Version of OCP installer')
         booleanParam('REMOVE_CLUSTER', false, 'If true, instead of cluster deployment removes said cluster')
-//      OSIA REPO
-        stringParam('OSIA_GIT_SECRET', 'osia-git-repo', 'ID of secret containing OSIA configuration repository')
-        stringParam('OSIA_GIT_REPOSITORY', '', 'OSIA configuration repository. If empty, repository from previous secret is used. Use SSH format')
-        stringParam('OSIA_GIT_BRANCH', 'persistence', 'A branch/tag of OSIA configuration repository')
     }
 
     definition {
