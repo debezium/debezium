de9e90362c8183aff08e2d19c74f1bec2704d810
Ondrej Babec <ond.babec@gmail.com>
2022-11-14 14:58:44 +0100

    DBZ-5844 Add Kubernetes plugin for cluster deployment

--- jenkins-jobs/pipelines/cluster_deployment_pipeline.groovy
+++ jenkins-jobs/pipelines/cluster_deployment_pipeline.groovy
@@ -1,74 +1,87 @@
 pipeline {
-    agent {
-        label 'Fresh'
+    environment {
+        OSIA_GIT_SECRET = "ocp-deployment-repo"
+        OSIA_GIT_BRANCH = "persistence"
+        ANSIBLE_VAULT_PASSWORD = "ansible-vault-password"
+        GITLAB_CREDENTIALS = "gitlab-debeziumci-ssh"
     }
-
+    agent {
+        label 'debezium-ci-tools'
+    }
     stages {
-        stage('CleanWorkspace') {
-            steps {
-                cleanWs()
-            }
-        }
-
         stage('Checkout and prepare git') {
             steps {
-                script {
-                    if (params.OSIA_GIT_REPOSITORY.isEmpty()) {
-                        withCredentials([
-                                string(credentialsId: "${params.OSIA_GIT_SECRET}", variable: 'TMP_OSIA_GIT_REPOSITORY')
-                        ]){env.OSIA_GIT_REPOSITORY = TMP_OSIA_GIT_REPOSITORY}
+                container("debezium-ci-tools") {
+                    script {
+                            withCredentials([
+                                    string(credentialsId: "${env.OSIA_GIT_SECRET}", variable: 'OSIA_GIT_REPOSITORY')
+                            ]) {
+                                checkout([
+                                        $class           : 'GitSCM',
+                                        branches         : [[name: "${env.OSIA_GIT_BRANCH}"]],
+                                        userRemoteConfigs: [[url: "${OSIA_GIT_REPOSITORY}",
+                                                             credentialsId: "${env.GITLAB_CREDENTIALS}"]],
+                                        extensions       : [[$class: 'CleanCheckout'],
+                                                            [$class: 'RelativeTargetDirectory',
+                                                             relativeTargetDir: 'OSIA-dbz']] +
+                                                [[$class: 'CloneOption', noTags: false, depth: 1, reference: '', shallow: true]],
+                                        submoduleCfg     : [],
+                                        doGenerateSubmoduleConfigurations: false,
+                                ])
+                            }
                     }
-                }
-                checkout([
-                        $class           : 'GitSCM',
-                        branches         : [[name: "${params.OSIA_GIT_BRANCH}"]],
-                        userRemoteConfigs: [[url: "${params.OSIA_GIT_REPOSITORY}",
-                                             credentialsId: "${params.GITLAB_CREDENTIALS}"]],
-                        extensions       : [[$class: 'CleanCheckout'],
-                                            [$class: 'RelativeTargetDirectory',
-                                             relativeTargetDir: 'OSIA-dbz']] +
-                                [[$class: 'CloneOption', noTags: false, depth: 1, reference: '', shallow: true]],
-                        submoduleCfg     : [],
-                        doGenerateSubmoduleConfigurations: false,
-                ])
-                script {
-                    withCredentials([
-                            sshUserPrivateKey(credentialsId: "${params.GITLAB_CREDENTIALS}", keyFileVariable: 'TMP_SSH_KEY_FILE')
-                    ]){
-                        sh '''
-                        git config --global user.name "Debezium CI"
-                        git config --global user.email "debezium-qe@redhat.com"
-                        cp ${TMP_SSH_KEY_FILE} SSH_KEY_FILE
-                        git config --add --global core.sshCommand "ssh -i $(readlink -f SSH_KEY_FILE)"
-                        cd "${WORKSPACE}/OSIA-dbz/"
-                        git checkout ${OSIA_GIT_BRANCH}
-                        '''
+                    script {
+                        withCredentials([
+                                sshUserPrivateKey(credentialsId: "${env.GITLAB_CREDENTIALS}", keyFileVariable: 'TMP_SSH_KEY_FILE')
+                        ]){
+                            sh '''
+                                cd "${WORKSPACE}/OSIA-dbz/"
+                                ls
+                                git status
+                                git config --local user.name "Debezium CI"
+                                git config --local user.email "debezium-qe@redhat.com"
+                                cp ${TMP_SSH_KEY_FILE} SSH_KEY_FILE
+                                git config --add --local core.sshCommand "ssh -i $(readlink -f SSH_KEY_FILE)"
+                                git checkout ${OSIA_GIT_BRANCH}
+                            '''
+                        }
                     }
                 }
             }
         }
-
+        stage("Setup AWS credentials") {
+            steps {
+                container("debezium-ci-tools") {
+                    withCredentials([string(credentialsId: "awscredentials", variable: "AWS_SHARED_CREDENTIALS_FILE")]) {
+                        sh(script: "mkdir -p /home/jenkins/.aws")
+                        sh(script: "echo -e \"${AWS_SHARED_CREDENTIALS_FILE}\" > /home/jenkins/.aws/credentials")
+                    }
+                }
+            }
+        }
         stage('Deploy_cluster') {
             when() {
                 expression { !params.REMOVE_CLUSTER }
             }
             steps {
-                withCredentials([
-                        string(credentialsId: "${params.ANSIBLE_VAULT_PASSWORD}", variable: 'ANSIBLE_PASSWORD')
-                ]) {
-                    sh '''
-                    set -ex
-                    cd "${WORKSPACE}/OSIA-dbz/secrets"
-                    echo "${ANSIBLE_PASSWORD}" > ../password.txt
-                    ansible-vault decrypt --vault-password-file ../password.txt *
-                    cd ..
-                    mv ./secrets/* ./
-                    osia install --cluster-name ${CLUSTER_NAME} --cloud aws --installer-version ${INSTALLER_VERSION} --cloud-env dbz-aws
-                    export KUBECONFIG="${WORKSPACE}/OSIA-dbz/${CLUSTER_NAME}/auth/kubeconfig"
-                    oc create secret generic htpass-secret --from-file=htpasswd=ocp-users.htpasswd -n openshift-config
-                    oc apply -f htpasswd.cr.yaml -n openshift-config
-                    oc adm policy add-cluster-role-to-user cluster-admin debezium
-                    '''
+                container("debezium-ci-tools") {
+                    withCredentials([
+                            string(credentialsId: "${env.ANSIBLE_VAULT_PASSWORD}", variable: 'ANSIBLE_PASSWORD')
+                    ]) {
+                        sh '''
+                            set -ex
+                            cd "${WORKSPACE}/OSIA-dbz/secrets"
+                            echo "${ANSIBLE_PASSWORD}" > ../password.txt
+                            ansible-vault decrypt --vault-password-file ../password.txt *
+                            cd ..
+                            mv ./secrets/* ./
+                            export AWS_SHARED_CREDENTIALS_FILE=/home/jenkins/.aws/credentials && osia install --cluster-name ${CLUSTER_NAME} --cloud aws --installer-version ${INSTALLER_VERSION} --cloud-env dbz-aws
+                            export KUBECONFIG="${WORKSPACE}/OSIA-dbz/${CLUSTER_NAME}/auth/kubeconfig"
+                            oc create secret generic htpass-secret --from-file=htpasswd=ocp-users.htpasswd -n openshift-config
+                            oc apply -f htpasswd.cr.yaml -n openshift-config
+                            oc adm policy add-cluster-role-to-user cluster-admin debezium
+                        '''
+                    }
                 }
             }
         }
@@ -78,18 +91,20 @@ pipeline {
                 expression { params.REMOVE_CLUSTER }
             }
             steps {
-                withCredentials([
-                        string(credentialsId: "${params.ANSIBLE_VAULT_PASSWORD}", variable: 'ANSIBLE_PASSWORD')
-                ]) {
-                    sh '''
-                    set -ex
-                    cd "${WORKSPACE}/OSIA-dbz/secrets"
-                    echo "${ANSIBLE_PASSWORD}" > ../password.txt
-                    ansible-vault decrypt --vault-password-file ../password.txt *
-                    cd ..
-                    mv ./secrets/* ./
-                    osia clean --cluster-name ${CLUSTER_NAME} --installer-version ${INSTALLER_VERSION}
-                    '''
+                container("debezium-ci-tools") {
+                    withCredentials([
+                            string(credentialsId: "${env.ANSIBLE_VAULT_PASSWORD}", variable: 'ANSIBLE_PASSWORD')
+                    ]) {
+                        sh '''
+                            set -ex
+                            cd "${WORKSPACE}/OSIA-dbz/secrets"
+                            echo "${ANSIBLE_PASSWORD}" > ../password.txt
+                            ansible-vault decrypt --vault-password-file ../password.txt *
+                            cd ..
+                            mv ./secrets/* ./
+                            export AWS_SHARED_CREDENTIALS_FILE=/home/jenkins/.aws/credentials && osia clean --cluster-name ${CLUSTER_NAME} --installer-version ${INSTALLER_VERSION}
+                        '''
+                    }
                 }
             }
         }
@@ -98,8 +113,8 @@ pipeline {
     post {
         always {
             mail to: 'debezium-qe@redhat.com', subject: "OCP cluster deployment/removal #${env.BUILD_NUMBER} finished", body: """
-${currentBuild.projectName} run ${env.BUILD_URL} finished with result: ${currentBuild.currentResult}
-"""
+                ${currentBuild.projectName} run ${env.BUILD_URL} finished with result: ${currentBuild.currentResult}
+            """
         }
     }
 }
