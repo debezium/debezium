003bef0736708ffa20527118200b3a1433515010
harveyyue <yw_yuewei@126.com>
2022-11-24 14:43:38 +0800

    DBZ-5743 Introduce a new unicode topic naming strategy

---
+++ debezium-core/src/main/java/io/debezium/schema/AbstractUnicodeTopicNamingStrategy.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.schema;
+
+import java.util.Properties;
+
+import io.debezium.common.annotation.Incubating;
+import io.debezium.spi.schema.DataCollectionId;
+import io.debezium.spi.topic.TopicNamingStrategy;
+
+/**
+ * An abstract unicode converter topic naming strategy implementation of {@link TopicNamingStrategy}.
+ * Any non encodable character or underscore would be encoded via _uxxxx sequence in the same way as Java works,
+ * and the underscore is an escape sequence like backslash in Java.
+ *
+ * @author Harvey Yue
+ */
+@Incubating
+public abstract class AbstractUnicodeTopicNamingStrategy extends AbstractTopicNamingStrategy<DataCollectionId> {
+
+    private static final ReplacementFunction UNICODE_REPLACEMENT = c -> {
+        String hex = Integer.toHexString(c);
+        if (hex.length() <= 2) {
+            hex = "00" + hex;
+        }
+        // Use underscore as escape sequence instead of backslash
+        return "_u" + hex;
+    };
+
+    public AbstractUnicodeTopicNamingStrategy(Properties props) {
+        super(props);
+        replacement = UNICODE_REPLACEMENT;
+    }
+
+    // The underscore is an escape sequence like backslash in Java, so need to convert it to corresponding unicode.
+    @Override
+    public boolean isValidCharacter(char c) {
+        return c == '.' || c == '-' || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+}
