9f0224526a173df31037d8a537f0b7a0703084a4
Erdinç Taşkın <erdinctaskin@gmail.com>
2022-11-25 23:01:27 +0300

    Remove nested if check
    Simplify to flow. 
    - Remove nested if check
    - Postpone possible unused stamement which is connection.isBinlogRowImageFull()

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlConnectorTask.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlConnectorTask.java
@@ -232,20 +232,17 @@ public class MySqlConnectorTask extends BaseSourceTask<MySqlPartition, MySqlOffs
         if (config.getSnapshotMode().shouldStream()) {
             // Check whether the row-level binlog is enabled ...
             final boolean binlogFormatRow = connection.isBinlogFormatRow();
-            final boolean binlogRowImageFull = connection.isBinlogRowImageFull();
-            final boolean rowBinlogEnabled = binlogFormatRow && binlogRowImageFull;
+            if (!binlogFormatRow) {
+                throw new DebeziumException("The MySQL server is not configured to use a ROW binlog_format, which is "
+                        + "required for this connector to work properly. Change the MySQL configuration to use a "
+                        + "binlog_format=ROW and restart the connector.");
+            }            
 
-            if (!rowBinlogEnabled) {
-                if (!binlogFormatRow) {
-                    throw new DebeziumException("The MySQL server is not configured to use a ROW binlog_format, which is "
-                            + "required for this connector to work properly. Change the MySQL configuration to use a "
-                            + "binlog_format=ROW and restart the connector.");
-                }
-                else {
-                    throw new DebeziumException("The MySQL server is not configured to use a FULL binlog_row_image, which is "
-                            + "required for this connector to work properly. Change the MySQL configuration to use a "
-                            + "binlog_row_image=FULL and restart the connector.");
-                }
+            final boolean binlogRowImageFull = connection.isBinlogRowImageFull();
+            if (!binlogRowImageFull) {
+                throw new DebeziumException("The MySQL server is not configured to use a FULL binlog_row_image, which is "
+                        + "required for this connector to work properly. Change the MySQL configuration to use a "
+                        + "binlog_row_image=FULL and restart the connector.");
             }
         }
     }
