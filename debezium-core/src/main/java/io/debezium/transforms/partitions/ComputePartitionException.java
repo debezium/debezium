4c941b63edc43d1d8324bae04d656a16fcde3269
mfvitale <mvitale86@gmail.com>
2022-11-30 11:04:32 +0100

    DBZ-5847: Add support for all connectors
    Support for qualified table names

--- debezium-core/src/main/java/io/debezium/transforms/partitions/ComputePartitionException.java
+++ debezium-core/src/main/java/io/debezium/transforms/partitions/ComputePartitionException.java
@@ -4,9 +4,11 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.debezium.connector.mysql.transforms.partitions;
+package io.debezium.transforms.partitions;
 
-public class ComputePartitionException extends RuntimeException {
+import io.debezium.DebeziumException;
+
+public class ComputePartitionException extends DebeziumException {
     public ComputePartitionException() {
         super();
     }
