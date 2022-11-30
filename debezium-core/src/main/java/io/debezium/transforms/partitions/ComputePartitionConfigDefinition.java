d305f8f87a7c9369ed3c5913872c928e5a829d10
mfvitale <mvitale86@gmail.com>
2022-11-30 11:04:32 +0100

    DBZ-5847: Add support for all connectors
    Support for qualified table names

--- debezium-core/src/main/java/io/debezium/transforms/partitions/ComputePartitionConfigDefinition.java
+++ debezium-core/src/main/java/io/debezium/transforms/partitions/ComputePartitionConfigDefinition.java
@@ -4,54 +4,51 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.debezium.connector.mysql.transforms.partitions;
+package io.debezium.transforms.partitions;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.kafka.common.config.ConfigDef;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.debezium.config.Configuration;
 import io.debezium.config.Field;
 
-public class ComputePartitionConf {
+public class ComputePartitionConfigDefinition {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComputePartitionConfigDefinition.class);
 
     public static final String MAPPING_SEPARATOR = ":";
     public static final String LIST_SEPARATOR = ",";
 
-    private ComputePartitionConf() { // intentionally blank
+    private ComputePartitionConfigDefinition() { // intentionally blank
     }
 
-    public static final String FIELD_TABLE_FIELD_NAME_MAPPINGS_CONF = "partition.table.field.mappings";
-    public static final String FIELD_TABLE_PARTITION_NUM_MAPPINGS_CONF = "partition.table.partition.num.mappings";
-    public static final String FIELD_TABLE_LIST_CONF = "partition.table.list";
-
-    static final Field PARTITION_TABLE_LIST_FIELD = Field.create(FIELD_TABLE_LIST_CONF)
-            .withDisplayName("List of the tables that activate the SMT")
-            .withType(ConfigDef.Type.LIST)
-            .withWidth(ConfigDef.Width.MEDIUM)
-            .withImportance(ConfigDef.Importance.MEDIUM)
-            .withDescription("Comma separated list of table names that will be considered for the transformation");
-
+    public static final String FIELD_TABLE_FIELD_NAME_MAPPINGS_CONF = "partition.data-collections.field.mappings";
+    public static final String FIELD_TABLE_PARTITION_NUM_MAPPINGS_CONF = "partition.data-collections.partition.num.mappings";
     static final Field PARTITION_TABLE_FIELD_NAME_MAPPINGS_FIELD = Field.create(FIELD_TABLE_FIELD_NAME_MAPPINGS_CONF)
-            .withDisplayName("Table field mapping")
+            .withDisplayName("Data collection field mapping")
             .withType(ConfigDef.Type.STRING)
-            .withValidation(ComputePartitionConf::isValidMapping)
+            .withValidation(ComputePartitionConfigDefinition::isValidMapping)
             .withWidth(ConfigDef.Width.MEDIUM)
             .withImportance(ConfigDef.Importance.MEDIUM)
-            .withDescription("comma-separated list of colon-delimited table field pairs, e.g. products:name,orders:purchaser");
+            .withDescription("Comma-separated list of colon-delimited data collection field pairs, e.g. inventory.products:name,inventory.orders:purchaser");
 
     static final Field FIELD_TABLE_PARTITION_NUM_MAPPINGS_FIELD = Field.create(FIELD_TABLE_PARTITION_NUM_MAPPINGS_CONF)
-            .withDisplayName("Table number of partition mapping")
+            .withDisplayName("Data collection number of partition mapping")
             .withType(ConfigDef.Type.STRING)
-            .withValidation(ComputePartitionConf::isValidMapping)
+            .withValidation(ComputePartitionConfigDefinition::isValidMapping)
             .withWidth(ConfigDef.Width.MEDIUM)
             .withImportance(ConfigDef.Importance.MEDIUM)
-            .withDescription("comma-separated list of colon-delimited table partition number pairs, e.g. products:2,orders:3");
+            .withDescription("Comma-separated list of colon-delimited data-collections partition number pairs, e.g. inventory.products:2,inventory.orders:3");
 
     static Map<String, String> parseMappings(List<String> mappings) {
+
         final Map<String, String> m = new HashMap<>();
+
         for (String mapping : mappings) {
             final String[] parts = mapping.split(MAPPING_SEPARATOR);
             if (parts.length != 2) {
@@ -63,21 +60,24 @@ public class ComputePartitionConf {
     }
 
     public static int isValidMapping(Configuration config, Field field, Field.ValidationOutput problems) {
+
         List<String> values = config.getStrings(field, LIST_SEPARATOR);
         try {
             parseMappings(values);
-            return 0;
         }
         catch (Exception e) {
+            LOGGER.error(String.format("Error while parsing values %s", values), e);
+            problems.accept(field, values, "Problem parsing list of colon-delimited pairs, e.g. <code>foo:bar,abc:xyz</code>");
+            return 1;
         }
-        problems.accept(field, values, "list of colon-delimited pairs, e.g. <code>foo:bar,abc:xyz</code>");
-        return 1;
+        return 0;
     }
 
     static Map<String, Integer> parseIntMappings(List<String> mappings) {
+
         final Map<String, Integer> m = new HashMap<>();
         for (String mapping : mappings) {
-            final String[] parts = mapping.split(":");
+            final String[] parts = mapping.split(MAPPING_SEPARATOR);
             if (parts.length != 2) {
                 throw new ComputePartitionException("Invalid mapping: " + mapping);
             }
@@ -86,7 +86,7 @@ public class ComputePartitionConf {
                 m.put(parts[0], value);
             }
             catch (NumberFormatException e) {
-                throw new ComputePartitionException("Invalid mapping value: " + e.getMessage());
+                throw new ComputePartitionException(String.format("Invalid mapping value: %s", parts[1]), e);
             }
         }
         return m;
