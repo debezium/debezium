63097f4730a4d5617fe5d4271b41f4ac2a623b52
Ondrej Babec <ond.babec@gmail.com>
2022-12-06 09:48:29 +0100

    DBZ-5905 Reduce job footprint in Jenkins

--- jenkins-jobs/job-dsl/connector_mysql_matrix_test.groovy
+++ jenkins-jobs/job-dsl/connector_mysql_matrix_test.groovy
@@ -41,8 +41,7 @@ matrixJob('connector-debezium-mysql-matrix-test') {
 
     publishers {
         archiveArtifacts {
-            pattern('**/target/surefire-reports/*.xml')
-            pattern('**/target/failsafe-reports/*.xml')
+            pattern('**/archive.tar.gz')
         }
         archiveJunit('**/target/surefire-reports/*.xml')
         archiveJunit('**/target/failsafe-reports/*.xml')
@@ -51,7 +50,7 @@ matrixJob('connector-debezium-mysql-matrix-test') {
 
     logRotator {
         daysToKeep(7)
-        numToKeep(10)
+        numToKeep(5)
     }
 
     steps {
@@ -93,6 +92,7 @@ mkdir -p $RESULTS_PATH
 cp **/target/surefire-reports/*.xml $RESULTS_PATH
 cp **/target/failsafe-reports/*.xml $RESULTS_PATH
 rm -rf $RESULTS_PATH/failsafe-summary.xml
+tar czf archive.tar.gz $RESULTS_PATH
 
 docker login quay.io -u "$QUAY_USERNAME" -p "$QUAY_PASSWORD"
 
