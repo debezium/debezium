82643e9d26436a7342b2d63874306ebaf196c9c4
jcechace <jcechace@gmail.com>
2022-12-11 21:16:44 +0100

    DBZ-5857 Fake DNS resolver allowing the use of MongoDB containers under Docker Desktop without hots file modifications

--- debezium-testing/debezium-testing-testcontainers/src/test/java/io/debezium/testing/testcontainers/MongoDbShardedClusterTest.java
+++ debezium-testing/debezium-testing-testcontainers/src/test/java/io/debezium/testing/testcontainers/MongoDbShardedClusterTest.java
@@ -13,6 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.assertj.core.api.ListAssert;
 import org.bson.Document;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,12 +26,24 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 
+import io.debezium.testing.testcontainers.util.DockerUtils;
+
 /**
  * @see <a href="https://issues.redhat.com/browse/DBZ-5857">DBZ-5857</a>
  */
 public class MongoDbShardedClusterTest {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(MongoDbShardedClusterTest.class);
+
+    @BeforeAll
+    static void setupAll() {
+        DockerUtils.enableFakeDnsIfRequired();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        DockerUtils.disableFakeDns();
+    }
 
     @Test
     public void testCluster() {
