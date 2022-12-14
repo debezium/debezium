60910872b9a8395942821dcec727b1c255a98f05
jcechace <jcechace@gmail.com>
2022-12-14 12:09:03 +0100

    DBZ-5857 Using single member replica as default

--- debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/junit/MongoDbDatabaseProvider.java
+++ debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/junit/MongoDbDatabaseProvider.java
@@ -10,6 +10,7 @@ import io.debezium.testing.testcontainers.util.ParsingPortResolver;
 
 public final class MongoDbDatabaseProvider {
 
+    public static final String MONGO_REPLICA_SIZE = "mongodb.replica.size";
     public static final String MONGO_DOCKER_DESKTOP_PORT_PROPERTY = "mongodb.docker.desktop.ports";
 
     // Should be aligned with definition in pom.xm
@@ -23,9 +24,10 @@ public final class MongoDbDatabaseProvider {
     public static MongoDbReplicaSet mongoDbReplicaSet() {
         // will be used only in environment with docker desktop
         var portResolver = ParsingPortResolver.parseProperty(MONGO_DOCKER_DESKTOP_PORT_PROPERTY, MONGO_DOCKER_DESKTOP_PORT_DEFAULT);
+        var replicaSize = Integer.parseInt(System.getProperty(MONGO_REPLICA_SIZE, "1"));
 
         return MongoDbReplicaSet.replicaSet()
-                .memberCount(3)
+                .memberCount(replicaSize)
                 .portResolver(portResolver)
                 .build();
     }
