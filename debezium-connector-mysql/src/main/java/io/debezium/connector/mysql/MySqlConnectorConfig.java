d503d6876a8bb0fe67e3166943d9fdb6199e6db6
Jiri Pechanec <jpechane@redhat.com>
2023-01-31 12:37:51 +0100

    DBZ-6059 Revert "DBZ-4663 Remove option for specifying driver class from MySQL Connector"
    This reverts commit 115d7c2d816fd7d644f73c5e88fbd3e0a26552ea.

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlConnectorConfig.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlConnectorConfig.java
@@ -613,6 +613,16 @@ public class MySqlConnectorConfig extends HistorizedRelationalDatabaseConnectorC
     public static final Field TABLES_IGNORE_BUILTIN = RelationalDatabaseConnectorConfig.TABLE_IGNORE_BUILTIN
             .withDependents(DATABASE_INCLUDE_LIST_NAME);
 
+    public static final Field JDBC_DRIVER = Field.create("database.jdbc.driver")
+            .withDisplayName("Jdbc Driver Class Name")
+            .withType(Type.CLASS)
+            .withGroup(Field.createGroupEntry(Field.Group.CONNECTION, 41))
+            .withWidth(Width.MEDIUM)
+            .withDefault(com.mysql.cj.jdbc.Driver.class.getName())
+            .withImportance(Importance.LOW)
+            .withValidation(Field::isClassName)
+            .withDescription("JDBC Driver class name used to connect to the MySQL database server.");
+
     /**
      * A comma-separated list of regular expressions that match source UUIDs in the GTID set used to find the binlog
      * position in the MySQL server. Only the GTID ranges that have sources matching one of these include patterns will
@@ -867,7 +877,8 @@ public class MySqlConnectorConfig extends HistorizedRelationalDatabaseConnectorC
                     SSL_KEYSTORE,
                     SSL_KEYSTORE_PASSWORD,
                     SSL_TRUSTSTORE,
-                    SSL_TRUSTSTORE_PASSWORD)
+                    SSL_TRUSTSTORE_PASSWORD,
+                    JDBC_DRIVER)
             .connector(
                     CONNECTION_TIMEOUT_MS,
                     KEEP_ALIVE,
