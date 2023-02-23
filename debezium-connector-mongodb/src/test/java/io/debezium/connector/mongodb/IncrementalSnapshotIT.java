25f529e1ce9ada3cec007977afa1d049ef2293c4
Bin Huang <binh@stripe.com>
2023-02-23 13:36:03 -0500

    cherry-pick: DBZ-6116: Support String type for key in Mongo incremental snapshot (#44)
    cherry-pick: https://github.com/debezium/debezium/commit/a3d75e798ab667b8ab4b341d70998268b1f6e6a3
    
    r? shichao ywu
    
    (Squashed by Merge Queue - Original PR: https://git.corp.stripe.com/stripe-private-oss-forks/debezium/pull/44)

--- debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/IncrementalSnapshotIT.java
+++ debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/IncrementalSnapshotIT.java
@@ -17,6 +17,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
