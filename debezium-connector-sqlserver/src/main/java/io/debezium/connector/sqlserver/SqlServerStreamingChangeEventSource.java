45652e65cc9489af0a22e8a49453b376ace001cb
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2023-03-10 14:07:47 +0100

    DBZ-7043: Emit a notification when completed reading from a capture instance
    Let the database know when the connector has finished reading
    from a capture instance by calling a stored procedure.

--- debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerStreamingChangeEventSource.java
+++ debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerStreamingChangeEventSource.java
@@ -12,10 +12,14 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
@@ -28,6 +32,8 @@ import org.slf4j.LoggerFactory;
 import io.debezium.connector.sqlserver.SqlServerConnectorConfig.SnapshotMode;
 import io.debezium.pipeline.ErrorHandler;
 import io.debezium.pipeline.EventDispatcher;
+import io.debezium.pipeline.notification.Notification;
+import io.debezium.pipeline.notification.NotificationService;
 import io.debezium.pipeline.source.spi.StreamingChangeEventSource;
 import io.debezium.relational.ChangeTable;
 import io.debezium.relational.Table;
@@ -87,14 +93,17 @@ public class SqlServerStreamingChangeEventSource implements StreamingChangeEvent
 
     private final ElapsedTimeStrategy pauseBetweenCommits;
     private final Map<SqlServerPartition, SqlServerStreamingExecutionContext> streamingExecutionContexts;
+    private final Map<SqlServerPartition, Set<SqlServerChangeTable>> changeTablesWithKnownStopLsn = new HashMap<>();
 
     private boolean checkAgent;
     private SqlServerOffsetContext effectiveOffset;
+    private final NotificationService<SqlServerPartition, SqlServerOffsetContext> notificationService;
 
     public SqlServerStreamingChangeEventSource(SqlServerConnectorConfig connectorConfig, SqlServerConnection dataConnection,
                                                SqlServerConnection metadataConnection,
                                                EventDispatcher<SqlServerPartition, TableId> dispatcher,
-                                               ErrorHandler errorHandler, Clock clock, SqlServerDatabaseSchema schema) {
+                                               ErrorHandler errorHandler, Clock clock, SqlServerDatabaseSchema schema,
+                                               NotificationService<SqlServerPartition, SqlServerOffsetContext> notificationService) {
         this.connectorConfig = connectorConfig;
         this.dataConnection = dataConnection;
         this.metadataConnection = metadataConnection;
@@ -102,6 +111,7 @@ public class SqlServerStreamingChangeEventSource implements StreamingChangeEvent
         this.errorHandler = errorHandler;
         this.clock = clock;
         this.schema = schema;
+        this.notificationService = notificationService;
         this.pollInterval = connectorConfig.getPollInterval();
         final Duration intervalBetweenCommitsBasedOnPoll = this.pollInterval.multipliedBy(INTERVAL_BETWEEN_COMMITS_BASED_ON_POLL_FACTOR);
         this.pauseBetweenCommits = ElapsedTimeStrategy.constant(clock,
@@ -211,9 +221,11 @@ public class SqlServerStreamingChangeEventSource implements StreamingChangeEvent
                             schemaChangeCheckpoints.add(table);
                         }
                     }
+                    collectChangeTablesWithKnownStopLsn(partition, tables);
                 }
                 if (tablesSlot.get() == null) {
                     tablesSlot.set(getChangeTablesToQuery(partition, offsetContext, toLsn));
+                    collectChangeTablesWithKnownStopLsn(partition, tablesSlot.get());
                 }
                 try {
                     dataConnection.getChangesForTables(databaseName, tablesSlot.get(), fromLsn, toLsn, resultSets -> {
@@ -350,6 +362,21 @@ public class SqlServerStreamingChangeEventSource implements StreamingChangeEvent
         return effectiveOffset;
     }
 
+    private void collectChangeTablesWithKnownStopLsn(SqlServerPartition partition, SqlServerChangeTable[] tables) {
+        for (SqlServerChangeTable table : tables) {
+            if (table.getStopLsn().isAvailable()) {
+                synchronized (changeTablesWithKnownStopLsn) {
+                    if (!changeTablesWithKnownStopLsn.containsKey(partition)) {
+                        changeTablesWithKnownStopLsn.put(partition, new HashSet<>());
+                    }
+
+                    LOGGER.info("The stop lsn of {} change table became known", table);
+                    changeTablesWithKnownStopLsn.get(partition).add(table);
+                }
+            }
+        }
+    }
+
     private void commitTransaction() throws SQLException {
         // When reading from read-only Always On replica the default and only transaction isolation
         // is snapshot. This means that CDC metadata are not visible for long-running transactions.
@@ -482,5 +509,50 @@ public class SqlServerStreamingChangeEventSource implements StreamingChangeEvent
         }
 
         return connection.getNthTransactionLsnFromLast(databaseName, fromLsn, maxTransactionsPerIteration);
+    }
+
+    @Override
+    public void commitOffset(Map<String, ?> sourcePartition, Map<String, ?> offset) {
+        Lsn commitLsn = Lsn.valueOf((String) offset.get("commit_lsn"));
+        synchronized (changeTablesWithKnownStopLsn) {
+            Optional<SqlServerPartition> optionalPartition = changeTablesWithKnownStopLsn.keySet().stream()
+                    .filter(p -> p.getSourcePartition().equals(sourcePartition))
+                    .findFirst();
+
+            if (optionalPartition.isEmpty()) {
+                return;
+            }
+
+            SqlServerPartition partition = optionalPartition.get();
+            Set<SqlServerChangeTable> partitionTables = changeTablesWithKnownStopLsn.get(partition);
+
+            List<SqlServerChangeTable> changeTablesToCompleteReadingFrom = partitionTables.stream()
+                    .filter(t -> t.getStopLsn().compareTo(commitLsn) < 0)
+                    .collect(Collectors.toList());
+
+            for (SqlServerChangeTable table : changeTablesToCompleteReadingFrom) {
+                Map<String, String> additionalData = new HashMap<>();
+                additionalData.put("connector_name", connectorConfig.getLogicalName());
+                additionalData.put("capture_instance", table.getCaptureInstance());
+                additionalData.put("start_lsn", table.getStartLsn().toString());
+                additionalData.put("stop_lsn", table.getStopLsn().toString());
+                additionalData.put("commit_lsn", commitLsn.toString());
+                additionalData.putAll(partition.getSourcePartition());
+
+                notificationService.notify(Notification.Builder.builder()
+                        .withId(UUID.randomUUID().toString())
+                        .withAggregateType("Capture Instance")
+                        .withType("COMPLETED")
+                        .withAdditionalData(additionalData)
+                        .withTimestamp(clock.currentTimeInMillis())
+                        .build());
+
+                partitionTables.remove(table);
+
+                LOGGER.info(
+                        "Complete reading from change table {} as the committed change lsn ({}) is greater than the table's stop lsn ({})",
+                        table, offset, table.getStopLsn().toString());
+            }
+        }
     }
 }
--- debezium-connector-sqlserver/src/test/java/io/debezium/connector/sqlserver/NotificationsIT.java
+++ debezium-connector-sqlserver/src/test/java/io/debezium/connector/sqlserver/NotificationsIT.java
@@ -7,13 +7,26 @@
 package io.debezium.connector.sqlserver;
 
 import java.sql.SQLException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Percentage;
+import org.awaitility.Awaitility;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
+import io.debezium.config.CommonConnectorConfig;
 import io.debezium.config.Configuration;
 import io.debezium.connector.sqlserver.util.TestHelper;
 import io.debezium.pipeline.notification.AbstractNotificationsIT;
+import io.debezium.pipeline.notification.channels.SinkNotificationChannel;
 import io.debezium.util.Testing;
 
 public class NotificationsIT extends AbstractNotificationsIT<SqlServerConnector> {
@@ -74,5 +87,84 @@ public class NotificationsIT extends AbstractNotificationsIT<SqlServerConnector>
     @Override
     protected String snapshotStatusResult() {
         return "COMPLETED";
+    }
+
+    @Test
+    public void completeReadingFromACaptureInstanceNotificationEmitted() throws SQLException {
+        startConnector(config -> config
+                .with(SinkNotificationChannel.NOTIFICATION_TOPIC, "io.debezium.notification")
+                .with(CommonConnectorConfig.NOTIFICATION_ENABLED_CHANNELS, "sink"));
+
+        assertConnectorIsRunning();
+
+        TestHelper.waitForStreamingStarted();
+
+        SqlServerConnection connection = TestHelper.testConnection();
+        connection.execute("INSERT INTO tablea VALUES(2, 'b')");
+        connection.execute("ALTER TABLE tablea ADD colb int NULL");
+        TestHelper.enableTableCdc(connection, "tablea", "tablea_c2");
+        connection.execute("INSERT INTO tablea VALUES(3, 'c', 3)");
+        connection.close();
+
+        List<SourceRecord> notifications = new ArrayList<>();
+        Awaitility.await().atMost(60, TimeUnit.SECONDS).until(() -> {
+            consumeAvailableRecords(r -> {
+                if (r.topic().equals("io.debezium.notification")) {
+                    notifications.add(r);
+                }
+            });
+            return notifications.size() == 3;
+        });
+
+        Assertions.assertThat(notifications).hasSize(3);
+        SourceRecord sourceRecord = notifications.get(2);
+        Assertions.assertThat(sourceRecord.topic()).isEqualTo("io.debezium.notification");
+        Struct value = (Struct) sourceRecord.value();
+        Assertions.assertThat(value.getString("aggregate_type")).isEqualTo("Capture Instance");
+        Assertions.assertThat(value.getString("type")).isEqualTo("COMPLETED");
+        Assertions.assertThat(value.getInt64("timestamp")).isCloseTo(Instant.now().toEpochMilli(), Percentage.withPercentage(1));
+        Map<String, String> additionalData = value.getMap("additional_data");
+        Assertions.assertThat(additionalData.get("server")).isEqualTo("server1");
+        Assertions.assertThat(additionalData.get("database")).isEqualTo(TestHelper.TEST_DATABASE_1);
+        Assertions.assertThat(additionalData.get("capture_instance")).isEqualTo("dbo_tablea");
+        Lsn startLsn = Lsn.valueOf(additionalData.get("start_lsn"));
+        Lsn stopLsn = Lsn.valueOf(additionalData.get("stop_lsn"));
+        Lsn commitLsn = Lsn.valueOf(additionalData.get("commit_lsn"));
+        Assertions.assertThat(startLsn).isLessThan(stopLsn);
+        Assertions.assertThat(stopLsn).isLessThan(commitLsn);
+
+        connection = TestHelper.testConnection();
+        connection.execute("EXEC sys.sp_cdc_disable_table @source_schema = N'dbo', @source_name = N'tablea', @capture_instance = 'dbo_tablea'");
+        connection.execute("ALTER TABLE tablea ADD colc int NULL");
+        TestHelper.enableTableCdc(connection, "tablea", "tablea_c3");
+        connection.execute("INSERT INTO tablea VALUES(4, 'c', 4, 4)");
+        connection.close();
+
+        List<SourceRecord> notifications2 = new ArrayList<>();
+        Awaitility.await().atMost(60, TimeUnit.SECONDS).until(() -> {
+            consumeAvailableRecords(r -> {
+                if (r.topic().equals("io.debezium.notification")) {
+                    notifications2.add(r);
+                }
+            });
+            return notifications2.size() == 1;
+        });
+
+        Assertions.assertThat(notifications2).hasSize(1);
+        sourceRecord = notifications2.get(0);
+        Assertions.assertThat(sourceRecord.topic()).isEqualTo("io.debezium.notification");
+        value = (Struct) sourceRecord.value();
+        Assertions.assertThat(value.getString("aggregate_type")).isEqualTo("Capture Instance");
+        Assertions.assertThat(value.getString("type")).isEqualTo("COMPLETED");
+        Assertions.assertThat(value.getInt64("timestamp")).isCloseTo(Instant.now().toEpochMilli(), Percentage.withPercentage(1));
+        additionalData = value.getMap("additional_data");
+        Assertions.assertThat(additionalData.get("server")).isEqualTo("server1");
+        Assertions.assertThat(additionalData.get("database")).isEqualTo(TestHelper.TEST_DATABASE_1);
+        Assertions.assertThat(additionalData.get("capture_instance")).isEqualTo("tablea_c2");
+        startLsn = Lsn.valueOf(additionalData.get("start_lsn"));
+        stopLsn = Lsn.valueOf(additionalData.get("stop_lsn"));
+        commitLsn = Lsn.valueOf(additionalData.get("commit_lsn"));
+        Assertions.assertThat(startLsn).isLessThan(stopLsn);
+        Assertions.assertThat(stopLsn).isLessThan(commitLsn);
     }
 }
