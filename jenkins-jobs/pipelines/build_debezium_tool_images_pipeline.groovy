cb602df8c7162188036c6cf35199282c8c6f3a63
Vojtech Juranek <vjuranek@redhat.com>
2023-03-29 09:34:59 +0200

    DBZ-6216 Revert removing Docker credentials
    This reverts commit d38f9444c4feaf339c712a1546c35e59697e7131.
    
    As the Docker re-evalute thei decision not to provide free organizations
    accounts, we will continue to publish images to Docker Hub for some
    time.

--- jenkins-jobs/pipelines/build_debezium_tool_images_pipeline.groovy
+++ jenkins-jobs/pipelines/build_debezium_tool_images_pipeline.groovy
@@ -3,6 +3,7 @@ import java.util.*
 
 IMAGES_DIR = 'images'
 GIT_CREDENTIALS_ID = 'debezium-github'
+DOCKER_CREDENTIALS_ID = 'debezium-dockerhub'
 QUAYIO_CREDENTIALS_ID = 'debezium-quay'
 
 node('Slave') {
@@ -19,6 +20,11 @@ node('Slave') {
                       userRemoteConfigs                : [[url: "https://$params.IMAGES_REPOSITORY", credentialsId: GIT_CREDENTIALS_ID]]
             ]
             )
+            withCredentials([usernamePassword(credentialsId: DOCKER_CREDENTIALS_ID, passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                sh """
+                    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+                """
+            }
             withCredentials([string(credentialsId: QUAYIO_CREDENTIALS_ID, variable: 'USERNAME_PASSWORD')]) {
                 def credentials = USERNAME_PASSWORD.split(':')
                 sh """
