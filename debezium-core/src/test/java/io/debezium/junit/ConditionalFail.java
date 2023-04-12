f64c35de29a579fa2589e090e8600c618a99ded0
Jiri Pechanec <jpechane@redhat.com>
2023-04-12 08:40:02 +0200

    DBZ-6324 Provide @Flaky annotation for CI unstable tests

--- debezium-core/src/test/java/io/debezium/junit/ConditionalFail.java
+++ debezium-core/src/test/java/io/debezium/junit/ConditionalFail.java
@@ -9,8 +9,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.function.Supplier;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * JUnit rule that inspects the presence of the {@link ShouldFailWhen} annotation on a test method.
@@ -21,12 +24,26 @@ import org.junit.runners.model.Statement;
  */
 public class ConditionalFail extends AnnotationBasedTestRule {
 
+    private static final Logger FLAKY_LOGGER = LoggerFactory.getLogger(Flaky.class);
+
+    private static final String JIRA_BASE_URL = "https://issues.jboss.org/browse/";
+
     @Override
     public Statement apply(final Statement base, final Description description) {
         final ShouldFailWhen conditionClass = hasAnnotation(description, ShouldFailWhen.class);
-        if (conditionClass == null) {
-            return base;
+        if (conditionClass != null) {
+            return failOnCondition(base, description, conditionClass);
         }
+
+        final Flaky flakyClass = hasAnnotation(description, Flaky.class);
+        if (flakyClass != null) {
+            return ignoreFlakyFailure(base, description, flakyClass);
+        }
+        return base;
+    }
+
+    private Statement failOnCondition(final Statement base, final Description description,
+                                      final ShouldFailWhen conditionClass) {
         try {
             Supplier<Boolean> condition = conditionClass.value().getDeclaredConstructor().newInstance();
             return new Statement() {
@@ -54,5 +71,32 @@ public class ConditionalFail extends AnnotationBasedTestRule {
         catch (final InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private Statement ignoreFlakyFailure(final Statement base, final Description description,
+                                         final Flaky flakyClass) {
+
+        final String flakyFailuresProperty = System.getProperty(Flaky.IGNORE_FLAKY_FAILURES_PROPERTY);
+        if (flakyFailuresProperty == null || !Boolean.valueOf(flakyFailuresProperty)) {
+            return base;
+        }
+
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                try {
+                    base.evaluate();
+                }
+                catch (final Throwable t) {
+                    FLAKY_LOGGER.error("Ignored failure for {}, tracked with {}", description, issueUrl(flakyClass.value()), t);
+                    // Marks test as skipped
+                    Assume.assumeTrue(String.format("Flaky test %s#%s failed", description.getTestClass().getSimpleName(), description.getMethodName()), false);
+                }
+            }
+        };
+    }
+
+    private String issueUrl(String jiraId) {
+        return JIRA_BASE_URL + jiraId;
     }
 }
