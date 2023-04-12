c9c415b90c0d46aee375aca1e4289c3cc0d0cb74
Jiri Pechanec <jpechane@redhat.com>
2023-04-12 08:40:02 +0200

    DBZ-6324 Provide @Flaky annotation for CI unstable tests

---
+++ debezium-core/src/test/java/io/debezium/junit/Flaky.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.junit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marker annotation used together with the {@link ConditionalFail} JUnit rule, that allows ignoring
+ * failures of tests know to be unstable on CI, using the {@code ignoreFlakyFailures} system property.
+ *
+ * @author Jiri Pechanec
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface Flaky {
+
+    String IGNORE_FLAKY_FAILURES_PROPERTY = "ignoreFlakyFailures";
+
+    /**
+     * The Jira id of the issue tracking the failing test
+     */
+    String value();
+}
