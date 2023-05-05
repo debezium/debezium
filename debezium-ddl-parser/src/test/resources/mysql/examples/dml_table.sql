f0810be6b0aef3f69e5c850557cbfe3f67e3ed3b
caicancai <2356672992@qq.com>
2023-05-05 20:58:42 +0800

    DBZ-6435 MySql in debezium-parser-ddl does not support table keyword parsing

--- debezium-ddl-parser/src/test/resources/mysql/examples/dml_table.sql
+++ debezium-ddl-parser/src/test/resources/mysql/examples/dml_table.sql
@@ -1,12 +1,8 @@
 ###https://dev.mysql.com/doc/refman/8.0/en/table.html
 #begin
+--###https://dev.mysql.com/doc/refman/8.0/en/table.html
 TABLE t;
-#end
-
-#begin
+TABLE t ORDER BY b LIMIT 3;
 TABLE t LIMIT 3;
 #end
 
-#begin
-TABLE t ORDER BY b LIMIT 3;
-#end
