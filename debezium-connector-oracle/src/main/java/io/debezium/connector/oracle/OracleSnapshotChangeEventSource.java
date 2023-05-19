d5a30957172eb3e445fd46e3c1fa4c44bd84747c
Jiri Pechanec <jpechane@redhat.com>
2023-05-19 13:26:00 +0200

    DBZ-6457 Fix formatting

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleSnapshotChangeEventSource.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleSnapshotChangeEventSource.java
@@ -98,7 +98,8 @@ public class OracleSnapshotChangeEventSource extends RelationalSnapshotChangeEve
 
     @Override
     protected void connectionPoolConnectionCreated(RelationalSnapshotContext<OraclePartition, OracleOffsetContext> snapshotContext,
-                                                   JdbcConnection connection) throws SQLException {
+                                                   JdbcConnection connection)
+            throws SQLException {
         if (connectorConfig.getPdbName() != null) {
             ((OracleConnection) connection).setSessionToPdb(connectorConfig.getPdbName());
         }
