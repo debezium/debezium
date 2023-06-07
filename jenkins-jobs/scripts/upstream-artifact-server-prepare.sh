a8af7d468070eb03cee8e944d15f517a87bbfb5c
Martin Medek <mmedek@redhat.com>
2023-06-07 16:20:06 +0200

    DBZ-6535 removed upstream strimzi prepare job and deleted whole strimzi.kc.build=false option from testsuite, only artifact server option remains

--- jenkins-jobs/scripts/upstream-artifact-server-prepare.sh
+++ jenkins-jobs/scripts/upstream-artifact-server-prepare.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DOCKER_FILE=${DIR}/../docker/Dockerfile.Strimzi
+DOCKER_FILE=${DIR}/../docker/artifact-server/Dockerfile
 PLUGIN_DIR="plugins"
 
-OPTS=$(getopt -o d:f:r:o:t:a: --long dir:,dockerfile:,registry:,organisation:,tags:,auto-tag:,kc-base-tag:,kafka-version:,apicurio-version:,dest-login:,dest-pass:,img-output:,oracle-included:,maven-repo: -n 'parse-options' -- "$@")
+
+OPTS=$(getopt -o d:f:r:o:t:a: --long dir:,dockerfile:,registry:,organisation:,tags:,auto-tag:,dest-login:,dest-pass:,img-output:,oracle-included:,maven-repo: -n 'parse-options' -- "$@")
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 eval set -- "$OPTS"
 
@@ -17,9 +18,6 @@ while true; do
     -o | --organisation )       ORGANISATION=$2;                    shift; shift ;;
     -t | --tags )               TAGS=$2;                            shift; shift ;;
     -a | --auto-tag )           AUTO_TAG=$2;                        shift; shift ;;
-    --kc-base-tag )             CONNECT_BASE_TAG=$2;                shift; shift ;;
-    --kafka-version )           KAFKA_VERSION=$2;                   shift; shift ;;
-    --apicurio-version )        APICURIO_VERSION=$2;                shift; shift ;;
     --dest-login )              DEST_LOGIN=$2;                      shift; shift ;;
     --dest-pass )               DEST_PASS=$2;                       shift; shift ;;
     --img-output )              IMAGE_OUTPUT_FILE=$2;               shift; shift ;;
@@ -45,53 +43,43 @@ mkdir -p "${PLUGIN_DIR}"
 # Get debezium project version from pom.xml
 project_version=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 
-# Copy build debezium connectors and extra libs to plugins directory
 pushd "${PLUGIN_DIR}" || exit
 cp "$MAVEN_REPO"/io/debezium/debezium-connector-*/"${project_version}"/debezium-connector-*.zip .
-unzip debezium-connector-\*.zip && rm debezium-connector-*.zip
-
-cp "$MAVEN_REPO"/com/ibm/db2/jcc/*/jcc-*.jar debezium-connector-db2/
-cp "$MAVEN_REPO"/com/oracle/database/jdbc/ojdbc8/*/ojdbc8-*.jar debezium-connector-oracle/
-
-cp "$MAVEN_REPO"/io/apicurio/apicurio-registry-distro-connect-converter/*/apicurio-registry-*.zip .
 cp "$MAVEN_REPO"/io/debezium/debezium-scripting/"${project_version}"/debezium-scripting-*.zip .
+mkdir jdbc
+cp "$MAVEN_REPO"/com/ibm/db2/jcc/*/jcc-*.jar jdbc/
+cp "$MAVEN_REPO"/io/apicurio/apicurio-registry-distro-connect-converter/*/apicurio-registry-*.zip .
 
-CONNECTORS='debezium-connector-oracle debezium-connector-db2 debezium-connector-sqlserver'\
-' debezium-connector-mongodb debezium-connector-postgres debezium-connector-mysql'
-
-# Copy groovy plugins, Apicurio and debezium scripting to each connector
-for connector in $CONNECTORS
-do
-	unzip -o apicurio-registry-*.zip -d "${connector}/"
-	cp "$MAVEN_REPO"/org/codehaus/groovy/groovy/*/groovy-*.jar "${connector}/"
-  cp "$MAVEN_REPO"/org/codehaus/groovy/groovy-json/*/groovy-json-*.jar "${connector}/"
-  cp "$MAVEN_REPO"/org/codehaus/groovy/groovy-jsr223/*/groovy-jsr223-*.jar "${connector}/"
-  unzip -o debezium-scripting-*.zip -d "${connector}/"
-done
-
-rm apicurio-registry-*.zip
-rm debezium-scripting-*.zip
+# Copy groovy scripts
+mkdir groovy
+cp "$MAVEN_REPO"/org/codehaus/groovy/groovy/*/groovy-*.jar groovy/
+cp "$MAVEN_REPO"/org/codehaus/groovy/groovy-json/*/groovy-json-*.jar groovy/
+cp "$MAVEN_REPO"/org/codehaus/groovy/groovy-jsr223/*/groovy-jsr223-*.jar groovy/
 
 prefix=""
 if [ "${ORACLE}" = "false" ] ; then
-  rm -rf debezium-connector-oracle
+  rm debezium-connector-oracle*.zip
 else
-  echo "Changing quay organisation to private rh_integration since ORACLE connector is included"
+  cp "$MAVEN_REPO"/com/oracle/database/jdbc/ojdbc8/*/ojdbc8-*.jar jdbc/
+  echo "Changing quay organisation to private rh-integration since ORACLE connector is included"
   ORGANISATION="rh_integration"
   prefix="dbz-"
 fi
 
 popd || exit
 
-echo "Copying Dockerfile to" "${BUILD_DIR}"
-cp "$DOCKER_FILE" "${BUILD_DIR}/Dockerfile"
+echo "Copying scripts to" "${BUILD_DIR}"
+cp "${DIR}"/../docker/artifact-server/* "$BUILD_DIR"
 
-image=${REGISTRY}/${ORGANISATION}/${prefix}strimzi-kafka-upstream
-target=${image}:strz-${CONNECT_BASE_TAG}-kafka-${KAFKA_VERSION}-apc-${APICURIO_VERSION}-dbz-${project_version}
+echo "Copying Dockerfile to" "${BUILD_DIR}"
+cp "$DOCKER_FILE" "$BUILD_DIR"
+
+image=${REGISTRY}/${ORGANISATION}/${prefix}artifact-server-upstream
+target=${image}:${project_version}
 
 pushd "${BUILD_DIR}" || exit
 echo "[Build] Building $target"
-docker build . -t "$target" --build-arg FROM_IMAGE=quay.io/strimzi/kafka:"${CONNECT_BASE_TAG}"-kafka-"${KAFKA_VERSION}"
+docker build . -t "$target"
 popd || exit
 
 if [ "${AUTO_TAG}" = "true" ] ; then
