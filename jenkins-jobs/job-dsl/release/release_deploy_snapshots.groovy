f9056bae988abe014e1d0e37fb94519ae1e15f46
Jiri Pechanec <jpechane@redhat.com>
2023-06-08 08:38:36 +0200

    DBZ-6493 Add operator to the snapshots

--- jenkins-jobs/job-dsl/release/release_deploy_snapshots.groovy
+++ jenkins-jobs/job-dsl/release/release_deploy_snapshots.groovy
@@ -27,7 +27,7 @@ pipelineJob('release/release-deploy_snapshots_pipeline') {
         stringParam('DEBEZIUM_BRANCH', 'main', 'A branch from which Debezium is built')
         stringParam(
                 'DEBEZIUM_ADDITIONAL_REPOSITORIES',
-                'db2#github.com/debezium/debezium-connector-db2.git#main vitess#github.com/debezium/debezium-connector-vitess.git#main cassandra#github.com/debezium/debezium-connector-cassandra.git#main spanner#github.com/debezium/debezium-connector-spanner.git#main jdbc#github.com/debezium/debezium-connector-jdbc.git#main server#github.com/debezium/debezium-server.git#main',
+                'db2#github.com/debezium/debezium-connector-db2.git#main vitess#github.com/debezium/debezium-connector-vitess.git#main cassandra#github.com/debezium/debezium-connector-cassandra.git#main spanner#github.com/debezium/debezium-connector-spanner.git#main jdbc#github.com/debezium/debezium-connector-jdbc.git#main server#github.com/debezium/debezium-server.git#main operator#github.com/debezium/debezium-operator.git#main',
                 'A space separated list of additional repositories from which Debezium connectors are built (id#repo#branch)'
         )
     }
