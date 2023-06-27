0ddedb4661e84129bc6d1bced46527ab99317393
Chris Cranford <chris@hibernate.org>
2023-06-27 19:01:39 -0400

    DBZ-6610 Fix retrying nested ORA-01327 exception

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleErrorHandler.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleErrorHandler.java
@@ -48,7 +48,8 @@ public class OracleErrorHandler extends ErrorHandler {
     @Immutable
     private static final Set<String> RETRIABLE_ERROR_MESSAGES = Collect.unmodifiableSet(
             "No more data to read from socket",
-            "immediate shutdown or close in progress" // nested ORA-01089
+            "immediate shutdown or close in progress", // nested ORA-01089
+            "failed to exclusively lock system dictionary" // nested ORA-01327
     );
 
     public OracleErrorHandler(OracleConnectorConfig connectorConfig, ChangeEventQueue<?> queue) {
