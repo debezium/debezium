050bc304d4e9b19f92bc8ed5c21a2f5d16cafb2d
mfvitale <mvitale@redhat.com>
2023-09-01 14:05:26 +0200

    DBZ-6862 Skip tombstone events

--- debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/JdbcSinkConnectorTask.java
+++ debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/JdbcSinkConnectorTask.java
@@ -88,6 +88,11 @@ public class JdbcSinkConnectorTask extends SinkTask {
         for (Iterator<SinkRecord> iterator = records.iterator(); iterator.hasNext();) {
             final SinkRecord record = iterator.next();
             LOGGER.trace("Received {}", record);
+            if (isTombstone(record)) {
+                LOGGER.trace("Skipping tombstone record.");
+                markProcessed(record);
+                continue;
+            }
             try {
                 validate(record);
                 changeEventSink.execute(record);
@@ -105,6 +110,10 @@ public class JdbcSinkConnectorTask extends SinkTask {
                 markNotProcessed(iterator);
             }
         }
+    }
+
+    private static boolean isTombstone(SinkRecord record) {
+        return record.value() == null;
     }
 
     private void validate(SinkRecord record) {
