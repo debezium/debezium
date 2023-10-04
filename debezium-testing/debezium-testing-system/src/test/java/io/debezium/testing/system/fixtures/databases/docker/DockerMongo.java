4099d18f4ec80a92f15e49005124046b6aa3895a
Jakub Cechacek <jcechace@gmail.com>
2023-10-04 17:38:01 +0200

    DBZ-6996 Updating system tests to properly use MongoDB in a container

--- debezium-testing/debezium-testing-system/src/test/java/io/debezium/testing/system/fixtures/databases/docker/DockerMongo.java
+++ debezium-testing/debezium-testing-system/src/test/java/io/debezium/testing/system/fixtures/databases/docker/DockerMongo.java
@@ -5,16 +5,24 @@
  */
 package io.debezium.testing.system.fixtures.databases.docker;
 
+import static io.debezium.testing.system.tools.ConfigProperties.DATABASE_MONGO_DOCKER_DESKTOP_PORTS;
+
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.Network;
 
-import io.debezium.testing.system.tools.databases.mongodb.DockerMongoDeployer;
+import io.debezium.testing.system.tools.ConfigProperties;
+import io.debezium.testing.system.tools.databases.mongodb.DockerMongoController;
 import io.debezium.testing.system.tools.databases.mongodb.MongoDatabaseController;
+import io.debezium.testing.testcontainers.MongoDbReplicaSet;
+import io.debezium.testing.testcontainers.util.DockerUtils;
+import io.debezium.testing.testcontainers.util.ParsingPortResolver;
 
 import fixture5.annotations.FixtureContext;
 
 @FixtureContext(requires = { Network.class }, provides = { MongoDatabaseController.class })
 public class DockerMongo extends DockerDatabaseFixture<MongoDatabaseController> {
+
+    private MongoDbReplicaSet mongo;
 
     public DockerMongo(ExtensionContext.Store store) {
         super(MongoDatabaseController.class, store);
@@ -22,9 +30,26 @@ public class DockerMongo extends DockerDatabaseFixture<MongoDatabaseController> 
 
     @Override
     protected MongoDatabaseController databaseController() throws Exception {
-        DockerMongoDeployer deployer = new DockerMongoDeployer.Builder()
-                .withNetwork(network)
+        // Creating a deployer class for this is probably not worth it
+
+        var portResolver = new ParsingPortResolver(DATABASE_MONGO_DOCKER_DESKTOP_PORTS);
+        mongo = MongoDbReplicaSet.replicaSet()
+                .memberCount(ConfigProperties.DATABASE_MONGO_DOCKER_REPLICA_SIZE)
+                .portResolver(portResolver)
+                .network(network)
+                .authEnabled(true)
+                .rootUser(ConfigProperties.DATABASE_MONGO_USERNAME, ConfigProperties.DATABASE_MONGO_SA_PASSWORD)
                 .build();
-        return deployer.deploy();
+
+        DockerUtils.enableFakeDnsIfRequired();
+        mongo.start();
+
+        return new DockerMongoController(mongo);
+    }
+
+    @Override
+    public void teardown() throws Exception {
+        mongo.stop();
+        DockerUtils.disableFakeDns();
     }
 }
