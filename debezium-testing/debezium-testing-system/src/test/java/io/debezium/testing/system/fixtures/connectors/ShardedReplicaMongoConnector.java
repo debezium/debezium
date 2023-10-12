650e8dfe3fcbefdd112e2183d9f746160459ddaa
Ondrej Babec <ond.babec@gmail.com>
2023-10-12 14:29:31 +0200

    Fix dependencies

--- debezium-testing/debezium-testing-system/src/test/java/io/debezium/testing/system/fixtures/connectors/ShardedReplicaMongoConnector.java
+++ debezium-testing/debezium-testing-system/src/test/java/io/debezium/testing/system/fixtures/connectors/ShardedReplicaMongoConnector.java
@@ -2,18 +2,18 @@ package io.debezium.testing.system.fixtures.connectors;
 
 import fixture5.annotations.FixtureContext;
 import io.debezium.testing.system.resources.ConnectorFactories;
-import io.debezium.testing.system.tools.databases.mongodb.MongoDatabaseController;
+import io.debezium.testing.system.tools.databases.mongodb.OcpMongoShardedController;
 import io.debezium.testing.system.tools.kafka.ConnectorConfigBuilder;
 import io.debezium.testing.system.tools.kafka.KafkaConnectController;
 import io.debezium.testing.system.tools.kafka.KafkaController;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-@FixtureContext(requires = { KafkaController.class, KafkaConnectController.class, MongoDatabaseController.class }, provides = { ConnectorConfigBuilder.class })
-public class ShardedReplicaMongoConnector extends ConnectorFixture<MongoDatabaseController> {
+@FixtureContext(requires = { KafkaController.class, KafkaConnectController.class, OcpMongoShardedController.class }, provides = { ShardedReplicaMongoConnector.class })
+public class ShardedReplicaMongoConnector extends ConnectorFixture<OcpMongoShardedController> {
     private static final String CONNECTOR_NAME = "inventory-connector-mongo";
 
     public ShardedReplicaMongoConnector(ExtensionContext.Store store) {
-        super(CONNECTOR_NAME, MongoDatabaseController.class, store);
+        super(CONNECTOR_NAME, OcpMongoShardedController.class, store);
     }
 
     @Override
