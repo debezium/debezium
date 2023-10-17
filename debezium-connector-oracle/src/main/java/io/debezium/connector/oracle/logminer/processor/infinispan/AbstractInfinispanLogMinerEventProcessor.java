4954b9f5bd3bacb6f30c3eb475b892a3e72706fb
Chris Cranford <chris@hibernate.org>
2023-10-17 08:08:50 -0400

    DBZ-7047 Don't use parallel streaming
    Using parallel streams, this causes a severe loss of performance.

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/infinispan/AbstractInfinispanLogMinerEventProcessor.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/infinispan/AbstractInfinispanLogMinerEventProcessor.java
@@ -284,7 +284,7 @@ public abstract class AbstractInfinispanLogMinerEventProcessor extends AbstractL
         // todo: implement indexed keys when ISPN supports them
         return (int) getEventCache()
                 .keySet()
-                .parallelStream()
+                .stream()
                 .filter(k -> k.startsWith(transaction.getTransactionId() + "-"))
                 .count();
     }
