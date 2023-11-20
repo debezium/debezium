31538969f77c480b029db7f8e7b61ca1769774c4
Vojtech Juranek <vjuranek@redhat.com>
2023-11-20 17:13:07 +0100

    DBZ-7024 Move required setup into EmbeddedWorkerConfig
    Move required by `WorkerConfig` validators into `EmbeddedWorkerConfig`
    so we have all Kafka related stuff in the same class.

--- debezium-embedded/src/main/java/io/debezium/embedded/EmbeddedWorkerConfig.java
+++ debezium-embedded/src/main/java/io/debezium/embedded/EmbeddedWorkerConfig.java
@@ -1,13 +1,21 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.embedded;
 
 import java.util.Map;
 
-import io.debezium.config.Field;
 import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.json.JsonConverter;
 import org.apache.kafka.connect.runtime.WorkerConfig;
 
+import io.debezium.config.Field;
+
 /**
- * Extension to Kafka's {@link WorkerConfig} with additions needed by {@link io.debezium.engine.DebeziumEngine}.
+ * Extension to Kafka's {@link WorkerConfig} with additions needed by {@link io.debezium.engine.DebeziumEngine}
+ * and required {@link WorkerConfig} fields which are not used by {@link io.debezium.engine.DebeziumEngine}.
  * Should be removed once {@link io.debezium.engine.DebeziumEngine} is independent on Kafka model (DBZ-6234).
  */
 class EmbeddedWorkerConfig extends WorkerConfig {
@@ -23,6 +31,16 @@ class EmbeddedWorkerConfig extends WorkerConfig {
     }
 
     protected EmbeddedWorkerConfig(Map<String, String> props) {
-        super(CONFIG, props);
+        super(CONFIG, addRequiredWorkerConfig(props));
+    }
+
+    /**
+     * Add extra fields that are required for validation of a worker config, but that are not used within
+     * the embedded engine (since the source records are never serialized) ...
+     */
+    protected static Map<String, String> addRequiredWorkerConfig(Map<String, String> props) {
+        props.put(WorkerConfig.KEY_CONVERTER_CLASS_CONFIG, JsonConverter.class.getName());
+        props.put(WorkerConfig.VALUE_CONVERTER_CLASS_CONFIG, JsonConverter.class.getName());
+        return props;
     }
 }
