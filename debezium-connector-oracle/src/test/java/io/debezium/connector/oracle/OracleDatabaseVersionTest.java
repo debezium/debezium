15ad814f7533810b8fbf14c22ac4041a01982aa9
Vojtech Juranek <vjuranek@redhat.com>
2023-12-14 17:27:35 +0100

    DBZ-7257 Fix regression in parsin Oracle 12 version

--- debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/OracleDatabaseVersionTest.java
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/OracleDatabaseVersionTest.java
@@ -9,12 +9,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
+import io.debezium.doc.FixFor;
+
 /**
  * Test paring of various Oracle version strings.
  *
  * @author vjuranek
  */
 public class OracleDatabaseVersionTest {
+
+    @Test
+    @FixFor("DBZ-7257")
+    public void shouldParseOracle11g() throws Exception {
+        String banner = "Oracle Database 11g Enterprise Edition Release 11.2.0.4.0 - 64bit Production";
+        OracleDatabaseVersion version = OracleDatabaseVersion.parse(banner);
+        assertOracleVersion(version, 11, 2, 0, 4, 0, banner);
+    }
+
+    @Test
+    @FixFor("DBZ-7257")
+    public void shouldParseOracle12c() throws Exception {
+        String banner = "Oracle Database 12c Enterprise Edition Release 12.1.0.2.0 - 64bit Production";
+        OracleDatabaseVersion version = OracleDatabaseVersion.parse(banner);
+        assertOracleVersion(version, 12, 1, 0, 2, 0, banner);
+    }
 
     @Test
     public void shouldParseOracle19c() throws Exception {
@@ -28,6 +46,13 @@ public class OracleDatabaseVersionTest {
         String banner = "Oracle Database 21c Express Edition Release 21.0.0.0.0 - Production\nVersion 21.3.0.0.0";
         OracleDatabaseVersion version = OracleDatabaseVersion.parse(banner);
         assertOracleVersion(version, 21, 3, 0, 0, 0, banner);
+    }
+
+    @Test
+    public void shouldParseOracle23cFree() throws Exception {
+        String banner = "Oracle Database 23c Free Release 23.0.0.0.0 - Develop, Learn, and Run for Free\nVersion 23.3.0.23.09";
+        OracleDatabaseVersion version = OracleDatabaseVersion.parse(banner);
+        assertOracleVersion(version, 23, 3, 0, 23, 9, banner);
     }
 
     @Test
