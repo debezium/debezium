3d22ed30a312a10091bb8471295f425aa02969e0
Chris Cranford <chris@hibernate.org>
2024-01-19 09:21:28 -0500

    DBZ-7439 [ci] Update TF with Oracle 23
    * Introduce new InstantClient version property
      Needed because ojdbc8 versions for Oracle 23 have different version semantics
      Also adjusted documentation/build pipelines
    * Introduce oracle-23 profile
      Allows testing Oracle 23 separately from distribution builds

--- jenkins-jobs/pipelines/release/release-pipeline.groovy
+++ jenkins-jobs/pipelines/release/release-pipeline.groovy
@@ -327,11 +327,12 @@ node('release-node') {
 
             dir(DEBEZIUM_DIR) {
                 ORACLE_ARTIFACT_VERSION = (readFile('pom.xml') =~ /(?ms)<version.oracle.driver>(.+)<\/version.oracle.driver>/)[0][1]
+                ORACLE_INSTANTCLIENT_ARTIFACT_VERSION = (readFile('pom.xml') =~ /(?ms)<version.oracle.instantclient>(.+)<\/version.oracle.instantclient>/)[0][1]
                 ORACLE_ARTIFACT_DIR = "$HOME_DIR/oracle-libs/${ORACLE_ARTIFACT_VERSION}.0"
             }
             dir(ORACLE_ARTIFACT_DIR) {
                 sh "mvn install:install-file -DgroupId=com.oracle.instantclient -DartifactId=ojdbc8 -Dversion=$ORACLE_ARTIFACT_VERSION -Dpackaging=jar -Dfile=ojdbc8.jar"
-                sh "mvn install:install-file -DgroupId=com.oracle.instantclient -DartifactId=xstreams -Dversion=$ORACLE_ARTIFACT_VERSION -Dpackaging=jar -Dfile=xstreams.jar"
+                sh "mvn install:install-file -DgroupId=com.oracle.instantclient -DartifactId=xstreams -Dversion=$ORACLE_INSTANTCLIENT_ARTIFACT_VERSION -Dpackaging=jar -Dfile=xstreams.jar"
             }
         }
 
