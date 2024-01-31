ea981f0d38c84ed0324db5eb150a663e3c3f40a8
Jakub Cechacek <jcechace@gmail.com>
2024-01-31 11:49:50 +0100

    DBZ-7379 Ability to use connector specific keystore/truststore

--- debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/MongoDbConnectorConfig.java
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/MongoDbConnectorConfig.java
@@ -5,6 +5,9 @@
  */
 package io.debezium.connector.mongodb;
 
+import static io.debezium.function.Predicates.not;
+
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -728,6 +731,61 @@ public class MongoDbConnectorConfig extends CommonConnectorConfig {
             .withValidation(Field::isBoolean)
             .withDescription("Whether invalid host names are allowed when using SSL. If true the connection will not prevent man-in-the-middle attacks");
 
+    public static final Field SSL_KEYSTORE = Field.create("mongodb.ssl.keystore")
+            .withDisplayName("SSL Keystore")
+            .withType(Type.STRING)
+            .withGroup(Field.createGroupEntry(Field.Group.CONNECTION_ADVANCED_SSL, 1))
+            .withWidth(Width.LONG)
+            .withImportance(Importance.MEDIUM)
+            .withDescription("The location of the key store file. "
+                    + "This is optional and can be used for two-way authentication between the client and the MySQL Server.");
+
+    public static final Field SSL_KEYSTORE_PASSWORD = Field.create("mongodb.ssl.keystore.password")
+            .withDisplayName("SSL Keystore Password")
+            .withType(Type.PASSWORD)
+            .withGroup(Field.createGroupEntry(Field.Group.CONNECTION_ADVANCED_SSL, 2))
+            .withWidth(Width.MEDIUM)
+            .withImportance(Importance.MEDIUM)
+            .withDescription("The password for the key store file. "
+                    + "This is optional and only needed if 'mongodb.ssl.keystore' is configured.");
+
+    public static final Field SSL_KEYSTORE_TYPE = Field.create("mongodb.ssl.keystore.type")
+            .withDisplayName("SSL Keystore Type")
+            .withType(Type.STRING)
+            .withDefault("PKCS12")
+            .withGroup(Field.createGroupEntry(Field.Group.CONNECTION_ADVANCED_SSL, 3))
+            .withWidth(Width.MEDIUM)
+            .withImportance(Importance.MEDIUM)
+            .withDescription("The type of key store file. "
+                    + "This is optional and only needed if 'mongodb.ssl.keystore' is configured.");
+
+    public static final Field SSL_TRUSTSTORE = Field.create("mongodb.ssl.truststore")
+            .withDisplayName("SSL Truststore")
+            .withType(Type.STRING)
+            .withGroup(Field.createGroupEntry(Field.Group.CONNECTION_ADVANCED_SSL, 4))
+            .withWidth(Width.LONG)
+            .withImportance(Importance.MEDIUM)
+            .withDescription("The location of the trust store file for the server certificate verification.");
+
+    public static final Field SSL_TRUSTSTORE_PASSWORD = Field.create("mongodb.ssl.truststore.password")
+            .withDisplayName("SSL Truststore Password")
+            .withType(Type.PASSWORD)
+            .withGroup(Field.createGroupEntry(Field.Group.CONNECTION_ADVANCED_SSL, 5))
+            .withWidth(Width.MEDIUM)
+            .withImportance(Importance.MEDIUM)
+            .withDescription("The password for the trust store file. "
+                    + "Used to check the integrity of the truststore, and unlock the truststore.");
+
+    public static final Field SSL_TRUSTSTORE_TYPE = Field.create("mongodb.ssl.truststore.type")
+            .withDisplayName("SSL Keystore Type")
+            .withType(Type.STRING)
+            .withDefault("PKCS12")
+            .withGroup(Field.createGroupEntry(Field.Group.CONNECTION_ADVANCED_SSL, 6))
+            .withWidth(Width.MEDIUM)
+            .withImportance(Importance.MEDIUM)
+            .withDescription("The type of key store file. "
+                    + "This is optional and only needed if 'mongodb.ssl.truststore' is configured.");
+
     public static final Field CONNECT_TIMEOUT_MS = Field.create("mongodb.connect.timeout.ms")
             .withDisplayName("Connect Timeout MS")
             .withType(Type.INT)
@@ -1073,6 +1131,14 @@ public class MongoDbConnectorConfig extends CommonConnectorConfig {
     private final OversizeHandlingMode oversizeHandlingMode;
     private final FiltersMatchMode filtersMatchMode;
     private final int oversizeSkipThreshold;
+    private final boolean sslEnabled;
+    private final boolean sslAllowInvalidHostnames;
+    private final String sslKeyStore;
+    private final String sslKeyStorePassword;
+    private final String sslKeyStoreType;
+    private final String sslTrustStore;
+    private final String sslTrustStorePassword;
+    private final String sslTrustStoreType;
 
     public MongoDbConnectorConfig(Configuration config) {
         super(config, DEFAULT_SNAPSHOT_FETCH_SIZE);
@@ -1108,6 +1174,16 @@ public class MongoDbConnectorConfig extends CommonConnectorConfig {
         this.cursorMaxAwaitTimeMs = config.getInteger(MongoDbConnectorConfig.CURSOR_MAX_AWAIT_TIME_MS, 0);
 
         this.replicaSets = resolveReplicaSets(config);
+
+        // ssl configuration
+        this.sslEnabled = config.getBoolean(MongoDbConnectorConfig.SSL_ENABLED);
+        this.sslAllowInvalidHostnames = config.getBoolean(MongoDbConnectorConfig.SSL_ALLOW_INVALID_HOSTNAMES);
+        this.sslKeyStore = config.getString(MongoDbConnectorConfig.SSL_KEYSTORE);
+        this.sslKeyStorePassword = config.getString(MongoDbConnectorConfig.SSL_KEYSTORE_PASSWORD);
+        this.sslKeyStoreType = config.getString(MongoDbConnectorConfig.SSL_KEYSTORE_TYPE);
+        this.sslTrustStore = config.getString(MongoDbConnectorConfig.SSL_TRUSTSTORE);
+        this.sslTrustStorePassword = config.getString(MongoDbConnectorConfig.SSL_TRUSTSTORE_PASSWORD);
+        this.sslTrustStoreType = config.getString(MongoDbConnectorConfig.SSL_TRUSTSTORE_TYPE);
     }
 
     private static int validateHosts(Configuration config, Field field, ValidationOutput problems) {
@@ -1327,6 +1403,42 @@ public class MongoDbConnectorConfig extends CommonConnectorConfig {
     @Override
     public int getSnapshotMaxThreads() {
         return snapshotMaxThreads;
+    }
+
+    public boolean isSslEnabled() {
+        return sslEnabled;
+    }
+
+    public boolean isSslAllowInvalidHostnames() {
+        return sslAllowInvalidHostnames;
+    }
+
+    public Optional<Path> getSslKeyStore() {
+        return Optional.ofNullable(sslKeyStore)
+                .filter(not(Strings::isNullOrBlank))
+                .map(Path::of);
+    }
+
+    public char[] getSslKeyStorePassword() {
+        return sslKeyStorePassword != null ? sslKeyStorePassword.toCharArray() : null;
+    }
+
+    public String getSslKeyStoreType() {
+        return sslKeyStoreType;
+    }
+
+    public Optional<Path> getSslTrustStore() {
+        return Optional.ofNullable(sslTrustStore)
+                .filter(not(Strings::isNullOrBlank))
+                .map(Path::of);
+    }
+
+    public char[] getSslTrustStorePassword() {
+        return sslTrustStorePassword != null ? sslTrustStorePassword.toCharArray() : null;
+    }
+
+    public String getSslTrustStoreType() {
+        return sslTrustStoreType;
     }
 
     @Override
