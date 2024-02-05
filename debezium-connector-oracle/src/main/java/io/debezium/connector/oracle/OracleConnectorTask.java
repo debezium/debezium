6392a1b816be56e931da0119ac6069a1c4475929
mfvitale <mvitale@redhat.com>
2024-02-05 16:54:46 +0100

    DBZ-7302 Implement SnapshotLock and SnapshotQuery for Oracle connector

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleConnectorTask.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleConnectorTask.java
@@ -22,6 +22,7 @@ import io.debezium.config.Field;
 import io.debezium.connector.base.ChangeEventQueue;
 import io.debezium.connector.common.BaseSourceTask;
 import io.debezium.connector.oracle.StreamingAdapter.TableNameCaseSensitivity;
+import io.debezium.connector.oracle.snapshot.OracleSnapshotLockProvider;
 import io.debezium.document.DocumentReader;
 import io.debezium.jdbc.DefaultMainConnectionProvidingConnectionFactory;
 import io.debezium.jdbc.JdbcConfiguration;
@@ -36,6 +37,7 @@ import io.debezium.pipeline.spi.Offsets;
 import io.debezium.relational.TableId;
 import io.debezium.schema.SchemaFactory;
 import io.debezium.schema.SchemaNameAdjuster;
+import io.debezium.service.spi.ServiceRegistry;
 import io.debezium.snapshot.SnapshotterService;
 import io.debezium.spi.topic.TopicNamingStrategy;
 import io.debezium.util.Clock;
@@ -204,6 +206,17 @@ public class OracleConnectorTask extends BaseSourceTask<OraclePartition, OracleO
         if (schema != null) {
             schema.close();
         }
+    }
+
+    @Override
+    protected void registerServiceProviders(ServiceRegistry serviceRegistry) {
+
+        super.registerServiceProviders(serviceRegistry);
+        serviceRegistry.registerServiceProvider(new OracleSnapshotLockProvider());
+        /*
+         * serviceRegistry.registerServiceProvider(new OracleSnapshotQueryProvider());
+         * serviceRegistry.registerServiceProvider(new OracleSnapshotterServiceProvider());
+         */
     }
 
     @Override
---
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/snapshot/lock/NoSnapshotLock.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.oracle.snapshot.lock;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import io.debezium.connector.oracle.OracleConnectorConfig;
+import io.debezium.snapshot.spi.SnapshotLock;
+
+public class NoSnapshotLock implements SnapshotLock {
+
+    @Override
+    public String name() {
+        return OracleConnectorConfig.SnapshotLockingMode.NONE.getValue();
+    }
+
+    @Override
+    public void configure(Map<String, ?> properties) {
+
+    }
+
+    @Override
+    public Optional<String> tableLockingStatement(Duration lockTimeout, Set<String> tableIds) {
+
+        return Optional.empty();
+    }
+}
