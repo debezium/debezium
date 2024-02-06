b47ebebca5d904e6f70d5bb4b355d16597553d06
Vojtech Juranek <vjuranek@redhat.com>
2024-02-06 15:09:31 +0100

    DBZ-7024 Don't provide default ChangeConsumer
    To allow user to use different processors, don't provide the default
    ChangeHandler.

--- debezium-embedded/src/test/java/io/debezium/embedded/AsyncEmbeddedEngineTest.java
+++ debezium-embedded/src/test/java/io/debezium/embedded/AsyncEmbeddedEngineTest.java
@@ -42,6 +42,8 @@ import io.debezium.junit.logging.LogInterceptor;
 import io.debezium.util.LoggingContext;
 import io.debezium.util.Testing;
 
+import ch.qos.logback.classic.Level;
+
 /**
  * Tests for {@link AsyncEmbeddedEngine} implementation of {@link DebeziumEngine}.
  *
@@ -521,6 +523,70 @@ public class AsyncEmbeddedEngineTest {
         assertThat(interceptor.containsMessage("Engine state has changed from 'POLLING_TASKS' to 'STOPPING'")).isTrue();
         // And engine was successfully stopped.
         assertThat(interceptor.containsMessage("Engine state has changed from 'STOPPING' to 'STOPPED'")).isTrue();
+    }
+
+    @Test
+    public void testEngineBasicLifecycleConsumerSequentially() throws Exception {
+        final Properties props = new Properties();
+        props.setProperty(ConnectorConfig.NAME_CONFIG, "debezium-engine");
+        props.setProperty(ConnectorConfig.TASKS_MAX_CONFIG, "1");
+        props.setProperty(ConnectorConfig.CONNECTOR_CLASS_CONFIG, FileStreamSourceConnector.class.getName());
+        props.setProperty(StandaloneConfig.OFFSET_STORAGE_FILE_FILENAME_CONFIG, OFFSET_STORE_PATH.toAbsolutePath().toString());
+        props.setProperty(WorkerConfig.OFFSET_COMMIT_INTERVAL_MS_CONFIG, "0");
+        props.setProperty(FileStreamSourceConnector.FILE_CONFIG, TEST_FILE_PATH.toAbsolutePath().toString());
+        props.setProperty(FileStreamSourceConnector.TOPIC_CONFIG, "testTopic");
+        props.setProperty(AsyncEngineConfig.RECORD_PROCESSING_SEQUENTIALLY.name(), "true");
+
+        runEngineBasicLifecycleWithConsumer(props);
+    }
+
+    @Test
+    public void testEngineBasicLifecycleConsumerNonSequentially() throws Exception {
+        final Properties props = new Properties();
+        props.setProperty(ConnectorConfig.NAME_CONFIG, "debezium-engine");
+        props.setProperty(ConnectorConfig.TASKS_MAX_CONFIG, "1");
+        props.setProperty(ConnectorConfig.CONNECTOR_CLASS_CONFIG, FileStreamSourceConnector.class.getName());
+        props.setProperty(StandaloneConfig.OFFSET_STORAGE_FILE_FILENAME_CONFIG, OFFSET_STORE_PATH.toAbsolutePath().toString());
+        props.setProperty(WorkerConfig.OFFSET_COMMIT_INTERVAL_MS_CONFIG, "0");
+        props.setProperty(FileStreamSourceConnector.FILE_CONFIG, TEST_FILE_PATH.toAbsolutePath().toString());
+        props.setProperty(FileStreamSourceConnector.TOPIC_CONFIG, "testTopic");
+        props.setProperty(AsyncEngineConfig.RECORD_PROCESSING_SEQUENTIALLY.name(), "false");
+
+        runEngineBasicLifecycleWithConsumer(props);
+    }
+
+    private void runEngineBasicLifecycleWithConsumer(final Properties props) throws IOException, InterruptedException {
+
+        final LogInterceptor interceptor = new LogInterceptor(AsyncEmbeddedEngine.class);
+        interceptor.setLoggerLevel(AsyncEmbeddedEngine.class, Level.DEBUG);
+
+        appendLinesToSource(NUMBER_OF_LINES);
+        CountDownLatch allLatch = new CountDownLatch(6 * NUMBER_OF_LINES);
+
+        DebeziumEngine.Builder<SourceRecord> builder = new AsyncEmbeddedEngine.AsyncEngineBuilder();
+        engine = builder
+                .using(props)
+                .using(new TestEngineConnectorCallback())
+                .notifying(r -> {
+                    allLatch.countDown();
+                }).build();
+
+        engineExecSrv.submit(() -> {
+            LoggingContext.forConnector(getClass().getSimpleName(), "", "engine");
+            engine.run();
+        });
+
+        for (int i = 0; i < 5; i++) {
+            // Add a few more lines, and then verify they are consumed ...
+            appendLinesToSource(NUMBER_OF_LINES);
+            Thread.sleep(10);
+        }
+        allLatch.await(1, TimeUnit.SECONDS);
+        assertThat(allLatch.getCount()).isEqualTo(0);
+
+        assertThat(interceptor.containsMessage("Using io.debezium.embedded.AsyncEmbeddedEngine$ParallelSmtConsumerProcessor processor"));
+
+        stopEngine();
     }
 
     protected void stopEngine() {
