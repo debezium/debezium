f7cdb3928bb78cdd77d65141104308de24912080
Chris Cranford <chris@hibernate.org>
2024-02-19 11:54:55 -0500

    DBZ-6236 Fix test failures

--- debezium-core/src/test/java/io/debezium/connector/common/BaseSourceTaskTest.java
+++ debezium-core/src/test/java/io/debezium/connector/common/BaseSourceTaskTest.java
@@ -171,6 +171,11 @@ public class BaseSourceTaskTest {
         }
 
         @Override
+        protected void resetErrorHandlerRetriesIfNeeded() {
+            // do nothing as we don't have a coordinator mocked
+        }
+
+        @Override
         protected void doStop() {
             stopCount.incrementAndGet();
         }
