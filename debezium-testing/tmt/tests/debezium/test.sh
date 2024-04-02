f27bf28895b3e6659ef42bfc080f95c6c8b245f2
Martin Medek <mmedek@redhat.com>
2024-04-02 14:57:50 +0200

    DBZ-7807 added noncdb option for oracle connector tests in tmt

--- debezium-testing/tmt/tests/debezium/test.sh
+++ debezium-testing/tmt/tests/debezium/test.sh
@@ -35,6 +35,10 @@ then
     export ORACLE_CONNECTION="-Ddatabase.dbname=FREEPDB1 -Ddatabase.pdb.name=FREEPDB1"
     debezium-testing/tmt/tests/debezium/init-db.sh
   fi
+  DATABASE_USER="c##dbzuser"
+  if [[ "$ORACLE_VERSION" = *noncdb ]]; then
+    DATABASE_USER="dbzuser"
+  fi
   mvn clean verify -U -pl debezium-connector-oracle -am -fae \
     -Poracle-tests                              \
     ${ORACLE_PROFILE_ARGS:-}                    \
@@ -47,7 +51,7 @@ then
     -Dorg.slf4j.simpleLogger.showDateTime=true \
     -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss,SSS \
     -Dinstantclient.dir=${ORACLE_ARTIFACT_DIR}     \
-    -Ddatabase.user="c##dbzuser"                  \
+    -Ddatabase.user=${DATABASE_USER}                  \
     -Dinsecure.repositories=WARN                \
     ${ORACLE_CONNECTION:-}                      \
     -Papicurio
