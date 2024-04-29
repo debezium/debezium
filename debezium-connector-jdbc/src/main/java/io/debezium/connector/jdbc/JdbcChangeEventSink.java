d67b28ff6214069cb519343f24360861033a0047
Duc Le Tu <tuduc93@gmail.com>
2024-04-29 16:13:47 +0700

    DBZ-7830 Flush buffer when a delete arrives

--- debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/JdbcChangeEventSink.java
+++ debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/JdbcChangeEventSink.java
@@ -115,6 +115,13 @@ public class JdbcChangeEventSink implements ChangeEventSink {
 
             if (sinkRecordDescriptor.isDelete()) {
 
+                if (updateBufferByTable.get(tableId) != null && !updateBufferByTable.get(tableId).isEmpty()) {
+                    // When an delete arrives, update buffer must be flushed to avoid losing an
+                    // delete for the same record after its update.
+                    flushBuffer(tableId, updateBufferByTable.get(tableId).flush());
+
+                }
+
                 if (!config.isDeleteEnabled()) {
                     LOGGER.debug("Deletes are not enabled, skipping delete for topic '{}'", sinkRecordDescriptor.getTopicName());
                     continue;
