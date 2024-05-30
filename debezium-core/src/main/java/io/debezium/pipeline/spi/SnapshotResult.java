354f2dfd74a8ea0623ae0f50f78c1973adc8b0bc
rajdangwal <dangwalrajendra888@gmail.com>
2024-05-30 23:06:44 +0530

    DBZ-7902: Add configurable delay after snapshotting, before starting streaming

--- debezium-core/src/main/java/io/debezium/pipeline/spi/SnapshotResult.java
+++ debezium-core/src/main/java/io/debezium/pipeline/spi/SnapshotResult.java
@@ -31,6 +31,10 @@ public class SnapshotResult<O extends OffsetContext> {
         return this.status == SnapshotResultStatus.SKIPPED || this.status == SnapshotResultStatus.COMPLETED;
     }
 
+    public boolean isCompleted() {
+        return this.status == SnapshotResultStatus.COMPLETED;
+    }
+
     public SnapshotResultStatus getStatus() {
         return status;
     }
