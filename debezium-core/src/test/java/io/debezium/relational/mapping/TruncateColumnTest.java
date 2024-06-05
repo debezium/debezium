430b8125a8c5c95ff08d487dffe04e4572b99105
twthorn <thomaswilliamthornton@gmail.com>
2024-06-05 16:42:23 -0400

    DBZ-7925 Expand truncate support to byte arrays in addition to strings

--- debezium-core/src/test/java/io/debezium/relational/mapping/TruncateColumnTest.java
+++ debezium-core/src/test/java/io/debezium/relational/mapping/TruncateColumnTest.java
@@ -7,6 +7,8 @@ package io.debezium.relational.mapping;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.nio.ByteBuffer;
+
 import org.junit.Test;
 
 import io.debezium.relational.Column;
@@ -16,14 +18,14 @@ import io.debezium.relational.ValueConverter;
  * @author Randall Hauch
  *
  */
-public class TruncateStringsTest {
+public class TruncateColumnTest {
 
     private final Column column = Column.editor().name("col").create();
     private ValueConverter converter;
 
     @Test
     public void shouldTruncateStrings() {
-        converter = new TruncateStrings(5).create(column);
+        converter = new TruncateColumn(5).create(column);
         assertThat(converter.convert("1234567890").toString()).isEqualTo("12345");
         assertThat(converter.convert("123456").toString()).isEqualTo("12345");
         assertThat(converter.convert("12345").toString()).isEqualTo("12345");
@@ -32,6 +34,31 @@ public class TruncateStringsTest {
         assertThat(converter.convert("12").toString()).isEqualTo("12");
         assertThat(converter.convert("1").toString()).isEqualTo("1");
         assertThat(converter.convert(null)).isNull();
+    }
+
+    @Test
+    public void shouldTruncateByteBuffer() {
+        converter = new TruncateColumn(3).create(column);
+        ByteBuffer buffer5 = createBuffer(5);
+        ByteBuffer buffer4 = createBuffer(4);
+        ByteBuffer buffer3 = createBuffer(3);
+        ByteBuffer buffer2 = createBuffer(2);
+        ByteBuffer buffer1 = createBuffer(1);
+        assertThat(converter.convert(buffer5)).isEqualTo(buffer3);
+        assertThat(converter.convert(buffer4)).isEqualTo(buffer3);
+        assertThat(converter.convert(buffer3)).isEqualTo(buffer3);
+        assertThat(converter.convert(buffer2)).isEqualTo(buffer2);
+        assertThat(converter.convert(buffer1)).isEqualTo(buffer1);
+        assertThat(converter.convert(null)).isNull();
+    }
+
+    private ByteBuffer createBuffer(int size) {
+        ByteBuffer buffer = ByteBuffer.allocate(size);
+        for (int i = 0; i < size; i++) {
+            buffer.put((byte) i);
+        }
+        buffer.position(0);
+        return buffer;
     }
 
 }
