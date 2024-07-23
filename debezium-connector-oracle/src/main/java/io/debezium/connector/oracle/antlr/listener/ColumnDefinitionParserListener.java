0b3e65d21de7046c0be2397b565798abbec9c133
Chris Cranford <chris@hibernate.org>
2024-07-23 13:54:26 -0400

    DBZ-8089 Reset column scale on MODIFY ddl statements

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/antlr/listener/ColumnDefinitionParserListener.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/antlr/listener/ColumnDefinitionParserListener.java
@@ -68,6 +68,11 @@ public class ColumnDefinitionParserListener extends BaseParserListener {
 
     @Override
     public void enterModify_col_properties(PlSqlParser.Modify_col_propertiesContext ctx) {
+        // Scale should always get unset
+        // It should be parsed by the data type resolver, if its applicable
+        // This standardizes the handling of scale when data types shift from one to another
+        columnEditor.unsetScale();
+
         resolveColumnDataType(ctx);
         if (ctx.DEFAULT() != null) {
             columnEditor.defaultValueExpression(ctx.column_default_value().getText());
