2cc1853fe30405e04fdccda149216c3a908d2a50
Chris Cranford <chris@hibernate.org>
2024-08-20 16:01:50 -0400

    DBZ-7758 Move configuration to XML

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/ehcache/EhcacheLogMinerEventProcessor.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/processor/ehcache/EhcacheLogMinerEventProcessor.java
@@ -5,18 +5,31 @@
  */
 package io.debezium.connector.oracle.logminer.processor.ehcache;
 
-import org.ehcache.PersistentCacheManager;
-import org.ehcache.config.CacheConfiguration;
-import org.ehcache.config.builders.CacheConfigurationBuilder;
+import static io.debezium.connector.oracle.OracleConnectorConfig.LOG_MINING_BUFFER_EHCACHE_EVENTS_CONFIG;
+import static io.debezium.connector.oracle.OracleConnectorConfig.LOG_MINING_BUFFER_EHCACHE_GLOBAL_CONFIG;
+import static io.debezium.connector.oracle.OracleConnectorConfig.LOG_MINING_BUFFER_EHCACHE_PROCESSED_TRANSACTIONS_CONFIG;
+import static io.debezium.connector.oracle.OracleConnectorConfig.LOG_MINING_BUFFER_EHCACHE_SCHEMA_CHANGES_CONFIG;
+import static io.debezium.connector.oracle.OracleConnectorConfig.LOG_MINING_BUFFER_EHCACHE_TRANSACTIONS_CONFIG;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.stream.Collectors;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.ehcache.CacheManager;
 import org.ehcache.config.builders.CacheManagerBuilder;
-import org.ehcache.config.builders.ResourcePoolsBuilder;
-import org.ehcache.config.units.MemoryUnit;
-import org.ehcache.core.internal.statistics.DefaultStatisticsService;
-import org.ehcache.expiry.ExpiryPolicy;
-import org.ehcache.spi.serialization.Serializer;
+import org.ehcache.xml.XmlConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
+import io.debezium.DebeziumException;
+import io.debezium.config.Configuration;
 import io.debezium.connector.oracle.OracleConnection;
 import io.debezium.connector.oracle.OracleConnectorConfig;
 import io.debezium.connector.oracle.OracleDatabaseSchema;
@@ -27,8 +40,6 @@ import io.debezium.connector.oracle.logminer.events.LogMinerEvent;
 import io.debezium.connector.oracle.logminer.events.LogMinerEventRow;
 import io.debezium.connector.oracle.logminer.processor.AbstractTransactionCachingLogMinerEventProcessor;
 import io.debezium.connector.oracle.logminer.processor.LogMinerCache;
-import io.debezium.connector.oracle.logminer.processor.ehcache.serialization.EhcacheTransactionSerializer;
-import io.debezium.connector.oracle.logminer.processor.ehcache.serialization.LogMinerEventSerializer;
 import io.debezium.pipeline.EventDispatcher;
 import io.debezium.pipeline.source.spi.ChangeEventSource.ChangeEventSourceContext;
 import io.debezium.relational.TableId;
@@ -43,7 +54,7 @@ public class EhcacheLogMinerEventProcessor extends AbstractTransactionCachingLog
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EhcacheLogMinerEventProcessor.class);
 
-    private final PersistentCacheManager cacheManager;
+    private final CacheManager cacheManager;
     private final LogMinerCache<String, EhcacheTransaction> transactionsCache;
     private final LogMinerCache<String, LogMinerEvent> eventCache;
     private final LogMinerCache<String, String> processedTransactionsCache;
@@ -100,39 +111,58 @@ public class EhcacheLogMinerEventProcessor extends AbstractTransactionCachingLog
         }
     }
 
-    private PersistentCacheManager createCacheManager(OracleConnectorConfig connectorConfig) {
-        final String cachePath = connectorConfig.getLogMiningBufferEhcacheStoragePath();
-        final long transactionCacheSize = connectorConfig.getLogMiningBufferEhcacheTransactionCacheSizeBytes();
-        final long processedTransactionsCacheSize = connectorConfig.getLogMiningBufferEhcacheProcessedTransactionsCacheSizeBytes();
-        final long schemaChangesCacheSize = connectorConfig.getLogMiningBufferEhcacheSchemaChangesCacheSizeBytes();
-        final long eventsCacheSize = connectorConfig.getLogMiningBufferEhcacheEventsCacheSizeBytes();
+    private CacheManager createCacheManager(OracleConnectorConfig connectorConfig) {
+        try {
+            final Configuration ehcacheConfig = connectorConfig.getLogMiningEhcacheConfiguration();
 
-        return CacheManagerBuilder.newCacheManagerBuilder()
-                .with(CacheManagerBuilder.persistence(cachePath))
-                .using(new DefaultStatisticsService())
-                .withCache(TRANSACTIONS_CACHE_NAME, createCacheConfiguration(String.class, EhcacheTransaction.class, transactionCacheSize, null))
-                .withCache(PROCESSED_TRANSACTIONS_CACHE_NAME, createCacheConfiguration(String.class, String.class, processedTransactionsCacheSize, null))
-                .withCache(SCHEMA_CHANGES_CACHE_NAME, createCacheConfiguration(String.class, String.class, schemaChangesCacheSize, null))
-                .withCache(EVENTS_CACHE_NAME, createCacheConfiguration(String.class, LogMinerEvent.class, eventsCacheSize, null))
-                .withSerializer(EhcacheTransaction.class, EhcacheTransactionSerializer.class)
-                .withSerializer(LogMinerEvent.class, LogMinerEventSerializer.class)
-                .build(true);
+            // Create the full XML configuration based on configuration template
+            final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder builder = factory.newDocumentBuilder();
+
+            final String xmlData = getConfigurationWithSubstitutions(ehcacheConfig);
+            LOGGER.debug("Using Ehcache XML configuration:\n{}", xmlData);
+
+            final Document xmlDocument = builder.parse(new InputSource(new StringReader(xmlData)));
+
+            final CacheManager cacheManager = CacheManagerBuilder.newCacheManager(new XmlConfiguration(xmlDocument));
+            cacheManager.init();
+
+            return cacheManager;
+        }
+        catch (Exception e) {
+            throw new DebeziumException("Failed to create Ehcache cache manager", e);
+        }
     }
 
-    @SuppressWarnings("SameParameterValue")
-    private <K, V> CacheConfiguration<K, V> createCacheConfiguration(Class<K> keyClass, Class<V> valueClass, long sizeMb,
-                                                                     Class<? extends Serializer<V>> valueSerializer) {
-        final CacheConfigurationBuilder<K, V> builder = CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                keyClass,
-                valueClass,
-                ResourcePoolsBuilder.newResourcePoolsBuilder()
-                        .disk(sizeMb, MemoryUnit.B, !getConfig().isLogMiningBufferDropOnStop()))
-                .withExpiry(ExpiryPolicy.NO_EXPIRY);
+    private String getConfigurationWithSubstitutions(Configuration configuration) {
+        return readConfigurationTemplate()
+                .replace("${log.mining.buffer.ehcache.global.config}",
+                        configuration.getString(LOG_MINING_BUFFER_EHCACHE_GLOBAL_CONFIG, ""))
+                .replace("${log.mining.buffer.ehcache.transactions.config}",
+                        configuration.getString(LOG_MINING_BUFFER_EHCACHE_TRANSACTIONS_CONFIG, ""))
+                .replace("${log.mining.buffer.ehcache.processedtransactions.config}",
+                        configuration.getString(LOG_MINING_BUFFER_EHCACHE_PROCESSED_TRANSACTIONS_CONFIG, ""))
+                .replace("${log.mining.buffer.ehcache.schemachanges.config}",
+                        configuration.getString(LOG_MINING_BUFFER_EHCACHE_SCHEMA_CHANGES_CONFIG, ""))
+                .replace("${log.mining.buffer.ehcache.events.config}",
+                        configuration.getString(LOG_MINING_BUFFER_EHCACHE_EVENTS_CONFIG, ""));
+    }
 
-        if (valueSerializer != null) {
-            builder.withValueSerializer(valueSerializer);
+    private String readConfigurationTemplate() {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("ehcache/configuration-template.xml")) {
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
+            finally {
+                if (reader != null) {
+                    reader.close();
+                }
+            }
         }
-
-        return builder.build();
+        catch (Exception e) {
+            throw new DebeziumException("Failed to read Ehcache configuration template", e);
+        }
     }
 }
