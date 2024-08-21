f20d05fab9241f9bd64d1d71247d78037d0cb4b8
Jakub Cechacek <jcechace@gmail.com>
2024-08-21 15:22:36 +0200

    DBZ-8160 AsyncEngineSignaler is now initialised with list of channel writers instead of entire engine

--- debezium-embedded/src/main/java/io/debezium/embedded/async/AsyncEngineSignaler.java
+++ debezium-embedded/src/main/java/io/debezium/embedded/async/AsyncEngineSignaler.java
@@ -6,33 +6,20 @@
 package io.debezium.embedded.async;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
-import io.debezium.DebeziumException;
 import io.debezium.engine.DebeziumEngine;
-import io.debezium.engine.source.EngineSourceTask;
 import io.debezium.pipeline.signal.SignalRecord;
-import io.debezium.pipeline.signal.channels.process.InProcessSignalChannel;
+import io.debezium.pipeline.signal.channels.process.SignalChannelWriter;
 
 public class AsyncEngineSignaler implements DebeziumEngine.Signaler {
-    private AsyncEmbeddedEngine<?> engine;
-    private List<InProcessSignalChannel> channels;
+    private final List<SignalChannelWriter> channels;
 
-    public AsyncEngineSignaler(AsyncEmbeddedEngine<?> engine) {
-        this.engine = Objects.requireNonNull(engine);
+    public AsyncEngineSignaler(List<SignalChannelWriter> channels) {
+        this.channels = channels;
     }
-    
+
     @Override
     public void signal(DebeziumEngine.Signal signal) {
-        if (channels == null) {
-            channels = engine.tasks().stream()
-                    .map(EngineSourceTask::debeziumConnectTask)
-                    .flatMap(Optional::stream)
-                    .map(task -> task.getSignalChannel(InProcessSignalChannel.class))
-                    .flatMap(Optional::stream)
-                    .toList();
-        }
         var sr = new SignalRecord(signal);
         channels.forEach(channel -> channel.signal(sr));
     }
