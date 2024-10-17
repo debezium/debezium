68b7bde041d01c0e6a12c745da195d06f145878f
Vojtech Juranek <vjuranek@redhat.com>
2024-10-17 22:44:27 +0200

    DBZ-8331 Add jackson afterburner to classpath

--- debezium-testing/debezium-testing-system/src/main/java/io/debezium/testing/system/tools/artifacts/OcpArtifactServerController.java
+++ debezium-testing/debezium-testing-system/src/main/java/io/debezium/testing/system/tools/artifacts/OcpArtifactServerController.java
@@ -23,6 +23,7 @@ import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.debezium.testing.system.tools.ConfigProperties;
 import io.debezium.testing.system.tools.OpenShiftUtils;
 import io.debezium.testing.system.tools.WaitConditions;
 import io.fabric8.kubernetes.api.model.Pod;
@@ -122,6 +123,9 @@ public class OcpArtifactServerController {
                 "jackson/jackson-module-jaxb-annotations",
                 "jackson/jackson-module-scala_2.13");
         List<String> artifacts = Stream.concat(commonArtifacts.stream(), extraArtifacts.stream()).collect(toList());
+        if (!ConfigProperties.PRODUCT_BUILD) {
+            artifacts.add("jackson/jackson-module-afterburner");
+        }
         return createPlugin("debezium-connector-" + database, artifacts);
     }
 
