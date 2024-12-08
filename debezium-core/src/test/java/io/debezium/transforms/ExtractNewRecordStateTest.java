35cc05a90acfa520ac8322513771feab3092790d
gpanice <giovanni.panice@gmail.com>
2024-12-08 18:41:38 +0100

    DBZ-7094 refactoring test:  remove useless information

--- debezium-core/src/test/java/io/debezium/transforms/ExtractNewRecordStateTest.java
+++ debezium-core/src/test/java/io/debezium/transforms/ExtractNewRecordStateTest.java
@@ -970,7 +970,7 @@ public class ExtractNewRecordStateTest extends AbstractExtractStateTest {
 
             final SourceRecord createRecord = createCreateRecordWithOptionalNull();
             final SourceRecord unwrapped = transform.apply(createRecord);
-            assertThat(((Struct) unwrapped.value()).getInt8("id")).isEqualTo((byte) 1);
+
             assertThat(((Struct) unwrapped.value()).getString("name")).isNull();
         }
     }
@@ -985,7 +985,7 @@ public class ExtractNewRecordStateTest extends AbstractExtractStateTest {
 
             final SourceRecord createRecord = createCreateRecordWithOptionalNull();
             final SourceRecord unwrapped = transform.apply(createRecord);
-            assertThat(((Struct) unwrapped.value()).getInt8("id")).isEqualTo((byte) 1);
+
             assertThat(((Struct) unwrapped.value()).getString("name")).isEqualTo("default_str");
         }
     }
