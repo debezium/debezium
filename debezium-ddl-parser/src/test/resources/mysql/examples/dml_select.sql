8e4fee024e92a6ab2f09cf4c9c5e6f04be352398
Jiri Pechanec <jpechane@redhat.com>
2024-12-19 12:59:32 +0100

    DBZ-8270 Revert "DBZ-8270 Align MySQL tests with upstream grammar; remove MariaDB tests"
    This reverts commit a7c6045f92bbae7cb5efcadd1d84388da7038511.

--- debezium-ddl-parser/src/test/resources/mysql/examples/dml_select.sql
+++ debezium-ddl-parser/src/test/resources/mysql/examples/dml_select.sql
@@ -10,8 +10,8 @@ select 'afdf' "erwhg" "ads" 'dgs' "rter" as tstDiffQuoteConcat;
 select 'some string' COLLATE latin1_danish_ci as tstCollate;
 select _latin1'some string' COLLATE latin1_danish_ci as tstCollate;
 select '\'' as c1, '\"' as c2, '\b' as c3, '\n' as c4, '\r' as c5, '\t' as c6, '\Z' as c7, '\\' as c8, '\%' as c9, '\_' as c10;
-select * from t1 for update;
-select * from t1 lock in share mode;
+select * from t1 for update skip locked;
+select * from t1 lock in share mode nowait;
 #end
 #begin
 -- -- -- String literal spec symbols
@@ -23,6 +23,7 @@ select * from `select` where `varchar` = 'abc \' ' and `varchar2` = '\'bca';
 #begin
 -- -- -- Number literal
 SELECT 1;
+SELECT 1.;
 select 1.e-3 as 123e;
 select del1.e123 as c from del1;
 select -1, 3e-2, 2.34E0;
@@ -31,6 +32,7 @@ SELECT .1e10;
 SELECT -.1e10;
 select 15e3, .2e5 as col1;
 select .2e3 c1, .2e-4 as c5;
+select * from tab where `field` = 1.;
 #end
 #begin
 -- -- -- Number float collision test
@@ -65,9 +67,6 @@ select 'abc' ' bcd' ' \' \' ' as col, \N c2, -.1e-3;
 #begin
 -- -- Variables
 SELECT @myvar;
-select * from t1 limit @varcount;
-select * from t1 limit @varoffset, @varcount;
-select * from t1 limit @varcount offset @varoffset;
 #end
 
 #begin
@@ -122,7 +121,6 @@ SELECT * FROM TABLE1 T1 STRAIGHT_JOIN TABLE2 T2 STRAIGHT_JOIN TABLE3 T3 ON T3.ID
 #begin
 -- where_condition test
 select col1 from t1 inner join t2 on (t1.col1 = t2.col2);
-select table_name from information_schema.TABLES where table_schema = DATABASE();
 #end
 #begin
 -- identifiers tests
@@ -185,17 +183,26 @@ SELECT DISTANCE(b1, b2, "COSINE"), STRING_TO_VECTOR('[]'), VECTOR_DIM(b1), VECTO
 SELECT trigger.num FROM test `trigger`;
 -- Valid when SELECT is in stored procedure
 SELECT * FROM test LIMIT LIMIT1,LIMIT2;
--- SCHEMA as a function name
-SELECT SCHEMA();
 -- Functions
-SELECT REPEAT('X',2);
-SELECT mod(3,2);SELECT * FROM TEST WHERE TB_SCHEMA = SCHEMA();
--- Group By with computed column
-SELECT 1 AS col1, t1.Id FROM t1 GROUP BY col1;
+SELECT mod(3,2);
+SELECT SCHEMA();
 -- Non Aggregate Functions
 SELECT pk, LEAD(pk) OVER (ORDER BY pk) AS l;
 SELECT COALESCE(LAG(last_eq.end_variation) OVER (PARTITION BY eq.account_id, eq.execution_name_id, eq.currency ORDER BY eq.start_date), 0) AS start_variation FROM t1;
+SELECT REPEAT('X',2);
+#begin
 -- Window Functions
+SELECT
+  year, country, product, profit,
+  SUM(profit) OVER() AS total_profit,
+  SUM(profit) OVER(PARTITION BY country) AS country_profit
+FROM sales
+  ORDER BY country, year, product, profit;
+SELECT
+  year, country, product, profit,
+  ROW_NUMBER() OVER(PARTITION BY country) AS row_num1,
+  ROW_NUMBER() OVER(PARTITION BY country ORDER BY year, product) AS row_num2
+FROM sales;
 SELECT
     e.id,
     SUM(e.bin_volume) AS bin_volume,
@@ -218,7 +225,31 @@ SELECT
 FROM table2
     WINDOW w AS (PARTITION BY id, bin_volume ORDER BY id ROWS UNBOUNDED PRECEDING),
            w2 AS (PARTITION BY id, bin_volume ORDER BY id DESC ROWS 10 PRECEDING);
--- Index hints: https://dev.mysql.com/doc/refman/5.7/en/index-hints.html
+#end
+
+#begin
+-- https://dev.mysql.com/doc/refman/8.0/en/lateral-derived-tables.html
+SELECT
+  salesperson.name,
+  max_sale.amount,
+  max_sale.customer_name
+FROM
+  salesperson,
+  LATERAL
+  (SELECT amount, customer_name
+    FROM all_sales
+    WHERE all_sales.salesperson_id = salesperson.id
+    ORDER BY amount DESC LIMIT 1)
+  AS max_sale;
+#end
+
+#begin
+-- From MariaDB 10.1.2, pre-query variables are supported
+-- src: https://mariadb.com/kb/en/set-statement/
+SET STATEMENT some_statement=60 FOR SELECT a FROM some_table;
+#end
+
+-- Index hints: https://dev.mysql.com/doc/refman/8.0/en/index-hints.html
 SELECT * FROM table1 USE INDEX (col1_index,col2_index) WHERE col1=1 AND col2=2 AND col3=3;
 SELECT * FROM table1 FORCE INDEX (col1_index,col2_index) WHERE col1=1 AND col2=2 AND col3=3;
 SELECT * FROM t1 USE INDEX (PRIMARY) ORDER BY a;
@@ -239,6 +270,50 @@ SELECT *
            NESTED PATH '$.b[*]' COLUMNS (b INT PATH '$')
          )
         ) AS tt;
+
+#begin
+---- From MySQL 5.7, withStatement are supported
+--https://dev.mysql.com/doc/refman/8.0/en/with.html
+-- Recursive CTE
+WITH RECURSIVE cte (n) AS (
+  SELECT 1
+  UNION ALL
+  SELECT n + 1 FROM cte WHERE n < 10
+)
+SELECT n FROM cte;
+
+WITH RECURSIVE cte AS (
+    SELECT id, name, manager_id
+    FROM employees
+    WHERE id = 1
+    UNION ALL
+    SELECT e.id, e.name, e.manager_id
+    FROM employees e
+             JOIN cte ON e.manager_id = cte.id
+)
+SELECT * FROM cte;
+
+WITH RECURSIVE cte AS (
+    SELECT id, name, parent_id
+    FROM departments
+    WHERE id = 1
+    UNION ALL
+    SELECT d.id, d.name, d.parent_id
+    FROM departments d
+             JOIN cte ON d.parent_id = cte.id
+)
+SELECT * FROM cte;
+#end
+#begin
+--Non-recursive Ctes
+WITH cte1 AS (
+  SELECT * FROM table1 WHERE col1 = 'value'
+),
+cte2 AS (
+  SELECT * FROM table2 WHERE col2 = 'value'
+)
+SELECT cte1.col1, cte2.col2 FROM cte1 JOIN cte2 ON cte1.id = cte2.id;
+#end
 SELECT !(1 + @sum:=1) AS ss;
 SELECT (@sum:=1 + 1) AS ss;
 SELECT 1 + @sum:=1 AS ss;
@@ -252,6 +327,3 @@ FROM
     (SELECT @sum := 0) AS init
 ORDER BY
     some_order_column;
-
---- statement is supported as the field name
-SELECT REPLACE(statement, ' ','') as statement from your_table;
