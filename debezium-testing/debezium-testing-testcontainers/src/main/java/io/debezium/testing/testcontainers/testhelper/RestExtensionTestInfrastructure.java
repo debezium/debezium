8cd69fad69108616f0e6e9758e9d4e9f6527864c
ani-sha <anishamohanty23@gmail.com>
2025-01-09 17:04:16 +0530

    DBZ-8564 Don't use dockerhub pulling for MongoDB

--- debezium-testing/debezium-testing-testcontainers/src/main/java/io/debezium/testing/testcontainers/testhelper/RestExtensionTestInfrastructure.java
+++ debezium-testing/debezium-testing-testcontainers/src/main/java/io/debezium/testing/testcontainers/testhelper/RestExtensionTestInfrastructure.java
@@ -88,7 +88,7 @@ public class RestExtensionTestInfrastructure {
             .name("rs0")
             .memberCount(1)
             .network(NETWORK)
-            .imageName(DockerImageName.parse("mongo:5.0"))
+            .imageName(DockerImageName.parse("mirror.gcr.io/library/mongo:5.0"))
             .startupTimeout(Duration.ofSeconds(CI_CONTAINER_STARTUP_TIME))
             .build();
 
