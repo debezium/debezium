40239fe8dcb97408c2f5a7ad3e162ae36196d47f
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2025-01-10 12:30:06 +0000

    DBZ-8568 update to latest Go PDK

--- debezium-scripting/debezium-scripting/src/test/resources/wasm/go/filter3.go
+++ debezium-scripting/debezium-scripting/src/test/resources/wasm/go/filter3.go
@@ -6,7 +6,7 @@ import (
 
 //export process
 func process(proxyPtr uint32) uint32 {
-	var headerIdh = debezium.GetUInt32(debezium.Get(proxyPtr, "header.idh.value"))
+	var headerIdh = debezium.GetInt(debezium.Get(proxyPtr, "header.idh.value"))
 
 	// header.idh.value == 1
 	return debezium.SetBool(headerIdh == 1)
