cb43167194bf3b3be0149e75522f85c29a4a6deb
Chris Cranford <chris@hibernate.org>
2025-01-17 00:53:03 -0500

    DBZ-8585 Rename SparseVector to SparseDoubleVector

--- debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/VectorDatabaseTest.java
+++ debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/VectorDatabaseTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import io.debezium.data.vector.SparseVector;
+import io.debezium.data.vector.SparseDoubleVector;
 
 public class VectorDatabaseTest {
 
@@ -20,31 +20,31 @@ public class VectorDatabaseTest {
         final var expectedVector = Map.of((short) 1, 10.0, (short) 11, 20.0, (short) 111, 30.0);
         final var expectedDimensions = (short) 1000;
 
-        var vector = SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30}/1000");
+        var vector = SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30}/1000");
         Assertions.assertThat(vector.getInt16("dimensions")).isEqualTo(expectedDimensions);
         Assertions.assertThat(vector.getMap("vector")).isEqualTo(expectedVector);
 
-        vector = SparseVector.fromLogical(SparseVector.schema(), "{1:10, 11:20, 111:30}/1000");
+        vector = SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10, 11:20, 111:30}/1000");
         Assertions.assertThat(vector.getInt16("dimensions")).isEqualTo(expectedDimensions);
         Assertions.assertThat(vector.getMap("vector")).isEqualTo(expectedVector);
 
-        vector = SparseVector.fromLogical(SparseVector.schema(), " {1:10,11:20,111:30}/1000");
+        vector = SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), " {1:10,11:20,111:30}/1000");
         Assertions.assertThat(vector.getInt16("dimensions")).isEqualTo(expectedDimensions);
         Assertions.assertThat(vector.getMap("vector")).isEqualTo(expectedVector);
 
-        vector = SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30} /1000");
+        vector = SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30} /1000");
         Assertions.assertThat(vector.getInt16("dimensions")).isEqualTo(expectedDimensions);
         Assertions.assertThat(vector.getMap("vector")).isEqualTo(expectedVector);
 
-        vector = SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30}/ 1000");
+        vector = SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30}/ 1000");
         Assertions.assertThat(vector.getInt16("dimensions")).isEqualTo(expectedDimensions);
         Assertions.assertThat(vector.getMap("vector")).isEqualTo(expectedVector);
 
-        vector = SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30}/1000 ");
+        vector = SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30}/1000 ");
         Assertions.assertThat(vector.getInt16("dimensions")).isEqualTo(expectedDimensions);
         Assertions.assertThat(vector.getMap("vector")).isEqualTo(expectedVector);
 
-        vector = SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30 }/1000");
+        vector = SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30 }/1000");
         Assertions.assertThat(vector.getInt16("dimensions")).isEqualTo(expectedDimensions);
         Assertions.assertThat(vector.getMap("vector")).isEqualTo(expectedVector);
 
@@ -52,17 +52,17 @@ public class VectorDatabaseTest {
 
     @Test
     public void shouldIgnoreErrorInSparseVectorFormat() {
-        Assertions.assertThat(SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30}")).isNull();
-        Assertions.assertThat(SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30/1000")).isNull();
-        Assertions.assertThat(SparseVector.fromLogical(SparseVector.schema(), "1:10,11:20,111:30}/1000")).isNull();
-        Assertions.assertThat(SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:30}1000")).isNull();
-        Assertions.assertThat(SparseVector.fromLogical(SparseVector.schema(), "/1000")).isNull();
-        Assertions.assertThat(SparseVector.fromLogical(SparseVector.schema(), "{10,11:20,111:30}/1000")).isNull();
-        Assertions.assertThat(SparseVector.fromLogical(SparseVector.schema(), "{1:10,11#20,111:30}/1000")).isNull();
+        Assertions.assertThat(SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30}")).isNull();
+        Assertions.assertThat(SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30/1000")).isNull();
+        Assertions.assertThat(SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "1:10,11:20,111:30}/1000")).isNull();
+        Assertions.assertThat(SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:30}1000")).isNull();
+        Assertions.assertThat(SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "/1000")).isNull();
+        Assertions.assertThat(SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{10,11:20,111:30}/1000")).isNull();
+        Assertions.assertThat(SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11#20,111:30}/1000")).isNull();
     }
 
     @Test(expected = NumberFormatException.class)
     public void shouldFailOnNumberInSparseVectorFormat() {
-        SparseVector.fromLogical(SparseVector.schema(), "{1:10,11:20,111:x}/1000");
+        SparseDoubleVector.fromLogical(SparseDoubleVector.schema(), "{1:10,11:20,111:x}/1000");
     }
 }
