f082e9127f4a402ca02e3fe96129617507c25879
James Johnston <james.johnston@thumbtack.com>
2025-02-21 06:34:16 +0000

    DBZ-8680 Postgres: Probe streaming connection during blocking snapshots
    If there is no activity on the streaming connection while the blocking
    snapshot is being taken, there is a possibility that the streaming
    connection might be disconnected.
    
    Periodically run a probing query on the streaming connection during the
    blocking snapshot.
    
    This is done by adding an overload of waitSnapshotCompletion which
    allows the user to provide a heartbeat callback function, which is
    regularly called while waiting.  This will allow other databases to
    easily add the functionality as well.

--- debezium-core/src/main/java/io/debezium/pipeline/source/spi/ChangeEventSource.java
+++ debezium-core/src/main/java/io/debezium/pipeline/source/spi/ChangeEventSource.java
@@ -19,12 +19,39 @@ public interface ChangeEventSource {
          */
         boolean isRunning();
 
+        /**
+         * Called to indicate that the snapshot has been completed and that streaming should therefore continue.
+         */
         void resumeStreaming() throws InterruptedException;
 
+        /**
+         * Wait for the resumeStreaming function to be called, which indicates that a snapshot is done
+         * and that streaming should resume.
+         */
         void waitSnapshotCompletion() throws InterruptedException;
 
+        /**
+         * Wait for the resumeStreaming function to be called, which indicates that a snapshot is done
+         * and that streaming should resume.
+         *
+         * @param heartbeatCallback A callback function which will be periodically called while waiting for
+         *                          the snapshot to be completed.  Implementations should be kept simple and
+         *                          relatively fast: only do activities like generating activity on the
+         *                          streaming database connection to prevent idle timeouts.  Implementations
+         *                          should also check their own ElapsedTimeStrategy to control how often any
+         *                          heartbeat activities actually occur.
+         */
+        void waitSnapshotCompletion(Runnable heartbeatCallback) throws InterruptedException;
+
+        /**
+         * Called by the StreamingChangeEventSource to indicate that the streaming has now been paused, and
+         * that no streaming records are being processed anymore.
+         */
         void streamingPaused();
 
+        /**
+         * Wait for the streamingPaused function to be called.
+         */
         void waitStreamingPaused() throws InterruptedException;
     }
 }
