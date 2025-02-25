cca077dde33ddc94effd4e68eabe1cbbe1ea78ea
Vojtech Juranek <vjuranek@redhat.com>
2025-02-25 15:31:46 +0100

    DBZ-8595 Remove unnecesarry refreshes of schema
    Both `connectionCreated()` and `lockTablesForSchemaSnapshot()` are
    called only from `RelationalSnapshotChangeEventSource#doExecute()`.
    Before it we just determine snapshot tables, offset and do the locking
    of the captured tables. We don't need schema for these operations and
    it should be sufficient to refresh the schema only before taking the
    actual snapshot. At this point fo time tables are already lock, so
    we take schema refresh from consitent DB view.

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresSnapshotChangeEventSource.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresSnapshotChangeEventSource.java
@@ -106,7 +106,6 @@ public class PostgresSnapshotChangeEventSource extends RelationalSnapshotChangeE
         if (snapshotterService.getSnapshotter().shouldStreamEventsStartingFromSnapshot() && startingSlotInfo == null) {
             setSnapshotTransactionIsolationLevel(snapshotContext.onDemand);
         }
-        schema.refresh(jdbcConnection, false);
     }
 
     @Override
@@ -141,8 +140,6 @@ public class PostgresSnapshotChangeEventSource extends RelationalSnapshotChangeE
 
             LOGGER.info("Waiting a maximum of '{}' seconds for each table lock", lockTimeout.getSeconds());
             jdbcConnection.executeWithoutCommitting(statements.toString());
-            // now that we have the locks, refresh the schema
-            schema.refresh(jdbcConnection, false);
         }
     }
 
