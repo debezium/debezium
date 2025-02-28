ac3b7ff92d5c23613ce97579e31bb5cda3a60555
minleejae <mmj9808@gmail.com>
2025-02-28 15:26:31 +0900

    DBZ-8727 Implement ErrorHandler to throw RetriableException during SinkTask put operations

--- debezium-sink/src/main/java/io/debezium/sink/spi/ChangeEventSinkRetriableException.java
+++ debezium-sink/src/main/java/io/debezium/sink/spi/ChangeEventSinkRetriableException.java
@@ -1,5 +1,13 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.sink.spi;
 
+/**
+ * This exception is thrown when a retriable error occurs in the ChangeEventSink.
+ */
 public class ChangeEventSinkRetriableException extends RuntimeException {
     public ChangeEventSinkRetriableException(String message) {
         super(message);
