d480f67005b5619c050c9d2341288a5014cc1d1c
Bhagyashree <bhagyashree@confluent.io>
2025-03-11 21:43:31 +0530

    DBZ-8525: Centralise sensitive data logging using the Loggings Class

--- debezium-core/src/main/java/io/debezium/util/Loggings.java
+++ debezium-core/src/main/java/io/debezium/util/Loggings.java
@@ -72,4 +72,34 @@ public class Loggings {
         logger.error(message, t);
         LOGGER.trace("Source of error is record '{}'", record);
     }
+
+    /**
+     * Log an info message and explicitly append the source of the info entry as a separate log
+     * entry that uses trace logging to prevent unintended leaking of sensitive data.
+     *
+     * @param logger the logger instance
+     * @param record the record the log entry is based upon
+     * @param message the info message to be logged
+     * @param arguments the arguments passed to the info message
+     */
+    public static void logInfoAndTraceRecord(Logger logger, Object record, String message,
+                                             Object... arguments) {
+        logger.info(message, arguments);
+        LOGGER.trace("Source of info is record '{}'", record);
+    }
+
+    /**
+     * Log trace message and explicitly append the source of the trace entry as a separate log
+     * entry that uses trace logging to centralise all record loggings.
+     *
+     * @param logger the logger instance
+     * @param record the record the log entry is based upon
+     * @param message the trace message to be logged
+     * @param arguments the arguments passed to the trace message
+     */
+    public static void logTraceAndTraceRecord(Logger logger, Object record, String message,
+                                              Object... arguments) {
+        logger.trace(message, arguments);
+        LOGGER.trace("Source of trace is record '{}'", record);
+    }
 }
