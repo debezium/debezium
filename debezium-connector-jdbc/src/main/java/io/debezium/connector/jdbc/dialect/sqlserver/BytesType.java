c4fe878d6284f2c5ebad96c701d6da0c4437af86
Yuriy Vikulov <yuriyvikulov@gmail.com>
2025-03-18 17:39:29 +0700

    DBZ-8790 fixed failed tests, added new test for varbinary

--- debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/dialect/sqlserver/BytesType.java
+++ debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/dialect/sqlserver/BytesType.java
@@ -5,12 +5,17 @@
  */
 package io.debezium.connector.jdbc.dialect.sqlserver;
 
+import java.nio.ByteBuffer;
 import java.sql.Types;
+import java.util.List;
 
 import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.errors.ConnectException;
 import org.hibernate.engine.jdbc.Size;
 
+import io.debezium.connector.jdbc.ValueBindDescriptor;
 import io.debezium.connector.jdbc.dialect.DatabaseDialect;
+import io.debezium.connector.jdbc.relational.ColumnDescriptor;
 import io.debezium.connector.jdbc.type.AbstractBytesType;
 import io.debezium.connector.jdbc.type.Type;
 import io.debezium.connector.jdbc.util.ByteArrayUtils;
@@ -39,5 +44,26 @@ class BytesType extends AbstractBytesType {
             return dialect.getTypeName(Types.VARBINARY, Size.length(dialect.getMaxVarbinaryLength()));
         }
         return dialect.getTypeName(Types.VARBINARY);
+    }
+
+    @Override
+    public String getQueryBinding(ColumnDescriptor column, Schema schemas, Object value) {
+        return "convert(varbinary(max),?,1)";
+    }
+
+    @Override
+    public List<ValueBindDescriptor> bind(int index, Schema schemas, Object value) {
+        if (value == null) {
+            return List.of(new ValueBindDescriptor(index, null));
+        }
+        if (value instanceof ByteBuffer) {
+            return List.of(new ValueBindDescriptor(index, ((ByteBuffer) value).array()));
+        }
+        if (value instanceof byte[]) {
+            return List.of(new ValueBindDescriptor(index, value));
+        }
+
+        throw new ConnectException(String.format("Unexpected %s value '%s' with type'%s'", getClass().getSimpleName(),
+                value, value.getClass().getSimpleName()));
     }
 }
