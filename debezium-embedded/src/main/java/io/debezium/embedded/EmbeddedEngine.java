0514f2dc79065921aebc7b71fc63acde4755a732
rajdangwal <dangwalrajendra888@gmail.com>
2025-04-14 16:39:23 +0530

    DBZ-8875: Replace deprecated poll and commitRecord methods to support Kafka 4.0

--- debezium-embedded/src/main/java/io/debezium/embedded/EmbeddedEngine.java
+++ debezium-embedded/src/main/java/io/debezium/embedded/EmbeddedEngine.java
@@ -820,7 +820,7 @@ public final class EmbeddedEngine implements DebeziumEngine<SourceRecord>, Embed
 
             @Override
             public synchronized void markProcessed(SourceRecord record) throws InterruptedException {
-                task.commitRecord(record);
+                task.commitRecord(record, null);
                 recordsSinceLastCommit += 1;
                 offsetWriter.offset((Map<String, Object>) record.sourcePartition(), (Map<String, Object>) record.sourceOffset());
             }
