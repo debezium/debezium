98d647a5dec3322dfbb12a6a0c04aca266cd1089
Chris Cranford <chris@hibernate.org>
2025-04-24 14:40:16 -0400

    DBZ-8924 Move offset context loader to buffered package

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/buffered/LogMinerOracleOffsetContextLoader.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/buffered/LogMinerOracleOffsetContextLoader.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.debezium.connector.oracle.logminer;
+package io.debezium.connector.oracle.logminer.buffered;
 
 import java.util.Map;
 
