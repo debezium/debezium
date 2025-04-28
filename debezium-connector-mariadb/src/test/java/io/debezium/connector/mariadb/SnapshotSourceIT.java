bb06974d49e41cf66a00d17213da32151c8024d2
kmos <gpanice@redhat.com>
2025-04-29 01:01:12 +0200

    DBZ-8940 add tests to reproduce the issue

--- debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/SnapshotSourceIT.java
+++ debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/SnapshotSourceIT.java
@@ -5,8 +5,12 @@
  */
 package io.debezium.connector.mariadb;
 
+import org.junit.Test;
+
 import io.debezium.config.Field;
+import io.debezium.connector.binlog.BinlogConnectorConfig;
 import io.debezium.connector.binlog.BinlogSnapshotSourceIT;
+import io.debezium.doc.FixFor;
 
 /**
  * @author Chris Cranford
@@ -25,5 +29,37 @@ public class SnapshotSourceIT extends BinlogSnapshotSourceIT<MariaDbConnector> i
     @Override
     protected String getSnapshotLockingModeNone() {
         return MariaDbConnectorConfig.SnapshotLockingMode.NONE.getValue();
+    }
+
+    @Test
+    @FixFor("DBZ-8940")
+    public void shouldCompleteTheSnapshotWithZeroDateValues() throws InterruptedException {
+        executeStatements(DATABASE.getDatabaseName(),
+                "CREATE TABLE zero_date_table (zero_date DATETIME);",
+                "INSERT INTO zero_date_table VALUES (\"2015-00-12 00:00:00\");");
+
+        config = simpleConfig()
+                .with(BinlogConnectorConfig.TABLE_INCLUDE_LIST, "connector_test_ro_(.*).zero_date_table")
+                .build();
+
+        start(getConnectorClass(), config);
+
+        waitForSnapshotToBeCompleted(getConnectorName(), DATABASE.getServerName());
+    }
+
+    @Test
+    @FixFor("DBZ-8940")
+    public void shouldCompleteTheSnapshotWithDefaultZeroDateValue() throws InterruptedException {
+        executeStatements(DATABASE.getDatabaseName(),
+                "CREATE TABLE zero_date_table (zero_date DATETIME DEFAULT '2015-00-12 00:00:00');",
+                "INSERT INTO zero_date_table VALUES (NULL);");
+
+        config = simpleConfig()
+                .with(BinlogConnectorConfig.TABLE_INCLUDE_LIST, "connector_test_ro_(.*).zero_date_table")
+                .build();
+
+        start(getConnectorClass(), config);
+
+        waitForSnapshotToBeCompleted(getConnectorName(), DATABASE.getServerName());
     }
 }
