9d4690fed2c36bfdf5d073c0df25a93a96745388
Chris Cranford <chris@hibernate.org>
2025-05-02 01:52:11 -0400

    DBZ-8999 Fix test - Enable TRACE logging for `Loggings` class

--- debezium-connector-binlog/src/test/java/io/debezium/relational/history/AbstractKafkaSchemaHistoryTest.java
+++ debezium-connector-binlog/src/test/java/io/debezium/relational/history/AbstractKafkaSchemaHistoryTest.java
@@ -40,7 +40,10 @@ import io.debezium.relational.ddl.DdlParser;
 import io.debezium.storage.kafka.history.KafkaSchemaHistory;
 import io.debezium.text.ParsingException;
 import io.debezium.util.Collect;
+import io.debezium.util.Loggings;
 import io.debezium.util.Testing;
+
+import ch.qos.logback.classic.Level;
 
 /**
  * @author Randall Hauch
@@ -301,6 +304,9 @@ public abstract class AbstractKafkaSchemaHistoryTest<P extends BinlogPartition, 
     @Test
     public void shouldSkipMessageOnDDLFilter() throws Exception {
         String topicName = "stop-on-ddlfilter-schema-changes";
+
+        final LogInterceptor logInterceptor = new LogInterceptor(Loggings.class);
+        logInterceptor.setLoggerLevel(Loggings.class, Level.TRACE);
 
         // Create the empty topic ...
         kafka.createTopic(topicName, 1, 1);
