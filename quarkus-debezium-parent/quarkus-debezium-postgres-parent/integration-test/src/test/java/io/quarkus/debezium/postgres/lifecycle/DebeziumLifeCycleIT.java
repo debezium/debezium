61aca9f63a053566f23b2a6a53e90830f669b45e
kmos <gpanice@redhat.com>
2025-05-03 17:38:48 +0200

    DBZ-8956 introduce tests

---
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/test/java/io/quarkus/debezium/postgres/lifecycle/DebeziumLifeCycleIT.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.quarkus.debezium.postgres.lifecycle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.given;
+
+import java.util.concurrent.TimeUnit;
+
+import jakarta.inject.Inject;
+
+import org.assertj.core.api.Assertions;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.debezium.engine.Debezium;
+import io.quarkus.debezium.engine.DebeziumManifest;
+import io.quarkus.debezium.engine.DebeziumManifest.Connector;
+import io.quarkus.debezium.engine.DebeziumManifest.Status;
+import io.quarkus.debezium.engine.DebeziumManifest.Status.State;
+import io.quarkus.runtime.Application;
+import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.common.QuarkusTestResource;
+
+@QuarkusTestResource(DatabaseTestResource.class)
+class DebeziumLifeCycleIT {
+
+    @Inject
+    Debezium debezium;
+
+    @RegisterExtension
+    static final QuarkusUnitTest application = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class))
+            .overrideConfigKey("quarkus.debezium.configuration.offset.storage", "org.apache.kafka.connect.storage.MemoryOffsetBackingStore")
+            .overrideConfigKey("quarkus.debezium.configuration.name", "test")
+            .overrideConfigKey("quarkus.debezium.configuration.topic.prefix", "dbserver1")
+            .overrideConfigKey("quarkus.debezium.configuration.table.include.list", "inventory.products")
+            .overrideConfigKey("quarkus.debezium.configuration.plugin.name", "pgoutput")
+            .overrideConfigKey("quarkus.debezium.configuration.snapshot.mode", "never")
+            .setLogRecordPredicate(record -> record.getLoggerName().equals("io.quarkus.debezium.engine.DebeziumRunner"))
+            .assertLogRecords((records) -> {
+                assertThat(records.getFirst().getMessage()).isEqualTo("Starting Debezium Engine...");
+                assertThat(records.get(1).getMessage()).isEqualTo("Shutting down Debezium Engine...");
+            });
+
+    @Test
+    @DisplayName("debezium should be integrated in the quarkus lifecycle")
+    void shouldDebeziumBeIntegratedInTheQuarkusLifeCycle() {
+        Assertions.assertThat(debezium.configuration().get("connector.class"))
+                .isEqualTo("io.debezium.connector.postgresql.PostgresConnector");
+
+        given().await()
+                .atMost(10, TimeUnit.SECONDS)
+                .untilAsserted(() -> Assertions.assertThat(debezium.manifest())
+                        .isEqualTo(new DebeziumManifest(new Connector("io.debezium.connector.postgresql.PostgresConnector"),
+                                new Status(State.POLLING))));
+
+        Application.currentApplication().close();
+
+        given().await()
+                .atMost(30, TimeUnit.SECONDS)
+                .untilAsserted(() -> Assertions.assertThat(debezium.manifest().status())
+                        .isEqualTo(new Status(State.STOPPED)));
+    }
+
+}
