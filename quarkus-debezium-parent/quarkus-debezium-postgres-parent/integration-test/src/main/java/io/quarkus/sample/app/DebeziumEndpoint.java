6925e0fd07cf0f9879174ad4de52616ca014babb
kmos <gpanice@redhat.com>
2025-05-05 23:54:50 +0200

    DBZ-8956 add tests and devservice

---
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/main/java/io/quarkus/sample/app/DebeziumEndpoint.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *  * Copyright Debezium Authors.
+ *  *
+ *  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
+package io.quarkus.sample.app;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import io.quarkus.debezium.engine.Debezium;
+import io.quarkus.debezium.engine.DebeziumManifest;
+
+@Path("/api/debezium")
+public class DebeziumEndpoint {
+
+    @Inject
+    private Debezium debezium;
+
+    @GET
+    @Path("manifest")
+    public DebeziumManifest getManifest() {
+        return debezium.manifest();
+    }
+}
---
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/test/java/io/quarkus/debezium/postgres/lifecycle/MyIT.java
@@ -0,0 +1,21 @@
+/*
+ *
+ *  * Copyright Debezium Authors.
+ *  *
+ *  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
+package io.quarkus.debezium.postgres.lifecycle;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class MyIT {
+    @Test
+    void name() {
+        System.out.println("poooooo");
+    }
+}
