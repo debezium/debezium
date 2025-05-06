acf9dfca6f5fbeb7708dcb8ecc2da850e0b269b2
kmos <gpanice@redhat.com>
2025-05-06 21:01:13 +0200

    DBZ-8956 add javadocs

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/Debezium.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/Debezium.java
@@ -6,13 +6,31 @@
 package io.quarkus.debezium.engine;
 
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 
 import io.debezium.engine.DebeziumEngine.Signaler;
 
+/**
+ * The Debezium engine abstraction in the Quarkus CDI
+ * <p>
+ * The Engine is submitted to an {@link Executor} or {@link ExecutorService} for execution by a single thread
+ */
 public interface Debezium {
+
+    /**
+     * @return engine's signaler, if it supports signaling
+     * @throws UnsupportedOperationException if signaling is not supported by this engine
+     */
     Signaler signaler();
 
+    /**
+     * @return engine's configuration
+     */
     Map<String, String> configuration();
 
+    /**
+     * @return engine's status information
+     */
     DebeziumManifest manifest();
 }
