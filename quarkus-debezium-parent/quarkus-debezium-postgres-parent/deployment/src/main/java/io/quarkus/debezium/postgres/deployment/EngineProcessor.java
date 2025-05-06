1707cb1b4f20311b5c624721f0746d8cde996c30
kmos <gpanice@redhat.com>
2025-05-06 11:15:03 +0200

    DBZ-8956 add reflection classes

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/deployment/src/main/java/io/quarkus/debezium/postgres/deployment/EngineProcessor.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/deployment/src/main/java/io/quarkus/debezium/postgres/deployment/EngineProcessor.java
@@ -5,12 +5,48 @@
  */
 package io.quarkus.debezium.postgres.deployment;
 
-import java.net.URI;
-import java.util.Map;
+import org.apache.kafka.common.security.authenticator.SaslClientAuthenticator;
+import org.apache.kafka.connect.json.JsonConverter;
+import org.apache.kafka.connect.transforms.predicates.TopicNameMatches;
 
+import io.debezium.connector.postgresql.PostgresConnector;
+import io.debezium.connector.postgresql.PostgresConnectorTask;
+import io.debezium.connector.postgresql.PostgresSourceInfoStructMaker;
+import io.debezium.connector.postgresql.snapshot.lock.NoSnapshotLock;
+import io.debezium.connector.postgresql.snapshot.lock.SharedSnapshotLock;
+import io.debezium.connector.postgresql.snapshot.query.SelectAllSnapshotQuery;
+import io.debezium.embedded.ConvertingEngineBuilderFactory;
+import io.debezium.embedded.async.ConvertingAsyncEngineBuilderFactory;
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.engine.spi.OffsetCommitPolicy;
+import io.debezium.pipeline.notification.channels.LogNotificationChannel;
+import io.debezium.pipeline.notification.channels.SinkNotificationChannel;
+import io.debezium.pipeline.notification.channels.jmx.JmxNotificationChannel;
+import io.debezium.pipeline.signal.actions.StandardActionProvider;
+import io.debezium.pipeline.signal.channels.FileSignalChannel;
+import io.debezium.pipeline.signal.channels.KafkaSignalChannel;
+import io.debezium.pipeline.signal.channels.SourceSignalChannel;
+import io.debezium.pipeline.signal.channels.jmx.JmxSignalChannel;
+import io.debezium.pipeline.signal.channels.process.InProcessSignalChannel;
+import io.debezium.pipeline.txmetadata.DefaultTransactionMetadataFactory;
+import io.debezium.schema.SchemaTopicNamingStrategy;
+import io.debezium.snapshot.lock.NoLockingSupport;
+import io.debezium.snapshot.mode.AlwaysSnapshotter;
+import io.debezium.snapshot.mode.ConfigurationBasedSnapshotter;
+import io.debezium.snapshot.mode.InitialOnlySnapshotter;
+import io.debezium.snapshot.mode.InitialSnapshotter;
+import io.debezium.snapshot.mode.NeverSnapshotter;
+import io.debezium.snapshot.mode.NoDataSnapshotter;
+import io.debezium.snapshot.mode.RecoverySnapshotter;
+import io.debezium.snapshot.mode.SchemaOnlyRecoverySnapshotter;
+import io.debezium.snapshot.mode.SchemaOnlySnapshotter;
+import io.debezium.snapshot.mode.WhenNeededSnapshotter;
+import io.debezium.snapshot.spi.SnapshotLock;
+import io.debezium.transforms.ExtractNewRecordState;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.processor.DotNames;
 import io.quarkus.datasource.deployment.spi.DevServicesDatasourceResultBuildItem;
+import io.quarkus.debezium.configuration.DebeziumEngineConfiguration;
 import io.quarkus.debezium.producer.PostgresEngineProducer;
 import io.quarkus.debezium.recorder.DebeziumRecorder;
 import io.quarkus.deployment.IsNormal;
@@ -23,7 +59,11 @@ import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
 import io.quarkus.deployment.builditem.ExecutorBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageConfigBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.dev.devservices.DevServicesConfig;
+import io.quarkus.deployment.pkg.steps.NativeOrNativeSourcesBuild;
 
 public class EngineProcessor {
 
@@ -55,17 +95,9 @@ public class EngineProcessor {
             return;
         }
 
-        URI uri = URI.create(datasource.getConfigProperties().get("quarkus.datasource.jdbc.url").substring(5));
-        devServicesProducer.produce(new DevServicesResultBuildItem(
-                "debezium-postgres",
+        devServicesProducer.produce(new DevServicesResultBuildItem("debezium-postgres",
                 "debezium",
-                Map.of(
-                        "quarkus.debezium.configuration.database.hostname", uri.getHost(),
-                        "quarkus.debezium.configuration.database.user", datasource.getConfigProperties().get("quarkus.datasource.username"),
-                        "quarkus.debezium.configuration.database.password", datasource.getConfigProperties().get("quarkus.datasource.password"),
-                        "quarkus.debezium.configuration.database.dbname", uri.getPath().substring(1),
-                        "quarkus.debezium.configuration.database.port",
-                        String.valueOf(uri.getPort()))));
+                QuarkusDatasource.generateDebeziumConfiguration(datasource.getConfigProperties())));
     }
 
     @BuildStep
@@ -78,4 +110,62 @@ public class EngineProcessor {
         recorder.startEngine(executorBuildItem.getExecutorProxy(), shutdownContextBuildItem);
     }
 
+    @BuildStep(onlyIf = NativeOrNativeSourcesBuild.class)
+    void registerClassesThatAreLoadedThroughReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClasses,
+                                                       DebeziumEngineConfiguration debeziumEngineConfiguration) {
+
+        reflectiveClasses.produce(ReflectiveClassBuildItem.builder(
+                DebeziumEngine.BuilderFactory.class,
+                ConvertingEngineBuilderFactory.class,
+                ConvertingAsyncEngineBuilderFactory.class,
+                SaslClientAuthenticator.class,
+                JsonConverter.class,
+                PostgresConnector.class,
+                PostgresSourceInfoStructMaker.class,
+                DefaultTransactionMetadataFactory.class,
+                SchemaTopicNamingStrategy.class,
+                OffsetCommitPolicy.class,
+                PostgresConnectorTask.class,
+                SinkNotificationChannel.class,
+                LogNotificationChannel.class,
+                JmxNotificationChannel.class,
+                SnapshotLock.class,
+                NoLockingSupport.class,
+                NoSnapshotLock.class,
+                SharedSnapshotLock.class,
+                SelectAllSnapshotQuery.class,
+                AlwaysSnapshotter.class,
+                InitialSnapshotter.class,
+                InitialOnlySnapshotter.class,
+                NoDataSnapshotter.class,
+                RecoverySnapshotter.class,
+                WhenNeededSnapshotter.class,
+                NeverSnapshotter.class,
+                SchemaOnlySnapshotter.class,
+                SchemaOnlyRecoverySnapshotter.class,
+                ConfigurationBasedSnapshotter.class,
+                SourceSignalChannel.class,
+                KafkaSignalChannel.class,
+                FileSignalChannel.class,
+                JmxSignalChannel.class,
+                InProcessSignalChannel.class,
+                StandardActionProvider.class,
+                TopicNameMatches.class,
+                OffsetCommitPolicy.class,
+                OffsetCommitPolicy.PeriodicCommitOffsetPolicy.class,
+                ExtractNewRecordState.class).reason(getClass().getName())
+                .build());
+    }
+
+    @BuildStep
+    NativeImageConfigBuildItem nativeImageConfiguration() {
+        return NativeImageConfigBuildItem.builder()
+                .addRuntimeInitializedClass("org.apache.kafka.common.security.authenticator.SaslClientAuthenticator")
+                .build();
+    }
+
+    @BuildStep
+    void registerNativeImageResources(BuildProducer<NativeImageResourceBuildItem> resources) {
+        resources.produce(new NativeImageResourceBuildItem("META-INF/services/*"));
+    }
 }
