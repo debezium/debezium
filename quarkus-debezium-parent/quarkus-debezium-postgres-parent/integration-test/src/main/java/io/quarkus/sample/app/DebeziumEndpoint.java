510c6abf97a01d920aeab766eca451996d7eba06
kmos <gpanice@redhat.com>
2025-05-06 09:50:28 +0200

    DBZ-8956 fix linting

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/main/java/io/quarkus/sample/app/DebeziumEndpoint.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/main/java/io/quarkus/sample/app/DebeziumEndpoint.java
@@ -1,9 +1,7 @@
 /*
+ * Copyright Debezium Authors.
  *
- *  * Copyright Debezium Authors.
- *  *
- *  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
 package io.quarkus.sample.app;
