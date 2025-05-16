b122fa1f4257b3da3b62ceb51276ffb7a207e7c6
kmos <gpanice@redhat.com>
2025-05-16 11:58:42 +0200

    DBZ-8961 introduce capturing event

---
+++ quarkus-debezium-parent/quarkus-debezium-engine-parent/runtime/src/main/java/io/quarkus/debezium/engine/CapturingHandlerProducer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.quarkus.debezium.engine;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import jakarta.enterprise.inject.Instance;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+public class CapturingHandlerProducer {
+
+    @Inject
+    Instance<CapturingInvoker> invokers;
+
+    @Produces
+    @Singleton
+    public DefaultCapturingHandler produce() {
+        CapturingInvokerRegistry capturingInvokerRegistry = new CapturingInvokerRegistry(invokers
+                .stream()
+                .collect(Collectors.toMap(CapturingInvoker::getTable, Function.identity())));
+
+        return new DefaultCapturingHandler(capturingInvokerRegistry);
+    }
+}
