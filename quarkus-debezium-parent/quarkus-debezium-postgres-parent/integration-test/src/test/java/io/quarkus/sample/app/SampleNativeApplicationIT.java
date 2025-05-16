c48108624328fa82b10ced6d09c69d5bb90aaff6
kmos <gpanice@redhat.com>
2025-05-16 19:57:54 +0200

    DBZ-8961 add integration test

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/test/java/io/quarkus/sample/app/SampleNativeApplicationIT.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/test/java/io/quarkus/sample/app/SampleNativeApplicationIT.java
@@ -10,21 +10,36 @@ import static io.restassured.RestAssured.get;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.debezium.runtime.DebeziumStatus;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.RestAssured;
 
 @QuarkusIntegrationTest
 public class SampleNativeApplicationIT {
 
     @Test
-    void smokeTest() {
+    @DisplayName("Debezium should start polling")
+    void shouldDebeziumStartPolling() {
         await().untilAsserted(() -> assertThat(
                 get("/api/debezium/status")
                         .then()
                         .statusCode(200)
                         .extract().body().as(DebeziumStatus.class))
                 .isEqualTo(new DebeziumStatus(DebeziumStatus.State.POLLING)));
+    }
+
+    @Test
+    @DisplayName("Debezium should capture events")
+    void shouldDebeziumSendCaptureEvents() {
+        await().untilAsserted(() -> RestAssured
+                .given()
+                .redirects().follow(false)
+                .when()
+                .get("/api/debezium/captured")
+                .then()
+                .statusCode(302));
     }
 }
