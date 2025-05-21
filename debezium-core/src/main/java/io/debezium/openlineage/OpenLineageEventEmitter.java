c1b4777dffe936a54c442e6f8d825d689d0822f2
Fiore Mario Vitale <mvitale@redhat.com>
2025-05-21 15:21:41 +0200

    DBZ-9020 Add "openlineage.integration.*" properties to CommonConnectorConfig

--- debezium-core/src/main/java/io/debezium/openlineage/OpenLineageEventEmitter.java
+++ debezium-core/src/main/java/io/debezium/openlineage/OpenLineageEventEmitter.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.debezium.Module;
-import io.debezium.config.Configuration;
 import io.openlineage.client.Clients;
 import io.openlineage.client.OpenLineage;
 import io.openlineage.client.OpenLineageClient;
@@ -24,15 +23,18 @@ public class OpenLineageEventEmitter {
     private static final Logger LOGGER = LoggerFactory.getLogger(OpenLineageEventEmitter.class);
 
     private static final String OPEN_LINEAGE_PRODUCER_URI_FORMAT = "https://github.com/debezium/debezium/%s";
+    private static final String NOT_AVAILABLE = "N/A";
+    private static final String SNAPSHOT = "SNAPSHOT";
+    private static final String MAIN_BRANCH_NAME = "main";
+    private static final String VERSION_FORMAT = "v%s";
 
     private final OpenLineageClient openLineageClient;
 
-    public OpenLineageEventEmitter(Configuration config) {
+    public OpenLineageEventEmitter(DebeziumOpenLineageConfiguration config) {
 
-        // TODO Move configs to CommonConnectorConfig
-        if (config.getBoolean("openlineage.integration.enabled", false)) {
+        if (config.enabled()) {
             openLineageClient = Clients.newClient(() -> List.of(
-                    Path.of(config.getString("openlineage.integration.config.path", "."))));
+                    Path.of(config.config().path())));
         }
         else {
             openLineageClient = null;
@@ -62,13 +64,13 @@ public class OpenLineageEventEmitter {
         Package pkg = OpenLineageClient.class.getPackage();
         if (pkg != null) {
             String version = pkg.getImplementationVersion();
-            return version != null ? version : "N/A";
+            return version != null ? version : NOT_AVAILABLE;
         }
-        return "N/A";
+        return NOT_AVAILABLE;
     }
 
     public URI getProducer() {
-        String gitTag = Module.version().contains("SNAPSHOT") ? "main" : String.format("v%s", Module.version());
+        String gitTag = Module.version().contains(SNAPSHOT) ? MAIN_BRANCH_NAME : String.format(VERSION_FORMAT, Module.version());
         return URI.create(String.format(OPEN_LINEAGE_PRODUCER_URI_FORMAT, gitTag));
     }
 }
