a195e72ffdf6d951a11d8a04501b9470397742cb
Fiore Mario Vitale <mvitale@redhat.com>
2025-05-21 18:10:35 +0200

    DBZ-9020 Add json schema for DebeziumConfigFacet

--- debezium-core/src/main/java/io/debezium/openlineage/OpenLineageEmitter.java
+++ debezium-core/src/main/java/io/debezium/openlineage/OpenLineageEmitter.java
@@ -14,6 +14,7 @@ import io.debezium.config.CommonConnectorConfig;
 import io.debezium.config.Configuration;
 import io.debezium.connector.common.BaseSourceTask;
 import io.debezium.jdbc.JdbcConfiguration;
+import io.debezium.openlineage.facets.DebeziumConfigFacet;
 import io.debezium.relational.Table;
 import io.openlineage.client.OpenLineage;
 
