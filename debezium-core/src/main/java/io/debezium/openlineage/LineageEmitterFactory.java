241ce74f5e6f05083db802fa7fa1a2c326ab1697
Fiore Mario Vitale <mvitale@redhat.com>
2025-05-26 14:54:22 +0200

    DBZ-9020 Code refactor to be compatible with quarkus extension native build

---
+++ debezium-core/src/main/java/io/debezium/openlineage/LineageEmitterFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.openlineage;
+
+import io.debezium.config.Configuration;
+
+public interface LineageEmitterFactory {
+
+    LineageEmitter get(Configuration connectorConfig, String connName);
+}
