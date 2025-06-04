fa72c05f099c99d55b5431621ec4504425b93683
Fiore Mario Vitale <mvitale@redhat.com>
2025-06-04 10:22:27 +0200

    DBZ-9020 Add JavaDocs for field that express a formatting

--- debezium-core/src/main/java/io/debezium/openlineage/dataset/DefaultOutputDatasetNamespaceResolver.java
+++ debezium-core/src/main/java/io/debezium/openlineage/dataset/DefaultOutputDatasetNamespaceResolver.java
@@ -12,6 +12,20 @@ import io.debezium.config.Configuration;
 public class DefaultOutputDatasetNamespaceResolver implements OutputDatasetNamespaceResolver {
 
     private static final String LIST_SEPARATOR = ",";
+    /**
+     * Format string for constructing Kafka namespace identifiers according to the OpenLineage specification.
+     * <p>
+     * The namespace format follows the pattern "kafka://{bootstrap server host}:{port}" where:
+     * <ul>
+     * <li>bootstrap server host - the hostname or IP address of the Kafka bootstrap server</li>
+     * <li>port - the port number on which the Kafka bootstrap server is listening</li>
+     * </ul>
+     * <p>
+     * Example usage: {@code String.format(KAFKA_NAMESPACE_FORMAT, "localhost:9092")}
+     * results in "kafka://localhost:9092"
+     *
+     * @see <a href="https://openlineage.io/docs/spec/naming">OpenLineage Naming Convention</a>
+     */
     private static final String KAFKA_NAMESPACE_FORMAT = "kafka://%s";
     private static final String SCHEMA_HISTORY_INTERNAL_KAFKA_BOOTSTRAP_SERVERS_PROPERTY = "schema.history.internal.kafka.bootstrap.servers";
 
