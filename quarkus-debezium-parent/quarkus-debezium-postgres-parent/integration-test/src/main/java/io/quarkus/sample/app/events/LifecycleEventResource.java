19e9baf6f956d1eecfff5151ff9bda323d0a97e8
Chris Cranford <chris@hibernate.org>
2025-06-20 14:37:13 -0400

    DBZ-8959 Add Lifecycle events test

---
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/main/java/io/quarkus/sample/app/events/LifecycleEventResource.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.quarkus.sample.app.events;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import io.debezium.runtime.Debezium;
+
+/**
+ * @author Chris Cranford
+ */
+@Path("/lifecycle-events")
+@ApplicationScoped
+public class LifecycleEventResource {
+
+    @Inject
+    private Debezium debezium;
+
+    @Inject
+    LifecycleEventObserver observer;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> getEvents() {
+        // Returns a list of all observed event names
+        return observer.getLifecycleEvents()
+                .stream()
+                .map(o -> o.getClass().getName())
+                .toList();
+    }
+
+}
