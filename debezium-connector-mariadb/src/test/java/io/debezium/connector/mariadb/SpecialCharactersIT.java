36c2cdc70bc1fef963e72c136405a21399871f5b
Sergei Morozov <morozov@tut.by>
2025-06-21 14:51:58 -0700

    DBZ-9168: Escape special characters in MySQL connector

---
+++ debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/SpecialCharactersIT.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.mariadb;
+
+import io.debezium.connector.binlog.BinlogSpecialCharactersIT;
+
+public class SpecialCharactersIT extends BinlogSpecialCharactersIT<MariaDbConnector> implements MariaDbCommon {
+
+}
