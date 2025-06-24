04a4261b28ea9903a0040269d39050776c280cc5
kmos <gpanice@redhat.com>
2025-06-24 16:11:26 +0200

    DBZ-9176 introduce composite

--- debezium-core/src/main/java/io/debezium/heartbeat/DatabaseHeartbeat.java
+++ debezium-core/src/main/java/io/debezium/heartbeat/DatabaseHeartbeat.java
@@ -6,7 +6,6 @@
 package io.debezium.heartbeat;
 
 import java.sql.SQLException;
-import java.time.Duration;
 import java.util.Map;
 
 import org.apache.kafka.common.config.ConfigDef;
@@ -17,12 +16,11 @@ import org.slf4j.LoggerFactory;
 import io.debezium.config.Field;
 import io.debezium.function.BlockingConsumer;
 import io.debezium.jdbc.JdbcConnection;
-import io.debezium.schema.SchemaNameAdjuster;
 
 /**
  *  Implementation of the heartbeat feature that allows for a DB query to be executed with every heartbeat.
  */
-public class DatabaseHeartbeat extends DefaultHeartbeat {
+public class DatabaseHeartbeat implements Heartbeat {
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHeartbeat.class);
 
     public static final String HEARTBEAT_ACTION_QUERY_PROPERTY_NAME = "heartbeat.action.query";
@@ -39,13 +37,26 @@ public class DatabaseHeartbeat extends DefaultHeartbeat {
     private final JdbcConnection jdbcConnection;
     private final HeartbeatErrorHandler errorHandler;
 
-    public DatabaseHeartbeat(Duration heartbeatInterval, String topicName, String key, JdbcConnection jdbcConnection, String heartBeatActionQuery,
-                             HeartbeatErrorHandler errorHandler, SchemaNameAdjuster schemaNameAdjuster) {
-        super(heartbeatInterval, topicName, key, schemaNameAdjuster);
-
+    public DatabaseHeartbeat(JdbcConnection jdbcConnection, String heartBeatActionQuery,
+                             HeartbeatErrorHandler errorHandler) {
         this.heartBeatActionQuery = heartBeatActionQuery;
         this.jdbcConnection = jdbcConnection;
         this.errorHandler = errorHandler;
+    }
+
+    @Override
+    public void heartbeat(Map<String, ?> partition, Map<String, ?> offset, BlockingConsumer<SourceRecord> consumer) throws InterruptedException {
+        // ignore
+    }
+
+    @Override
+    public void heartbeat(Map<String, ?> partition, OffsetProducer offsetProducer, BlockingConsumer<SourceRecord> consumer) throws InterruptedException {
+        // ignore
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
@@ -57,11 +68,10 @@ public class DatabaseHeartbeat extends DefaultHeartbeat {
             if (errorHandler != null) {
                 errorHandler.onError(e);
             }
-            LOGGER.error("Could not execute heartbeat action (Error: " + e.getSQLState() + ")", e);
+            LOGGER.error("Could not execute heartbeat action (Error: {})", e.getSQLState(), e);
         }
         LOGGER.debug("Executed heartbeat action query");
 
-        super.forcedBeat(partition, offset, consumer);
     }
 
     @Override
