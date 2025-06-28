47373c11fea79e7847efa4561e5959bea4b9935b
kmos <gpanice@redhat.com>
2025-06-28 22:14:27 +0200

    DBZ-8957 Debezium Engine Quarkus Extension: Use Quarkus-style Configuration Properties

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/PostgresEngineProducer.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/PostgresEngineProducer.java
@@ -6,46 +6,81 @@
 
 package io.quarkus.debezium.engine;
 
+import static io.debezium.config.CommonConnectorConfig.DATABASE_CONFIG_PREFIX;
 import static io.debezium.config.CommonConnectorConfig.NOTIFICATION_ENABLED_CHANNELS;
+import static io.debezium.embedded.EmbeddedEngineConfig.CONNECTOR_CLASS;
+import static java.util.Collections.emptyMap;
+
+import java.util.Map;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import org.apache.kafka.connect.source.SourceRecord;
 
+import io.debezium.connector.postgresql.PostgresConnector;
 import io.debezium.engine.RecordChangeEvent;
+import io.debezium.jdbc.JdbcConfiguration;
 import io.debezium.runtime.Connector;
 import io.debezium.runtime.ConnectorProducer;
 import io.debezium.runtime.Debezium;
 import io.debezium.runtime.configuration.DebeziumEngineConfiguration;
+import io.quarkus.debezium.configuration.PostgresDatasourceConfiguration;
 import io.quarkus.debezium.engine.capture.CapturingInvokerRegistry;
 import io.quarkus.debezium.notification.QuarkusNotificationChannel;
 
 @ApplicationScoped
 public class PostgresEngineProducer implements ConnectorProducer {
 
-    public static final String CONNECTOR_CLASS = "connector.class";
-    public static final Connector POSTGRES = new Connector("io.debezium.connector.postgresql.PostgresConnector");
+    public static final Connector POSTGRES = new Connector(PostgresConnector.class.getName());
+    public static final String DEBEZIUM_DATASOURCE_HOSTNAME = DATABASE_CONFIG_PREFIX + JdbcConfiguration.HOSTNAME.name();
+
+    private final CapturingInvokerRegistry<RecordChangeEvent<SourceRecord>> registry;
+    private final StateHandler stateHandler;
+    private final Instance<PostgresDatasourceConfiguration> configurations;
+    private final QuarkusNotificationChannel channel;
 
     @Inject
-    private CapturingInvokerRegistry<RecordChangeEvent<SourceRecord>> registry;
-
-    @Inject
-    StateHandler stateHandler;
-
-    @Inject
-    QuarkusNotificationChannel channel;
+    public PostgresEngineProducer(CapturingInvokerRegistry<RecordChangeEvent<SourceRecord>> registry,
+                                  StateHandler stateHandler,
+                                  Instance<PostgresDatasourceConfiguration> configurations, QuarkusNotificationChannel channel) {
+        this.registry = registry;
+        this.stateHandler = stateHandler;
+        this.configurations = configurations;
+        this.channel = channel;
+    }
 
     @Produces
     @Singleton
     public Debezium engine(DebeziumEngineConfiguration debeziumEngineConfiguration) {
-        debeziumEngineConfiguration.configuration().put(CONNECTOR_CLASS, POSTGRES.name());
-        debeziumEngineConfiguration.configuration().compute(NOTIFICATION_ENABLED_CHANNELS.name(),
-                (key, value) -> value == null ? channel.name() : value.concat("," + channel.name()));
+        Map<String, String> configurationMap = debeziumEngineConfiguration.configuration();
 
-        return new SourceRecordDebezium(debeziumEngineConfiguration,
+        configurationMap.compute(NOTIFICATION_ENABLED_CHANNELS.name(),
+                (key, value) -> value == null ? channel.name() : value.concat("," + channel.name()));
+        configurationMap.put(CONNECTOR_CLASS.name(), POSTGRES.name());
+
+        if (configurationMap.get(DEBEZIUM_DATASOURCE_HOSTNAME) != null) {
+            return new SourceRecordDebezium(configurationMap,
+                    stateHandler,
+                    POSTGRES,
+                    registry);
+        }
+
+        /**
+         * it's possible to manage multiple configurations and multiple Debezium Instances
+         * the {@link engine(DebeziumEngineConfiguration debeziumEngineConfiguration)} should return
+         * a registry of Debezium instances
+         */
+        configurationMap.putAll(configurations
+                .stream()
+                .findFirst()
+                .map(PostgresDatasourceConfiguration::asDebezium)
+                .orElse(emptyMap()));
+
+        return new SourceRecordDebezium(configurationMap,
                 stateHandler,
                 POSTGRES,
                 registry);
