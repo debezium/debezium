49f60de5041e591319ea1cf8492cf59a41b8bceb
kmos <gpanice@redhat.com>
2025-06-30 19:23:43 +0200

    DBZ-8964 wiring in CDI QuarkusNotificationChannel

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/PostgresEngineProducer.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/PostgresEngineProducer.java
@@ -6,6 +6,8 @@
 
 package io.quarkus.debezium.engine;
 
+import static io.debezium.config.CommonConnectorConfig.NOTIFICATION_ENABLED_CHANNELS;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
@@ -19,6 +21,7 @@ import io.debezium.runtime.ConnectorProducer;
 import io.debezium.runtime.Debezium;
 import io.debezium.runtime.configuration.DebeziumEngineConfiguration;
 import io.quarkus.debezium.engine.capture.CapturingInvokerRegistry;
+import io.quarkus.debezium.notification.QuarkusNotificationChannel;
 
 @ApplicationScoped
 public class PostgresEngineProducer implements ConnectorProducer {
@@ -32,10 +35,15 @@ public class PostgresEngineProducer implements ConnectorProducer {
     @Inject
     StateHandler stateHandler;
 
+    @Inject
+    QuarkusNotificationChannel channel;
+
     @Produces
     @Singleton
     public Debezium engine(DebeziumEngineConfiguration debeziumEngineConfiguration) {
         debeziumEngineConfiguration.configuration().put(CONNECTOR_CLASS, POSTGRES.name());
+        debeziumEngineConfiguration.configuration().compute(NOTIFICATION_ENABLED_CHANNELS.name(),
+                (key, value) -> value == null ? channel.name() : value.concat("," + channel.name()));
 
         return new SourceRecordDebezium(debeziumEngineConfiguration,
                 stateHandler,
