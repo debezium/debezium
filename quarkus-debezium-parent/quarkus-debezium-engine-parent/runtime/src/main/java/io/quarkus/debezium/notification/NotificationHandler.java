652f2951ab89de65c90ac1621b96a50652d9a14a
kmos <gpanice@redhat.com>
2025-07-03 01:10:45 +0200

    DBZ-8964 docs

--- quarkus-debezium-parent/quarkus-debezium-engine-parent/runtime/src/main/java/io/quarkus/debezium/notification/NotificationHandler.java
+++ quarkus-debezium-parent/quarkus-debezium-engine-parent/runtime/src/main/java/io/quarkus/debezium/notification/NotificationHandler.java
@@ -6,8 +6,23 @@
 
 package io.quarkus.debezium.notification;
 
+/**
+ *
+ * {@link NotificationHandler} is used to manage Debezium Notification
+ *
+ */
 public interface NotificationHandler {
+
+    /**
+     * Define the handler availability {@param aggregateType}
+     * @param aggregateType
+     * @return
+     */
     boolean isAvailable(String aggregateType);
 
+    /**
+     * handle the Debezium Notification
+     * @param notification
+     */
     void handle(io.debezium.pipeline.notification.Notification notification);
 }
