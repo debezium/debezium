c9a0a3362617648f143c0dae445325cce309fef0
kmos <gpanice@redhat.com>
2025-07-07 14:41:51 +0200

    DBZ-8966 add interfaces and annotation for CustomConverter and filtering

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/deployment/src/test/resources/init.sql
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/deployment/src/test/resources/init.sql
@@ -23,8 +23,15 @@ CREATE TABLE injected(
     name TEXT NOT NULL
 );
 
+CREATE TABLE convert_table(
+    id SERIAL PRIMARY KEY,
+    num FLOAT NOT NULL
+);
+
 INSERT INTO products (name) VALUES ('t-shirt'), ('smartphone');
 INSERT INTO orders (name) VALUES ('one'), ('two');
 INSERT INTO users (name) VALUES ('giovanni'), ('mario');
 INSERT INTO not_assigned (name) VALUES ('something'), ('should'), ('happens');
 INSERT INTO injected (name) VALUES ('called'), ('without'), ('injecting');
+INSERT INTO injected (name) VALUES ('called'), ('without'), ('injecting');
+INSERT INTO convert_table (num) VALUES (1), (2), (3);
