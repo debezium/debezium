3cd2cbcfbc95205ad83a853823dd5a660a188f2c
pranavt84 <pranavt84@gmail.com>
2025-07-14 21:50:10 +0530

    DBZ-8582 Support MariaDB 11.7+ vector data type in JDBCSinkConnector

---
+++ debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/dialect/mariadb/FloatVectorType.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.jdbc.dialect.mariadb;
+
+import java.util.Optional;
+
+import org.apache.kafka.connect.data.Schema;
+
+import io.debezium.connector.jdbc.type.debezium.AbstractFloatVectorType;
+import io.debezium.sink.column.ColumnDescriptor;
+
+/**
+ * An implementation of {@link AbstractFloatVectorType} for MariaDB's {@code vector} data type.
+ *
+ * @author Pranav Tiwari
+ */
+public class FloatVectorType extends AbstractFloatVectorType {
+
+    public static FloatVectorType INSTANCE = new FloatVectorType();
+
+    @Override
+    public String getTypeName(Schema schema, boolean isKey) {
+        final Optional<String> size = getSourceColumnSize(schema);
+        return size.map(s -> String.format("vector(%s)", s)).orElse("vector(2048)");
+    }
+
+    @Override
+    public String getQueryBinding(ColumnDescriptor column, Schema schema, Object value) {
+        return "VEC_FromText(?)";
+    }
+}
