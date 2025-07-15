bdb1067e9dc6f469024b345de639e27020977d42
kmos <gpanice@redhat.com>
2025-07-15 21:10:00 +0200

    DBZ-8960 test native for Arc Heartbeat

---
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/test/java/io/quarkus/sample/app/events/HeartbeatEventIT.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.quarkus.sample.app.events;
+
+import static io.restassured.RestAssured.get;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.debezium.runtime.events.DebeziumHeartbeat;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HeartbeatEventIT {
+
+    @Test
+    @DisplayName("should get an heartbeat")
+    void shouldGetHeartbeat() {
+        await().untilAsserted(() -> assertThat(
+                get("/heartbeat")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(DebeziumHeartbeat.class).connector().name())
+                .isEqualTo("io.debezium.connector.postgresql.PostgresConnector"));
+    }
+}
