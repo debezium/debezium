97e3279f71a6b4e56caab3ae92a60ea4daf0f3ba
pranavt84 <pranavt84@gmail.com>
2025-07-25 22:07:05 +0530

    DBZ-8470: Added copyright.

--- debezium-core/src/main/java/io/debezium/data/TsVector.java
+++ debezium-core/src/main/java/io/debezium/data/TsVector.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package io.debezium.data;
 
 import org.apache.kafka.connect.data.SchemaBuilder;
