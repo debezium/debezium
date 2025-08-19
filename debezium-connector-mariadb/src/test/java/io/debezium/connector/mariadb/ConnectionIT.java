62622ab1c2fd210b4e467718e2167cae89393941
Nick Chomey <88559987+nickchomey@users.noreply.github.com>
2025-08-19 09:55:07 -0600

    debezium/dbz#2259 Support Mariadb 12.3 InnoDB binlog
    Signed-off-by: Nick Chomey <88559987+nickchomey@users.noreply.github.com>

--- debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/ConnectionIT.java
+++ debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/ConnectionIT.java
@@ -32,7 +32,7 @@ public class ConnectionIT extends BinlogConnectionIT<MariaDbConnector> implement
 
             assertThatThrownBy(() -> conn.execute("SELECT SLEEP(10)"))
                     .isInstanceOf(SQLTimeoutException.class)
-                    .hasMessageContaining("Query execution was interrupted (max_statement_time exceeded)");
+                    .hasMessageContaining("Query was interrupted");
 
         }
     }
