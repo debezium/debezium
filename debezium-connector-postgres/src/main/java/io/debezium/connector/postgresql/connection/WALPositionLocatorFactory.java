8e54c8091bdf67eeb0765982207f9df74a76c5d1
pranavt84 <pranavt84@gmail.com>
2025-08-27 15:27:04 +0530

    DBZ-9309- Added streaming support in postgres logical replication.

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/WALPositionLocatorFactory.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/WALPositionLocatorFactory.java
@@ -1,6 +1,26 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package io.debezium.connector.postgresql.connection;
 
 import io.debezium.connector.postgresql.PostgresConnectorConfig;
+
+/**
+ * Factory class for creating {@link PositionLocator} instances based on the configured
+ * logical replication mode.
+ * <p>
+ * If the replication mode is STREAMING, a {@link WalPositionLocatorStreaming} is returned;
+ * otherwise, a {@link WalPositionLocator} is returned.
+ * </p>
+ *
+ * This helps abstract away the construction logic based on configuration, making the rest of the
+ * codebase cleaner and easier to maintain.
+ *
+ * @author Pranav Tiwari
+ */
 
 public class WALPositionLocatorFactory {
 
