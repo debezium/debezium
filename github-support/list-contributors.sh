781880cc4207c71005c13a66786096d36d29ce21
Chris Cranford <chris@hibernate.org>
2025-09-04 14:23:47 -0400

    [ci] Fix typo in list-contributors script

--- github-support/list-contributors.sh
+++ github-support/list-contributors.sh
@@ -20,7 +20,7 @@ CONTRIBUTORS_FILTERS="$DIR/FilteredNames.txt"
 
 cp $ALIASES $FILTERS $DIR && cd $DIR
 
-declare -a DEBEZIUM_REPOS=("debezium" "debezium-server" "debezium-operator" "debezium-connector-db2" "debezium-connector-cassandra" "debezium-connector-vitess" "debezium-connector-spanner" "debezium-ui" "container-images" "debezium-connector-informix" "debezium-connector-ibmi" "debezium-connector-cockroachdb" "debezium-operator" "debezium-examples"")
+declare -a DEBEZIUM_REPOS=("debezium" "debezium-server" "debezium-operator" "debezium-connector-db2" "debezium-connector-cassandra" "debezium-connector-vitess" "debezium-connector-spanner" "debezium-ui" "container-images" "debezium-connector-informix" "debezium-connector-ibmi" "debezium-connector-cockroachdb" "debezium-operator" "debezium-examples")
 
 for REPO in "${DEBEZIUM_REPOS[@]}";
 do
