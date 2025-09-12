752f74d6ca55cc6b81568224690c9bcc416f2188
AlvarVG <alvigo92@gmail.com>
2025-09-12 11:59:59 +0200

    DBZ-9462 Prepare the code to use kraft or zookeeper

--- debezium-testing/debezium-testing-system/src/main/java/io/debezium/testing/system/tools/kafka/DockerKafkaDeployer.java
+++ debezium-testing/debezium-testing-system/src/main/java/io/debezium/testing/system/tools/kafka/DockerKafkaDeployer.java
@@ -7,9 +7,13 @@ package io.debezium.testing.system.tools.kafka;
 
 import java.util.stream.Stream;
 
+import org.apache.maven.artifact.versioning.ComparableVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.lifecycle.Startables;
 
 import io.debezium.testing.system.tools.AbstractDockerDeployer;
+import io.debezium.testing.system.tools.ConfigProperties;
 import io.debezium.testing.system.tools.Deployer;
 import io.debezium.testing.system.tools.kafka.docker.KafkaContainer;
 import io.debezium.testing.system.tools.kafka.docker.ZookeeperContainer;
@@ -17,6 +21,8 @@ import io.debezium.testing.system.tools.kafka.docker.ZookeeperContainer;
 public class DockerKafkaDeployer
         extends AbstractDockerDeployer<DockerKafkaController, KafkaContainer>
         implements Deployer<DockerKafkaController> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DockerKafkaDeployer.class);
 
     public DockerKafkaDeployer(KafkaContainer container) {
         super(container);
@@ -29,14 +35,29 @@ public class DockerKafkaDeployer
 
     @Override
     public DockerKafkaController deploy() {
-        ZookeeperContainer zookeeperContainer = new ZookeeperContainer()
-                .withNetwork(container.getNetwork());
-        container.withZookeeper(zookeeperContainer);
+        DockerKafkaController controller;
 
-        Startables.deepStart(Stream.of(zookeeperContainer, container)).join();
-        DockerKafkaController controller = getController(container);
-        controller.setZookeeperContainer(zookeeperContainer);
+        if (!shouldKRaftBeUsed()) {
+            ZookeeperContainer zookeeperContainer = new ZookeeperContainer()
+                    .withNetwork(container.getNetwork());
+            container.withZookeeper(zookeeperContainer);
+            Startables.deepStart(Stream.of(zookeeperContainer, container)).join();
+
+            controller = getController(container);
+            controller.setZookeeperContainer(zookeeperContainer);
+            LOGGER.info("Using Kafka in Zookeeper mode");
+        }
+        else {
+            controller = getController(container);
+            LOGGER.info("Using Kafka in KRaft mode");
+        }
+
         return controller;
+    }
+
+    private boolean shouldKRaftBeUsed() {
+        ComparableVersion kafkaVersion = new ComparableVersion(ConfigProperties.VERSION_KAFKA);
+        return ConfigProperties.FORCE_KRAFT || kafkaVersion.compareTo(new ComparableVersion("4.0.0")) >= 0;
     }
 
     public static class Builder
