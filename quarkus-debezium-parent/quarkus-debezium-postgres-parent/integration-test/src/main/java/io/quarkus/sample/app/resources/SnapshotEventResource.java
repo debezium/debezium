8e932348a92de1e9ab35d195c531ee631edbfef2
kmos <giovanni.panice@gmail.com>
2025-09-12 19:25:17 +0200

    DBZ-9160 address notification in a multiengine environment

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/main/java/io/quarkus/sample/app/resources/SnapshotEventResource.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/main/java/io/quarkus/sample/app/resources/SnapshotEventResource.java
@@ -6,9 +6,11 @@
 package io.quarkus.sample.app.resources;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -26,13 +28,13 @@ public class SnapshotEventResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response notification() {
-        if (snapshotEventObserver.getSnapshotEvents().isEmpty()) {
+    public Response notification(@QueryParam("engine") @DefaultValue("default") String engine) {
+        if (snapshotEventObserver.getDefaultSnapshotEvents("default").isEmpty()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
         return Response.ok(snapshotEventObserver
-                .getSnapshotEvents()
+                .getDefaultSnapshotEvents(engine)
                 .stream()
                 .map(a -> a.getClass().getName()))
                 .build();
