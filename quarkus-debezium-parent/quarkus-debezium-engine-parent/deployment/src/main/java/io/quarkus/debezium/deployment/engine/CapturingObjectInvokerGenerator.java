d10a517ec0cd317c554197165aed21177bd3092f
kmos <giovanni.panice@gmail.com>
2025-09-18 11:43:19 +0200

    DBZ-9160 reduce duplicate code for invokers

--- quarkus-debezium-parent/quarkus-debezium-engine-parent/deployment/src/main/java/io/quarkus/debezium/deployment/engine/CapturingObjectInvokerGenerator.java
+++ quarkus-debezium-parent/quarkus-debezium-engine-parent/deployment/src/main/java/io/quarkus/debezium/deployment/engine/CapturingObjectInvokerGenerator.java
@@ -6,7 +6,6 @@
 
 package io.quarkus.debezium.deployment.engine;
 
-import java.lang.reflect.Modifier;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,7 +14,6 @@ import org.jboss.jandex.MethodInfo;
 import org.jboss.jandex.ParameterizedType;
 import org.jboss.jandex.Type;
 
-import io.debezium.runtime.Capturing;
 import io.quarkus.arc.processor.BeanInfo;
 import io.quarkus.arc.processor.DotNames;
 import io.quarkus.debezium.deployment.dotnames.DebeziumDotNames;
@@ -24,11 +22,9 @@ import io.quarkus.gizmo.ClassCreator;
 import io.quarkus.gizmo.ClassOutput;
 import io.quarkus.gizmo.FieldDescriptor;
 import io.quarkus.gizmo.MethodCreator;
-import io.quarkus.gizmo.MethodDescriptor;
-import io.quarkus.gizmo.ResultHandle;
 import io.quarkus.runtime.util.HashUtil;
 
-public class CapturingObjectInvokerGenerator implements CapturingInvokerGenerator {
+public class CapturingObjectInvokerGenerator implements GizmoBasedCapturingInvokerGenerator {
 
     private final ClassOutput output;
 
@@ -67,36 +63,9 @@ public class CapturingObjectInvokerGenerator implements CapturingInvokerGenerato
                 .interfaces(CapturingObjectInvoker.class)
                 .build()) {
 
-            FieldDescriptor beanInstanceField = invoker.getFieldCreator("beanInstance", methodInfo
-                    .declaringClass()
-                    .name()
-                    .toString())
-                    .setModifiers(Modifier.PRIVATE)
-                    .getFieldDescriptor();
+            FieldDescriptor beanInstanceField = constructorWithObjectField(methodInfo, invoker, "beanInstance");
 
-            try (MethodCreator constructor = invoker.getMethodCreator("<init>", void.class, Object.class)) {
-                constructor.setModifiers(Modifier.PUBLIC);
-                constructor.invokeSpecialMethod(MethodDescriptor.ofConstructor(Object.class), constructor.getThis());
-                ResultHandle constructorThis = constructor.getThis();
-                ResultHandle beanInstance = constructor.getMethodParam(0);
-                constructor.writeInstanceField(beanInstanceField, constructorThis, beanInstance);
-                constructor.returnValue(null);
-            }
-
-            try (MethodCreator capture = invoker.getMethodCreator("capture", void.class, Object.class)) {
-                ResultHandle captureThis = capture.getThis();
-                ResultHandle delegate = capture.readInstanceField(beanInstanceField, captureThis);
-                ResultHandle event = capture.getMethodParam(0);
-
-                MethodDescriptor methodDescriptor = MethodDescriptor.ofMethod(
-                        methodInfo.declaringClass().toString(),
-                        methodInfo.name(),
-                        "V",
-                        methodInfo.parameterType(0).name().toString());
-
-                capture.invokeVirtualMethod(methodDescriptor, delegate, event);
-                capture.returnVoid();
-            }
+            createCaptureMethod(methodInfo, invoker, beanInstanceField, Object.class);
 
             try (MethodCreator destination = invoker.getMethodCreator("destination", String.class)) {
                 Optional.ofNullable(methodInfo
@@ -114,19 +83,7 @@ public class CapturingObjectInvokerGenerator implements CapturingInvokerGenerato
                                 });
             }
 
-            try (MethodCreator group = invoker.getMethodCreator("group", String.class)) {
-                Optional.ofNullable(methodInfo
-                        .annotation(DebeziumDotNames.CAPTURING)
-                        .value("group"))
-                        .map(AnnotationValue::asString)
-                        .ifPresentOrElse(s -> {
-                            if (s.isEmpty()) {
-                                throw new IllegalArgumentException("empty groups are not allowed for @Capturing annotation  " + methodInfo.declaringClass());
-                            }
-                            group.returnValue(group.load(s));
-                        },
-                                () -> group.returnValue(group.load(Capturing.DEFAULT)));
-            }
+            createGroupMethod(methodInfo, invoker);
 
             return new GeneratedClassMetaData(UUID.randomUUID(), name.replace('/', '.'), beanInfo, CapturingObjectInvoker.class);
         }
