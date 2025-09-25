b8ccfa4280b803dbeffcad54e1af5558bad7ec13
kmos <kmos@commonhaus.dev>
2025-09-25 12:02:13 +0200

    DBZ-9382 add DebeziumLifeCycleTest

---
+++ quarkus-debezium-parent/quarkus-debezium-mongodb-parent/deployment/src/test/java/io/quarkus/debezium/mongodb/deployment/DebeziumLifeCycleTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.quarkus.debezium.mongodb.deployment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.given;
+
+import java.util.concurrent.TimeUnit;
+
+import jakarta.inject.Inject;
+
+import org.assertj.core.api.Assertions;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.debezium.runtime.DebeziumConnectorRegistry;
+import io.debezium.runtime.DebeziumStatus;
+import io.debezium.runtime.EngineManifest;
+import io.quarkus.runtime.Application;
+import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.common.QuarkusTestResource;
+
+@QuarkusTestResource(value = MongoDbTestResource.class)
+public class DebeziumLifeCycleTest {
+
+    @Inject
+    DebeziumConnectorRegistry registry;
+
+    @RegisterExtension
+    static final QuarkusUnitTest application = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class))
+            .overrideConfigKey("quarkus.debezium.offset.storage", "org.apache.kafka.connect.storage.MemoryOffsetBackingStore")
+            .overrideConfigKey("quarkus.debezium.name", "test")
+            .overrideConfigKey("quarkus.debezium.topic.prefix", "dbserver1")
+            .overrideConfigKey("quarkus.debezium.snapshot.mode", "no_data")
+            .overrideConfigKey("quarkus.datasource.devservices.enabled", "false")
+            .setLogRecordPredicate(record -> record.getLoggerName().equals("io.quarkus.debezium.engine.DebeziumRunner"))
+            .assertLogRecords((records) -> {
+                assertThat(records.getFirst().getMessage()).isEqualTo("Starting Debezium Engine dbz-default-1");
+                assertThat(records.get(1).getMessage()).isEqualTo("Shutting down Debezium Engine dbz-default-1");
+            });
+
+    @Test
+    @DisplayName("debezium should be integrated in the quarkus lifecycle")
+    void shouldDebeziumBeIntegratedInTheQuarkusLifeCycle() {
+        Assertions.assertThat(registry.get(new EngineManifest("default")).configuration().get("connector.class"))
+                .isEqualTo("io.debezium.connector.mongodb.MongoDbConnector");
+
+        given().await()
+                .atMost(10, TimeUnit.SECONDS)
+                .untilAsserted(() -> Assertions.assertThat(registry.get(new EngineManifest("default")).status())
+                        .isEqualTo(new DebeziumStatus(DebeziumStatus.State.POLLING)));
+
+        Application.currentApplication().close();
+
+        given().await()
+                .atMost(30, TimeUnit.SECONDS)
+                .untilAsserted(() -> Assertions.assertThat(registry.get(new EngineManifest("default")).status())
+                        .isEqualTo(new DebeziumStatus(DebeziumStatus.State.STOPPED)));
+    }
+}
