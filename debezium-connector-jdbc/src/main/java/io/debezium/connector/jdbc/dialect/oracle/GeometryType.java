19e5f2f98075ce7a7db05d6fbbf3bbf22872d6b0
PIATEK Marcin <marcin.piatek@eurocontrol.int>
2025-09-29 13:13:21 +0000

    DBZ-9508 Added support for SDO_GEOMETRY columns in jdbc-connector.

---
+++ debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/dialect/oracle/GeometryType.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.jdbc.dialect.oracle;
+
+import org.apache.kafka.connect.data.Schema;
+
+import io.debezium.connector.jdbc.dialect.DatabaseDialect;
+import io.debezium.connector.jdbc.type.AbstractGeoType;
+import io.debezium.connector.jdbc.type.JdbcType;
+import io.debezium.data.geometry.Geometry;
+import io.debezium.sink.SinkConnectorConfig;
+import io.debezium.sink.column.ColumnDescriptor;
+
+public class GeometryType extends AbstractGeoType {
+
+    public static final JdbcType INSTANCE = new GeometryType();
+
+    static final String GEO_FROM_WKB_FUNCTION = "SDO_UTIL.FROM_WKBGEOMETRY(TO_BLOB(?), ?)";
+    private static final String TYPE_NAME = "MDSYS.SDO_GEOMETRY";
+
+    @Override
+    public void configure(SinkConnectorConfig config, DatabaseDialect dialect) {
+        super.configure(config, dialect);
+    }
+
+    @Override
+    public String getQueryBinding(ColumnDescriptor column, Schema schema, Object value) {
+        return GEO_FROM_WKB_FUNCTION;
+    }
+
+    @Override
+    public String[] getRegistrationKeys() {
+        return new String[]{ Geometry.LOGICAL_NAME };
+    }
+
+    @Override
+    public String getTypeName(Schema schema, boolean isKey) {
+        return TYPE_NAME;
+    }
+}
