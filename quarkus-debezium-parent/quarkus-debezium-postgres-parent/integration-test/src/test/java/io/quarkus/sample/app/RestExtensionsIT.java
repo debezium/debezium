5da70f871bc1c3ed3a954e5012a5b9b1f57dbc86
Stefano Paolucci <stefano.paolucci91@gmail.com>
2025-10-04 16:01:48 +0200

    DBZ-7174 Add Apache license header to RestExtensionsIT.java

--- quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/test/java/io/quarkus/sample/app/RestExtensionsIT.java
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/integration-test/src/test/java/io/quarkus/sample/app/RestExtensionsIT.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package io.quarkus.sample.app;
 
 import static io.restassured.RestAssured.get;
