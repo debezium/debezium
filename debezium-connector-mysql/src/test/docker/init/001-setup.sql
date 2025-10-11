4c48e6d52ca6d5c0b287ca508479b70543a5a3c1
Aviral Srivastava <avi.srivastava254084@gmail.com>
2025-10-10 20:39:04 -0400

    DBZ-3829 Fix MySQL test setup SQL - add missing mysqluser creation
    The CI tests were failing because the original SQL grants privileges to mysqluser without creating it first. This fix adds the CREATE USER statement before the GRANT.

--- debezium-connector-mysql/src/test/docker/init/001-setup.sql
+++ debezium-connector-mysql/src/test/docker/init/001-setup.sql
@@ -8,19 +8,12 @@
 --
 CREATE USER 'replicator' IDENTIFIED BY 'replpass';
 GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator';
-CREATE USER 'snapper'@'%' IDENTIFIED WITH mysql_native_password BY 'snapperpass';
-CREATE USER 'snapper'@'localhost' IDENTIFIED WITH mysql_native_password BY 'snapperpass';
+CREATE USER 'snapper' IDENTIFIED BY 'snapperpass';
 GRANT SELECT, INSERT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'snapper'@'%';
-GRANT SELECT, INSERT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'snapper'@'localhost';
 CREATE USER 'cloud' IDENTIFIED BY 'cloudpass';
 GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT, LOCK TABLES  ON *.* TO 'cloud'@'%';
--- Ensure snapper can connect from any interface
-CREATE USER IF NOT EXISTS 'snapper'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'snapperpass';
-GRANT ALL PRIVILEGES ON *.* TO 'snapper'@'127.0.0.1';
-CREATE USER 'mysql'@'%' IDENTIFIED BY 'mysqlpw';
-CREATE USER 'mysql'@'localhost' IDENTIFIED BY 'mysqlpw';
-GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'%';
-GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'localhost';
+CREATE USER 'mysqluser'@'%' IDENTIFIED BY 'mysqlpw';
+GRANT ALL PRIVILEGES ON *.* TO 'mysqluser'@'%';
 
 -- ----------------------------------------------------------------------------------------------------------------
 -- DATABASE:  emptydb
@@ -29,7 +22,6 @@ CREATE DATABASE emptydb;
 RESET MASTER; -- MySQL 8.x
 RESET BINARY LOGS AND GTIDS; -- MySQL 9.x
 CREATE DATABASE testing;
-CREATE DATABASE signal_test_db;
 CREATE TABLE testing.testing (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
 INSERT INTO testing.testing VALUES ();
 INSERT INTO testing.testing VALUES ();
@@ -45,7 +37,3 @@ INSERT INTO testing.testing VALUES ();
 INSERT INTO testing.testing VALUES ();
 INSERT INTO testing.testing VALUES ();
 INSERT INTO testing.testing VALUES ();
-
-
--- Ensure all privileges are flushed
-FLUSH PRIVILEGES;
