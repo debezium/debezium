8e1c59a7e475101f678bfcff3ad763d59bc89b81
kmos <kmos@commonhaus.dev>
2025-10-13 12:33:57 +0200

    DBZ-9382 add some docs

--- quarkus-debezium-parent/quarkus-debezium-engine-spi-parent/runtime/src/main/java/io/debezium/runtime/recorder/DatasourceRecorder.java
+++ quarkus-debezium-parent/quarkus-debezium-engine-spi-parent/runtime/src/main/java/io/debezium/runtime/recorder/DatasourceRecorder.java
@@ -3,12 +3,18 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.quarkus.debezium.configuration;
+package io.debezium.runtime.recorder;
 
 import java.util.function.Supplier;
 
 import io.debezium.runtime.configuration.QuarkusDatasourceConfiguration;
+import io.quarkus.runtime.annotations.Recorder;
 
+/**
+ * Given a reference to a configuration returns a {@link QuarkusDatasourceConfiguration}
+ * @param <T>
+ */
+@Recorder
 public interface DatasourceRecorder<T extends QuarkusDatasourceConfiguration> {
     Supplier<T> convert(String name, boolean isDefault);
 }
