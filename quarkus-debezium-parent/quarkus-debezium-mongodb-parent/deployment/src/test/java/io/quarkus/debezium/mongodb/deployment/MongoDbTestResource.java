ab73db7a83a4bdc53772a9fbe159884ea10fde8c
kmos <kmos@commonhaus.dev>
2025-10-15 17:54:42 +0200

    DBZ-9382 fix packages

--- quarkus-debezium-parent/quarkus-debezium-mongodb-parent/deployment/src/test/java/io/quarkus/debezium/mongodb/deployment/MongoDbTestResource.java
+++ quarkus-debezium-parent/quarkus-debezium-mongodb-parent/deployment/src/test/java/io/quarkus/debezium/mongodb/deployment/MongoDbTestResource.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.quarkus.debezium.testsuite.deployment;
+package io.quarkus.debezium.mongodb.deployment;
 
 import java.io.Serializable;
 import java.time.Duration;
