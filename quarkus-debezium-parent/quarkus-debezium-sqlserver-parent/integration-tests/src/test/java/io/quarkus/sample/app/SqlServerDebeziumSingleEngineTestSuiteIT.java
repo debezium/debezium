2353ace81173eeb74d1a2e8d84c2b2b207663d89
kmos <kmos@commonhaus.dev>
2025-10-17 19:01:38 +0200

    DBZ-9384 configure integration tests for single engine

--- quarkus-debezium-parent/quarkus-debezium-sqlserver-parent/integration-tests/src/test/java/io/quarkus/sample/app/SqlServerDebeziumSingleEngineTestSuiteIT.java
+++ quarkus-debezium-parent/quarkus-debezium-sqlserver-parent/integration-tests/src/test/java/io/quarkus/sample/app/SqlServerDebeziumSingleEngineTestSuiteIT.java
@@ -6,5 +6,30 @@
 
 package io.quarkus.sample.app;
 
+import static io.quarkus.sample.app.Scripts.applySql;
+
+import java.sql.SQLException;
+
+import org.junit.platform.suite.api.BeforeSuite;
+
+import io.debezium.config.Configuration;
+import io.debezium.config.ConfigurationNames;
+import io.debezium.connector.sqlserver.SqlServerConnectorConfig;
+import io.debezium.jdbc.JdbcConfiguration;
+
 public class SqlServerDebeziumSingleEngineTestSuiteIT implements QuarkusDebeziumSingleEngineTestSuite {
+
+    @BeforeSuite
+    public static void init() throws SQLException {
+        applySql(new SqlServerConnectorConfig(
+                JdbcConfiguration.copy(Configuration.fromSystemProperties(ConfigurationNames.DATABASE_CONFIG_PREFIX))
+                        .with("database.hostname", "localhost")
+                        .with("database.port", 1433)
+                        .with("database.user", "sa")
+                        .with("database.encrypt", "false")
+                        .with("database.trustServerCertificate", "false")
+                        .with("database.password", "Password!")
+                        .build()),
+                Scripts.DEFAULT);
+    }
 }
