e09978e0b028ea34e4b2272dcb53916432cc9660
kmos <kmos@commonhaus.dev>
2025-10-21 16:34:09 +0200

    DBZ-9384 introduce agroal datasource and record

---
+++ quarkus-debezium-parent/quarkus-debezium-agroal-parent/deployment/src/main/java/io/quarkus/debezium/agroal/deployment/AgroalEngineProcessor.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.quarkus.debezium.agroal.deployment;
+
+import java.util.List;
+
+import jakarta.inject.Singleton;
+
+import io.quarkus.agroal.spi.JdbcDataSourceBuildItem;
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.debezium.agroal.configuration.AgroalDatasourceConfiguration;
+import io.quarkus.debezium.agroal.configuration.AgroalDatasourceRecorder;
+import io.quarkus.debezium.agroal.engine.AgroalParser;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+
+public class AgroalEngineProcessor {
+
+    @BuildStep
+    public void parser(BuildProducer<AdditionalBeanBuildItem> additionalBeanProducer) {
+        // Beans that will be injected by child modules
+        additionalBeanProducer.produce(AdditionalBeanBuildItem
+                .builder()
+                .addBeanClasses(AgroalParser.class)
+                .build());
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    public void produceSqlServerDatasourceConfig(List<JdbcDataSourceBuildItem> jdbcDataSources,
+                                                 AgroalDatasourceRecorder recorder,
+                                                 BuildProducer<SyntheticBeanBuildItem> producer) {
+        jdbcDataSources.forEach(item -> producer.produce(SyntheticBeanBuildItem
+                .configure(AgroalDatasourceConfiguration.class)
+                .scope(Singleton.class)
+                .supplier(recorder.convert(item.getName(), item.isDefault(), item.getDbKind()))
+                .setRuntimeInit()
+                .named(item.getDbKind() + item.getName())
+                .done()));
+    }
+}
