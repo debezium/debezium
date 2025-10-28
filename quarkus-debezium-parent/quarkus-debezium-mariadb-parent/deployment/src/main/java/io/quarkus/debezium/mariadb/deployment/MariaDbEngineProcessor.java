5053699d1064291f42e822b12524cbc74acb7c2f
kmos <kmos@commonhaus.dev>
2025-10-28 11:52:30 +0100

    DBZ-9381 make mariaDB extension native

--- quarkus-debezium-parent/quarkus-debezium-mariadb-parent/deployment/src/main/java/io/quarkus/debezium/mariadb/deployment/MariaDbEngineProcessor.java
+++ quarkus-debezium-parent/quarkus-debezium-mariadb-parent/deployment/src/main/java/io/quarkus/debezium/mariadb/deployment/MariaDbEngineProcessor.java
@@ -6,7 +6,20 @@
 
 package io.quarkus.debezium.mariadb.deployment;
 
+import io.debezium.connector.binlog.charset.BinlogCharsetRegistry;
+import io.debezium.connector.binlog.snapshot.lock.MinimalAtLeastOnceSnapshotLock;
+import io.debezium.connector.mariadb.MariaDbConnector;
+import io.debezium.connector.mariadb.MariaDbConnectorTask;
+import io.debezium.connector.mariadb.MariaDbSourceInfoStructMaker;
 import io.debezium.connector.mariadb.Module;
+import io.debezium.connector.mariadb.charset.MariaDbCharsetRegistry;
+import io.debezium.connector.mariadb.snapshot.lock.DefaultSnapshotLock;
+import io.debezium.connector.mariadb.snapshot.lock.ExtendedSnapshotLock;
+import io.debezium.connector.mariadb.snapshot.lock.MinimalSnapshotLock;
+import io.debezium.connector.mariadb.snapshot.lock.NoneSnapshotLock;
+import io.debezium.connector.mariadb.snapshot.query.SelectAllSnapshotQuery;
+import io.debezium.relational.history.SchemaHistory;
+import io.debezium.storage.kafka.history.KafkaSchemaHistory;
 import io.quarkus.debezium.agroal.configuration.AgroalDatasourceConfiguration;
 import io.quarkus.debezium.deployment.QuarkusEngineProcessor;
 import io.quarkus.debezium.deployment.items.DebeziumConnectorBuildItem;
@@ -14,6 +27,8 @@ import io.quarkus.debezium.deployment.items.DebeziumExtensionNameBuildItem;
 import io.quarkus.debezium.engine.MariaDbEngineProducer;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.NativeImageEnableAllCharsetsBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.pkg.steps.NativeOrNativeSourcesBuild;
 
@@ -37,10 +52,43 @@ public class MariaDbEngineProcessor implements QuarkusEngineProcessor<AgroalData
     @Override
     public void registerClassesThatAreLoadedThroughReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClassBuildItemBuildProducer) {
 
+        reflectiveClassBuildItemBuildProducer.produce(ReflectiveClassBuildItem.builder(
+                "io.debezium.connector.mariadb.charset.MariaDbCharsetRegistry$CharacterSetMappings",
+                "io.debezium.connector.mariadb.charset.MariaDbCharsetRegistry$CharacterSetMapping",
+                "io.debezium.connector.mariadb.charset.MariaDbCharsetRegistry$CharacterSetMapping",
+                "io.debezium.connector.mariadb.charset.MariaDbCharsetRegistry$CollationMapping")
+                .build());
+
+        reflectiveClassBuildItemBuildProducer.produce(ReflectiveClassBuildItem.builder(
+                SchemaHistory.class,
+                KafkaSchemaHistory.class,
+                MariaDbConnector.class,
+                MariaDbSourceInfoStructMaker.class,
+                MariaDbConnectorTask.class,
+                MinimalAtLeastOnceSnapshotLock.class,
+                DefaultSnapshotLock.class,
+                ExtendedSnapshotLock.class,
+                MinimalSnapshotLock.class,
+                NoneSnapshotLock.class,
+                MariaDbCharsetRegistry.class,
+                BinlogCharsetRegistry.class,
+                SelectAllSnapshotQuery.class)
+                .reason(getClass().getName())
+                .build());
+    }
+
+    @BuildStep(onlyIf = NativeOrNativeSourcesBuild.class)
+    void registerCharsets(BuildProducer<NativeImageResourceBuildItem> resources) {
+        resources.produce(new NativeImageResourceBuildItem("charset_mappings.json"));
     }
 
     @Override
     public Class<AgroalDatasourceConfiguration> quarkusDatasourceConfiguration() {
         return AgroalDatasourceConfiguration.class;
+    }
+
+    @BuildStep(onlyIf = NativeOrNativeSourcesBuild.class)
+    public NativeImageEnableAllCharsetsBuildItem enableAllCharsets() {
+        return new NativeImageEnableAllCharsetsBuildItem();
     }
 }
