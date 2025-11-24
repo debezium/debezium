108bdbfe0f203c3a203fedbe0e5ac8735c74a70e
rishabh singh <rissingh@confluent.io>
2025-11-25 01:12:36 +0530

    DBZ-9693: Fix formatting issues

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/pgoutput/PgOutputMessageDecoder.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/pgoutput/PgOutputMessageDecoder.java
@@ -25,11 +25,11 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 
-import io.debezium.DebeziumException;
 import org.postgresql.replication.fluent.logical.ChainedLogicalStreamBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.debezium.DebeziumException;
 import io.debezium.connector.postgresql.PostgresStreamingChangeEventSource.PgConnectionSupplier;
 import io.debezium.connector.postgresql.PostgresType;
 import io.debezium.connector.postgresql.TypeRegistry;
