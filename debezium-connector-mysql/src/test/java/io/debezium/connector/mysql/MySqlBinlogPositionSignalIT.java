849647b64232bdcf3715b9adf24ad9f8c55867d2
Aviral Srivastava <avi.srivastava254084@gmail.com>
2025-11-25 14:45:38 -0500

    DBZ-3829 Fix restart verification to account for heartbeat messages in MySqlBinlogPositionSignalIT
    After connector restart, consumeRecordsByTopic(1) would only get 1 heartbeat message
    and miss the actual data record. Increased to consumeRecordsByTopic(2) to account for
    heartbeat messages during streaming in both test methods.

--- debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlBinlogPositionSignalIT.java
+++ debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/MySqlBinlogPositionSignalIT.java
@@ -149,7 +149,8 @@ public class MySqlBinlogPositionSignalIT extends AbstractBinlogConnectorIT<MySql
         start(MySqlConnector.class, config);
 
         // Verify we only get record 5 (skipped 3 and 4)
-        records = consumeRecordsByTopic(1);
+        // Account for heartbeat messages during streaming
+        records = consumeRecordsByTopic(2);
         List<SourceRecord> tableRecords = records.recordsForTopic(SERVER_NAME + "." + DATABASE.getDatabaseName() + ".test_table");
         assertThat(tableRecords).hasSize(1);
 
@@ -248,7 +249,8 @@ public class MySqlBinlogPositionSignalIT extends AbstractBinlogConnectorIT<MySql
         start(MySqlConnector.class, config);
 
         // Verify we only get record 5 (skipped 3 and 4)
-        records = consumeRecordsByTopic(1);
+        // Account for heartbeat messages during streaming
+        records = consumeRecordsByTopic(2);
         List<SourceRecord> tableRecords = records.recordsForTopic(SERVER_NAME + "." + DATABASE.getDatabaseName() + ".test_table");
         assertThat(tableRecords).hasSize(1);
 
