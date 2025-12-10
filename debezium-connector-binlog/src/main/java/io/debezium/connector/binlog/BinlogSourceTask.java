91a98b6078ba77fa92ddbec8c4b60f80f462f3bb
Animesh Kumar <animkumar@confluent.io>
2025-12-10 11:29:33 +0530

    CC-37771 Log a warning about binlog retention (#249)
    ## What
    We want to log a warning about ensuring binlog retention is at least a
    week. For mysql cdc connector, this is important since other wise this
    can lead to data loss in case of downtime.

--- debezium-connector-binlog/src/main/java/io/debezium/connector/binlog/BinlogSourceTask.java
+++ debezium-connector-binlog/src/main/java/io/debezium/connector/binlog/BinlogSourceTask.java
@@ -75,6 +75,11 @@ public abstract class BinlogSourceTask<P extends Partition, O extends OffsetCont
                         + "binlog_row_image=FULL and restart the connector.");
             }
         }
+        LOGGER.warn("Please ensure that the binlog is retained for at least 7 days. In disaster "
+                    + "recovery scenarios, connector might be down for some time and then resume from "
+                    + "last read binlog position. In such cases, if the required binlog files are "
+                    + "available, the connector can recover seamlessly. If not, data recovery "
+                    + "mechanisms such as incremental or initial snapshot might need to be employed.");
     }
 
     /**
