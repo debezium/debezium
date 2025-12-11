adcafdaec1b1861a162b325e1e26f51b10ef4dce
AlvarVG <alvigo92@gmail.com>
2025-12-11 10:40:38 +0100

    debezium/dbz#18 Make checkstyle pass
    Signed-off-by: AlvarVG <alvigo92@gmail.com>

--- debezium-connector-jdbc/src/test/java/io/debezium/connector/jdbc/integration/AbstractJdbcSinkPrimaryKeyModeTest.java
+++ debezium-connector-jdbc/src/test/java/io/debezium/connector/jdbc/integration/AbstractJdbcSinkPrimaryKeyModeTest.java
@@ -15,9 +15,9 @@ import java.util.stream.Collectors;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.sink.SinkRecord;
+import org.assertj.core.data.Index;
 import org.assertj.db.api.TableAssert;
 import org.assertj.db.type.ValueType;
-import org.assertj.core.data.Index;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
