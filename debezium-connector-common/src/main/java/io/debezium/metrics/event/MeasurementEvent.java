0bdac6ca619293e5648fe43c9cd15133d91d3f0d
Vojtech Juranek <vjuranek@redhat.com>
2025-12-12 23:23:09 +0100

    debezium/dbz#1417 Add quantiles support using DDSketch
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

---
+++ debezium-connector-common/src/main/java/io/debezium/metrics/event/MeasurementEvent.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.metrics.event;
+
+/**
+ * // TODO: Document this
+ * @author vjuranek
+ * @since 4.0
+ */
+public interface MeasurementEvent {
+}
---
+++ debezium-connector-common/src/main/java/io/debezium/metrics/stats/MeasurementStatistics.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.metrics.stats;
+
+import java.util.function.Consumer;
+
+import io.debezium.metrics.event.SingleValueEvent;
+
+/**
+ * // TODO: Document this
+ * @author vjuranek
+ */
+public interface MeasurementStatistics<T extends SingleValueEvent<V>, V> extends Consumer<T> {
+
+    void reset();
+
+    Double getValueAtQuantile(double quantile);
+
+    default Double getValueAtP50() {
+        return getValueAtQuantile(0.50);
+    }
+
+    default Double getValueAtP95() {
+        return getValueAtQuantile(0.95);
+    }
+
+    default Double getValueAtP99() {
+        return getValueAtQuantile(0.99);
+    }
+}
