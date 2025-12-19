219ba1fc6fb51765886b31162ce1b209f3bac238
Vaibhav Kushwaha <34186745+fourpointfour@users.noreply.github.com>
2025-12-19 19:34:49 +0530

    debezium/dbz#1408 replaced casting connection with Connection.unwrap() method in postgres connector
    Signed-off-by: Vaibhav Kushwaha <34186745+fourpointfour@users.noreply.github.com>

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/PostgresConnection.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/connection/PostgresConnection.java
@@ -7,6 +7,7 @@
 package io.debezium.connector.postgresql.connection;
 
 import java.nio.charset.Charset;
+import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
