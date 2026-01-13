4f6ea210f03c96116bd8299868ec6ff86b0a6884
Olivier CHÉDRU <ochedru@gmail.com>
2026-01-13 21:54:50 +0100

    debezium/dbz#1504 MySQL: New charset not taken into account
    Signed-off-by: Olivier CHÉDRU <ochedru@gmail.com>

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/antlr/listener/AlterTableParserListener.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/antlr/listener/AlterTableParserListener.java
@@ -11,6 +11,7 @@ import static io.debezium.antlr.AntlrDdlParser.getText;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.slf4j.Logger;
@@ -359,5 +360,26 @@ public class AlterTableParserListener extends TableCommonParserListener {
             }, tableEditor);
         }
         super.enterTableOptionComment(ctx);
+    }
+
+    @Override
+    public void enterAlterByConvertCharset(MySqlParser.AlterByConvertCharsetContext ctx) {
+        parser.runIfNotNull(() -> {
+            String charsetName = parser.extractCharset(ctx.charsetName(), ctx.collationName());
+            if (charsetName != null) {
+                tableEditor.setDefaultCharsetName(charsetName);
+                tableEditor.setColumns(tableEditor.columns().stream()
+                        .map(
+                                column -> {
+                                    final ColumnEditor columnEditor = column.edit();
+                                    columnEditor.charsetNameOfTable(charsetName);
+                                    columnEditor.charsetName(charsetName);
+                                    return columnEditor;
+                                })
+                        .map(ColumnEditor::create)
+                        .collect(Collectors.toList()));
+            }
+        }, tableEditor);
+        super.enterAlterByConvertCharset(ctx);
     }
 }
