f5b2f08eed6ba078c38c4374f730d8e01fba2fa4
Fiore Mario Vitale <mvitale@redhat.com>
2026-01-30 10:53:18 +0100

    debezium/dbz#1544 Enable schema generation on debezium-transforms module
    Signed-off-by: Fiore Mario Vitale <mvitale@redhat.com>

--- debezium-transforms/src/main/java/io/debezium/transforms/metadata/TransformsMetadataProvider.java
+++ debezium-transforms/src/main/java/io/debezium/transforms/metadata/TransformsMetadataProvider.java
@@ -3,27 +3,31 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.debezium.metadata;
+package io.debezium.transforms.metadata;
 
 import java.util.List;
 
-import io.debezium.Module;
 import io.debezium.config.Field;
+import io.debezium.metadata.ComponentDescriptor;
+import io.debezium.metadata.ComponentMetadata;
+import io.debezium.metadata.ComponentMetadataProvider;
+import io.debezium.metadata.ComponentMetadataUtils;
 import io.debezium.transforms.ByLogicalTableRouter;
 import io.debezium.transforms.ExtractChangedRecordState;
 import io.debezium.transforms.ExtractNewRecordState;
 import io.debezium.transforms.ExtractSchemaToNewRecord;
 import io.debezium.transforms.GeometryFormatTransformer;
 import io.debezium.transforms.HeaderToValue;
+import io.debezium.transforms.Module;
 import io.debezium.transforms.SchemaChangeEventFilter;
 import io.debezium.transforms.SwapGeometryCoordinates;
 import io.debezium.transforms.TimezoneConverter;
 import io.debezium.transforms.VectorToJsonConverter;
 
 /**
- * Aggregator for all debezium-core transformation metadata.
+ * Aggregator for all debezium-transforms transformation metadata.
  */
-public class DebeziumCoreMetadataProvider implements ComponentMetadataProvider {
+public class TransformsMetadataProvider implements ComponentMetadataProvider {
 
     @Override
     public List<ComponentMetadata> getConnectorMetadata() {
