d5ca4d54b5748c8b5ef663c7871b976f2c49defc
Fiore Mario Vitale <mvitale@redhat.com>
2026-02-18 15:57:53 +0100

    debezium/dbz#1698 Implement ConfigDescriptor in connectors
    Signed-off-by: Fiore Mario Vitale <mvitale@redhat.com>

--- debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/metadata/JdbcMetadataProvider.java
+++ debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/metadata/JdbcMetadataProvider.java
@@ -7,13 +7,10 @@ package io.debezium.connector.jdbc.metadata;
 
 import java.util.List;
 
-import io.debezium.config.Field;
 import io.debezium.connector.jdbc.JdbcSinkConnector;
-import io.debezium.connector.jdbc.JdbcSinkConnectorConfig;
 import io.debezium.connector.jdbc.Module;
 import io.debezium.connector.jdbc.transforms.CollectionNameTransformation;
 import io.debezium.connector.jdbc.transforms.FieldNameTransformation;
-import io.debezium.metadata.ComponentDescriptor;
 import io.debezium.metadata.ComponentMetadata;
 import io.debezium.metadata.ComponentMetadataFactory;
 import io.debezium.metadata.ComponentMetadataProvider;
@@ -28,23 +25,9 @@ public class JdbcMetadataProvider implements ComponentMetadataProvider {
     @Override
     public List<ComponentMetadata> getConnectorMetadata() {
         return List.of(
-                createSinkConnectorMetadata(),
+                componentMetadataFactory.createComponentMetadata(new JdbcSinkConnector(), Module.version()),
                 componentMetadataFactory.createComponentMetadata(new CollectionNameTransformation<>(), Module.version()),
                 componentMetadataFactory.createComponentMetadata(new FieldNameTransformation<>(), Module.version()));
-    }
-
-    private ComponentMetadata createSinkConnectorMetadata() {
-        return new ComponentMetadata() {
-            @Override
-            public ComponentDescriptor getComponentDescriptor() {
-                return new ComponentDescriptor(JdbcSinkConnector.class.getName(), Module.version());
-            }
-
-            @Override
-            public Field.Set getComponentFields() {
-                return JdbcSinkConnectorConfig.ALL_FIELDS;
-            }
-        };
     }
 
 }
