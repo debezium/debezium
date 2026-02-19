7841900e4ae7a7ac50f556d392ffc85e86084850
Chris Cranford <chris@hibernate.org>
2026-02-19 01:29:53 -0500

    debezium/dbz#1279 Fix archive log only mode logic
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/unbuffered/UnbufferedLogMinerStreamingChangeEventSource.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/unbuffered/UnbufferedLogMinerStreamingChangeEventSource.java
@@ -116,11 +116,13 @@ public class UnbufferedLogMinerStreamingChangeEventSource extends AbstractLogMin
             // except once per iteration.
             databaseOffset = getMetrics().getDatabaseOffset();
 
-            minLogScn = computeResumeScnAndUpdateOffsets(minLogScn, minCommitScn);
-            getMetrics().setOffsetScn(minLogScn);
-
             Scn currentScn = getCurrentScn();
             getMetrics().setCurrentScn(currentScn);
+
+            collectLogs(minLogScn, getCurrentScn());
+
+            minLogScn = computeResumeScnAndUpdateOffsets(minLogScn, minCommitScn);
+            getMetrics().setOffsetScn(minLogScn);
 
             upperBoundsScn = calculateUpperBounds(minLogScn, upperBoundsScn, currentScn);
             if (upperBoundsScn.isNull()) {
@@ -128,8 +130,6 @@ public class UnbufferedLogMinerStreamingChangeEventSource extends AbstractLogMin
                 pauseBetweenMiningSessions();
                 continue;
             }
-
-            collectLogs(minLogScn, getCurrentScn());
 
             if (firstBatch) {
                 applyLogsToSession(false);
