405e9f59ad824c456df1fa0765a39d9d3a4277c5
Chris Cranford <chris@hibernate.org>
2026-03-17 10:13:39 -0400

    debezium/dbz#1373 Support for XML using clob storage
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/XmlBeginParserTest.java
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/XmlBeginParserTest.java
@@ -8,12 +8,11 @@ package io.debezium.connector.oracle.logminer;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.sql.SQLException;
-
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import io.debezium.connector.oracle.junit.SkipWhenAdapterNameIsNot;
-import io.debezium.connector.oracle.logminer.parser.LogMinerDmlEntry;
+import io.debezium.connector.oracle.logminer.events.LogMinerEventRow;
 import io.debezium.connector.oracle.logminer.parser.XmlBeginParser;
 import io.debezium.doc.FixFor;
 import io.debezium.relational.Column;
@@ -33,39 +32,72 @@ public class XmlBeginParserTest {
 
     @Test
     @FixFor("DBZ-3605")
-    public void shouldParseSimpleXmlBeginRedoSql() throws SQLException {
+    public void shouldParseSimpleBinaryXmlBeginRedoSql() {
         final Table table = Table.editor()
                 .tableId(TableId.parse("DEBEZIUM.XML_TEST"))
                 .addColumn(Column.editor().name("ID").create())
                 .addColumn(Column.editor().name("DATA").create())
                 .create();
 
-        String redoSql = "XML DOC BEGIN:  select \"DATA\" from \"DEBEZIUM\".\"XML_TEST\" where \"ID\" = '1'";
-        final LogMinerDmlEntry entry = parser.parse(redoSql, table);
-        assertThat(parser.getColumnName()).isEqualTo("DATA");
-        assertThat(entry.getObjectOwner()).isEqualTo("DEBEZIUM");
-        assertThat(entry.getObjectName()).isEqualTo("XML_TEST");
+        LogMinerEventRow event = binarySqlEvent("XML DOC BEGIN:  select \"DATA\" from \"DEBEZIUM\".\"XML_TEST\" where \"ID\" = '1'");
+        final XmlBeginParser.XmlBegin result = parser.parse(event, table);
+        assertThat(result.columnName()).isEqualTo("DATA");
+        assertThat(result.parsedEvent().getObjectOwner()).isEqualTo("DEBEZIUM");
+        assertThat(result.parsedEvent().getObjectName()).isEqualTo("XML_TEST");
+    }
+
+    @Test
+    @FixFor("dbz#1373")
+    public void shouldParseSimpleTextXmlBeginRedoSql() {
+        final Table table = Table.editor()
+                .tableId(TableId.parse("DEBEZIUM.XML_TEST"))
+                .addColumn(Column.editor().name("ID").create())
+                .addColumn(Column.editor().name("DATA").create())
+                .create();
+
+        // update "DEBEZIUM"."DBZ1373" a set a."DATA" = XMLType(:1) where a."ID" = '2';
+        LogMinerEventRow event = textSqlEvent("update \"DEBEZIUM\".\"XML_TEST\" a set a.\"DATA\" = XMLType(:1)  where a.\"ID\" = '1';");
+        final XmlBeginParser.XmlBegin result = parser.parse(event, table);
+        assertThat(result.columnName()).isEqualTo("DATA");
+        assertThat(result.parsedEvent().getObjectOwner()).isEqualTo("DEBEZIUM");
+        assertThat(result.parsedEvent().getObjectName()).isEqualTo("XML_TEST");
     }
 
     @Test
     @FixFor("DBZ-3605")
-    public void shouldParseSimpleXmlBeginRedoSqlWithSpacesInObjectNames() throws SQLException {
+    public void shouldParseSimpleBinaryXmlBeginRedoSqlWithSpacesInObjectNames() {
         final Table table = Table.editor()
                 .tableId(TableId.parse("\"DEBEZIUM OBJ\".\"XML_TEST OBJ\""))
                 .addColumn(Column.editor().name("ID").create())
                 .addColumn(Column.editor().name("DATA OBJ").create())
                 .create();
 
-        String redoSql = "XML DOC BEGIN:  select \"DATA OBJ\" from \"DEBEZIUM OBJ\".\"XML_TEST OBJ\" where \"ID\" = '1'";
-        final LogMinerDmlEntry entry = parser.parse(redoSql, table);
-        assertThat(parser.getColumnName()).isEqualTo("DATA OBJ");
-        assertThat(entry.getObjectOwner()).isEqualTo("DEBEZIUM OBJ");
-        assertThat(entry.getObjectName()).isEqualTo("XML_TEST OBJ");
+        LogMinerEventRow event = binarySqlEvent("XML DOC BEGIN:  select \"DATA OBJ\" from \"DEBEZIUM OBJ\".\"XML_TEST OBJ\" where \"ID\" = '1'");
+        final XmlBeginParser.XmlBegin result = parser.parse(event, table);
+        assertThat(result.columnName()).isEqualTo("DATA OBJ");
+        assertThat(result.parsedEvent().getObjectOwner()).isEqualTo("DEBEZIUM OBJ");
+        assertThat(result.parsedEvent().getObjectName()).isEqualTo("XML_TEST OBJ");
     }
 
     @Test
     @FixFor("DBZ-3605")
-    public void shouldNotParseSimpleXmlBeginRedoSqlWithInvalidPreamble() {
+    public void shouldParseSimpleTextXmlBeginRedoSqlWithSpacesInObjectNames() {
+        final Table table = Table.editor()
+                .tableId(TableId.parse("\"DEBEZIUM OBJ\".\"XML_TEST OBJ\""))
+                .addColumn(Column.editor().name("ID").create())
+                .addColumn(Column.editor().name("DATA OBJ").create())
+                .create();
+
+        LogMinerEventRow event = textSqlEvent("update \"DEBEZIUM OBJ\".\"XML_TEST OBJ\" a set a.\"DATA OBJ\" = XMLType(:1)  where a.\"ID\" = '1';");
+        final XmlBeginParser.XmlBegin result = parser.parse(event, table);
+        assertThat(result.columnName()).isEqualTo("DATA OBJ");
+        assertThat(result.parsedEvent().getObjectOwner()).isEqualTo("DEBEZIUM OBJ");
+        assertThat(result.parsedEvent().getObjectName()).isEqualTo("XML_TEST OBJ");
+    }
+
+    @Test
+    @FixFor("DBZ-3605")
+    public void shouldNotParseSimpleBinaryXmlBeginRedoSqlWithInvalidPreamble() {
         assertThrows(ParsingException.class, () -> {
             final Table table = Table.editor()
                     .tableId(TableId.parse("DEBEZIUM.XML_TEST"))
@@ -73,14 +105,29 @@ public class XmlBeginParserTest {
                     .addColumn(Column.editor().name("DATA").create())
                     .create();
 
-            String redoSql = "XMLDOCBEGIN:  select \"DATA\" from \"DEBEZIUM\".\"XML_TEST\" where \"ID\" = '1'";
-            parser.parse(redoSql, table);
+            LogMinerEventRow event = binarySqlEvent("XMLDOCBEGIN:  select \"DATA\" from \"DEBEZIUM\".\"XML_TEST\" where \"ID\" = '1'");
+            parser.parse(event, table);
+        });
+    }
+
+    @Test
+    @FixFor("DBZ-3605")
+    public void shouldNotParseSimpleTextXmlBeginRedoSqlWithInvalidPreamble() {
+        assertThrows(ParsingException.class, () -> {
+            final Table table = Table.editor()
+                    .tableId(TableId.parse("DEBEZIUM.XML_TEST"))
+                    .addColumn(Column.editor().name("ID").create())
+                    .addColumn(Column.editor().name("DATA").create())
+                    .create();
+
+            LogMinerEventRow event = textSqlEvent("updater \"DEBEZIUM\".\"XML_TEST\" a set a.\"DATA\" = XMLType(:1)  where a.\"ID\" = '1';");
+            parser.parse(event, table);
         });
     }
 
     @Test
     @FixFor("DBZ-7489")
-    public void shouldParseXmlDocBeginThatEndsWithIsNull() {
+    public void shouldParseBinaryXmlDocBeginThatEndsWithIsNull() {
         final Table table = Table.editor()
                 .tableId(TableId.parse("SCHEMA.TABLE"))
                 .addColumn(Column.editor().name("COLUMN_A").create())
@@ -92,8 +139,42 @@ public class XmlBeginParserTest {
                 .addColumn(Column.editor().name("PROPERTIES").create())
                 .create();
 
-        String redoSql = "XML DOC BEGIN:  select \"PROPERTIES\" from \"SCHEMA\".\"TABLE\" where \"COLUMN_A\" = '314107' and \"COLUMN_B\" = '69265' and \"COLUMN_D\" = '74' and \"TIME_A\" = TO_TIMESTAMP_TZ('2024-02-14 10:58:02.202590 +01:00') and \"TIME_B\" = TO_TIMESTAMP_TZ('3000-01-01 00:00:00.000000 +00:00') and \"MODIFICATIONTIME\" IS NULL";
-        parser.parse(redoSql, table);
+        LogMinerEventRow event = binarySqlEvent(
+                "XML DOC BEGIN:  select \"PROPERTIES\" from \"SCHEMA\".\"TABLE\" where \"COLUMN_A\" = '314107' and \"COLUMN_B\" = '69265' and \"COLUMN_D\" = '74' and \"TIME_A\" = TO_TIMESTAMP_TZ('2024-02-14 10:58:02.202590 +01:00') and \"TIME_B\" = TO_TIMESTAMP_TZ('3000-01-01 00:00:00.000000 +00:00') and \"MODIFICATIONTIME\" IS NULL");
+        parser.parse(event, table);
     }
 
+    @Test
+    @FixFor("DBZ-7489")
+    public void shouldParseTextXmlDocBeginThatEndsWithIsNull() {
+        final Table table = Table.editor()
+                .tableId(TableId.parse("SCHEMA.TABLE"))
+                .addColumn(Column.editor().name("COLUMN_A").create())
+                .addColumn(Column.editor().name("COLUMN_B").create())
+                .addColumn(Column.editor().name("COLUMN_D").create())
+                .addColumn(Column.editor().name("TIME_A").create())
+                .addColumn(Column.editor().name("TIME_B").create())
+                .addColumn(Column.editor().name("MODIFICATIONTIME").create())
+                .addColumn(Column.editor().name("PROPERTIES").create())
+                .create();
+
+        LogMinerEventRow event = textSqlEvent(
+                "update \"SCHEMA\".\"TABLE\" a set a.\"PROPERTIES\" = XMLType(:1)  where a.\"COLUMN_A\" = '314107' and a.\"COLUMN_B\" = '69265' and a.\"COLUMN_D\" = '74' and a.\"TIME_A\" = TO_TIMESTAMP_TZ('2024-02-14 10:58:02.202590 +01:00') and a.\"TIME_B\" = TO_TIMESTAMP_TZ('3000-01-01 00:00:00.000000 +00:00') and a.\"MODIFICATIONTIME\" IS NULL;");
+        parser.parse(event, table);
+    }
+
+    private LogMinerEventRow binarySqlEvent(String sql) {
+        return createSqlEvent(sql, "XML sql_redo not re-executable");
+    }
+
+    private LogMinerEventRow textSqlEvent(String sql) {
+        return createSqlEvent(sql, "XML sql_redo needs assembly");
+    }
+
+    private LogMinerEventRow createSqlEvent(String sql, String info) {
+        final LogMinerEventRow event = Mockito.mock(LogMinerEventRow.class);
+        Mockito.when(event.getRedoSql()).thenReturn(sql);
+        Mockito.when(event.getInfo()).thenReturn(info);
+        return event;
+    }
 }
