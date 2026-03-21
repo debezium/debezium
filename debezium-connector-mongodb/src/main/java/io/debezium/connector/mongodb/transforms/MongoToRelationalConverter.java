7c30b4ecd86967d63420432f23cf0b7aa206bb55
Divyansh Agrawal <divyanshagra18@gmail.com>
2026-03-21 18:17:36 +0530

    debezium/dbz#1715 Added imports and configuration for schema_mapping and handling missing fields
    Signed-off-by: Divyansh Agrawal <divyanshagra18@gmail.com>

---
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/transforms/MongoToRelationalConverter.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.mongodb.transforms;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.transforms.util.Requirements;
+import org.bson.BsonDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.debezium.config.Field;
+import io.debezium.data.Envelope;
+import io.debezium.transforms.AbstractExtractNewRecordState;
+import static io.debezium.transforms.ExtractNewRecordStateConfigDefinition.CONFIG_FIELDS;
+
+/**
+ * Converts MongoDB CDC events to relational-style format where 'before' and 'after'
+ * fields are nested Struct objects instead of JSON strings. This enables MongoDB events
+ * to be processed by relational SMTs like ExtractChangedRecordState.
+ *
+ * @param <R> the subtype of {@link ConnectRecord} on which this transformation will operate
+ * @author Divyansh Agrawal
+ */
+public class MongoToRelationalConverter<R extends ConnectRecord<R>> extends AbstractExtractNewRecordState<R> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoToRelationalConverter.class);
+
+    // Configuration field for schema mapping
+    private static final Field SCHEMA_MAPPING = Field.create("schema.mapping")
+            .withDisplayName("Schema mapping configuration")
+            .withType(ConfigDef.Type.STRING)
+            .withWidth(ConfigDef.Width.LONG)
+            .withImportance(ConfigDef.Importance.MEDIUM)
+            .withDefault("")
+            .withDescription("JSON configuration for mapping MongoDB documents to a target schema. "
+                    + "When provided, the SMT will normalize documents to match this schema, "
+                    + "adding null values for missing fields.");
+
+    // Configuration for handling missing fields
+    private static final Field ADD_MISSING_FIELDS = Field.create("add.missing.fields")
+            .withDisplayName("Add missing fields")
+            .withType(ConfigDef.Type.BOOLEAN)
+            .withWidth(ConfigDef.Width.SHORT)
+            .withImportance(ConfigDef.Importance.LOW)
+            .withDefault(true)
+            .withDescription("When true and schema mapping is provided, missing fields will be "
+                    + "added with null values to ensure schema consistency.");
+
+    // Field set for configuration validation
+    private final Field.Set configFields = CONFIG_FIELDS.with(SCHEMA_MAPPING, ADD_MISSING_FIELDS);
+
+}
