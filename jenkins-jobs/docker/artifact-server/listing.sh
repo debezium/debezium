e8852c39cf06c355b68c80954098509216e42040
Alvar Viana Gomez <alvigo92@gmail.com>
2026-04-14 12:35:32 +0200

    debezium/dbz#1648 Fix artifact listing names (#7342)
    Signed-off-by: AlvarVG <alvigo92@gmail.com>

--- jenkins-jobs/docker/artifact-server/listing.sh
+++ jenkins-jobs/docker/artifact-server/listing.sh
@@ -52,7 +52,7 @@ for driver in **/jdbc/*.{zip,jar}; do
 done
 
 for groovy_script in **/groovy/*.{zip,jar}; do
-    name=$(echo "$groovy_script" | sed -rn 's@^(.*)-[0-9][0-9A-Za-z_.-]*\..*$@\1@p')
+    name=$(echo "$groovy_script" | sed -rn 's@^(.*)-[0-9]\..*$@\1@p')
     artifact="$name"
     if [[ ! $artifact ]]; then
         continue
@@ -62,7 +62,7 @@ for groovy_script in **/groovy/*.{zip,jar}; do
 done
 
 for jackson_lib in **/jackson/*.jar; do
-    name=$(echo "$jackson_lib" | sed -rn 's@^(.*)-[0-9][0-9A-Za-z_.-]*\..*$@\1@p')
+    name=$(echo "$jackson_lib" | sed -rn 's@^(.*)-[0-9]\..*$@\1@p')
     artifact="$name"
     if [[ ! $artifact ]]; then
         continue
