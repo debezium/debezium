ce53b9032d00f0ed660dcdab9be47277fe2de4aa
Vojtech Juranek <vjuranek@redhat.com>
2026-05-05 21:49:22 +0200

    debezium/dbz#188 Don't use offset store from KafkaConnectUtil
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- debezium-embedded/src/main/java/io/debezium/embedded/KafkaConnectUtil.java
+++ debezium-embedded/src/main/java/io/debezium/embedded/KafkaConnectUtil.java
@@ -5,24 +5,11 @@
  */
 package io.debezium.embedded;
 
-import static org.apache.kafka.clients.CommonClientConfigs.CLIENT_ID_CONFIG;
-
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Stream;
 
 import org.apache.kafka.connect.json.JsonConverter;
 import org.apache.kafka.connect.json.JsonConverterConfig;
-import org.apache.kafka.connect.runtime.distributed.DistributedConfig;
 import org.apache.kafka.connect.storage.Converter;
-import org.apache.kafka.connect.storage.FileOffsetBackingStore;
-import org.apache.kafka.connect.storage.KafkaOffsetBackingStore;
-import org.apache.kafka.connect.storage.MemoryOffsetBackingStore;
-import org.apache.kafka.connect.util.SharedTopicAdmin;
-
-import io.debezium.DebeziumException;
 
 /**
  * An auxilliary class that provides internal Kafka Connect related data structures and operations.
@@ -38,41 +25,4 @@ public class KafkaConnectUtil {
         converter.configure(Collections.singletonMap(JsonConverterConfig.SCHEMAS_ENABLE_CONFIG, "false"), true);
         return converter;
     }
-
-    public static final FileOffsetBackingStore fileOffsetBackingStore() {
-        return new FileOffsetBackingStore(converterForOffsetStore());
-    }
-
-    public static final MemoryOffsetBackingStore memoryOffsetBackingStore() {
-
-        return new MemoryOffsetBackingStore() {
-
-            @Override
-            public Set<Map<String, Object>> connectorPartitions(String connectorName) {
-                return null;
-            }
-        };
-    }
-
-    public static final KafkaOffsetBackingStore kafkaOffsetBackingStore(Map<String, String> config) {
-        final String clientId = "debezium-server";
-        final Map<String, Object> adminProps = new HashMap<>(config);
-        adminProps.put(CLIENT_ID_CONFIG, clientId + "shared-admin");
-
-        Stream.of(
-                DistributedConfig.BOOTSTRAP_SERVERS_CONFIG,
-                DistributedConfig.OFFSET_STORAGE_TOPIC_CONFIG,
-                DistributedConfig.OFFSET_STORAGE_PARTITIONS_CONFIG,
-                DistributedConfig.OFFSET_STORAGE_REPLICATION_FACTOR_CONFIG).forEach(prop -> {
-                    if (!adminProps.containsKey(prop)) {
-                        throw new DebeziumException(String.format(
-                                "Cannot initialize Kafka offset storage, mandatory configuration option '%s' is missing",
-                                prop));
-                    }
-                });
-        SharedTopicAdmin sharedAdmin = new SharedTopicAdmin(adminProps);
-
-        return new KafkaOffsetBackingStore(sharedAdmin, () -> clientId, converterForOffsetStore());
-    }
-
 }
