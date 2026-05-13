ffdc73986c167936227c55c9976d233751f15b6f
Chris Cranford <chris@hibernate.org>
2026-05-13 15:30:04 -0400

    debezium/dbz#1925 Refactor platform strategy per review feedback
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/platforms/RdsLogMinerPlatformStrategy.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/platforms/RdsLogMinerPlatformStrategy.java
@@ -3,16 +3,10 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
-package io.debezium.connector.oracle.logminer;
+package io.debezium.connector.oracle.logminer.platforms;
 
-import java.sql.CallableStatement;
-import java.sql.SQLException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.debezium.connector.oracle.OracleConnection;
 import io.debezium.connector.oracle.Scn;
+import io.debezium.connector.oracle.logminer.LogMinerPlatformStrategy;
 
 /**
  * LogMiner platform strategy for AWS RDS Oracle deployments using CDB architecture.
@@ -23,7 +17,7 @@ import io.debezium.connector.oracle.Scn;
  * uses those RDS-specific packages and ensures the connector operates entirely within
  * the PDB context.
  *
- * <p>Key differences from the standard strategy:
+ * <p>Key differences from the default strategy:
  * <ul>
  *   <li>{@code rdsadmin.rdsadmin_util.logmnr_add_logfile} replaces {@code sys.dbms_logmnr.add_logfile}</li>
  *   <li>{@code rdsadmin.rdsadmin_util.logmnr_start} replaces {@code sys.dbms_logmnr.start_logmnr}</li>
@@ -36,18 +30,14 @@ import io.debezium.connector.oracle.Scn;
  */
 public class RdsLogMinerPlatformStrategy implements LogMinerPlatformStrategy {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RdsLogMinerPlatformStrategy.class);
-
     @Override
-    public void addLogFile(OracleConnection connection, String fileName) throws SQLException {
-        connection.executeWithoutCommitting(
-                "BEGIN rdsadmin.rdsadmin_util.logmnr_add_logfile(LOGFILENAME => '" + fileName + "', OPTIONS => DBMS_LOGMNR.ADDFILE); END;");
+    public String getAddLogFileSql(String fileName) {
+        return "BEGIN rdsadmin.rdsadmin_util.logmnr_add_logfile(LOGFILENAME => '" + fileName
+                + "', OPTIONS => DBMS_LOGMNR.ADDFILE); END;";
     }
 
     @Override
-    public void startSession(OracleConnection connection, Scn startScn, Scn endScn, String miningOptions,
-                             String dictionaryFilePath)
-            throws SQLException {
+    public String getStartSessionSql(Scn startScn, Scn endScn, String miningOptions, String dictionaryFilePath) {
         final var query = new StringBuilder(64);
         query.append("BEGIN rdsadmin.rdsadmin_util.logmnr_start(");
         if (!startScn.isNull()) {
@@ -61,26 +51,22 @@ public class RdsLogMinerPlatformStrategy implements LogMinerPlatformStrategy {
             query.append(", DICTFILENAME => '").append(dictionaryFilePath).append("'");
         }
         query.append("); END;");
-        connection.executeWithoutCommitting(query.toString());
+        return query.toString();
     }
 
     @Override
-    public void endSession(OracleConnection connection) throws SQLException {
-        connection.executeWithoutCommitting("BEGIN rdsadmin.rdsadmin_util.logmnr_end(); END;");
+    public String getEndSessionSql() {
+        return "BEGIN rdsadmin.rdsadmin_util.logmnr_end(); END;";
     }
 
     @Override
-    public void writeDataDictionaryToRedoLogs(OracleConnection connection) throws SQLException {
-        connection.executeWithoutCommitting("BEGIN DBMS_LOGMNR_D.BUILD (options => DBMS_LOGMNR_D.STORE_IN_REDO_LOGS); END;");
+    public String getWriteDataDictionaryToRedoLogsSql() {
+        return "BEGIN DBMS_LOGMNR_D.BUILD (options => DBMS_LOGMNR_D.STORE_IN_REDO_LOGS); END;";
     }
 
     @Override
-    public void removeLogFile(OracleConnection connection, String fileName) throws SQLException {
-        LOGGER.debug("Removing file {} from LogMiner mining session.", fileName);
-        final String sql = "BEGIN rdsadmin.rdsadmin_util.logmnr_remove_logfile(LOGFILENAME => '" + fileName + "');END;";
-        try (CallableStatement statement = connection.connection(false).prepareCall(sql)) {
-            statement.execute();
-        }
+    public String getRemoveLogFileSql(String fileName) {
+        return "BEGIN rdsadmin.rdsadmin_util.logmnr_remove_logfile(LOGFILENAME => '" + fileName + "');END;";
     }
 
     @Override
