de2e512dd05e874e915bce8a775035453625e4d4
Vojtech Juranek <vjuranek@redhat.com>
2026-05-18 16:13:01 +0200

    debezium/dbz#188 Create Debezium HeaderConverter and its Kafka adapter Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- debezium-embedded/src/test/java/io/debezium/embedded/EmbeddedEngineTest.java
+++ debezium-embedded/src/test/java/io/debezium/embedded/EmbeddedEngineTest.java
@@ -29,15 +29,12 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 import org.apache.kafka.common.config.ConfigDef;
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.file.FileStreamSourceConnector;
 import org.apache.kafka.connect.header.ConnectHeaders;
 import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.json.JsonDeserializer;
 import org.apache.kafka.connect.runtime.standalone.StandaloneConfig;
 import org.apache.kafka.connect.source.SourceRecord;
-import org.apache.kafka.connect.storage.HeaderConverter;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.apache.kafka.connect.transforms.predicates.Predicate;
 import org.apache.kafka.connect.util.SafeObjectInputStream;
@@ -57,6 +54,7 @@ import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.Header;
 import io.debezium.engine.RecordChangeEvent;
+import io.debezium.engine.converter.HeaderConverter;
 import io.debezium.engine.format.ChangeEventFormat;
 import io.debezium.engine.format.Json;
 import io.debezium.engine.format.JsonByteArray;
@@ -965,22 +963,12 @@ public class EmbeddedEngineTest extends AbstractAsyncEngineConnectorTest {
     public static class NullReturningHeaderConverter implements HeaderConverter {
 
         @Override
-        public byte[] fromConnectHeader(String topic, String headerKey, Schema schema, Object value) {
-            return null;
-        }
-
-        @Override
-        public SchemaAndValue toConnectHeader(String topic, String headerKey, byte[] value) {
+        public byte[] fromHeader(String topic, String headerKey, Object schema, Object value) {
             return null;
         }
 
         @Override
         public void configure(Map<String, ?> configs) {
-        }
-
-        @Override
-        public ConfigDef config() {
-            return new ConfigDef();
         }
 
         @Override
