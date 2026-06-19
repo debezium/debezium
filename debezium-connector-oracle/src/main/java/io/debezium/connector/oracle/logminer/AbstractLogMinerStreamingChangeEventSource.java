c80675c436ce1aeaf8c9627553419bd40f44b789
Chris Cranford <chris@hibernate.org>
2026-06-19 01:12:25 -0400

    debezium/dbz#1849 Use config to drive conditional logic
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/AbstractLogMinerStreamingChangeEventSource.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/AbstractLogMinerStreamingChangeEventSource.java
@@ -37,7 +37,7 @@ import io.debezium.connector.oracle.OracleSchemaChangeEventEmitter;
 import io.debezium.connector.oracle.RedoThreadState;
 import io.debezium.connector.oracle.RedoThreadState.RedoThread;
 import io.debezium.connector.oracle.Scn;
-import io.debezium.connector.oracle.jdbc.DualOracleConnectionFactory;
+import io.debezium.connector.oracle.jdbc.CaptureMode;
 import io.debezium.connector.oracle.jdbc.OracleConnectionFactory;
 import io.debezium.connector.oracle.logminer.LogFileCollector.LogFilesResult;
 import io.debezium.connector.oracle.logminer.LogFileSessionSelector.SessionLogSelection;
@@ -934,7 +934,7 @@ public abstract class AbstractLogMinerStreamingChangeEventSource
             result = deviatedScn.get();
         }
 
-        if (!(connectionFactory instanceof DualOracleConnectionFactory)) {
+        if (CaptureMode.PRIMARY.equals(connectorConfig.getCaptureMode())) {
 
             // Retrieve the redo thread state and get the minimum flushed SCN across all open redo threads
             Scn minOpenRedoThreadLastScn = streamingConnection.getRedoThreadState()
