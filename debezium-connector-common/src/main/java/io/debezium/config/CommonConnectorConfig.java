f42bf3cd5bb22e50cfb02baf1a5d2c5bad647ca6
Binayak490-cyber <binayak490@gmail.com>
2026-07-02 16:43:17 +0530

    debezium/dbz#453 Restore dbz#1605 heartbeat fallback lost during rebase
    Signed-off-by: Binayak490-cyber <binayak490@gmail.com>

--- debezium-connector-common/src/main/java/io/debezium/config/CommonConnectorConfig.java
+++ debezium-connector-common/src/main/java/io/debezium/config/CommonConnectorConfig.java
@@ -1536,7 +1536,7 @@ public abstract class CommonConnectorConfig {
     private final Duration pollInterval;
     protected final String logicalName;
     private final String heartbeatTopicsPrefix;
-    private final Duration heartbeatInterval;
+    private Duration heartbeatInterval;
     private final Duration snapshotDelay;
     private final Duration streamingDelay;
     private final Duration retriableRestartWait;
@@ -1746,6 +1746,10 @@ public abstract class CommonConnectorConfig {
 
     public Duration getHeartbeatInterval() {
         return heartbeatInterval;
+    }
+
+    protected void setHeartbeatInterval(int heartbeatIntervalMs) {
+        this.heartbeatInterval = Duration.ofMillis(heartbeatIntervalMs);
     }
 
     public Duration getSnapshotDelay() {
