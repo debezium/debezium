a780a49767dab074c3c55f9142da0c9f8f96461f
AlvarVG <alvigo92@gmail.com>
2026-07-28 13:10:14 +0200

    debezium/dbz#2247 Add missed files from the V3 migration
    Signed-off-by: AlvarVG <alvigo92@gmail.com>

--- debezium-testing/debezium-testing-testcontainers/src/test/java/io/debezium/testing/testcontainers/ApicurioRegistryTestIT.java
+++ debezium-testing/debezium-testing-testcontainers/src/test/java/io/debezium/testing/testcontainers/ApicurioRegistryTestIT.java
@@ -313,7 +313,7 @@ public class ApicurioRegistryTestIT {
     private String getApicurioUrl() {
         final String host = apicurioContainer.getContainerInfo().getConfig().getHostName();
         final int port = apicurioContainer.getExposedPorts().get(0);
-        final String apicurioUrl = "http://" + host + ":" + port + "/apis/registry/v2";
+        final String apicurioUrl = "http://" + host + ":" + port;
         return apicurioUrl;
     }
 
