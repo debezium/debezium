6b861f2b659ea51d7c8e66e2ba679f64e6a15087
Chris Cranford <chris@hibernate.org>
2026-08-03 20:48:06 -0400

    debezium/dbz#2339 Avoid database-based heartbeat NullPointerException
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-common/src/main/java/io/debezium/heartbeat/DatabaseHeartbeatFactory.java
+++ debezium-connector-common/src/main/java/io/debezium/heartbeat/DatabaseHeartbeatFactory.java
@@ -8,6 +8,9 @@ package io.debezium.heartbeat;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.debezium.config.CommonConnectorConfig;
 import io.debezium.connector.base.ChangeEventQueue;
 import io.debezium.pipeline.DataChangeEvent;
@@ -15,6 +18,9 @@ import io.debezium.relational.RelationalDatabaseConnectorConfig;
 import io.debezium.util.Strings;
 
 public class DatabaseHeartbeatFactory implements DebeziumHeartbeatFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHeartbeatFactory.class);
+
     @Override
     public Optional<Heartbeat> getHeartbeat(CommonConnectorConfig connectorConfig,
                                             HeartbeatConnectionProvider connectionProvider,
@@ -22,6 +28,12 @@ public class DatabaseHeartbeatFactory implements DebeziumHeartbeatFactory {
                                             ChangeEventQueue<DataChangeEvent> queue) {
         if (connectorConfig instanceof RelationalDatabaseConnectorConfig relConfig) {
             if (!Strings.isNullOrBlank(relConfig.getHeartbeatActionQuery())) {
+                if (connectionProvider == null) {
+                    LOGGER.warn("The '{}' option is configured but the connector does not provide a heartbeat connection; " +
+                            "the heartbeat action query will not be executed",
+                            DatabaseHeartbeatImpl.HEARTBEAT_ACTION_QUERY_PROPERTY_NAME);
+                    return Optional.empty();
+                }
                 return Optional.of(new DatabaseHeartbeatImpl(
                         connectionProvider.get(),
                         relConfig.getHeartbeatActionQuery(),
