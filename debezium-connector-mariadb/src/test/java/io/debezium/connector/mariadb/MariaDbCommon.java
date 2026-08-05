c89c5570b3358077c87b61e8f7f4dbec13636e43
Nick Chomey <88559987+nickchomey@users.noreply.github.com>
2026-08-05 15:17:58 -0600

    debezium/dbz#2259 Address Naros review comments on InnoDB binlog support
    * fixed idef::community[] typo and wrapped InnoDB section in community conditional
    * merged the two-line 2PC sentence into a single line in the docs
    * removed the dangling InnoDB-based binary log fragment from the log_bin property description
    * guard the binlog position assertion on the database.innodb.binlog system property instead of offset position
    * added the mariadb-innodb-binlog profile to the MariaDB CI workflow matrix
    
    Signed-off-by: Nick Chomey <88559987+nickchomey@users.noreply.github.com>

--- debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/MariaDbCommon.java
+++ debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/MariaDbCommon.java
@@ -47,4 +47,8 @@ public interface MariaDbCommon extends BinlogConnectorTest<MariaDbConnector> {
     default boolean isMariaDb() {
         return true;
     }
+
+    default boolean isInnoDbBinlogEnabled() {
+        return Boolean.getBoolean("database.innodb.binlog");
+    }
 }
