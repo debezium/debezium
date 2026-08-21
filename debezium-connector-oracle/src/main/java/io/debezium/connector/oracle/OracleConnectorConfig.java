e95c6da209816979522567a414974195306fce26
Chris Cranford <chris@hibernate.org>
2026-08-20 21:28:25 -0400

    debezium/dbz#2473 Validate username/client filters w/tracking
    When username and/or client tracking is disabled, the connector should fail
    to start when a username and/or client include/exclude filter is specified.
    Such filters cannot be applied when the relevant columns aren't tracked and
    aren't fetched from Oracle.
    
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleConnectorConfig.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/OracleConnectorConfig.java
@@ -364,7 +364,7 @@ public class OracleConnectorConfig extends HistorizedRelationalDatabaseConnector
             .withWidth(Width.SHORT)
             .withImportance(Importance.LOW)
             .withDefault(true)
-            .withValidation(Field::isRequired)
+            .withValidation(Field::isRequired, OracleConnectorConfig::validateLogMiningBufferTrackClientId)
             .withDescription("This controls whether the 'CLIENT_ID' column values are tracked. " +
                     "When set to true (the default), the 'CLIENT_ID' values are buffered and provided in events when available. " +
                     "When set to false, the 'CLIENT_ID' column is excluded from the LogMiner query and its values are not buffered, " +
@@ -376,7 +376,7 @@ public class OracleConnectorConfig extends HistorizedRelationalDatabaseConnector
             .withWidth(Width.SHORT)
             .withImportance(Importance.LOW)
             .withDefault(true)
-            .withValidation(Field::isRequired)
+            .withValidation(Field::isRequired, OracleConnectorConfig::validateLogMiningBufferTrackUsername)
             .withDescription("This controls whether the 'USERNAME' column values are tracked. " +
                     "When set to true (the default), the 'USERNAME' values are buffered and provided in events when available. " +
                     "When set to false, the 'USERNAME' column is excluded from the LogMiner query and its values are not buffered, " +
@@ -2509,6 +2509,26 @@ public class OracleConnectorConfig extends HistorizedRelationalDatabaseConnector
             if (includeList != null && excludeList != null) {
                 problems.accept(LOG_MINING_CLIENTID_EXCLUDE_LIST, excludeList,
                         String.format("\"%s\": is already specified", LOG_MINING_CLIENTID_INCLUDE_LIST.name()));
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static int validateLogMiningBufferTrackUsername(Configuration config, Field field, ValidationOutput problems) {
+        return validateTrackedAttributeFilters(config, field, problems, LOG_MINING_USERNAME_INCLUDE_LIST, LOG_MINING_USERNAME_EXCLUDE_LIST);
+    }
+
+    public static int validateLogMiningBufferTrackClientId(Configuration config, Field field, ValidationOutput problems) {
+        return validateTrackedAttributeFilters(config, field, problems, LOG_MINING_CLIENTID_INCLUDE_LIST, LOG_MINING_CLIENTID_EXCLUDE_LIST);
+    }
+
+    private static int validateTrackedAttributeFilters(Configuration config, Field field, ValidationOutput problems, Field includeList, Field excludeList) {
+        if (isLogMiner(config) && !config.getBoolean(field)) {
+            if (!Strings.isNullOrBlank(config.getString(includeList)) || !Strings.isNullOrBlank(config.getString(excludeList))) {
+                problems.accept(field, config.getBoolean(field), String.format(
+                        "The configuration property '%s' cannot be disabled when '%s' or '%s' is configured.",
+                        field.name(), includeList.name(), excludeList.name()));
                 return 1;
             }
         }
