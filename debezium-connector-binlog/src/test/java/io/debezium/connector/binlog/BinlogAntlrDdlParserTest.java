c385c030ffac8b2085ac27a51e3b325207f952f3
minleejae <55116920+minleejae@users.noreply.github.com>
2026-08-25 09:01:17 +0900

    debezium/dbz#332 Add regression test for dollar signs in index identifiers
    Signed-off-by: minleejae <55116920+minleejae@users.noreply.github.com>

--- debezium-connector-binlog/src/test/java/io/debezium/connector/binlog/BinlogAntlrDdlParserTest.java
+++ debezium-connector-binlog/src/test/java/io/debezium/connector/binlog/BinlogAntlrDdlParserTest.java
@@ -3479,6 +3479,23 @@ public abstract class BinlogAntlrDdlParserTest<V extends BinlogValueConverters, 
     }
 
     @Test
+    @FixFor("debezium/dbz#332")
+    public void shouldParseAlterTableRenameIndexWithDollarSignIdentifiers() {
+        String ddl = "CREATE TABLE testtbl (id INT PRIMARY KEY, col INT, INDEX t$testtbl$tbl$id (id), INDEX testtbl$t$col (col));"
+                + "ALTER TABLE testtbl RENAME INDEX t$testtbl$tbl$id TO testtbl$tbl$id;"
+                + "ALTER TABLE testtbl RENAME INDEX testtbl$t$col TO testtbl$col;"
+                + "ALTER TABLE testtbl RENAME INDEX testtbl$col TO testtbl$t$post$col;";
+        parser.parse(ddl, tables);
+        assertThat(parser.getParsingExceptionsFromWalker()).isEmpty();
+        assertThat(tables.size()).isEqualTo(1);
+
+        Table table = tables.forTable(new TableId(null, null, "testtbl"));
+        assertThat(table).isNotNull();
+        assertThat(table.retrieveColumnNames()).containsExactly("id", "col");
+        assertThat(table.primaryKeyColumnNames()).containsExactly("id");
+    }
+
+    @Test
     @FixFor("dbz#1504")
     public void shouldProcessAlterConvertToCharset() {
 
