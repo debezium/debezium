f1866b6d01e950ccc696cfb5f06d8b83cab78be8
Fiore Mario Vitale <mvitale@redhat.com>
2026-09-07 22:04:04 +0200

    debezium/dbz#2537 Add data collections to be snapshot into a dedicated notification for initial snapshot.
    Signed-off-by: Fiore Mario Vitale <mvitale@redhat.com>

--- debezium-embedded/src/test/java/io/debezium/pipeline/notification/AbstractNotificationsIT.java
+++ debezium-embedded/src/test/java/io/debezium/pipeline/notification/AbstractNotificationsIT.java
@@ -277,6 +277,42 @@ public abstract class AbstractNotificationsIT<T extends SourceConnector> extends
         waitForStreamingRunning(connector(), server(), "streaming", task());
     }
 
+    @Test
+    @FixFor("debezium/dbz#2537")
+    public void initialSnapshotStartNotificationMustContainsDataCollections() throws InterruptedException {
+        // Testing.Print.enable();
+
+        startConnector(config -> config.with("slot.drop.on.stop", "false")
+                .with(SinkNotificationChannel.NOTIFICATION_TOPIC, "io.debezium.notification")
+                .with(CommonConnectorConfig.NOTIFICATION_ENABLED_CHANNELS, "sink"));
+
+        assertConnectorIsRunning();
+
+        waitForSnapshotToBeCompleted(connector(), server(), task(), database());
+
+        List<SourceRecord> notifications = new ArrayList<>();
+        Awaitility.await().atMost(60, TimeUnit.SECONDS).until(() -> {
+
+            consumeAvailableRecords(r -> {
+                if (r.topic().equals("io.debezium.notification")) {
+                    notifications.add(r);
+                }
+            });
+            return notifications.size() == calculateNotificationSize();
+        });
+
+        assertThat(notifications).hasSize(calculateNotificationSize());
+        SourceRecord sourceRecord = notifications.get(1);
+        Assertions.assertThat(sourceRecord.topic()).isEqualTo("io.debezium.notification");
+        Assertions.assertThat(((Struct) sourceRecord.value()).getString("aggregate_type")).isEqualTo("Initial Snapshot");
+        Assertions.assertThat(((Struct) sourceRecord.value()).getString("type")).isEqualTo("DATA_COLLECTIONS_RESOLVED");
+        Assertions.assertThat(((Struct) sourceRecord.value()).getInt64("timestamp")).isCloseTo(Instant.now().toEpochMilli(), Percentage.withPercentage(1));
+        Assertions.assertThat(((Struct) sourceRecord.value()).getMap("additional_data")).containsEntry("data_collections",
+                String.join(",", collections().stream().sorted().toList()));
+
+        stopConnector();
+    }
+
     private void assertTableNotificationsSentToJmx(List<Notification> notifications, String tableName) {
         // For debugging purposes
         Optional<Notification> tableNotification;
@@ -383,7 +419,7 @@ public abstract class AbstractNotificationsIT<T extends SourceConnector> extends
     }
 
     private int calculateNotificationSize() {
-        return (collections().size() * 2) + 2;
+        return (collections().size() * 2) + 3;
     }
 
     public static class ClientListener implements NotificationListener {
