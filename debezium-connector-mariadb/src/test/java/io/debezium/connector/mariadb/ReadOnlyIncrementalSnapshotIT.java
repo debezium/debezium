8ede9588cefe29a6d39d1683b2248c09430d7de8
minleejae <55116920+minleejae@users.noreply.github.com>
2026-09-10 19:17:56 +0900

    debezium/dbz#2604 Mark MariaDB ReadOnlyIncrementalSnapshotIT.schemaChanges as flaky
    Signed-off-by: minleejae <55116920+minleejae@users.noreply.github.com>

--- debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/ReadOnlyIncrementalSnapshotIT.java
+++ debezium-connector-mariadb/src/test/java/io/debezium/connector/mariadb/ReadOnlyIncrementalSnapshotIT.java
@@ -5,8 +5,11 @@
  */
 package io.debezium.connector.mariadb;
 
+import org.junit.jupiter.api.Test;
+
 import io.debezium.connector.binlog.BinlogReadOnlyIncrementalSnapshotIT;
 import io.debezium.connector.mariadb.jdbc.MariaDbFieldReader;
+import io.debezium.junit.Flaky;
 
 /**
  * @author Chris Cranford
@@ -15,5 +18,17 @@ public class ReadOnlyIncrementalSnapshotIT extends BinlogReadOnlyIncrementalSnap
     @Override
     protected Class<?> getFieldReader() {
         return MariaDbFieldReader.class;
+    }
+
+    /**
+     * The DDL loop competes for the table metadata lock with the in-flight incremental snapshot chunk
+     * reads; on a slow CI runner an ALTER can stay blocked past the 600s query timeout that MariaDB
+     * Connector/J enforces via {@code SET STATEMENT max_statement_time}, killing the statement.
+     */
+    @Override
+    @Test
+    @Flaky("debezium/dbz#2604")
+    public void schemaChanges() throws Exception {
+        super.schemaChanges();
     }
 }
