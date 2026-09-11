db8d79bc9f4f983abf9619b7ce40428e720006b8
Chris Cranford <chris@hibernate.org>
2026-09-10 23:47:42 -0400

    debezium/dbz#1960 Add ehcache serialization coverage
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/buffered/EmbeddedInfinispanFindRolledBackRangeTest.java
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/buffered/EmbeddedInfinispanFindRolledBackRangeTest.java
@@ -5,25 +5,17 @@
  */
 package io.debezium.connector.oracle.logminer.buffered;
 
-import org.infinispan.configuration.cache.ConfigurationBuilder;
-
 import io.debezium.config.Configuration;
 import io.debezium.connector.oracle.OracleConnectorConfig;
+import io.debezium.connector.oracle.OracleConnectorConfig.LogMiningBufferType;
 import io.debezium.connector.oracle.logminer.buffered.infinispan.EmbeddedInfinispanCacheProvider;
 import io.debezium.connector.oracle.logminer.buffered.infinispan.InfinispanTransaction;
 import io.debezium.connector.oracle.logminer.buffered.infinispan.InfinispanTransactionFactory;
+import io.debezium.connector.oracle.util.TestHelper;
 
 public class EmbeddedInfinispanFindRolledBackRangeTest extends AbstractFindRolledBackRangeTest<InfinispanTransaction> {
-    private static final Configuration CONFIG = Configuration.create()
-            .with(OracleConnectorConfig.LOG_MINING_BUFFER_INFINISPAN_CACHE_TRANSACTIONS, heapOnlyCacheConfig("transactions"))
-            .with(OracleConnectorConfig.LOG_MINING_BUFFER_INFINISPAN_CACHE_PROCESSED_TRANSACTIONS, heapOnlyCacheConfig("processed-transactions"))
-            .with(OracleConnectorConfig.LOG_MINING_BUFFER_INFINISPAN_CACHE_SCHEMA_CHANGES, heapOnlyCacheConfig("schema-changes"))
-            .with(OracleConnectorConfig.LOG_MINING_BUFFER_INFINISPAN_CACHE_EVENTS, heapOnlyCacheConfig("events"))
-            .build();
 
-    private static String heapOnlyCacheConfig(String cacheName) {
-        return new ConfigurationBuilder().build().toStringConfiguration(cacheName);
-    }
+    private static final Configuration CONFIG = getConfiguration();
 
     @Override
     protected CacheProvider<InfinispanTransaction> getCacheProvider() {
@@ -33,5 +25,14 @@ public class EmbeddedInfinispanFindRolledBackRangeTest extends AbstractFindRolle
     @Override
     protected TransactionFactory<InfinispanTransaction> getTransactionFactory() {
         return new InfinispanTransactionFactory();
+    }
+
+    private static Configuration getConfiguration() {
+        final LogMiningBufferType bufferType = LogMiningBufferType.INFINISPAN_EMBEDDED;
+        final Configuration.Builder configBuilder = Configuration.create()
+                .with(OracleConnectorConfig.LOG_MINING_BUFFER_TYPE, bufferType.getValue())
+                .with(OracleConnectorConfig.LOG_MINING_BUFFER_DROP_ON_STOP, Boolean.TRUE);
+
+        return TestHelper.withDefaultInfinispanCacheConfigurations(bufferType, configBuilder).build();
     }
 }
