d88c148db89a6e6615ffd380895d3883bcca412a
Chris Cranford <chris@hibernate.org>
2026-09-10 23:47:42 -0400

    debezium/dbz#1960 Add ehcache serialization coverage
    Signed-off-by: Chris Cranford <chris@hibernate.org>

---
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/buffered/EhcacheStreamingChangeEventSourceIT.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.oracle.logminer.buffered;
+
+import io.debezium.config.Configuration;
+import io.debezium.connector.oracle.OracleConnectorConfig;
+import io.debezium.connector.oracle.OracleConnectorConfig.LogMiningBufferType;
+import io.debezium.connector.oracle.junit.SkipWhenAdapterNameIsNot;
+import io.debezium.connector.oracle.util.TestHelper;
+
+/**
+ *
+ * @author Chris Cranford
+ */
+@SkipWhenAdapterNameIsNot(value = SkipWhenAdapterNameIsNot.AdapterName.LOGMINER_BUFFERED)
+public class EhcacheStreamingChangeEventSourceIT extends AbstractBufferedLogMinerStreamingChangeEventSourceIT {
+
+    @Override
+    protected Configuration.Builder getBufferImplementationConfig() {
+        return TestHelper.withDefaultEhcacheConfigurations(TestHelper.defaultConfig()
+                .with(OracleConnectorConfig.LOG_MINING_BUFFER_TYPE, LogMiningBufferType.EHCACHE.getValue())
+                .with(OracleConnectorConfig.LOG_MINING_BUFFER_DROP_ON_STOP, Boolean.TRUE),
+                1024 * 66);
+    }
+
+    @Override
+    protected boolean hasPersistedState() {
+        return true;
+    }
+
+}
