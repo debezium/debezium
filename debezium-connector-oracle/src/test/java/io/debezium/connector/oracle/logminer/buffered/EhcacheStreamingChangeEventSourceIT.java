2ab26975c6be1fbf2987ab2a6528cc213686798a
Chris Cranford <chris@hibernate.org>
2026-09-13 20:34:01 -0400

    debezium/dbz#1960 Exclude tests on Oracle 21
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/buffered/EhcacheStreamingChangeEventSourceIT.java
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/buffered/EhcacheStreamingChangeEventSourceIT.java
@@ -10,12 +10,15 @@ import io.debezium.connector.oracle.OracleConnectorConfig;
 import io.debezium.connector.oracle.OracleConnectorConfig.LogMiningBufferType;
 import io.debezium.connector.oracle.junit.SkipWhenAdapterNameIsNot;
 import io.debezium.connector.oracle.util.TestHelper;
+import io.debezium.junit.EqualityCheck;
+import io.debezium.junit.SkipWhenDatabaseVersion;
 
 /**
  *
  * @author Chris Cranford
  */
 @SkipWhenAdapterNameIsNot(value = SkipWhenAdapterNameIsNot.AdapterName.LOGMINER_BUFFERED)
+@SkipWhenDatabaseVersion(major = 21, check = EqualityCheck.EQUAL, reason = "Oracle 21 shared memory pool fragments and crashes")
 public class EhcacheStreamingChangeEventSourceIT extends AbstractBufferedLogMinerStreamingChangeEventSourceIT {
 
     @Override
