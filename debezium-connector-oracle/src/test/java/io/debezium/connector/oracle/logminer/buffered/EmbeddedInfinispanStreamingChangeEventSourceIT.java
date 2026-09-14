9d88a1b92dbe32cd120399af1477231f39f9942a
Chris Cranford <chris@hibernate.org>
2026-09-13 20:34:01 -0400

    debezium/dbz#1960 Exclude tests on Oracle 21
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/buffered/EmbeddedInfinispanStreamingChangeEventSourceIT.java
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/logminer/buffered/EmbeddedInfinispanStreamingChangeEventSourceIT.java
@@ -12,6 +12,8 @@ import io.debezium.connector.oracle.OracleConnectorConfig;
 import io.debezium.connector.oracle.OracleConnectorConfig.LogMiningBufferType;
 import io.debezium.connector.oracle.junit.SkipWhenAdapterNameIsNot;
 import io.debezium.connector.oracle.util.TestHelper;
+import io.debezium.junit.EqualityCheck;
+import io.debezium.junit.SkipWhenDatabaseVersion;
 import io.debezium.util.Testing;
 
 /**
@@ -19,6 +21,7 @@ import io.debezium.util.Testing;
  * @author Chris Cranford
  */
 @SkipWhenAdapterNameIsNot(value = SkipWhenAdapterNameIsNot.AdapterName.LOGMINER_BUFFERED)
+@SkipWhenDatabaseVersion(major = 21, check = EqualityCheck.EQUAL, reason = "Oracle 21 shared memory pool fragments and crashes")
 public class EmbeddedInfinispanStreamingChangeEventSourceIT extends AbstractBufferedLogMinerStreamingChangeEventSourceIT {
     @BeforeEach
     void before() throws Exception {
