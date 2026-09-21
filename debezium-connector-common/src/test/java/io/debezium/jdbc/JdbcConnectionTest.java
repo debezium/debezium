213c13f892b341379a119435cebf4ab5e7952aeb
Vojtech Juranek <vjuranek@redhat.com>
2026-09-21 23:04:27 +0200

    debezium/dbz#2670 Add missing import
    Missed due to backport (already present in the `main` branch).
    
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- debezium-connector-common/src/test/java/io/debezium/jdbc/JdbcConnectionTest.java
+++ debezium-connector-common/src/test/java/io/debezium/jdbc/JdbcConnectionTest.java
@@ -5,6 +5,7 @@
  */
 package io.debezium.jdbc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
