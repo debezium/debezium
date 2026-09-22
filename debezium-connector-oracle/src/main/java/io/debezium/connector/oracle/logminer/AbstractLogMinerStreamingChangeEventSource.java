48937d675b335a47562d3dec8764f92c3953ee77
Chris Cranford <chris@hibernate.org>
2026-09-22 13:21:59 -0400

    debezium/dbz#2668 Limit getMinNextScnAcrossAllThreadMaxNextScnValues to session logs
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/AbstractLogMinerStreamingChangeEventSource.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/AbstractLogMinerStreamingChangeEventSource.java
@@ -2142,7 +2142,7 @@ public abstract class AbstractLogMinerStreamingChangeEventSource
     }
 
     private Scn getMinNextScnAcrossAllThreadMaxNextScnValues() {
-        return getCurrentLogFiles().stream()
+        return getSessionLogFiles().stream()
                 .filter(LogFile::isArchive)
                 .collect(Collectors.groupingBy(
                         LogFile::getThread,
